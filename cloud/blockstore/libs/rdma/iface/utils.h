ca47c77773acb85e25769d23b1cc3c64b4c4c976
tpashkin <tpashkin@yandex-team.com>
2023-06-27 12:01:17 +0300

    drain queues before attempting to reconnect;

--- cloud/blockstore/libs/rdma/iface/utils.h
+++ cloud/blockstore/libs/rdma/iface/utils.h
@@ -2,6 +2,7 @@
 
 #include "public.h"
 
+#include <util/generic/vector.h>
 #include <util/generic/utility.h>
 
 namespace NCloud::NBlockStore::NRdma {
@@ -13,6 +14,13 @@ TOut SafeCast(TIn value)
 {
     Y_VERIFY(Min<TOut>() <= value && value <= Max<TOut>());
     return static_cast<TOut>(value);
+}
+
+template <typename T, typename A>
+bool OwnedBy(uintptr_t addr, const TVector<T, A>& vec)
+{
+    auto ptr = reinterpret_cast<T*>(addr);
+    return ptr >= &vec.front() && &vec.back() >= ptr;
 }
 
 }   // namespace NCloud::NBlockStore::NRdma
