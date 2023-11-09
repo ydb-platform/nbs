a889a7d1b6974ff39faf862a8fffeee8732be652
robot-piglet <robot-piglet@yandex-team.com>
2023-11-09 19:53:39 +0300

    Intermediate changes

--- cloud/disk_manager/internal/pkg/persistence/logger.go
+++ cloud/disk_manager/internal/pkg/persistence/logger.go
@@ -4,9 +4,41 @@ import (
 	"context"
 
 	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/logging"
-	ydb_core_log "github.com/ydb-platform/nbs/library/go/yandex/ydb/log"
 	ydb_log "github.com/ydb-platform/ydb-go-sdk/v3/log"
 )
+
+////////////////////////////////////////////////////////////////////////////////
+
+func fieldToField(field ydb_log.Field) logging.Field {
+	switch field.Type() {
+	case ydb_log.IntType:
+		return logging.Int(field.Key(), field.IntValue())
+	case ydb_log.Int64Type:
+		return logging.Int64(field.Key(), field.Int64Value())
+	case ydb_log.StringType:
+		return logging.String(field.Key(), field.StringValue())
+	case ydb_log.BoolType:
+		return logging.Bool(field.Key(), field.BoolValue())
+	case ydb_log.DurationType:
+		return logging.Duration(field.Key(), field.DurationValue())
+	case ydb_log.StringsType:
+		return logging.Strings(field.Key(), field.StringsValue())
+	case ydb_log.ErrorType:
+		return logging.ErrorValue(field.ErrorValue())
+	case ydb_log.StringerType:
+		return logging.String(field.Key(), field.Stringer().String())
+	default:
+		return logging.Any(field.Key(), field.AnyValue())
+	}
+}
+
+func toFields(fields []ydb_log.Field) []logging.Field {
+	ff := make([]logging.Field, len(fields))
+	for i, f := range fields {
+		ff[i] = fieldToField(f)
+	}
+	return ff
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -29,17 +61,17 @@ func (l *logger) Log(ctx context.Context, msg string, fields ...ydb_log.Field) {
 
 	switch ydb_log.LevelFromContext(ctx) {
 	case ydb_log.TRACE:
-		ll.Trace(msg, ydb_core_log.ToCoreFields(fields)...)
+		ll.Trace(msg, toFields(fields)...)
 	case ydb_log.DEBUG:
-		ll.Debug(msg, ydb_core_log.ToCoreFields(fields)...)
+		ll.Debug(msg, toFields(fields)...)
 	case ydb_log.INFO:
-		ll.Info(msg, ydb_core_log.ToCoreFields(fields)...)
+		ll.Info(msg, toFields(fields)...)
 	case ydb_log.WARN:
-		ll.Warn(msg, ydb_core_log.ToCoreFields(fields)...)
+		ll.Warn(msg, toFields(fields)...)
 	case ydb_log.ERROR:
-		ll.Error(msg, ydb_core_log.ToCoreFields(fields)...)
+		ll.Error(msg, toFields(fields)...)
 	case ydb_log.FATAL:
-		ll.Fatal(msg, ydb_core_log.ToCoreFields(fields)...)
+		ll.Fatal(msg, toFields(fields)...)
 	default:
 	}
 }
