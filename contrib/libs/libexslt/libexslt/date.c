f918da31a333f45d987f9051980e45160e2cf108
robot-contrib <robot-contrib@yandex-team.com>
2023-12-09 02:20:35 +0300

    Update contrib/libs/libexslt to 1.1.39

--- contrib/libs/libexslt/libexslt/date.c
+++ contrib/libs/libexslt/libexslt/date.c
@@ -38,6 +38,7 @@
 
 #include "exslt.h"
 
+#include <stdlib.h>
 #include <string.h>
 #include <limits.h>
 #include <errno.h>
@@ -49,16 +50,17 @@
 #define __EXTENSIONS__
 #endif
 #endif
-
-#include <time.h>
-
 #ifdef HAVE_LOCALE_H
 #include <locale.h>
 #endif
 
-#include <stdlib.h>
+#include <time.h>
 
-
+#if defined(_MSC_VER) && _MSC_VER >= 1400 || \
+    defined(_WIN32) && \
+    defined(__MINGW64_VERSION_MAJOR) && __MINGW64_VERSION_MAJOR >= 4
+  #define HAVE_MSVCRT
+#endif
 
 /*
  * types of date and/or time (from schema datatypes)
@@ -246,12 +248,14 @@ _exsltDateParseGYear (exsltDateValPtr dt, const xmlChar **str)
 static void
 exsltFormatGYear(xmlChar **cur, xmlChar *end, long yr)
 {
+    long year;
+    xmlChar tmp_buf[100], *tmp = tmp_buf, *tmp_end = tmp_buf + 99;
+
     if (yr <= 0 && *cur < end) {
         *(*cur)++ = '-';
     }
 
-    long year = (yr <= 0) ? -yr + 1 : yr;
-    xmlChar tmp_buf[100], *tmp = tmp_buf, *tmp_end = tmp_buf + 99;
+    year = (yr <= 0) ? -yr + 1 : yr;
     /* result is in reverse-order */
     while (year > 0 && tmp < tmp_end) {
         *tmp++ = '0' + (xmlChar)(year % 10);
@@ -678,7 +682,7 @@ static exsltDateValPtr
 exsltDateCurrent (void)
 {
     struct tm localTm, gmTm;
-#if !defined(HAVE_GMTIME_R) && !defined(_WIN32)
+#if !defined(HAVE_GMTIME_R) && !defined(HAVE_MSVCRT)
     struct tm *tb = NULL;
 #endif
     time_t secs;
@@ -700,7 +704,7 @@ exsltDateCurrent (void)
         errno = 0;
 	secs = (time_t) strtol (source_date_epoch, NULL, 10);
 	if (errno == 0) {
-#ifdef _WIN32
+#ifdef HAVE_MSVCRT
 	    struct tm *gm = gmtime_s(&localTm, &secs) ? NULL : &localTm;
 	    if (gm != NULL)
 	        override = 1;
@@ -721,7 +725,7 @@ exsltDateCurrent (void)
     /* get current time */
 	secs    = time(NULL);
 
-#ifdef _WIN32
+#ifdef HAVE_MSVCRT
 	localtime_s(&localTm, &secs);
 #elif HAVE_LOCALTIME_R
 	localtime_r(&secs, &localTm);
@@ -742,7 +746,7 @@ exsltDateCurrent (void)
     ret->sec  = (double) localTm.tm_sec;
 
     /* determine the time zone offset from local to gm time */
-#ifdef _WIN32
+#ifdef HAVE_MSVCRT
     gmtime_s(&gmTm, &secs);
 #elif HAVE_GMTIME_R
     gmtime_r(&secs, &gmTm);
