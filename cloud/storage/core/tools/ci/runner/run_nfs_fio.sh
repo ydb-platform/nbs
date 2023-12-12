303ac9ed125d67eec435d4ae97d27f221ae6e149
qkrorlqr <qkrorlqr@yandex-team.com>
2023-12-12 16:34:13 +0300

    fixed nfs_fio results-path [2];

--- cloud/storage/core/tools/ci/runner/run_nfs_fio.sh
+++ cloud/storage/core/tools/ci/runner/run_nfs_fio.sh
@@ -7,7 +7,7 @@ cluster="nemax"
 function run_test () {
     test_suite=$1
     shift
-    results_path="/var/www/build/results/fio/${cluster}/nfs/${test_suite}/$(date +%Y-%m-%d)"
+    results_path="/var/www/build/results/nfs_fio/${cluster}/nfs/${test_suite}/$(date +%Y-%m-%d)"
     mkdir -p $results_path
 
     $d/yc-nbs-ci-fio-performance-test-suite --test-suite $test_suite --service nfs $@ --cluster $cluster --profile-name "${cluster}-tests" --zone-id eu-north1-a --instance-cores 16 --instance-ram 16 --ssh-key-path /root/.ssh/test-ssh-key \
