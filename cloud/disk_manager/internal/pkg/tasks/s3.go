df46dc940cb653b80d34ea1ef67c7da1331ba1c2
BarkovBG <barkovbg@gmail.com>
2024-01-16 12:24:19 +0000

    rebase

--- cloud/disk_manager/internal/pkg/tasks/s3.go
+++ cloud/disk_manager/internal/pkg/tasks/s3.go
@@ -14,9 +14,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	aws_s3 "github.com/aws/aws-sdk-go/service/s3"
 	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/logging"
-	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/monitoring/metrics"
-	persistence_config "github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/persistence/config"
 	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/tasks/errors"
+	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/tasks/metrics"
+	persistence_config "github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/tasks/persistence/config"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
