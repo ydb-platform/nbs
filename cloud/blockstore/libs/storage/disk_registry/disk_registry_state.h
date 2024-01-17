355eaee99e981b69f5545f99309e6403825e46a5
qkrorlqr <thinkingwhat@yandex.ru>
2024-01-17 10:31:37 +0000

    NBSNEBIUS-76: RejectAgentTimeout dynamic calculation should deduplicate agent disconnects by rack - problems in a single rack should not make RejectAgentTimeout reach max values

--- cloud/blockstore/libs/storage/disk_registry/disk_registry_state.h
+++ cloud/blockstore/libs/storage/disk_registry/disk_registry_state.h
@@ -776,9 +776,9 @@ public:
         return AgentList.GetRejectAgentTimeout(now, agentId);
     }
 
-    void OnAgentDisconnected(TInstant now)
+    void OnAgentDisconnected(TInstant now, const TString& agentId)
     {
-        AgentList.OnAgentDisconnected(now);
+        AgentList.OnAgentDisconnected(now, agentId);
     }
 
     void SetDiskRegistryAgentListParams(
