8098321559a77419ec1dc5f1fee59be14339690b
Dmitry Razumov <dvrazumov@yandex-team.ru>
2024-01-17 16:16:44 +0000

    fix review issues

--- example/prepare_binaries.sh
+++ example/prepare_binaries.sh
@@ -1,4 +1,4 @@
-export NBS_ROOT=".."
+NBS_ROOT=".."
 YDBD_BIN="$NBS_ROOT/contrib/ydb/apps/ydbd/ydbd"
 NBSD_BIN="$NBS_ROOT/cloud/blockstore/apps/server/nbsd"
 BLOCKSTORE_CLIENT_BIN="$NBS_ROOT/cloud/blockstore/apps/client/blockstore-client"
@@ -13,22 +13,27 @@ do
   fi
 done
 
-function ydbd () {
+function ydbd {
   LD_LIBRARY_PATH=$(dirname $YDBD_BIN) $YDBD_BIN $@
 }
-function nbsd () {
+
+function nbsd {
   LD_LIBRARY_PATH=$(dirname $NBSD_BIN) $NBSD_BIN $@
 }
-function blockstore-client () {
+
+function blockstore-client {
   LD_LIBRARY_PATH=$(dirname $BLOCKSTORE_CLIENT_BIN) $BLOCKSTORE_CLIENT_BIN $@
 }
-function diskagentd () {
+
+function diskagentd {
   LD_LIBRARY_PATH=$(dirname $DISK_AGENT_BIN) $DISK_AGENT_BIN $@
 }
-function blockstore-nbd () {
+
+function blockstore-nbd {
   LD_LIBRARY_PATH=$(dirname $BLOCKSTORE_NBD_BIN) $BLOCKSTORE_NBD_BIN $@
 }
-function sudo-blockstore-nbd () {
+
+function sudo-blockstore-nbd {
   LD_LIBRARY_PATH=$(dirname $BLOCKSTORE_NBD_BIN)
   sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH $BLOCKSTORE_NBD_BIN $@
 }
