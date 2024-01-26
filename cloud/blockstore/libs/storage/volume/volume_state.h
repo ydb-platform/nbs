117803053790e72c5da7b5173035708972841dee
qkrorlqr <thinkingwhat@yandex.ru>
2024-01-26 10:32:07 +0000

    NBSNEBIUS-86: moved FreshDeviceIds filtration to a separate func

--- cloud/blockstore/libs/storage/volume/volume_state.h
+++ cloud/blockstore/libs/storage/volume/volume_state.h
@@ -620,6 +620,8 @@ private:
         ui64 proposedFillGeneration);
 
     THistoryLogKey AllocateHistoryLogKey(TInstant timestamp);
+
+    THashSet<TString> MakeFilteredDeviceIds() const;
 };
 
 }   // namespace NCloud::NBlockStore::NStorage
