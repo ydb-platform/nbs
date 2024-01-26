292f670db523827c2911c905197c06a17f6a4c21
s-vorobe-v <sergei.vorobev@nebius.com>
2024-01-26 14:58:35 +0100

    Add test.py and add test.py to ya.make

---
+++ cloud/disk_manager/test/remote/test.py
@@ -0,0 +1,18 @@
+import library.python.testing.yatest_common.yatest.common as common
+import library.python.testing.yatest_common.yatest.common.process as process
+
+
+def test_remote():
+    binary_path = common.binary_path(
+        "cloud/disk_manager/test/remote/cmd/cmd")
+    dm_config = common.get_param("disk-manager-client-config")
+    assert dm_config is not None
+    nbs_config = common.get_param("nbs-client-config")
+    assert nbs_config is not None
+
+    cmd = [
+        binary_path,
+        "--disk-manager-client-config", common.source_path(dm_config),
+        "--nbs-client-config", common.source_path(nbs_config),
+    ]
+    process.execute(cmd)
--- cloud/disk_manager/test/remote/ya.make
+++ cloud/disk_manager/test/remote/ya.make
@@ -1,3 +1,34 @@
+PY3TEST()
+
+TEST_SRCS(
+    test.py
+)
+
+TAG(
+    ya:external
+    ya:fat
+    ya:manual
+)
+
+SIZE(LARGE)
+
+REQUIREMENTS(
+    cpu:4
+    ram:16
+)
+
+DEPENDS(
+    cloud/disk_manager/test/remote/cmd
+)
+
+DATA(
+    arcadia/cloud/disk_manager/test/remote/cmd/cmd
+)
+
+TIMEOUT(14400)
+
+END()
+
 RECURSE(
     cmd
 )
