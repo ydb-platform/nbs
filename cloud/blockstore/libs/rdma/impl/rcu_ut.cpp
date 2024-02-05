ff0d3b70574110b69612839a9e870a3bcf9c3849
Evgeny Budilovsky <evgenybud@nebius.com>
2024-02-05 13:56:06 +0200

    add rcu list unit tests

---
+++ cloud/blockstore/libs/rdma/impl/rcu_ut.cpp
@@ -0,0 +1,72 @@
+#include "rcu.h"
+
+#include <library/cpp/testing/unittest/registar.h>
+
+namespace NCloud::NBlockStore::NRdma {
+
+////////////////////////////////////////////////////////////////////////////////
+
+Y_UNIT_TEST_SUITE(TRcuListTest)
+{
+    Y_UNIT_TEST(ShouldAddItems)
+    {
+        TRCUList<int> rcuList;
+        for (int i = 0; i < 10; ++i) {
+            rcuList.Add(i);
+        }
+
+        auto list = rcuList.Get();
+        UNIT_ASSERT_EQUAL(list->size(), 10);
+
+        for (int i = 0; i < 10; ++i) {
+            UNIT_ASSERT_EQUAL(list->at(i), i);
+        }
+    }
+
+    Y_UNIT_TEST(ShouldDeleteItems)
+    {
+        TRCUList<int> rcuList;
+        for (int i = 0; i < 10; ++i) {
+            rcuList.Add(i);
+        }
+
+        rcuList.Delete([](auto other) {
+            return other % 2 == 0;
+        });
+
+        auto list = rcuList.Get();
+        UNIT_ASSERT_EQUAL(list->size(), 5);
+
+        for (int i = 0; i < 5; ++i) {
+            UNIT_ASSERT_EQUAL(list->at(i), i*2 + 1);
+        }
+
+        rcuList.Delete([](auto) {
+            return true;
+        });
+
+        UNIT_ASSERT_EQUAL(rcuList.Get()->size(), 0);
+
+    }
+
+    Y_UNIT_TEST(ShouldNotModifyAccessedList)
+    {
+        TRCUList<int> rcuList;
+        for (int i = 0; i < 10; ++i) {
+            rcuList.Add(i);
+        }
+
+        auto list = rcuList.Get();
+        rcuList.Delete([](auto) {
+            return true;
+        });
+
+        UNIT_ASSERT_EQUAL(list->size(), 10);
+
+        for (int i = 0; i < 10; ++i) {
+            UNIT_ASSERT_EQUAL(list->at(i), i);
+        }
+    }
+};
+
+}   // namespace NCloud::NBlockStore::NRdma
--- cloud/blockstore/libs/rdma/impl/ut/ya.make
+++ cloud/blockstore/libs/rdma/impl/ut/ya.make
@@ -5,9 +5,10 @@ INCLUDE(${ARCADIA_ROOT}/cloud/storage/core/tests/recipes/small.inc)
 SRCS(
     buffer_ut.cpp
     client_ut.cpp
-    server_ut.cpp
-    poll_ut.cpp
     list_ut.cpp
+    poll_ut.cpp
+    rcu_ut.cpp
+    server_ut.cpp
 )
 
 PEERDIR(
