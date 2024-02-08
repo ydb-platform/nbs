042730754447270b1c4aa6762cb61ff33b119e08
Mikhail Montsev <svartmetal@github.com>
2024-02-08 15:42:38 +0000

    [Disk Manager] recipe's stdin/stdout/stderr files should contain 'service name'

--- cloud/disk_manager/test/recipe/disk_manager_launcher.py
+++ cloud/disk_manager/test/recipe/disk_manager_launcher.py
@@ -322,7 +322,8 @@ class DiskManagerServer(Daemon):
 
         super(DiskManagerServer, self).__init__(
             commands=[command],
-            cwd=working_dir)
+            cwd=working_dir,
+            service_name=SERVICE_NAME)
 
 
 class DiskManagerLauncher:
