d807c3814eae4869239d0cd9f4b0cd60ae583233
Pavel Misko <misko.pawel@gmail.com>
2024-02-08 17:13:58 +0100

    NBSNEBIUS-45: reset MaxMigrationTime on the last finished device migration; show Start migration timestamp on the volume's mon. page (#374)

--- cloud/blockstore/libs/storage/disk_registry/disk_registry_state.cpp
+++ cloud/blockstore/libs/storage/disk_registry/disk_registry_state.cpp
@@ -3139,6 +3139,7 @@ NProto::TError TDiskRegistryState::GetDiskInfo(
     diskInfo.CheckpointId = disk.CheckpointReplica.GetCheckpointId();
     diskInfo.SourceDiskId = disk.CheckpointReplica.GetSourceDiskId();
     diskInfo.History = disk.History;
+    diskInfo.MigrationStartTs = disk.MigrationStartTs;
 
     auto error = FillAllDiskDevices(diskId, disk, diskInfo);
 
@@ -5512,6 +5513,8 @@ void TDiskRegistryState::CancelDeviceMigration(
     disk.MigrationTarget2Source.erase(targetId);
     disk.MigrationSource2Target.erase(it);
 
+    ResetMigrationStartTsIfNeeded(disk);
+
     // searching in all pools - in theory pool name might change between
     // the start/cancel/finish events
     for (auto& x: SourceDeviceMigrationsInProgress) {
@@ -5525,10 +5528,6 @@ void TDiskRegistryState::CancelDeviceMigration(
         .SeqNo = seqNo
     });
 
-    if (disk.MigrationSource2Target.empty()) {
-        disk.MigrationStartTs = {};
-    }
-
     NProto::TDiskHistoryItem historyItem;
     historyItem.SetTimestamp(now.MicroSeconds());
     historyItem.SetMessage(TStringBuilder() << "cancelled migration: "
@@ -5538,6 +5537,13 @@ void TDiskRegistryState::CancelDeviceMigration(
     db.UpdateDisk(BuildDiskConfig(diskId, disk));
 
     UpdatePlacementGroup(db, diskId, disk, "CancelDeviceMigration");
+}
+
+void TDiskRegistryState::ResetMigrationStartTsIfNeeded(TDiskState& disk)
+{
+    if (disk.MigrationSource2Target.empty()) {
+        disk.MigrationStartTs = {};
+    }
 }
 
 NProto::TError TDiskRegistryState::FinishDeviceMigration(
@@ -5570,6 +5576,9 @@ NProto::TError TDiskRegistryState::FinishDeviceMigration(
     } else {
         disk.MigrationTarget2Source.erase(it);
         disk.MigrationSource2Target.erase(sourceId);
+
+        ResetMigrationStartTsIfNeeded(disk);
+
         // searching in all pools - in theory pool name might change between
         // the start/cancel/finish events
         for (auto& x: SourceDeviceMigrationsInProgress) {
