67242e6e5e98ec36b431fbdc3f05800c0c90b0aa
drbasic <drbasic@yandex-team.ru>
2024-02-12 18:45:55 +0700

    Introduce PoisonPill helper (#384)
    * Introduce PoisonPill helper
    
    * Fix review issues
    
    * Fix review issues. Remove virtual

---
+++ cloud/storage/core/libs/actors/poison_pill_helper.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "public.h"
+
+#include <contrib/ydb/library/actors/core/actor.h>
+#include <contrib/ydb/library/actors/core/event.h>
+
+namespace NCloud {
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Helps to handle the TEvPoisonPill for actor who owns other actors. The helper
+// sends TEvPoisonPill to all owned actors and waits for a response
+// TEvPoisonTaken from everyone. After that, it responds with the TEvPoisonTaken
+// message.
+class TPoisonPillHelper
+{
+private:
+    struct TPoisoner
+    {
+        NActors::TActorId Sender;
+        ui64 Cookie = 0;
+    };
+
+    NActors::IActor* Owner;
+    TSet<NActors::TActorId> OwnedActors;
+    std::optional<TPoisoner> Poisoner;
+
+public:
+    explicit TPoisonPillHelper(NActors::IActor* owner);
+    virtual ~TPoisonPillHelper();
+
+    void TakeOwnership(
+        const NActors::TActorContext& ctx,
+        NActors::TActorId actor);
+    void ReleaseOwnership(
+        const NActors::TActorContext& ctx,
+        NActors::TActorId actor);
+
+    void HandlePoisonPill(
+        const NActors::TEvents::TEvPoisonPill::TPtr& ev,
+        const NActors::TActorContext& ctx);
+
+    void HandlePoisonTaken(
+        const NActors::TEvents::TEvPoisonTaken::TPtr& ev,
+        const NActors::TActorContext& ctx);
+
+private:
+    void KillActors(const NActors::TActorContext& ctx);
+    void ReplyAndDie(const NActors::TActorContext& ctx);
+};
+
+}   // namespace NCloud
