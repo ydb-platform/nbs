9581db704b4914d944f5d498682e8456697d9de3
EvgeniyKozev <127485837+EvgeniyKozev@users.noreply.github.com>
2024-02-13 16:40:16 +0300

    CLOUDDUTY-25033 (#421)

--- cloud/blockstore/libs/storage/partition/part_state_ut.cpp
+++ cloud/blockstore/libs/storage/partition/part_state_ut.cpp
@@ -628,7 +628,6 @@ Y_UNIT_TEST_SUITE(TPartitionStateTest)
             UNIT_ASSERT_VALUES_EQUAL(2, state.GetAlmostFullChannelCount());
 
             firstChannelSelected = 0;
-            secondChannelSelected = 0;
             for (ui32 i = 0; i < 1000; ++i) {
                 auto blobId = state.GenerateBlobId(
                     kind,
@@ -643,7 +642,6 @@ Y_UNIT_TEST_SUITE(TPartitionStateTest)
                         ui32(TPartitionSchema::FirstDataChannel + 1),
                         blobId.Channel()
                     );
-                    ++secondChannelSelected;
                 }
             }
 
