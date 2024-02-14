4b734e0e8d96fa82ec73551e7fd39c56b6dc78f7
Andrei Strelkovskii <thinkingwhat@yandex.ru>
2024-02-14 22:57:26 +0300

    issue-324: added ut and loadtest for the TwoStageRead feature, fixed a crash in TReadDataActor::HandleReadBlobResponse (#441)
    * issue-324: added ut and loadtest for the TwoStageRead feature, fixed a crash in TReadDataActor::HandleReadBlobResponse
    
    * issue-324: added ut and loadtest for the TwoStageRead feature, fixed a crash in TReadDataActor::HandleReadBlobResponse - forgot nfs-storage.txt

---
+++ cloud/filestore/tests/loadtest/service-kikimr-newfeatures-test/test.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+
+from cloud.filestore.tests.python.lib.loadtest import run_load_test
+
+import yatest.common as common
+
+
+class Case(object):
+
+    def __init__(self, name, config_path):
+        self.name = name
+        self.config_path = config_path
+
+
+TESTS = [
+    Case(
+        "read-write-validation",
+        "cloud/filestore/tests/loadtest/service-kikimr-test/read-write-validation.txt",
+    ),
+]
+
+
+@pytest.mark.parametrize("test_case", TESTS, ids=[x.name for x in TESTS])
+def test_load(test_case):
+    test_case.config_path = common.source_path(test_case.config_path)
+    run_load_test(
+        test_case.name,
+        test_case.config_path,
+        os.getenv("NFS_SERVER_PORT"),
+    )
+
+    return None
--- cloud/filestore/tests/loadtest/ya.make
+++ cloud/filestore/tests/loadtest/ya.make
@@ -2,5 +2,6 @@ RECURSE_FOR_TESTS(
     local-auth
     service-kikimr-nemesis-test
     service-kikimr-test
+    service-kikimr-newfeatures-test
     service-local-test
 )
