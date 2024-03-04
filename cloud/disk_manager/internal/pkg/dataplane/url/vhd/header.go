0ab068e92a3a63854501c94b9196db248c543226
BarkovBG <barkovbg@gmail.com>
2024-03-04 14:28:23 +0000

    issues

--- cloud/disk_manager/internal/pkg/dataplane/url/vhd/header.go
+++ cloud/disk_manager/internal/pkg/dataplane/url/vhd/header.go
@@ -15,18 +15,18 @@ const (
 // https://learn.microsoft.com/en-us/windows/win32/vstor/about-vhd
 type header struct {
 	Cookie               [8]byte
-	DataOffset           uint64    // This field contains the absolute byte offset to the next structure in the hard disk image.
-	TableOffset          uint64    // This field stores the absolute byte offset of the Block Allocation Table (BAT) in the file.
-	HeaderVersion        uint32    // This field stores the version of the dynamic disk header.
-	MaxTableEntries      uint32    // This field holds the maximum entries present in the BAT.
-	BlockSize            uint32    // A block is a unit of expansion for dynamic and differencing hard disks.
-	Checksum             uint32    // This field holds a basic checksum of the dynamic header.
-	ParentUniqueID       [16]byte  // This field is used for differencing hard disks. Not supported.
-	ParentTimeStamp      uint32    // This field is used for differencing hard disks. Not supported.
-	Reserved1            uint32    // This field should be set to zero.
-	ParentUnicodeName    [512]byte // This field is used for differencing hard disks. Not supported.
-	ParentLocatorEntries [192]byte // This field is used for differencing hard disks. Not supported.
-	Reserved2            [256]byte // This field should be set to zero.
+	DataOffset           uint64    // Absolute byte offset to the next structure in the hard disk image.
+	TableOffset          uint64    // Absolute byte offset of the Block Allocation Table (BAT) in the file.
+	HeaderVersion        uint32    // Version of the dynamic disk header.
+	MaxTableEntries      uint32    // Maximum entries present in the BAT.
+	BlockSize            uint32    // Unit of expansion for dynamic and differencing hard disks.
+	Checksum             uint32    // Basic checksum of the dynamic header.
+	ParentUniqueID       [16]byte  // Used for differencing hard disks. Not supported.
+	ParentTimeStamp      uint32    // Used for differencing hard disks. Not supported.
+	Reserved1            uint32
+	ParentUnicodeName    [512]byte // Used for differencing hard disks. Not supported.
+	ParentLocatorEntries [192]byte // USed for differencing hard disks. Not supported.
+	Reserved2            [256]byte
 }
 
 func (h header) validate() error {
