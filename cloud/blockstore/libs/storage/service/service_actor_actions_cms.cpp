76fe4b01b462a49fb5166906589cafaa013b7e98
sharpeye <misko.pawel@gmail.com>
2024-03-07 16:23:55 +0000

    fix build

--- cloud/blockstore/libs/storage/service/service_actor_actions_cms.cpp
+++ cloud/blockstore/libs/storage/service/service_actor_actions_cms.cpp
@@ -3,10 +3,9 @@
 #include <cloud/blockstore/libs/storage/core/probes.h>
 
 #include <library/cpp/actors/core/actor_bootstrapped.h>
-
-#include <contrib/ydb/library/actors/core/events.h>
-#include <contrib/ydb/library/actors/core/hfunc.h>
-#include <contrib/ydb/library/actors/core/log.h>
+#include <library/cpp/actors/core/events.h>
+#include <library/cpp/actors/core/hfunc.h>
+#include <library/cpp/actors/core/log.h>
 
 #include <google/protobuf/util/json_util.h>
 
