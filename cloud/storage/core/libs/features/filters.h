332c3748d553bfcc562f01daca30de0a7fdd0853
Taras Pashkin <32797206+tpashkin@users.noreply.github.com>
2024-03-09 19:09:43 +0200

    support both white and blacklist at the same time; issue-476 (#677)
    support both white and blacklist at the same time; issue-476

---
+++ cloud/storage/core/libs/features/filters.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cloud/storage/core/config/features.pb.h>
+
+#include <util/generic/hash_set.h>
+#include <util/generic/string.h>
+
+namespace NCloud::NFeatures {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TFilters
+{
+private:
+    THashSet<TString> CloudIds;
+    THashSet<TString> FolderIds;
+    THashSet<TString> EntityIds;   // DiskIds or FsIds
+
+public:
+    explicit TFilters(NProto::TFilters config);
+
+    bool Contains(
+        const TString& cloudId,
+        const TString& folderId,
+        const TString& entityId) const;
+};
+
+}   // namespace NCloud::NFeatures
