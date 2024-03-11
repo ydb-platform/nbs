d369af86c129a8b451c2886395bd1da22a552541
Daniil Komarevtsev <komarevtsev@yandex-team.ru>
2024-03-11 06:51:36 +0000

    Add "PutUserData" sensor line to YDB group monitoring url.

--- cloud/blockstore/libs/diagnostics/hostname.cpp
+++ cloud/blockstore/libs/diagnostics/hostname.cpp
@@ -120,17 +120,20 @@ TString GetMonitoringYDBGroupUrl(
     ui32 groupId,
     const TString& storagePool)
 {
-    TMonitoringUrlData data = config.GetMonitoringUrlData();
-    return TStringBuilder()
-           << data.MonitoringUrl
-           << "/projects/kikimr/explorer/"
-              "queries?q.0.s=histogram_percentile(99, {project=\"kikimr"
-           << "\", cluster=\"*\", storagePool=\"" << storagePool
-           << "\", group=\"" << groupId << "\", host=\"" << GetShortHostName()
-           << "\", service=\"vdisks\", "
-              "subsystem=\"latency_histo\", "
-              "handleclass=\"GetFast\"})&q.0.name=A&from=now-1d&to=now&refresh="
-              "60000";
+    constexpr TStringBuf GetFast =
+        R"(q.0.s=histogram_percentile(99, {project="kikimr", cluster="*", storagePool="%s", group="%)" PRIu32
+        R"(", host="*", service="vdisks", subsystem="latency_histo", handleclass="GetFast"})&q.0.name=A)";
+    constexpr TStringBuf PutUserData =
+        R"(q.1.s=histogram_percentile(99, {project="kikimr", cluster="*", storagePool="%s", group="%)" PRIu32
+        R"(", host="*", service="vdisks", subsystem="latency_histo", handleclass="PutUserData"})&q.1.name=B)";
+    constexpr TStringBuf Url =
+        "%s/projects/kikimr/explorer/"
+        "queries?%s&%s&from=now-1d&to=now&refresh=60000";
+    return Sprintf(
+        Url.data(),
+        config.GetMonitoringUrlData().MonitoringUrl.c_str(),
+        Sprintf(GetFast.data(), storagePool.c_str(), groupId).c_str(),
+        Sprintf(PutUserData.data(), storagePool.c_str(), groupId).c_str());
 }
 
 }   // namespace NCloud::NBlockStore
