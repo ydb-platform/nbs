d71fde56998963111d7b8ef27372978a051ac78c
Evgeny Budilovsky <evgenybud@nebius.com>
2024-03-12 13:07:10 +0200

    NBSNEBIUS-146: don't generate critical events on health check read during secure erase (#699)
    Additionally report non health check reads during secure erase and add extra info to the critical event report

--- cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_ut.cpp
+++ cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_ut.cpp
@@ -3093,6 +3093,36 @@ Y_UNIT_TEST_SUITE(TDiskAgentTest)
             true);
         UNIT_ASSERT_VALUES_EQUAL(counter->Val(), 3);
 
+        // Read during secure erase from HealthCheck client doesn't generate
+        // critical event
+        UNIT_ASSERT_VALUES_EQUAL(
+            E_REJECTED,
+            ReadDeviceBlocks(
+                runtime,
+                diskAgent,
+                "uuid",
+                0,
+                1,
+                TString(CheckHealthClientId))
+                    ->Record.GetError()
+                    .GetCode());
+        UNIT_ASSERT_VALUES_EQUAL(counter->Val(), 3);
+
+        // Write zero during secure erase from HealthCheck client generates
+        // critical event
+        UNIT_ASSERT_VALUES_EQUAL(
+            E_REJECTED,
+            ZeroDeviceBlocks(
+                runtime,
+                diskAgent,
+                "uuid",
+                0,
+                1,
+                TString(CheckHealthClientId))
+                    ->Record.GetError()
+                    .GetCode());
+        UNIT_ASSERT_VALUES_EQUAL(counter->Val(), 4);
+
         spdk->SecureEraseResult.SetValue(NProto::TError());
         runtime.DispatchEvents(TDispatchOptions(), TDuration::MilliSeconds(10));
 
