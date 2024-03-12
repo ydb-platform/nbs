d04e7fcdc142eb38dd8f7bc096dece6cb6407487
Nikita Menkovich <librarian@nebius.com>
2024-03-12 19:37:13 +0100

    feature: omit yc-github-runner in favor of python script

---
+++ .github/scripts/create-vm.py
@@ -0,0 +1,462 @@
+import os
+import grpc
+import json
+import requests
+import math
+import logging
+import argparse
+import yaml
+import random
+import string
+from github import Github, Auth as GithubAuth
+from yandexcloud import SDK, RetryInterceptor, backoff_linear_with_jitter
+from yandex.cloud.compute.v1.instance_service_pb2_grpc import InstanceServiceStub
+from yandex.cloud.compute.v1.instance_pb2 import Instance
+from yandex.cloud.compute.v1.instance_service_pb2 import (
+    CreateInstanceRequest,
+    ResourcesSpec,
+    AttachedDiskSpec,
+    NetworkInterfaceSpec,
+    PrimaryAddressSpec,
+    OneToOneNatSpec,
+    DeleteInstanceRequest,
+    CreateInstanceMetadata,
+    DeleteInstanceMetadata,
+)
+from yandex.cloud.compute.v1.instance_pb2 import IpVersion
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s"
+)
+
+
+def output(key, value, secret=False):
+    GITHUB_OUTPUT = os.environ.get("GITHUB_OUTPUT")
+
+    if GITHUB_OUTPUT:
+        with open(GITHUB_OUTPUT, "a") as fp:
+            fp.write(f"{key}={value}\n")
+
+    logging.info(f"echo \"{key}={'******' if secret else value}\" >> $GITHUB_OUTPUT")
+
+
+def generate_github_label():
+    generated_string = "".join(
+        random.choices(string.ascii_lowercase + string.digits, k=8)
+    )
+    logging.info(f"Generated label: {generated_string}")
+    return generated_string
+
+
+class KeyValueAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):  # noqa: U100
+        kv_dict = {}
+        for item in values.split(","):
+            key, value = item.split("=")
+            kv_dict[key] = value
+        setattr(namespace, self.dest, kv_dict)
+
+
+def fetch_github_team_public_keys(gh, github_org, team_slug):
+    org = gh.get_organization(github_org)
+    team = org.get_team_by_slug(team_slug)
+    members = [member for member in team.get_members()]
+
+    ssh_keys = []
+    logging.info(
+        f"Fetching SSH keys for members: {[member.login for member in members]}"
+    )
+    for member in members:
+        member_keys = []
+        for key in member.get_keys():
+            member_keys.append(key.key)
+            ssh_keys.append(key.key)
+
+        logging.debug(f"Fetched {len(member_keys)} SSH keys for {member.login}")
+
+    logging.debug(f"Fetched SSH keys: {ssh_keys}")
+    return ssh_keys
+
+
+def generate_cloud_init_script(user, ssh_keys, owner, repo, token, version, label):
+
+    script = [
+        "#!/bin/bash",
+        "set -x",
+        "mkdir -p /actions-runner && cd /actions-runner",
+        'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64";; esac',
+        f"export FILENAME=actions-runner-linux-${{ARCH}}-{version}.tar.gz",
+        # https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz
+        f'[ -f "$FILENAME" ] || curl -O -L "https://github.com/actions/runner/releases/download/v{version}/$FILENAME"',
+        'tar xzf "./$FILENAME"',
+        "export RUNNER_ALLOW_RUNASROOT=1",
+        f"./config.sh  --labels {label} --url https://github.com/{owner}/{repo} --token {token}",
+        "./run.sh",
+    ]
+
+    cloud_init = {}
+    cloud_init["ssh_pwauth"] = False
+    cloud_init["users"] = [
+        {
+            "name": user,
+            "sudo": "ALL=(ALL) NOPASSWD:ALL",
+            "passwd": os.environ["VM_USER_PASSWD"],
+            "shell": "/bin/bash",
+            "ssh_authorized_keys": ssh_keys,
+        }
+    ]
+    cloud_init["runcmd"] = ["\n".join(script)]
+    logging.info(
+        f"Cloud-init: \n{yaml.dump(cloud_init, default_flow_style=False, width=math.inf)}".replace(
+            token, "****"
+        )
+    )
+    return (
+        "#cloud-config\n"
+        + yaml.dump(cloud_init, default_flow_style=False, width=math.inf),  # noqa: W503
+        ssh_keys,
+    )
+
+
+def get_runner_token(github_repo_owner, github_repo, github_token):
+    result = requests.post(
+        f"https://api.github.com/repos/{github_repo_owner}/{github_repo}/actions/runners/registration-token",
+        headers={
+            "Authorization": f"Bearer {github_token}",
+            "Accept": "application/vnd.github+json",
+            "X-Github-Api-Version": "2022-11-28",
+        },
+    ).json()
+
+    token = result.get("token")
+    if token:
+        # Mask the token in the logs
+        print(f"::add-mask::{token}")
+        logging.debug(f"Got runner registration token: {token}")
+        return token
+    else:
+        raise ValueError(f"Failed to get runner registration token: {result}")
+
+
+def create_vm(sdk, args):
+    GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+
+    gh = Github(auth=GithubAuth.Token(GITHUB_TOKEN))
+
+    runner_registration_token = get_runner_token(
+        args.github_repo_owner, args.github_repo, GITHUB_TOKEN
+    )
+
+    ssh_keys = fetch_github_team_public_keys(gh, args.github_org, args.github_team_slug)
+
+    # fmt: off
+    standard_v2_cpu = [
+        2, 4, 6, 8, 10, 12, 14, 16, 20, 24,
+        28, 32, 36, 40, 44, 48, 52, 56, 60
+    ]
+    # fmt: on
+    if args.platform_id == "standard-v2" and args.cpu not in standard_v2_cpu:
+        raise ValueError(
+            f"CPU cores must be {', '.join(standard_v2_cpu)} for standard-v2 platform"
+        )
+
+    if args.ram % args.cpu != 0:
+        raise ValueError("RAM must be a multiple of CPU cores (1,2,4 etc)")
+
+    if (
+        args.disk_type in ["network-ssd-nonreplicated", "network-ssd-io-m3"]
+        and args.disk_size % 93 != 0  # noqa: W503
+    ):
+        raise ValueError(
+            "Disk size must be a multiple of 93GB for the selected disk type"
+        )
+
+    runner_github_label = generate_github_label()
+
+    user_data, ssh_keys = generate_cloud_init_script(
+        args.user,
+        ssh_keys,
+        args.github_repo_owner,
+        args.github_repo,
+        runner_registration_token,
+        args.github_runner_version,
+        runner_github_label,
+    )
+
+    labels = args.labels
+    labels["runner-label"] = runner_github_label
+
+    request = CreateInstanceRequest(
+        name=args.name,
+        folder_id=args.folder_id,
+        zone_id=args.zone_id,
+        platform_id=args.platform_id,
+        resources_spec=ResourcesSpec(
+            memory=args.ram * 1024 * 1024 * 1024, cores=args.cpu, core_fraction=100
+        ),
+        boot_disk_spec=AttachedDiskSpec(
+            auto_delete=True,
+            mode=AttachedDiskSpec.Mode.READ_WRITE,
+            disk_spec=AttachedDiskSpec.DiskSpec(
+                type_id=args.disk_type,
+                size=args.disk_size * 1024 * 1024 * 1024,
+                image_id=args.image_id,
+            ),
+        ),
+        labels=labels,
+        network_interface_specs=[
+            NetworkInterfaceSpec(
+                subnet_id=args.subnet_id,
+                primary_v4_address_spec=PrimaryAddressSpec(
+                    one_to_one_nat_spec=OneToOneNatSpec(ip_version=IpVersion.IPV4)
+                ),
+            )
+        ],
+        metadata={
+            "user-data": user_data,
+            "ssh-keys": "\n".join([f"{args.user}:{key}" for key in ssh_keys]),
+            "serial-port-enable": "1",
+        },
+    )
+
+    # Create the VM
+    if args.apply:
+        result = sdk.create_operation_and_get_result(
+            request=request,
+            service=InstanceServiceStub,
+            method_name="Create",
+            response_type=Instance,
+            meta_type=CreateInstanceMetadata,
+            timeout=args.timeout,
+            logger=logging.getLogger(),
+        )
+        instance_id = result.response.id
+        name = result.response.name
+        primary_ipv4 = result.response.network_interfaces[0].primary_v4_address
+        external_ipv4 = primary_ipv4.one_to_one_nat.address
+        local_ipv4 = primary_ipv4.address
+        if instance_id:
+            logging.info(
+                f"Created VM {name} with ID {instance_id} and label {runner_github_label}"
+            )
+            output("instance-id", instance_id)
+            output("label", runner_github_label)
+            output("local-ipv4", local_ipv4)
+            if external_ipv4:
+                output("external-ipv4", external_ipv4)
+        else:
+            logging.error(f"Failed to create VM with request: {request}")
+            logging.error(f"Response: {result}")
+    else:
+        logging.info(f"Would create VM with request: {request}")
+
+
+def remove_runner_from_github(github_repo_owner, github_repo, vm_id, apply):
+    github_token = os.environ["GITHUB_TOKEN"]
+
+    gh = Github(auth=GithubAuth.Token(github_token))
+    runners = gh.get_repo(
+        f"{github_repo_owner}/{github_repo}"
+    ).get_self_hosted_runners()
+
+    runner_id = None
+    for runner in runners:
+        if runner.name == str(vm_id):
+            runner_id = runner.id
+            break
+
+    if runner_id is None:
+        logging.info(f"Runner with name {vm_id} not found")
+        raise ValueError(f"Runner with name {vm_id} not found")
+
+    if apply:
+        delete_status_code = requests.delete(
+            f"https://api.github.com/repos/{github_repo_owner}/{github_repo}/actions/runners/{runner_id}",
+            headers={
+                "Authorization": f"Bearer {github_token}",
+                "Accept": "application/vnd.github+json",
+                "X-Github-Api-Version": "2022-11-28",
+            },
+        ).status_code
+
+        if delete_status_code == 204:
+            return True
+        else:
+            raise ValueError(f"Failed to remove runner with name {vm_id}")
+    else:
+        logging.info(f"Would remove runner with name {vm_id} and id {runner_id}")
+
+
+def remove_vm(sdk, args):
+    remove_runner_from_github(
+        args.github_repo_owner, args.github_repo, args.id, args.apply
+    )
+
+    if args.apply:
+
+        try:
+            result = sdk.create_operation_and_get_result(
+                request=DeleteInstanceRequest(instance_id=args.id),
+                service=InstanceServiceStub,
+                method_name="Delete",
+                response_type=Instance,
+                meta_type=DeleteInstanceMetadata,
+                timeout=args.timeout,
+                logger=logging.getLogger(),
+            )
+
+            logging.info(f"Deleted VM with ID {args.id}")
+        except Exception as e:
+            logging.error(f"Failed to delete VM with ID {args.id}")
+            logging.error(f"Response: {result}")
+            raise e
+    else:
+        logging.info(f"Would delete VM with ID {args.id}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="action", help="Action to perform")
+    parser.add_argument(
+        "--api-endpoint",
+        default="api.ai.nebius.cloud",
+        help="API Endpoint",
+    )
+
+    create = subparsers.add_parser("create", help="Create a new VM")
+    create.add_argument(
+        "--github-org",
+        required=True,
+        default="ydb-platform",
+        help="GitHub organization name (we will fetch SSH keys from this org)",
+    )
+
+    create.add_argument(
+        "--github-team-slug",
+        required=True,
+        default="NBS",
+        help="Slug of the GitHub team within the organization",
+    )
+
+    create.add_argument(
+        "--github-repo-owner",
+        required=True,
+        default="ydb-platform",
+        help="GitHub repository owner name",
+    )
+    create.add_argument(
+        "--github-repo",
+        required=True,
+        default="nbs",
+        help="GitHub repository name",
+    )
+
+    create.add_argument(
+        "--github-runner-version",
+        default="2.308.0",
+        help="GitHub Runner version",
+    )
+
+    create.add_argument(
+        "--service-account-key",
+        required=True,
+        help="Path to the service account key file",
+    )
+    create.add_argument(
+        "--folder-id",
+        required=True,
+        default="bjeuq5o166dq4ukv3eec",
+        help="Folder ID where the VM will be created",
+    )
+    create.add_argument("--name", default="", help="VM name")
+    create.add_argument(
+        "--zone-id", default="eu-north1-c", required=True, help="Zone ID for the VM"
+    )
+    create.add_argument("--platform-id", default="standard-v2", help="Platform ID")
+    create.add_argument(
+        "--cpu", type=int, default=60, required=True, help="Number of CPU cores"
+    )
+    create.add_argument(
+        "--ram", type=int, default=240, required=True, help="Amount of RAM in GB"
+    )
+    create.add_argument(
+        "--disk-type",
+        default="network-ssd-nonreplicated",
+        choices=[
+            "network-ssd",
+            "network-hdd",
+            "network-ssd-nonreplicated",
+            "network-ssd-io-m3",
+        ],
+        help="Type of disk",
+    )
+    create.add_argument(
+        "--disk-size", type=int, required=True, default=930, help="Disk size in GB"
+    )
+    create.add_argument(
+        "--image-id", type=str, required=True, help="Image ID to use for the VM"
+    )
+    create.add_argument("--subnet-id", required=True, help="Subnet ID for the VM")
+    create.add_argument("--user", default="github", help="Username for the VM")
+    create.add_argument(
+        "--labels",
+        action=KeyValueAction,
+        default="",
+        help="Label for the VM (k=v,k2=v2)",
+    )
+
+    create.add_argument("--retry-time", default=10, help="How often to retry (seconds)")
+    create.add_argument(
+        "--timeout", default=600, help="How long to wait for creation (seconds)"
+    )
+    create.add_argument("--apply", action="store_true", help="Apply the changes")
+
+    remove = subparsers.add_parser("remove", help="Remove an existing VM")
+    remove.add_argument(
+        "--service-account-key",
+        required=True,
+        help="Path to the service account key file",
+    )
+    remove.add_argument("--id", required=True, help="ID of the VM to remove")
+
+    remove.add_argument(
+        "--github-repo-owner",
+        required=True,
+        default="ydb-platform",
+        help="GitHub repository owner name",
+    )
+    remove.add_argument(
+        "--github-repo",
+        required=True,
+        default="nbs",
+        help="GitHub repository name",
+    )
+
+    remove.add_argument("--retry-time", default=10, help="How often to retry (seconds)")
+    remove.add_argument(
+        "--timeout", default=600, help="How long to wait for removal (seconds)"
+    )
+    remove.add_argument("--apply", action="store_true", help="Apply the changes")
+
+    create.set_defaults(func=create_vm)
+    remove.set_defaults(func=remove_vm)
+
+    args = parser.parse_args()
+
+    interceptor = RetryInterceptor(
+        max_retry_count=args.timeout / args.retry_time,
+        retriable_codes=[grpc.StatusCode.UNAVAILABLE],
+        back_off_func=backoff_linear_with_jitter(args.retry_time, 0),
+    )
+
+    with open(args.service_account_key, "r") as fp:
+        sdk = SDK(
+            service_account_key=json.load(fp),
+            endpoint=args.api_endpoint,
+            interceptor=interceptor,
+        )
+
+    if hasattr(args, "func"):
+        args.func(sdk, args)
+    else:
+        parser.print_help()
