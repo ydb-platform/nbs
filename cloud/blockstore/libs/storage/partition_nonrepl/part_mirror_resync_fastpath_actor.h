c8d98aef0adcecc73f194116d11ab5508e22fdfa
Darya Frolova <47457802+WilyTiger@users.noreply.github.com>
2024-03-13 15:49:55 +0300

    NBSNEBIUS-127: move checksum range requests to actor companion from resync range actor (#524)
    * move checksum range requests to new actor from resync range actor
    
    * replace checksum range actor with actor companion
    
    * fix issues
    
    * fix issues

--- cloud/blockstore/libs/storage/partition_nonrepl/part_mirror_resync_fastpath_actor.h
+++ cloud/blockstore/libs/storage/partition_nonrepl/part_mirror_resync_fastpath_actor.h
@@ -31,7 +31,7 @@ private:
     const TRequestInfoPtr RequestInfo;
     const ui32 BlockSize;
     const TBlockRange64 Range;
-    const TVector<TResyncReplica> Replicas;
+    const TVector<TReplicaDescriptor> Replicas;
     const TString ClientId;
     TGuardedSgList SgList;
 
@@ -44,7 +44,7 @@ public:
         ui32 blockSize,
         TBlockRange64 range,
         TGuardedSgList sgList,
-        TVector<TResyncReplica> replicas,
+        TVector<TReplicaDescriptor> replicas,
         TString clientId);
 
     void Bootstrap(const NActors::TActorContext& ctx);
