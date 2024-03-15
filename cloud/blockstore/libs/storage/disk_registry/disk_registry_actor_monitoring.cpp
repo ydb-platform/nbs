2dee74af8f8dca7f9bd0ac0ecee906113aeeba5c
Daniil Komarevtsev <komarevtsev@yandex-team.ru>
2024-03-15 16:32:16 +0700

    733: Move "Reallocate" button into disk page in DR (#737)
    * 733: Move "Reallocate" button into disk page in DR
    
    * Print physical offset as a raw number

--- cloud/blockstore/libs/storage/disk_registry/disk_registry_actor_monitoring.cpp
+++ cloud/blockstore/libs/storage/disk_registry/disk_registry_actor_monitoring.cpp
@@ -32,7 +32,7 @@ void BuildVolumeReallocateButton(
             <input
                 class='btn btn-primary'
                 type='button'
-                value='Reallocate'
+                value='Reallocate volume'
                 title="Initiate update volume config"
                 data-toggle='modal'
                 data-target='#fire-reallocate%s'
@@ -228,7 +228,7 @@ void TDiskRegistryActor::RenderDevicesWithDetails(
                     TABLEH() { out << "Block size"; }
                     TABLEH() { out << "Block count"; }
                     TABLEH() { out << "Size"; }
-                    TABLEH() { out << "PhysicalOffset"; }
+                    TABLEH() { out << "Physical offset"; }
                     TABLEH() { out << "Transport id"; }
                     TABLEH() { out << "Rdma endpoint"; }
                     TABLEH() { out << "DiskId"; }
@@ -261,7 +261,9 @@ void TDiskRegistryActor::RenderDevicesWithDetails(
                         out << FormatByteSize(bytes);
                     }
                     TABLED() {
-                        out << FormatByteSize(device.GetPhysicalOffset());
+                        out << device.GetPhysicalOffset() << " ("
+                            << FormatByteSize(device.GetPhysicalOffset())
+                            << ")";
                     }
                     TABLED() { out << device.GetTransportId(); }
                     TABLED() {
@@ -593,6 +595,11 @@ void TDiskRegistryActor::RenderDiskHtmlInfo(
             DIV() { out << "Acquire in progress"; }
         }
 
+        DIV()
+        {
+            BuildVolumeReallocateButton(out, TabletID(), id);
+        }
+
         const bool isNonrepl = info.Replicas.empty();
 
         auto makeHeaderCells = [&] (const ui32 replicaNo) {
@@ -821,7 +828,6 @@ void TDiskRegistryActor::RenderDiskList(IOutputStream& out) const
                     TABLEH() { out << "Disk"; }
                     TABLEH() { out << "State"; }
                     TABLEH() { out << "State Timestamp"; }
-                    TABLEH() { out << "Action"; }
                 }
             }
 
@@ -833,9 +839,6 @@ void TDiskRegistryActor::RenderDiskList(IOutputStream& out) const
                     TABLED() { DumpDiskLink(out, TabletID(), id); }
                     TABLED() { DumpDiskState(out, diskInfo.State); }
                     TABLED() { out << diskInfo.StateTs; }
-                    TABLED() {
-                        BuildVolumeReallocateButton(out, TabletID(), id);
-                    }
                 }
             }
         }
