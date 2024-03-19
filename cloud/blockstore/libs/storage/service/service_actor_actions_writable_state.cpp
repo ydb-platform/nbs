5c2f0372394c41f44fff0f3bc81af527338c1eb9
yegorskii <aym001@yandex.ru>
2024-03-19 13:48:11 +0000

    make DiskRegistrySetWritableState request visible in private api

--- cloud/blockstore/libs/storage/service/service_actor_actions_writable_state.cpp
+++ cloud/blockstore/libs/storage/service/service_actor_actions_writable_state.cpp
@@ -3,6 +3,7 @@
 #include <cloud/blockstore/libs/storage/api/disk_registry.h>
 #include <cloud/blockstore/libs/storage/api/disk_registry_proxy.h>
 #include <cloud/blockstore/libs/storage/core/probes.h>
+#include <cloud/blockstore/private/api/protos/disk.pb.h>
 
 #include <contrib/ydb/library/actors/core/actor_bootstrapped.h>
 #include <contrib/ydb/library/actors/core/events.h>
