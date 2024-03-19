a2f727ba15b8d7ca33e081805d01b6e2fadcd710
qkrorlqr <thinkingwhat@yandex.ru>
2024-03-19 14:43:35 +0300

    cleanup after https://github.com/ydb-platform/nbs/pull/769

--- cloud/storage/core/libs/version_ydb/version.cpp
+++ cloud/storage/core/libs/version_ydb/version.cpp
@@ -1,13 +1,5 @@
 #include <contrib/ydb/core/driver_lib/version/version.h>
 
-namespace NKikimr::NPDisk {
-
-////////////////////////////////////////////////////////////////////////////////
-
-extern const ui64 YdbDefaultPDiskSequence = 0;
-
-}   // namespace NKikimr::NPDisk
-
 NKikimrConfig::TCurrentCompatibilityInfo
 NKikimr::TCompatibilityInfo::MakeCurrent()
 {
