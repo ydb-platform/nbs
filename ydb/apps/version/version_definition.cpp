33bf87a027a9c716a42f7f585e11508d17329dc3
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2024-03-19 17:48:06 +0300

    Try fix cmake #11

--- ydb/apps/version/version_definition.cpp
+++ ydb/apps/version/version_definition.cpp
@@ -2,8 +2,13 @@
 
 NKikimrConfig::TCurrentCompatibilityInfo NKikimr::TCompatibilityInfo::MakeCurrent() {
     using TCurrentConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TCurrentCompatibilityInfo;
+    using TVersionConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TVersion;
 
     return TCurrentConstructor{
         .Application = "ydb",
+        .Version = TVersionConstructor{
+            .Year = 23,
+            .Major = 3,
+        }
     }.ToPB();
 }
