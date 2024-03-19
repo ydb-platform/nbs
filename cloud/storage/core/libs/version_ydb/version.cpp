1b600c22e7f6ae0e12591b804f7ba6d19bdceef0
qkrorlqr <thinkingwhat@yandex.ru>
2024-03-19 14:41:58 +0300

    cleanup after https://github.com/ydb-platform/nbs/pull/769

--- cloud/storage/core/libs/version_ydb/version.cpp
+++ cloud/storage/core/libs/version_ydb/version.cpp
@@ -1,14 +1,19 @@
 #include <contrib/ydb/core/driver_lib/version/version.h>
 
 namespace NKikimr::NPDisk {
+
+////////////////////////////////////////////////////////////////////////////////
+
 extern const ui64 YdbDefaultPDiskSequence = 0;
-}
+
+}   // namespace NKikimr::NPDisk
 
 NKikimrConfig::TCurrentCompatibilityInfo
 NKikimr::TCompatibilityInfo::MakeCurrent()
 {
-    using TCurrentConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::
-        TCurrentCompatibilityInfo;
+    using namespace NKikimr;
+    using TCurrentConstructor =
+        TCompatibilityInfo::TProtoConstructor::TCurrentCompatibilityInfo;
 
     return TCurrentConstructor{
         .Application = "nbs",
