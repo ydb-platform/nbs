9466067dc42e06fbd6d6d53264450bdde2ddc15a
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2024-03-19 11:11:11 +0700

    Fix compilation

--- cloud/storage/core/libs/version_ydb/version.cpp
+++ cloud/storage/core/libs/version_ydb/version.cpp
@@ -1,14 +1,16 @@
-#include <ydb/core/driver_lib/version/version.h>
+#include <contrib/ydb/core/driver_lib/version/version.h>
 
 namespace NKikimr::NPDisk {
 extern const ui64 YdbDefaultPDiskSequence = 0;
 }
 
-NKikimrConfig::TCurrentCompatibilityInfo NKikimr::TCompatibilityInfo::MakeCurrent() {
-    using TCurrentConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TCurrentCompatibilityInfo;
+NKikimrConfig::TCurrentCompatibilityInfo
+NKikimr::TCompatibilityInfo::MakeCurrent()
+{
+    using TCurrentConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::
+        TCurrentCompatibilityInfo;
 
     return TCurrentConstructor{
         .Application = "nbs",
     }.ToPB();
 }
-
