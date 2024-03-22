1477ac94b70a9079263ca95aabd970280d36dad7
Mikhail Montsev <svartmetal@nebius.com>
2024-03-22 14:41:49 +0100

    issue-757: Daemon should wait when terminating or killing process (#796)

--- cloud/storage/core/tools/common/python/daemon.py
+++ cloud/storage/core/tools/common/python/daemon.py
@@ -79,12 +79,16 @@ class Daemon(object):
         self.__verify_process()
         logger.info("terminating process")
         self.__process.terminate()
+        self.__process.wait(check_exit_code=False)
+        self.__process = None
 
     # Should be guarded by self.__lock.
     def __kill_process(self):
         self.__verify_process()
         logger.info("killing process")
         self.__process.kill()
+        self.__process.wait(check_exit_code=False)
+        self.__process = None
 
     # Should be guarded by self.__lock.
     def __ping(self):
@@ -174,7 +178,6 @@ class Daemon(object):
                     self.__timer = None
 
                 self.__terminate_process()
-                self.__process = None
 
     def kill(self):
         with self.__lock:
@@ -184,7 +187,6 @@ class Daemon(object):
                     self.__timer = None
 
                 self.__kill_process()
-                self.__process = None
 
     @property
     def command(self):
