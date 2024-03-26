1a51238574cd0f14343ab784456a324ac5521353
qkrorlqr <thinkingwhat@yandex.ru>
2024-03-26 15:05:39 +0300

    issue-196: better

--- cloud/blockstore/tools/testing/bad-guest/lib/io.cpp
+++ cloud/blockstore/tools/testing/bad-guest/lib/io.cpp
@@ -16,10 +16,7 @@ struct TIO::TImpl
 
 public:
     TImpl(const TString& filePath, ui32 blockSize)
-        : File(
-            filePath,
-            EOpenModeFlag::DirectAligned | EOpenModeFlag::RdWr
-        )
+        : File(filePath, EOpenModeFlag::DirectAligned | EOpenModeFlag::RdWr)
         , BlockSize(blockSize)
     {
         Y_ABORT_UNLESS(File.IsOpen());
@@ -32,28 +29,31 @@ public:
         File.Close();
     }
 
+    struct TAlignedBuffer
+    {
+        TString Buffer;
+        char* AlignedPtr;
+
+        TAlignedBuffer(ui64 sz, ui32 align)
+            : Buffer(sz + align, 0)
+        {
+            const ui64 baseAddr = reinterpret_cast<ui64>(Buffer.data());
+            const ui64 alignedAddr = AlignUp<ui64>(baseAddr, align);
+            const auto alignmentOffset = alignedAddr - baseAddr;
+            AlignedPtr = Buffer.begin() + alignmentOffset;
+        }
+    };
+
     TString Read(ui64 blockIndex, ui32 blockCount) const
     {
         const auto sz = blockCount * BlockSize;
-        TString buffer(sz + BlockSize, 0);
-        const ui64 baseAddr = reinterpret_cast<ui64>(buffer.data());
-        const ui64 alignedAddr = AlignUp<ui64>(baseAddr, BlockSize);
-        const auto alignmentOffset = alignedAddr - baseAddr;
-        char* alignedPtr = buffer.begin() + alignmentOffset;
+        TAlignedBuffer buffer(sz, BlockSize);
         const auto offset = blockIndex * BlockSize;
 
-        auto f = AsyncIO.Read(File, alignedPtr, sz, offset);
+        auto f = AsyncIO.Read(File, buffer.AlignedPtr, sz, offset);
         auto res = f.GetValueSync();
         Y_ABORT_UNLESS(res == sz);
-        return {alignedPtr, sz};
-    }
-
-    static void DumbMemcpy(char* dst, const char* src, ui64 sz)
-    {
-        // we need this loop to be as simple as possible - no simd
-        for (ui64 i = 0; i < sz; ++i) {
-            dst[i] = src[i];
-        }
+        return {buffer.AlignedPtr, sz};
     }
 
     void AlternatingWrite(ui64 blockIndex, const TVector<TStringBuf>& datas)
@@ -63,20 +63,18 @@ public:
             sz = Max(sz, data.Size());
         }
         sz = AlignUp<ui64>(sz);
-        TString buffer(sz + BlockSize, 0);
-        const ui64 baseAddr = reinterpret_cast<ui64>(buffer.data());
-        const ui64 alignedAddr = AlignUp<ui64>(baseAddr, BlockSize);
-        const auto alignmentOffset = alignedAddr - baseAddr;
-        auto* alignedPtr = buffer.begin() + alignmentOffset;
+        TAlignedBuffer buffer(sz, BlockSize);
         const auto offset = blockIndex * BlockSize;
-        memset(alignedPtr, 0, sz);
-        DumbMemcpy(alignedPtr, datas[0].Data(), datas[0].Size());
+        memcpy(buffer.AlignedPtr, datas[0].Data(), datas[0].Size());
 
-        auto f = AsyncIO.Write(File, alignedPtr, sz, offset);
+        auto f = AsyncIO.Write(File, buffer.AlignedPtr, sz, offset);
+        // corrupting the data buffer that we have just sent to the storage
+        // layer via AsyncIO.Write - we want to test how nbs would handle such
+        // a situation - nbs should not break in any way
         ui32 i = 0;
         while (!f.HasValue()) {
             const auto& data = datas[++i % datas.size()];
-            DumbMemcpy(alignedPtr, data.Data(), data.Size());
+            memcpy(buffer.AlignedPtr, data.Data(), data.Size());
         }
 
         Y_ABORT_UNLESS(f.GetValue() == sz);
@@ -92,7 +90,7 @@ TIO::TIO(const TString& filePath, ui32 blockSize)
 
 TIO::~TIO() = default;
 
-[[nodiscard]] TString TIO::Read(ui64 blockIndex, ui32 blockCount) const
+TString TIO::Read(ui64 blockIndex, ui32 blockCount) const
 {
     return Impl->Read(blockIndex, blockCount);
 }
