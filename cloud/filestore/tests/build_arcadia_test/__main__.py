b7e8d6c8febca3388ef17441320ddd9b19bec39b
ya-ksgamora <164517843+ya-ksgamora@users.noreply.github.com>
2024-04-05 14:38:12 +0300

    remove redundant unauthorized request to sandbox proxy (#803)

--- cloud/filestore/tests/build_arcadia_test/__main__.py
+++ cloud/filestore/tests/build_arcadia_test/__main__.py
@@ -1,5 +1,3 @@
-from functools import partial
-
 from cloud.blockstore.pylibs import common
 from cloud.filestore.tests.build_arcadia_test.coreutils import \
     execute_coreutils_test
@@ -9,7 +7,7 @@ from .entrypoint import main
 if __name__ == '__main__':
     main(
         {
-            'nfs-coreutils': partial(execute_coreutils_test, clone_original_repo=True),
+            'nfs-coreutils': execute_coreutils_test,
         },
         common.ModuleFactories(
             common.make_test_result_processor_stub,
--- cloud/filestore/tests/build_arcadia_test/scripts/coreutils.sh
+++ cloud/filestore/tests/build_arcadia_test/scripts/coreutils.sh
@@ -4,21 +4,10 @@ sudo apt update
 sudo apt -y install python3 python-is-python3 git build-essential autoconf automake autopoint bison gperf libtool texinfo wget
 cd $mountPath
 
-if [ "$cloneOriginalRepo" == true ]; then
-    # If cloneOriginalRepo is set to true, fetch stable version from original repo
-    git clone --depth=1 --branch v9.3 --single-branch https://git.savannah.gnu.org/git/coreutils.git
-    cd coreutils
-    ./bootstrap
-else
-    # If cloneOriginalRepo is set to false, fetch uploaded version to sanbox (for isolation and reproducibility purposes)
+git clone --depth=1 --branch v9.3 --single-branch https://git.savannah.gnu.org/git/coreutils.git
+cd coreutils
+./bootstrap
 
-    # Download latest published resource with matching attribute
-    wget 'https://link/to/nbs_coreutils.tar' -O arch.tar
-    tar xvf arch.tar
-    cd coreutils
-    git config --global --add safe.directory $$(pwd)
-    sh -x bootstrap --no-git --gnulib-srcdir=../gnulib
-fi
 # Allow root user too run ./configure
 export FORCE_UNSAFE_CONFIGURE=1
 ./configure
