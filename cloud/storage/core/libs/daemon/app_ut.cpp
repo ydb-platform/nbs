846fbeb00a4ed7bc1dea2f07fee51eb723b5857f
yegorskii <137099343+yegorskii@users.noreply.github.com>
2024-04-12 00:47:29 +0200

    issue-936: fix race during app termination because of signal (#950)
    * issue-936: fix race during app termination because of signal
    
    * add eol

---
+++ cloud/storage/core/libs/daemon/app_ut.cpp
@@ -0,0 +1,23 @@
+#include "app.h"
+
+#include <contrib/ydb/library/actors/util/should_continue.h>
+
+#include <library/cpp/testing/unittest/registar.h>
+
+
+namespace NCloud {
+
+////////////////////////////////////////////////////////////////////////////////
+
+Y_UNIT_TEST_SUITE(TAppMainThreadTest)
+{
+    Y_UNIT_TEST(ShouldHandleStopBeforeAppMain)
+    {
+        TProgramShouldContinue shouldContinue;
+        AppCreate();
+        AppStop();
+        AppMain(shouldContinue);
+    }
+}
+
+}   // namespace NCloud
