fd06a61ea860b2e5f2d0bcfcd9a928bed1981f63
Constantine Gamora <ya-ksgamora@yandex-team.com>
2024-04-12 13:32:41 +0300

    flags to variables

--- example/4-start_nbs_storage.sh
+++ example/4-start_nbs_storage.sh
@@ -6,10 +6,10 @@ GRPC_PORT=${GRPC_PORT:-9001}
 MON_PORT=${MON_PORT:-8769}
 
 find_bin_dir() {
-    readlink -e `dirname $0`
+    readlink -e $(dirname $0)
 }
 
-BIN_DIR=`find_bin_dir`
+BIN_DIR=$(find_bin_dir)
 source ./prepare_binaries.sh || exit 1
 
 blockstore-client ExecuteAction --action DiskRegistrySetWritableState --verbose error --input-bytes '{"State":true}'
@@ -21,30 +21,23 @@ fi
 blockstore-client UpdateDiskRegistryConfig --verbose error --input $BIN_DIR/nbs/nbs-disk-registry.txt --proto
 if [ $? -ne 0 ]; then
     echo "Disk registry config was not updated. Did you try to update it for the second time? " \
-      "Consider updating the Version value or adding IgnoreVersion to $BIN_DIR/nbs/nbs-disk-registry.txt"
+        "Consider updating the Version value or adding IgnoreVersion to $BIN_DIR/nbs/nbs-disk-registry.txt"
 fi
 
 start_nbs_agent() {
-  if [ -z "$1" ]; then echo "Agent number is required"; exit 1; fi
+    if [ -z "$1" ]; then
+        echo "Agent number is required"
+        exit 1
+    fi
 
-  diskagentd \
-    --domain             Root \
-    --node-broker        localhost:$GRPC_PORT \
-    --ic-port            $(( $IC_PORT + $1 * 100 )) \
-    --mon-port           $(( $MON_PORT + $1 * 100 )) \
-    --domains-file       $BIN_DIR/nbs/nbs-domains.txt \
-    --ic-file            $BIN_DIR/nbs/nbs-ic.txt \
-    --log-file           $BIN_DIR/nbs/nbs-log.txt \
-    --sys-file           $BIN_DIR/nbs/nbs-sys.txt \
-    --server-file        $BIN_DIR/nbs/nbs-server.txt \
-    --storage-file       $BIN_DIR/nbs/nbs-storage.txt \
-    --naming-file        $BIN_DIR/nbs/nbs-names.txt \
-    --diag-file          $BIN_DIR/nbs/nbs-diag.txt \
-    --auth-file          $BIN_DIR/nbs/nbs-auth.txt \
-    --dr-proxy-file      $BIN_DIR/nbs/nbs-dr-proxy.txt \
-    --load-configs-from-cms \
-    --location-file      $BIN_DIR/nbs/nbs-location-$1.txt \
-    --disk-agent-file    $BIN_DIR/nbs/nbs-disk-agent-$1.txt >logs/remote-da$1.log 2>&1 &
+    CLUSTER=$CLUSTER \
+    IC_PORT=$(($IC_PORT + $1 * 100)) \
+    GRPC_PORT=$GRPC_PORT \
+    MON_PORT=$(($MON_PORT + $1 * 100)) \
+    LOCATION_FILE=$BIN_DIR/nbs/nbs-location-$1.txt \
+    DISK_AGENT_FILE=$BIN_DIR/nbs/nbs-disk-agent-$1.txt \
+    ./disk-agent.sh \
+        --load-configs-from-cms
 }
 
 start_nbs_agent 1
@@ -68,7 +61,6 @@ function ctrl_c() {
     kill $pid3
     exit 0
 }
-
 
 while true; do
     sleep 5
--- example/disk-agent.sh
+++ example/disk-agent.sh
@@ -1,31 +1,56 @@
 #!/usr/bin/env bash
 
 CLUSTER=${CLUSTER:-local}
+DOMAIN=${DOMAIN:-Root}
 IC_PORT=${IC_PORT:-29012}
+GRPC_HOST=${GRPC_HOST:-localhost}
 GRPC_PORT=${GRPC_PORT:-9001}
 MON_PORT=${MON_PORT:-8769}
 
 find_bin_dir() {
-    readlink -e `dirname $0`
+    readlink -e $(dirname $0)
 }
 
-BIN_DIR=`find_bin_dir`
+BIN_DIR=$(find_bin_dir)
+
+DOMAINS_FILE=${DOMAINS_FILE:-$BIN_DIR/nbs/nbs-domains.txt}
+IC_FILE=${IC_FILE:-$BIN_DIR/nbs/nbs-ic.txt}
+LOG_FILE=${LOG_FILE:-$BIN_DIR/nbs/nbs-log.txt}
+SYS_FILE=${SYS_FILE:-$BIN_DIR/nbs/nbs-sys.txt}
+SERVER_FILE=${SERVER_FILE:-$BIN_DIR/nbs/nbs-server.txt}
+STORAGE_FILE=${STORAGE_FILE:-$BIN_DIR/nbs/nbs-storage.txt}
+NAMING_FILE=${NAMING_FILE:-$BIN_DIR/nbs/nbs-names.txt}
+DIAG_FILE=${DIAG_FILE:-$BIN_DIR/nbs/nbs-diag.txt}
+AUTH_FILE=${AUTH_FILE:-$BIN_DIR/nbs/nbs-auth.txt}
+DR_PROXY_FILE=${DR_PROXY_FILE:-$BIN_DIR/nbs/nbs-dr-proxy.txt}
+
+if [[ -z $LOCATION_FILE ]]; then
+    echo "LOCATION_FILE variable is not set"
+    exit 1
+fi
+
+if [[ -z $DISK_AGENT_FILE ]]; then
+    echo "DISK_AGENT_FILE variable is not set"
+    exit 1
+fi
+
 source ./prepare_binaries.sh || exit 1
 
 diskagentd \
-    --domain             Root \
-    --node-broker        localhost:$GRPC_PORT \
-    --ic-port            $IC_PORT \
-    --mon-port           $MON_PORT \
-    --domains-file       $BIN_DIR/nbs/nbs-domains.txt \
-    --ic-file            $BIN_DIR/nbs/nbs-ic.txt \
-    --log-file           $BIN_DIR/nbs/nbs-log.txt \
-    --sys-file           $BIN_DIR/nbs/nbs-sys.txt \
-    --server-file        $BIN_DIR/nbs/nbs-server.txt \
-    --storage-file       $BIN_DIR/nbs/nbs-storage.txt \
-    --naming-file        $BIN_DIR/nbs/nbs-names.txt \
-    --diag-file          $BIN_DIR/nbs/nbs-diag.txt \
-    --auth-file          $BIN_DIR/nbs/nbs-auth.txt \
-    --dr-proxy-file      $BIN_DIR/nbs/nbs-dr-proxy.txt \
-    --load-configs-from-cms \
+    --domain $DOMAIN \
+    --node-broker $GRPC_HOST:$GRPC_PORT \
+    --ic-port $IC_PORT \
+    --mon-port $MON_PORT \
+    --domains-file $DOMAINS_FILE \
+    --ic-file $IC_FILE \
+    --log-file $LOG_FILE \
+    --sys-file $SYS_FILE \
+    --server-file $SERVER_FILE \
+    --storage-file $STORAGE_FILE \
+    --naming-file $NAMING_FILE \
+    --diag-file $DIAG_FILE \
+    --auth-file $AUTH_FILE \
+    --dr-proxy-file $DR_PROXY_FILE \
+    --location-file $LOCATION_FILE \
+    --disk-agent-file $DISK_AGENT_FILE \
     $@
