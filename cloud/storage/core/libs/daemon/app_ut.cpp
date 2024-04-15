d8e5603878b0817f3948fca222781a5d81b0bd45
yegorskii <aym001@yandex.ru>
2024-04-15 14:56:35 +0000

    fix daemon tests

--- cloud/storage/core/libs/daemon/app_ut.cpp
+++ cloud/storage/core/libs/daemon/app_ut.cpp
@@ -1,6 +1,6 @@
 #include "app.h"
 
-#include <contrib/ydb/library/actors/util/should_continue.h>
+#include <ydb/library/actors/util/should_continue.h>
 
 #include <library/cpp/testing/unittest/registar.h>
 
