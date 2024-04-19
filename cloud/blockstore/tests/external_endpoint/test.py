22a072ed1a013056b2b6b3a448b7b907074c956e
Pavel Misko <misko.pawel@gmail.com>
2024-04-19 11:13:28 +0200

    NBSNEBIUS-105: ADD_HOST should register new devices (#996)

--- cloud/blockstore/tests/external_endpoint/test.py
+++ cloud/blockstore/tests/external_endpoint/test.py
@@ -125,6 +125,8 @@ def start_nbs_daemon(ydb, fake_vhost_server):
     server.VhostEnabled = True
     server.VhostServerPath = fake_vhost_server.path
 
+    cfg.files["storage"].NonReplicatedDontSuspendDevices = True
+
     daemon = start_nbs(cfg)
 
     yield daemon
@@ -156,23 +158,15 @@ def setup_env(nbs, disk_agent, data_path):
 
     request = TCmsActionRequest()
 
-    for i in range(2):
-        action = request.Actions.add()
-        action.Type = TAction.ADD_DEVICE
-        action.Host = get_fqdn()
-        action.Device = os.path.join(data_path, f"NVMENBS{i + 1:02}")
+    action = request.Actions.add()
+    action.Type = TAction.ADD_HOST
+    action.Host = get_fqdn()
 
     response = client.cms_action(request)
 
-    assert len(response.ActionResults) == 2
+    assert len(response.ActionResults) == 1
     for r in response.ActionResults:
         assert r.Result.Code == 0, r
-
-    for i in range(2):
-        client.resume_device(
-            get_fqdn(),
-            os.path.join(data_path, f"NVMENBS{i + 1:02}"),
-            dry_run=False)
 
     # wait for devices to be cleared
     nbs_client = NbsClient(nbs.port)
