b158e385a5a89beed3813914a733c35c3593b276
Dmitry Slobodchikov <sdimanx91@mail.ru>
2024-04-27 14:04:34 +0300

    NBS:4764: coverity fixes

--- cloud/blockstore/tools/nbd/bootstrap.cpp
+++ cloud/blockstore/tools/nbd/bootstrap.cpp
@@ -337,7 +337,8 @@ void TBootstrap::Stop()
     }
 
     if (Session) {
-        auto response = Session->UnmountVolume().GetValue(WaitTimeout);
+        auto future = Session->UnmountVolume();
+        const auto& response = future.GetValue(WaitTimeout);
         CheckError(response);
     }
 
@@ -466,7 +467,8 @@ void TBootstrap::InitClientSession()
 
 void TBootstrap::StartNbdServer(TNetworkAddress listenAddress)
 {
-    auto mountResponse = Session->MountVolume().GetValue(WaitTimeout);
+    auto mountFuture = Session->MountVolume();
+    const auto& mountResponse = mountFuture.GetValue(WaitTimeout);
     CheckError(mountResponse);
 
     TStorageOptions options;
