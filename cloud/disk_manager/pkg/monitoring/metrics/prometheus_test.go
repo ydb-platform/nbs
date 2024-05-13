3a4aca2e601a89e73cf9021cf53f6f55c8fc4a78
Sergei <149376659+jkuradobery@users.noreply.github.com>
2024-05-13 18:26:00 +0300

    [Disk Manager]: Add prometheus monitoring to opensource main (#874)
    * [Disk Manager]: NBSNEBIUS-204: Add prometheus registry
    
    Use prometheus registry in disk manager command line
    
    * Add component tag to prometheus registry

---
+++ cloud/disk_manager/pkg/monitoring/metrics/prometheus_test.go
@@ -0,0 +1,35 @@
+package metrics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func TestPrometheusSanitizeName(t *testing.T) {
+	testCases := []struct {
+		invalidName, expectedSanitizedName string
+	}{
+		{"9invalidName", "_9invalidName"},
+		{"validName", "validName"},
+		{"_valid_Name", "_valid_Name"},
+		{"invalid-Name", "invalid_Name"},
+		{"123", "_123"},
+		{"!@#$%^&*()name", "________name"},
+	}
+
+	for _, testCase := range testCases {
+		actualSanitizedName := sanitizeName(testCase.invalidName)
+		require.Equal(
+			t,
+			testCase.expectedSanitizedName,
+			actualSanitizedName,
+			"sanitizeName(%q) should be %q, got %q",
+			testCase.invalidName,
+			testCase.expectedSanitizedName,
+			actualSanitizedName,
+		)
+	}
+}
