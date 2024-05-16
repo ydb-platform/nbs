71b1e0dc37e07333e8e644b41a16db97efbc9198
Maxim Deb Natkh <debnatkh@yandex.ru>
2024-05-16 18:17:26 +0000

    Revert "[Filestore]: fix localdb-compaction-test: do not consider E_FS_NOENT and E_FS_EXIST fatal because of the filestore restarts"
    This reverts commit fd4574f99a828d5392f99e3bd3d492dba99dea6f.

--- cloud/filestore/tools/testing/loadtest/lib/test.cpp
+++ cloud/filestore/tools/testing/loadtest/lib/test.cpp
@@ -604,20 +604,11 @@ private:
 
             auto code = request->Error.GetCode();
             if (FAILED(code)) {
-                bool expectedErrorCode = false;
-                for (auto code: Config.GetSuccessOnError()) {
-                    if (code == request->Error.GetCode()) {
-                        expectedErrorCode = true;
-                        break;
-                    }
-                }
-                if (!expectedErrorCode) {
-                    STORAGE_ERROR("%s failing test due to: %s",
-                        MakeTestTag().c_str(),
-                        FormatError(request->Error).c_str());
+                STORAGE_ERROR("%s failing test due to: %s",
+                    MakeTestTag().c_str(),
+                    FormatError(request->Error).c_str());
 
-                    TestStats.Success = false;
-                }
+                TestStats.Success = false;
             }
 
             auto& stats = TestStats.ActionStats[request->Action];
