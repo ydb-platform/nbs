8e55f71b4ee0812d4842cde6da3502e0dd6731d9
BarkovBG <barkovbg@gmail.com>
2024-05-21 08:54:52 +0000

    draft

--- cloud/disk_manager/internal/pkg/services/pools/storage/consistency_check.go
+++ cloud/disk_manager/internal/pkg/services/pools/storage/consistency_check.go
@@ -10,6 +10,49 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////
 
+func (s *storageYDB) checkPoolsConsistency(
+	pools []pool,
+	baseDisks []baseDisk,
+	slots []slot,
+) error {
+
+	poolsActiveUnits := map[string]uint64{}
+	baseDisksActiveUnits := map[string]uint64{}
+	slotsActiveUnits := map[string]uint64{}
+
+	for _, pool := range pools {
+		poolsActiveUnits[pool.imageID] += pool.acquiredUnits
+	}
+
+	for _, baseDisk := range baseDisks {
+		baseDisksActiveUnits[baseDisk.imageID] += baseDisk.activeUnits
+	}
+
+	for _, slot := range slots {
+		slotsActiveUnits[slot.imageID] += slot.allottedUnits
+	}
+
+	for _, pool := range pools {
+		if poolsActiveUnits[pool.imageID] != baseDisksActiveUnits[pool.imageID] {
+			return errors.NewNonRetriableErrorf(
+				"pool acquiredUnits %v is not equal to baseDisksActiveUnits %v",
+				poolsActiveUnits[pool.imageID],
+				baseDisksActiveUnits[pool.imageID],
+			)
+		}
+
+		if poolsActiveUnits[pool.imageID] != slotsActiveUnits[pool.imageID] {
+			return errors.NewNonRetriableErrorf(
+				"pool acquiredUnits %v is not equal to slotsActiveUnits %v",
+				pool.acquiredUnits,
+				slotsActiveUnits[pool.imageID],
+			)
+		}
+	}
+
+	return nil
+}
+
 func (s *storageYDB) checkBaseDiskConsistency(
 	baseDisk baseDisk,
 	slots []slot,
@@ -40,6 +83,95 @@ func (s *storageYDB) checkBaseDiskConsistency(
 	return nil
 }
 
+func (s *storageYDB) getPools(
+	ctx context.Context,
+	tx *persistence.Transaction,
+) ([]pool, error) {
+
+	res, err := tx.Execute(ctx, fmt.Sprintf(`
+		--!syntax_v1
+		pragma TablePathPrefix = "%v";
+
+		select *
+		from pools
+	`, s.tablesPath))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+
+	var pools []pool
+	for res.NextResultSet(ctx) {
+		for res.NextRow() {
+			pool, err := scanPool(res)
+			if err != nil {
+				return nil, err
+			}
+
+			pools = append(pools, pool)
+		}
+	}
+
+	return pools, nil
+}
+
+func (s *storageYDB) getBaseDisks(
+	ctx context.Context,
+	tx *persistence.Transaction,
+) ([]baseDisk, error) {
+
+	res, err := tx.Execute(ctx, fmt.Sprintf(`
+		--!syntax_v1
+		pragma TablePathPrefix = "%v";
+
+		select *
+		from base_disks
+	`, s.tablesPath))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+
+	baseDisks, err := scanBaseDisks(ctx, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return baseDisks, nil
+}
+
+func (s *storageYDB) getSlots(
+	ctx context.Context,
+	tx *persistence.Transaction,
+) ([]slot, error) {
+
+	res, err := tx.Execute(ctx, fmt.Sprintf(`
+		--!syntax_v1
+		pragma TablePathPrefix = "%v";
+
+		select *
+		from slots
+	`, s.tablesPath))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+
+	var slots []slot
+	for res.NextResultSet(ctx) {
+		for res.NextRow() {
+			slot, err := scanSlot(res)
+			if err != nil {
+				return nil, err
+			}
+
+			slots = append(slots, slot)
+		}
+	}
+
+	return slots, nil
+}
+
 func (s *storageYDB) checkConsistency(
 	ctx context.Context,
 	session *persistence.Session,
@@ -51,48 +183,27 @@ func (s *storageYDB) checkConsistency(
 	}
 	defer tx.Rollback(ctx)
 
-	res, err := tx.Execute(ctx, fmt.Sprintf(`
-		--!syntax_v1
-		pragma TablePathPrefix = "%v";
-
-		select *
-		from base_disks
-	`, s.tablesPath))
-	if err != nil {
-		return err
-	}
-	defer res.Close()
-
-	baseDisks, err := scanBaseDisks(ctx, res)
+	pools, err := s.getPools(ctx, tx)
 	if err != nil {
 		return err
 	}
 
-	res, err = tx.Execute(ctx, fmt.Sprintf(`
-		--!syntax_v1
-		pragma TablePathPrefix = "%v";
-
-		select *
-		from slots
-	`, s.tablesPath))
+	baseDisks, err := s.getBaseDisks(ctx, tx)
 	if err != nil {
 		return err
 	}
-	defer res.Close()
 
-	var slots []slot
-	for res.NextResultSet(ctx) {
-		for res.NextRow() {
-			slot, err := scanSlot(res)
-			if err != nil {
-				return err
-			}
-
-			slots = append(slots, slot)
-		}
+	slots, err := s.getSlots(ctx, tx)
+	if err != nil {
+		return err
 	}
 
 	err = tx.Commit(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = s.checkPoolsConsistency(pools, baseDisks, slots)
 	if err != nil {
 		return err
 	}
