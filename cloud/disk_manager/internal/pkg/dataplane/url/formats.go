dd3c892e18ca025a8045f5295753771081c398ec
BarkovBG <barkovbg@gmail.com>
2024-05-24 09:18:18 +0000

    add comments

--- cloud/disk_manager/internal/pkg/dataplane/url/formats.go
+++ cloud/disk_manager/internal/pkg/dataplane/url/formats.go
@@ -9,12 +9,22 @@ import (
 
 //////////////////////////////////////////////////////////////////////////////
 
+const cookieBufferSize = 68 // Should be enough for all formats.
+
 var formatCookies = map[ImageFormat][]byte{
 	ImageFormatQCOW2: {0x51, 0x46, 0x49, 0xFB},                         // QFI\xfb
 	ImageFormatVMDK:  {0x4b, 0x44, 0x4d, 0x56},                         // KDMV
 	ImageFormatVHDX:  {0x76, 0x68, 0x64, 0x78, 0x66, 0x69, 0x6c, 0x65}, // vhdxfile
 	ImageFormatVHD:   {0x63, 0x6f, 0x6e, 0x65, 0x63, 0x74, 0x69, 0x78}, // connectix
 	ImageFormatVDI:   {0x7f, 0x10, 0xda, 0xbe},
+}
+
+var formatCookieOffset = map[ImageFormat]int{
+	ImageFormatVDI: 0x40,
+	// We want to recognize only VHD DYNAMIC format which has a copy of footer
+	// at the beginning of the image.
+	// VHD RAW format should be recognized as RAW format.
+	ImageFormatVHD: 0,
 }
 
 type ImageFormat string
@@ -26,8 +36,6 @@ const (
 	ImageFormatVHDX  ImageFormat = "vhdx"
 	ImageFormatVHD   ImageFormat = "vhd" // Also known as vpc.
 	ImageFormatVDI   ImageFormat = "vdi"
-
-	ImageFormatVDIHeaderOffset = 0x40
 )
 
 //////////////////////////////////////////////////////////////////////////////
@@ -48,18 +56,18 @@ func guessImageFormat(
 	reader common.Reader,
 ) (ImageFormat, error) {
 
-	for imageFormat := range formatCookies {
-		offset := 0
-		switch imageFormat {
-		case ImageFormatVDI:
-			offset = ImageFormatVDIHeaderOffset
-		}
+	buffer := make([]byte, cookieBufferSize)
+	_, err := reader.Read(ctx, 0, buffer)
+	if err != nil {
+		return "", err
+	}
 
+	for imageFormat := range formatCookies {
 		ok, err := hasAtOffset(
 			ctx,
-			reader,
+			buffer,
 			formatCookies[imageFormat],
-			offset,
+			formatCookieOffset[imageFormat],
 		)
 		if err != nil {
 			return "", err
@@ -75,20 +83,14 @@ func guessImageFormat(
 
 func hasAtOffset(
 	ctx context.Context,
-	reader common.Reader,
+	buffer []byte,
 	cookie []byte,
 	offset int,
 ) (bool, error) {
 
-	if offset+len(cookie) > int(reader.Size()) {
+	if offset+len(cookie) > len(buffer) {
 		return false, nil
 	}
 
-	buffer := make([]byte, len(cookie))
-	_, err := reader.Read(ctx, uint64(offset), buffer)
-	if err != nil {
-		return false, err
-	}
-
-	return bytes.Equal(buffer, cookie), nil
+	return bytes.HasPrefix(buffer[offset:], cookie), nil
 }
