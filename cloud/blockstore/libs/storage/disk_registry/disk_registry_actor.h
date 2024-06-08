9fc6f8053a48036591c90d4298cc7a5d53578d45
Daniil Komarevtsev <komarevtsev@yandex-team.ru>
2024-06-08 19:57:21 +0000

    Allow DR to work with more than one DA at once

--- cloud/blockstore/libs/storage/disk_registry/disk_registry_actor.h
+++ cloud/blockstore/libs/storage/disk_registry/disk_registry_actor.h
@@ -104,15 +104,16 @@ private:
     TInstant SecureEraseStartTs;
     TInstant StartMigrationStartTs;
 
-    THashMap<NActors::TActorId, TString> ServerToAgentId;
-
     struct TAgentRegInfo
     {
-        ui64 SeqNo = 0;
+        TString AgentId;
+        TMaybe<bool> TemporaryAgent;
         bool Connected = false;
     };
 
-    THashMap<TString, TAgentRegInfo> AgentRegInfo;
+    THashMap<NActors::TActorId, TAgentRegInfo> AgentRegInfo;
+    THashMap<TString, std::unique_ptr<NActors::TSchedulerCookieHolder>>
+        ScheduledAgentRejects;
 
     // Requests in-progress
     THashSet<NActors::TActorId> Actors;
@@ -215,7 +216,7 @@ private:
     void ScheduleRejectAgent(
         const NActors::TActorContext& ctx,
         TString agentId,
-        ui64 seqNo);
+        std::optional<NActors::TActorId> serverId);
 
     void ScheduleSwitchAgentDisksToReadOnly(
         const NActors::TActorContext& ctx,
@@ -301,6 +302,10 @@ private:
         const NActors::TActorContext& ctx);
 
     void InitializeState(TDiskRegistryStateSnapshot snapshot);
+
+    void OnDiskAgentRegistered(
+        const NActors::TActorContext& ctx,
+        const NProto::TAgentConfig& config);
 
 private:
     STFUNC(StateBoot);
