233252de80f7fc0567cd057c8e8fa538f46eafb0
s-vorobe-v <sergei.vorobev@nebius.com>
2024-06-12 18:40:29 +0200

    Add new IAM authorizer

---
+++ cloud/disk_manager/pkg/internal_auth/auth.go
@@ -0,0 +1,119 @@
+package internal_auth
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	auth_config "github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/auth/config"
+	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/common"
+	"github.com/ydb-platform/nbs/cloud/disk_manager/pkg/auth"
+	iamv1 "github.com/ydb-platform/nbs/cloud/disk_manager/pkg/internal_auth/access_service"
+	auth_credentials "github.com/ydb-platform/ydb-go-sdk/v3/credentials"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	grpc_status "google.golang.org/grpc/status"
+)
+
+type iamAuthorizer struct {
+	client iamv1.AccessServiceClient
+}
+
+func NewAuthenticator(config *auth_config.AuthConfig, creds auth_credentials.Credentials) (auth.Authorizer, error) {
+	duration, err := time.ParseDuration(config.GetAuthorizationCacheLifetime())
+	if err != nil {
+		return nil, err
+	}
+	common.Assert(err == nil, fmt.Sprintf("Error while parsing duration from config %v", err))
+	authorizer, err := newIamAuthorizer(config, creds)
+	if err != nil {
+		return nil, err
+	}
+	return auth.NewAuthorizerWithCache(
+		authorizer,
+		duration,
+	), nil
+}
+
+func newIamAuthorizer(config *auth_config.AuthConfig, creds auth_credentials.Credentials) (auth.Authorizer, error) {
+	ctx := context.Background()
+	conn, err := grpc.DialContext(
+		ctx,
+		config.GetAccessServiceEndpoint(),
+		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	client := iamv1.NewAccessServiceClient(conn)
+	return &iamAuthorizer{
+		client: client,
+	}, nil
+}
+
+func (a *iamAuthorizer) Authorize(ctx context.Context, permission string) (string, error) {
+	container := "" // todo
+	token, err := auth.GetAccessToken(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	response, err := a.client.Authorize(ctx, &iamv1.AuthorizeRequest{
+		Checks: map[int64]*iamv1.AuthorizeCheck{
+			0: {
+				Permission: &iamv1.Permission{
+					Name: permission,
+				},
+				ContainerId: container,
+				ResourcePath: &iamv1.ResourcePath{
+					Path: []*iamv1.Resource{},
+				},
+				Identifier: &iamv1.AuthorizeCheck_IamToken{
+					IamToken: token,
+				},
+			},
+		},
+	})
+	if err != nil {
+		return "", grpc_status.Errorf(
+			codes.Unauthenticated,
+			"Error while authorizing client: %s",
+			err.Error(),
+		)
+	}
+
+	result, ok := response.Results[0]
+
+	if !ok {
+		return "", grpc_status.Error(
+			codes.Unauthenticated,
+			"Authorize result missing",
+		)
+	}
+
+	user, ok := parseUser(result.Account)
+	if !ok {
+		return "", grpc_status.Error(codes.Unauthenticated, "invalid account")
+	}
+
+	if result.ResultCode != iamv1.AuthorizeResult_OK {
+		return "", grpc_status.Error(codes.PermissionDenied, "unauthorized")
+	}
+
+	return user, nil
+}
+
+func parseUser(acc *iamv1.Account) (string, bool) {
+	if userID := acc.GetUserAccount().GetId(); userID != "" {
+		return userID, true
+	}
+
+	if saID := acc.GetServiceAccount().GetId(); saID != "" {
+		return saID, true
+	}
+
+	return "", false
+}
