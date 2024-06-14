b89b715a88c0b1076c9b3cf43da446bb4cd5f71e
Daniil Komarevtsev <komarevtsev@yandex-team.ru>
2024-06-14 16:44:43 +0000

    fix build

--- cloud/blockstore/libs/storage/service/service_actor_actions_get_disk_agent_node_id.cpp
+++ cloud/blockstore/libs/storage/service/service_actor_actions_get_disk_agent_node_id.cpp
@@ -3,10 +3,10 @@
 #include <cloud/blockstore/libs/storage/api/disk_registry.h>
 #include <cloud/blockstore/libs/storage/api/disk_registry_proxy.h>
 
-#include <contrib/ydb/library/actors/core/actor_bootstrapped.h>
-#include <contrib/ydb/library/actors/core/events.h>
-#include <contrib/ydb/library/actors/core/hfunc.h>
-#include <contrib/ydb/library/actors/core/log.h>
+#include <library/cpp/actors/core/actor_bootstrapped.h>
+#include <library/cpp/actors/core/events.h>
+#include <library/cpp/actors/core/hfunc.h>
+#include <library/cpp/actors/core/log.h>
 
 #include <google/protobuf/util/json_util.h>
 
