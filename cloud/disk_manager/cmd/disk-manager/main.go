00a60fe648a294a97bd26e05f95e85dc0d20f7a2
s-vorobe-v <sergei.vorobev@nebius.com>
2024-06-17 17:47:13 +0200

    Add new auth to the entrypoint

--- cloud/disk_manager/cmd/disk-manager/main.go
+++ cloud/disk_manager/cmd/disk-manager/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/ydb-platform/nbs/cloud/disk_manager/pkg/app"
-	"github.com/ydb-platform/nbs/cloud/disk_manager/pkg/auth"
+	"github.com/ydb-platform/nbs/cloud/disk_manager/pkg/internal_auth"
 	"github.com/ydb-platform/nbs/cloud/disk_manager/pkg/monitoring/metrics"
 )
 
@@ -13,9 +13,6 @@ func main() {
 		"disk-manager",
 		"/etc/disk-manager/server-config.txt",
 		metrics.NewPrometheusRegistry,
-		func(config *auth.AuthConfig, creds auth.Credentials) (auth.Authorizer, error) {
-			// TODO: should use real authorizer, not stub.
-			return auth.NewStubAuthorizer(), nil
-		},
+		internal_auth.NewAuthenticator,
 	)
 }
