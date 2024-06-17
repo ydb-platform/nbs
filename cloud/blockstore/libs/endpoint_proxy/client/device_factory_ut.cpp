345d6ae090a0b13b424884a29375359811788af5
Aleksandr Rigachnyi <a.rigachnyy@gmail.com>
2024-06-17 19:35:00 +0200

    issue-1164: NBD::IDevice::{Start,Stop} should return futures (#1428) (#1433)
    * issue-1164: NBD::IDevice::{Start,Stop} should return futures
    
    * issue-1164: ProxyDevice should not call StopProxyEndpoint if deleteDevice == false
    
    * issue-1164: fix build
    
    Co-authored-by: Andrei Strelkovskii <thinkingwhat@yandex.ru>

--- cloud/blockstore/libs/endpoint_proxy/client/device_factory_ut.cpp
+++ cloud/blockstore/libs/endpoint_proxy/client/device_factory_ut.cpp
@@ -20,25 +20,46 @@ struct TTestEndpointProxyClient: NClient::IEndpointProxyClient
 
     template <typename T>
     using TFuture = NThreading::TFuture<T>;
+    template <typename T>
+    using TPromise = NThreading::TPromise<T>;
+
+    TPromise<NProto::TStartProxyEndpointResponse> StartResponse;
+    TPromise<NProto::TStopProxyEndpointResponse> StopResponse;
+
+    void CompleteStartProxyEndpoint(NProto::TError error = {})
+    {
+        NProto::TStartProxyEndpointResponse response;
+        response.SetInternalUnixSocketPath(
+            StartRequest.GetUnixSocketPath() + ".p");
+        *response.MutableError() = std::move(error);
+        StartResponse.SetValue(std::move(response));
+    }
+
+    void CompleteStopProxyEndpoint(NProto::TError error = {})
+    {
+        NProto::TStopProxyEndpointResponse response;
+        response.SetInternalUnixSocketPath(
+            StopRequest.GetUnixSocketPath() + ".p");
+        *response.MutableError() = std::move(error);
+        StopResponse.SetValue(std::move(response));
+    }
 
     TFuture<NProto::TStartProxyEndpointResponse> StartProxyEndpoint(
         std::shared_ptr<NProto::TStartProxyEndpointRequest> request) override
     {
         StartRequest = *request;
-        NProto::TStartProxyEndpointResponse response;
-        response.SetInternalUnixSocketPath(
-            StartRequest.GetUnixSocketPath() + ".p");
-        return NThreading::MakeFuture(response);
+        StartResponse =
+            NThreading::NewPromise<NProto::TStartProxyEndpointResponse>();
+        return StartResponse;
     }
 
     TFuture<NProto::TStopProxyEndpointResponse> StopProxyEndpoint(
         std::shared_ptr<NProto::TStopProxyEndpointRequest> request) override
     {
         StopRequest = *request;
-        NProto::TStopProxyEndpointResponse response;
-        response.SetInternalUnixSocketPath(
-            StartRequest.GetUnixSocketPath() + ".p");
-        return NThreading::MakeFuture(response);
+        StopResponse =
+            NThreading::NewPromise<NProto::TStopProxyEndpointResponse>();
+        return StopResponse;
     }
 
     TFuture<NProto::TListProxyEndpointsResponse> ListProxyEndpoints(
@@ -82,7 +103,15 @@ Y_UNIT_TEST_SUITE(TProxyDeviceFactoryTest)
         UNIT_ASSERT_VALUES_EQUAL("", client->StartRequest.GetUnixSocketPath());
         UNIT_ASSERT_VALUES_EQUAL("", client->StopRequest.GetUnixSocketPath());
 
-        device->Start();
+        auto startFuture = device->Start();
+        UNIT_ASSERT(!startFuture.HasValue());
+        client->CompleteStartProxyEndpoint();
+        UNIT_ASSERT(startFuture.HasValue());
+        auto error = startFuture.GetValue();
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            S_OK,
+            error.GetCode(),
+            error.GetMessage());
         UNIT_ASSERT_VALUES_EQUAL(
             socketPath,
             client->StartRequest.GetUnixSocketPath());
@@ -97,10 +126,66 @@ Y_UNIT_TEST_SUITE(TProxyDeviceFactoryTest)
             client->StartRequest.GetBlockSize());
         UNIT_ASSERT_VALUES_EQUAL("", client->StopRequest.GetUnixSocketPath());
 
-        device->Stop(true /* delete device */);
+        auto stopFuture = device->Stop(true /* delete device */);
+        UNIT_ASSERT(!stopFuture.HasValue());
+        client->CompleteStopProxyEndpoint();
+        UNIT_ASSERT(stopFuture.HasValue());
+        error = stopFuture.GetValue();
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            S_OK,
+            error.GetCode(),
+            error.GetMessage());
         UNIT_ASSERT_VALUES_EQUAL(
             socketPath,
             client->StopRequest.GetUnixSocketPath());
+    }
+
+    Y_UNIT_TEST(ShouldHandleClientErrors)
+    {
+        const TString socketPath = "/some/socket";
+        const TString deviceName = "/dev/nbdX";
+        const ui64 blockCount = 1024;
+        const ui32 blockSize = 4096;
+        const ui32 sectorSize = 512;
+
+        auto client = std::make_shared<TTestEndpointProxyClient>();
+        auto factory = CreateProxyDeviceFactory({sectorSize}, client);
+
+        auto device = factory->Create(
+            TNetworkAddress(TUnixSocketPath(socketPath)),
+            deviceName,
+            blockCount,
+            blockSize);
+        UNIT_ASSERT_VALUES_EQUAL("", client->StartRequest.GetUnixSocketPath());
+        UNIT_ASSERT_VALUES_EQUAL("", client->StopRequest.GetUnixSocketPath());
+
+        auto startFuture = device->Start();
+        UNIT_ASSERT(!startFuture.HasValue());
+        client->CompleteStartProxyEndpoint(MakeError(E_FAIL));
+        UNIT_ASSERT(startFuture.HasValue());
+        auto error = startFuture.GetValue();
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            E_FAIL,
+            error.GetCode(),
+            error.GetMessage());
+
+        auto stopFuture = device->Stop(true /* delete device */);
+        UNIT_ASSERT(!stopFuture.HasValue());
+        client->CompleteStopProxyEndpoint(MakeError(E_FAIL));
+        UNIT_ASSERT(stopFuture.HasValue());
+        error = stopFuture.GetValue();
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            E_FAIL,
+            error.GetCode(),
+            error.GetMessage());
+
+        stopFuture = device->Stop(false /* delete device */);
+        UNIT_ASSERT(stopFuture.HasValue());
+        error = stopFuture.GetValue();
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            S_OK,
+            error.GetCode(),
+            error.GetMessage());
     }
 }
 
--- cloud/blockstore/libs/nbd/device.h
+++ cloud/blockstore/libs/nbd/device.h
@@ -4,6 +4,10 @@
 
 #include <cloud/blockstore/libs/diagnostics/public.h>
 
+#include <cloud/storage/core/libs/common/error.h>
+
+#include <library/cpp/threading/future/future.h>
+
 #include <util/datetime/base.h>
 #include <util/generic/string.h>
 #include <util/network/address.h>
@@ -16,12 +20,12 @@ struct IDevice
 {
     virtual ~IDevice() = default;
 
-    virtual void Start() = 0;
+    virtual NThreading::TFuture<NProto::TError> Start() = 0;
 
     // some devices can be reconfigured, so we want to differentiate between
     // cases when user explicitly asked us to stop the device and it being
     // stopped for technical reasons like service restart
-    virtual void Stop(bool deleteDevice) = 0;
+    virtual NThreading::TFuture<NProto::TError> Stop(bool deleteDevice) = 0;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
