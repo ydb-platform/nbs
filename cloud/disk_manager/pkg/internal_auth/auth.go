7a9297f2170837ac7a42a4cdee44469d348583dc
s-vorobe-v <sergei.vorobev@nebius.com>
2024-06-17 15:41:25 +0200

    Add root container for authorization

--- cloud/disk_manager/pkg/internal_auth/auth.go
+++ cloud/disk_manager/pkg/internal_auth/auth.go
@@ -6,38 +6,61 @@ import (
 	"fmt"
 	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	grpc_status "google.golang.org/grpc/status"
+
 	auth_config "github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/auth/config"
 	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/common"
 	"github.com/ydb-platform/nbs/cloud/disk_manager/pkg/auth"
 	iamv1 "github.com/ydb-platform/nbs/cloud/disk_manager/pkg/internal_auth/access_service"
 	auth_credentials "github.com/ydb-platform/ydb-go-sdk/v3/credentials"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"
-	grpc_status "google.golang.org/grpc/status"
 )
 
-type iamAuthorizer struct {
-	client iamv1.AccessServiceClient
+////////////////////////////////////////////////////////////////////////////////
+
+func tryParseUser(acc *iamv1.Account) (string, bool) {
+	if userID := acc.GetUserAccount().GetId(); userID != "" {
+		return userID, true
+	}
+
+	if saID := acc.GetServiceAccount().GetId(); saID != "" {
+		return saID, true
+	}
+
+	return "", false
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 func NewAuthenticator(config *auth_config.AuthConfig, creds auth_credentials.Credentials) (auth.Authorizer, error) {
+
 	duration, err := time.ParseDuration(config.GetAuthorizationCacheLifetime())
 	if err != nil {
 		return nil, err
 	}
+
 	common.Assert(err == nil, fmt.Sprintf("Error while parsing duration from config %v", err))
 	authorizer, err := newIamAuthorizer(config, creds)
 	if err != nil {
 		return nil, err
 	}
+
 	return auth.NewAuthorizerWithCache(
 		authorizer,
 		duration,
 	), nil
 }
 
-func newIamAuthorizer(config *auth_config.AuthConfig, creds auth_credentials.Credentials) (auth.Authorizer, error) {
+type iamAuthorizer struct {
+	client  iamv1.AccessServiceClient
+	iamRoot string
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func newIamAuthorizer(config *auth_config.AuthConfig, _ auth_credentials.Credentials) (auth.Authorizer, error) {
 	ctx := context.Background()
 	conn, err := grpc.DialContext(
 		ctx,
@@ -50,12 +73,12 @@ func newIamAuthorizer(config *auth_config.AuthConfig, creds auth_credentials.Cre
 
 	client := iamv1.NewAccessServiceClient(conn)
 	return &iamAuthorizer{
-		client: client,
+		client:  client,
+		iamRoot: config.GetServiceAccount().GetRoot(),
 	}, nil
 }
 
 func (a *iamAuthorizer) Authorize(ctx context.Context, permission string) (string, error) {
-	container := "" // todo
 	token, err := auth.GetAccessToken(ctx)
 	if err != nil {
 		return "", err
@@ -67,7 +90,7 @@ func (a *iamAuthorizer) Authorize(ctx context.Context, permission string) (strin
 				Permission: &iamv1.Permission{
 					Name: permission,
 				},
-				ContainerId: container,
+				ContainerId: a.iamRoot,
 				ResourcePath: &iamv1.ResourcePath{
 					Path: []*iamv1.Resource{},
 				},
@@ -86,7 +109,6 @@ func (a *iamAuthorizer) Authorize(ctx context.Context, permission string) (strin
 	}
 
 	result, ok := response.Results[0]
-
 	if !ok {
 		return "", grpc_status.Error(
 			codes.Unauthenticated,
@@ -94,7 +116,7 @@ func (a *iamAuthorizer) Authorize(ctx context.Context, permission string) (strin
 		)
 	}
 
-	user, ok := parseUser(result.Account)
+	user, ok := tryParseUser(result.Account)
 	if !ok {
 		return "", grpc_status.Error(codes.Unauthenticated, "invalid account")
 	}
@@ -104,16 +126,4 @@ func (a *iamAuthorizer) Authorize(ctx context.Context, permission string) (strin
 	}
 
 	return user, nil
-}
-
-func parseUser(acc *iamv1.Account) (string, bool) {
-	if userID := acc.GetUserAccount().GetId(); userID != "" {
-		return userID, true
-	}
-
-	if saID := acc.GetServiceAccount().GetId(); saID != "" {
-		return saID, true
-	}
-
-	return "", false
 }
