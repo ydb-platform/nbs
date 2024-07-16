46566f7481221cf70a5c0ab2d5a86520ec707894
qkrorlqr <thinkingwhat@yandex.ru>
2024-07-16 21:28:47 +0000

    issue-1350: generalized TCreateNodeInFollowerActor to cover the needs of the CreateHandle op and got rid of TCreateNodeInFollowerUponCreateHandleActor

--- cloud/filestore/libs/storage/tablet/tablet_actor.h
+++ cloud/filestore/libs/storage/tablet/tablet_actor.h
@@ -369,7 +369,7 @@ private:
         NProto::TCreateNodeRequest request,
         ui64 requestId,
         ui64 opLogEntryId,
-        NProto::TCreateNodeResponse response);
+        TCreateNodeInFollowerResult result);
 
     void RegisterUnlinkNodeInFollowerActor(
         const NActors::TActorContext& ctx,
@@ -485,10 +485,6 @@ private:
 
     void HandleNodeCreatedInFollower(
         const TEvIndexTabletPrivate::TEvNodeCreatedInFollower::TPtr& ev,
-        const NActors::TActorContext& ctx);
-
-    void HandleNodeCreatedInFollowerUponCreateHandle(
-        const TEvIndexTabletPrivate::TEvNodeCreatedInFollowerUponCreateHandle::TPtr& ev,
         const NActors::TActorContext& ctx);
 
     void HandleNodeUnlinkedInFollower(
