459db6a65f89c3b28bf689f0ec4c5494418aada0
yegorskii <aym001@yandex.ru>
2024-07-19 12:27:53 +0000

    refactor node registration

--- cloud/blockstore/libs/server/config.cpp
+++ cloud/blockstore/libs/server/config.cpp
@@ -317,4 +317,22 @@ TServerAppConfig::DeprecatedGetRdmaClientConfig() const
     return ServerConfig->GetRdmaClientConfig();
 }
 
+TString GetCertFileFromConfig(const TServerAppConfig& serverConfig)
+{
+    const auto& certs = serverConfig.GetCerts();
+    if (certs.empty()) {
+        return serverConfig.GetCertFile();
+    }
+    return certs.front().CertFile;
+}
+
+TString GetCertPrivateKeyFileFromConfig(const TServerAppConfig& serverConfig)
+{
+    const auto& certs = serverConfig.GetCerts();
+    if (certs.empty()) {
+        return serverConfig.GetCertPrivateKeyFile();
+    }
+    return certs.front().CertPrivateKeyFile;
+}
+
 }   // namespace NCloud::NBlockStore::NServer
