e8eac4252873199e0581ef5907d80c93ab1871a9
yegorskii <aym001@yandex.ru>
2024-07-23 08:53:19 +0000

    update

--- cloud/storage/core/libs/kikimr/node_registration_settings.h
+++ cloud/storage/core/libs/kikimr/node_registration_settings.h
@@ -7,6 +7,10 @@ namespace NCloud::NStorage {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+// This struct encapsulates parameters required for node registration in
+// ydb cluster and initial configuration acquiring from cms. Right now these
+// parameters are actually the part of TServerConfig.
+// TODO: #https://github.com/ydb-platform/nbs/issues/1656
 struct TNodeRegistrationSettings
 {
     ui32 MaxAttempts = 0;
