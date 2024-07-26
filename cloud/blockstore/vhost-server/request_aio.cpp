b5aab97688d73ff2070fa433d37f7a997972dab6
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2024-07-26 11:36:58 +0300

    Use smart pointers for memory mangement  (#1662)
    * Small fixes
    
    * Use unique_ptr for memory management
    
    * Fix creation call
    
    * Fix review issues

--- cloud/blockstore/vhost-server/request_aio.cpp
+++ cloud/blockstore/vhost-server/request_aio.cpp
@@ -6,6 +6,7 @@
 #include <util/system/sanitizers.h>
 
 #include <algorithm>
+#include <memory>
 
 namespace NCloud::NBlockStore::NVHostServer {
 namespace {
@@ -31,7 +32,7 @@ void PrepareCompoundIO(
             return device.EndOffset <= offset;
         });
 
-    auto end = std::lower_bound(
+    const auto end = std::lower_bound(
         it,
         devices.end(),
         logicalOffset + totalBytes,
@@ -50,45 +51,51 @@ void PrepareCompoundIO(
         bio->first_sector,
         bio->total_sectors);
 
-    auto* req = static_cast<TAioCompoundRequest*>(
-        std::calloc(1, sizeof(TAioCompoundRequest)));
-
-    req->Inflight = n;
-    req->Io = io;
-    req->Buffer = static_cast<char*>(std::aligned_alloc(
-        VHD_SECTOR_SIZE,
-        totalBytes));
-    req->SubmitTs = now;
+    auto req = TAioCompoundRequest::CreateNew(n, io, totalBytes, now);
 
     if (bio->type == VHD_BDEV_WRITE) {
-        SgListCopy(bio->sglist, req->Buffer);
+        // TODO(drbasic): encryption
+        SgListCopy(bio->sglist, req->Buffer.get());
     }
 
     ui64 deviceOffset = logicalOffset - it->StartOffset;
-    char* ptr = req->Buffer;
+    char* ptr = req->Buffer.get();
     for (; it != end; ++it) {
-        iocb* sreq = static_cast<iocb*>(std::calloc(1, sizeof(iocb)));
+        auto subRequest = TAioSubRequest::CreateNew();
 
         const ui64 count = Min<ui64>(
             totalBytes,
             it->EndOffset - it->StartOffset - deviceOffset);
 
         if (bio->type == VHD_BDEV_READ) {
-            io_prep_pread(sreq, it->File, ptr, count, it->FileOffset + deviceOffset);
+            io_prep_pread(
+                subRequest.get(),
+                it->File,
+                ptr,
+                count,
+                it->FileOffset + deviceOffset);
         } else {
-            io_prep_pwrite(sreq, it->File, ptr, count, it->FileOffset + deviceOffset);
+            io_prep_pwrite(
+                subRequest.get(),
+                it->File,
+                ptr,
+                count,
+                it->FileOffset + deviceOffset);
         }
 
-        sreq->data = req;
+        // Save the address of TAioCompoundRequest in each subrequest to share
+        // ownership among all subrequests.
+        subRequest->data = req.get();
 
-        batch.push_back(sreq);
-        NSan::Release(sreq);
+        batch.push_back(subRequest.release());
 
         ptr += count;
         totalBytes -= count;
         deviceOffset = 0;
     }
-    NSan::Release(req);
+
+    // Ownership transferred to subrequests.
+    req.release();
 }
 
 }   // namespace
@@ -128,7 +135,7 @@ void PrepareIO(
     const i64 logicalOffset = bio->first_sector * VHD_SECTOR_SIZE;
     const i64 totalBytes = bio->total_sectors * VHD_SECTOR_SIZE;
 
-    auto it = std::lower_bound(
+    const auto *it = std::lower_bound(
         devices.begin(),
         devices.end(),
         logicalOffset,
@@ -139,6 +146,7 @@ void PrepareIO(
     Y_ABORT_UNLESS(it != devices.end());
 
     if (it->EndOffset < logicalOffset + totalBytes) {
+        // The request is cross-device, so we split it into two.
         PrepareCompoundIO(Log, devices, io, batch, now);
         return;
     }
@@ -152,11 +160,7 @@ void PrepareIO(
 
     auto [nbufs, buffers] = bio->sglist;
 
-    auto* req = static_cast<TAioRequest*>(std::calloc(1,
-        sizeof(TAioRequest) + sizeof(iovec) * nbufs));
-
-    req->Io = io;
-    req->SubmitTs = now;
+    auto req = TAioRequest::CreateNew(nbufs, io, now);
 
     for (ui32 i = 0; i != nbufs; ++i) {
         // Windows allows i/o with buffers not aligned to i/o block size, but
@@ -176,6 +180,7 @@ void PrepareIO(
             if (bio->type == VHD_BDEV_WRITE) {
                 char* dst = static_cast<char*>(req->Data[0].iov_base);
                 for (ui32 i = 0; i != nbufs; ++i) {
+                    // TODO(drbasic) encrypt
                     std::memcpy(dst, buffers[i].base, buffers[i].len);
                     dst += buffers[i].len;
                 }
@@ -193,15 +198,108 @@ void PrepareIO(
     const auto offset = it->FileOffset + logicalOffset - it->StartOffset;
 
     if (bio->type == VHD_BDEV_READ) {
-        io_prep_preadv(req, it->File, req->Data, nbufs, offset);
+        io_prep_preadv(req.get(), it->File, req->Data, nbufs, offset);
     } else {
-        io_prep_pwritev(req, it->File, req->Data, nbufs, offset);
+        io_prep_pwritev(req.get(), it->File, req->Data, nbufs, offset);
     }
 
-    STORAGE_DEBUG("Prepared IO request with addr: %p", req);
+    STORAGE_DEBUG("Prepared IO request with addr: %p", req.get());
 
-    batch.push_back(req);
-    NSan::Release(req);
+    batch.push_back(req.release());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void TFreeDeleter::operator()(void* obj)
+{
+    std::free(obj);
+}
+
+void TAioRequestDeleter::operator()(TAioRequest* obj)
+{
+    if (obj->BounceBuf) {
+        std::free(obj->Data[0].iov_base);
+    }
+    std::free(obj);
+}
+
+TAioRequest::TAioRequest(vhd_io* io, TCpuCycles submitTs)
+    : iocb()
+    , Io(io)
+    , SubmitTs(submitTs)
+{}
+
+// static
+TAioRequestHolder
+TAioRequest::CreateNew(size_t bufferCount, vhd_io* io, TCpuCycles submitTs)
+{
+    const size_t totalSize = sizeof(TAioRequest) + sizeof(iovec) * bufferCount;
+    return TAioRequestHolder{new (std::calloc(1, totalSize))
+                                 TAioRequest(io, submitTs)};
+}
+
+// static
+TAioRequestHolder TAioRequest::FromIocb(iocb* cb)
+{
+    Y_ABORT_UNLESS(cb->data == nullptr);
+    return TAioRequestHolder{static_cast<TAioRequest*>(cb)};
+}
+
+// static
+TAioSubRequestHolder TAioSubRequest::CreateNew()
+{
+    const size_t size = sizeof(TAioSubRequest);
+    return TAioSubRequestHolder{new (std::calloc(1, size)) TAioSubRequest};
+}
+
+// static
+TAioSubRequestHolder TAioSubRequest::FromIocb(iocb* cb) {
+    Y_ABORT_UNLESS(cb->data != nullptr);
+    return TAioSubRequestHolder{static_cast<TAioSubRequest*>(cb)};
+}
+
+TAioCompoundRequest* TAioSubRequest::GetParentRequest() const
+{
+    Y_ABORT_UNLESS(data != nullptr);
+    return static_cast<TAioCompoundRequest*>(data);
+}
+
+TAioCompoundRequestHolder TAioSubRequest::TakeParentRequest()
+{
+    Y_ABORT_UNLESS(data != nullptr);
+    auto result =
+        TAioCompoundRequestHolder{static_cast<TAioCompoundRequest*>(data)};
+    data = nullptr;
+    return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TAioCompoundRequest::TAioCompoundRequest(
+        int inflight,
+        vhd_io* io,
+        size_t bufferSize,
+        TCpuCycles submitTs)
+    : Inflight(inflight)
+    , Io(io)
+    , SubmitTs(submitTs)
+    , Buffer{
+          static_cast<char*>(std::aligned_alloc(VHD_SECTOR_SIZE, bufferSize)),
+      }
+{}
+
+// static
+std::unique_ptr<TAioCompoundRequest> TAioCompoundRequest::CreateNew(
+    int inflight,
+    vhd_io* io,
+    size_t bufferSize,
+    TCpuCycles submitTs)
+{
+    return std::make_unique<TAioCompoundRequest>(
+        inflight,
+        io,
+        bufferSize,
+        submitTs);
 }
 
 }   // namespace NCloud::NBlockStore::NVHostServer
--- cloud/blockstore/vhost-server/request_aio.h
+++ cloud/blockstore/vhost-server/request_aio.h
@@ -21,6 +21,28 @@ namespace NCloud::NBlockStore::NVHostServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+struct TAioRequest;
+struct TAioSubRequest;
+struct TAioCompoundRequest;
+
+// The unique_ptr<> deleter that performs release via std std::free(). It used
+// for memory blocks allocated via std::calloc().
+struct TFreeDeleter
+{
+    void operator()(void* obj);
+};
+
+// The unique_ptr<> deleter for TAioRequest.
+struct TAioRequestDeleter
+{
+    void operator()(TAioRequest* obj);
+};
+
+using TAioRequestHolder = std::unique_ptr<TAioRequest, TAioRequestDeleter>;
+using TAioSubRequestHolder = std::unique_ptr<TAioSubRequest, TFreeDeleter>;
+using TAioCompoundRequestHolder = std::unique_ptr<TAioCompoundRequest>;
+
+////////////////////////////////////////////////////////////////////////////////
 struct TAioDevice
 {
     i64 StartOffset = 0;
@@ -30,23 +52,61 @@ struct TAioDevice
 };
 
 // Single IO request. Also map libvhost's vhd_buffer to iovec.
+// The memory for these objects is allocated via std::calloc.
+// The size of the allocated memory is enough so that the Data can keep all the
+// request buffers. Therefore, Data is the last field in the class.
 struct TAioRequest
     : iocb
 {
     vhd_io* Io;
     TCpuCycles SubmitTs;
-    bool BounceBuf;
+    bool BounceBuf = false;
     iovec Data[ /* Bio->sglist.nbuffers */ ];
+
+    static TAioRequestHolder CreateNew(
+        size_t bufferCount,
+        vhd_io* io,
+        TCpuCycles submitTs);
+    static TAioRequestHolder FromIocb(iocb* cb);
+
+private:
+    TAioRequest(vhd_io* io, TCpuCycles submitTs);
 };
 
-// Compound IO request.
+// Cross-device sub IO request.
+// The memory for these objects is allocated via std::calloc.
+struct TAioSubRequest: public iocb
+{
+    static TAioSubRequestHolder CreateNew();
+    static TAioSubRequestHolder FromIocb(iocb* cb);
+
+    [[nodiscard]] TAioCompoundRequest* GetParentRequest() const;
+    [[nodiscard]] TAioCompoundRequestHolder TakeParentRequest();
+
+private:
+    TAioSubRequest() = default;;
+};
+
+// Cross-device request shared info.
 struct TAioCompoundRequest
 {
     std::atomic<int> Inflight;
     std::atomic<int> Errors;
     vhd_io* Io;
     TCpuCycles SubmitTs;
-    char* Buffer;
+    std::unique_ptr<char, TFreeDeleter> Buffer;
+
+    TAioCompoundRequest(
+        int inflight,
+        vhd_io* io,
+        size_t bufferSize,
+        TCpuCycles submitTs);
+
+    static TAioCompoundRequestHolder CreateNew(
+        int inflight,
+        vhd_io* io,
+        size_t bufferSize,
+        TCpuCycles submitTs);
 };
 
 ////////////////////////////////////////////////////////////////////////////////
