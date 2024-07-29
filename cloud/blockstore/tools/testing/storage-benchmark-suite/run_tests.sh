879fb751c94cb36643644fd115079235d70c8c2b
qkrorlqr <thinkingwhat@yandex.ru>
2024-07-29 11:51:02 +0200

    adding a bunch of bash scripts + fio scenarios to run the same set of fio tests on all storages that we test: extracted ssh params

--- cloud/blockstore/tools/testing/storage-benchmark-suite/run_tests.sh
+++ cloud/blockstore/tools/testing/storage-benchmark-suite/run_tests.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 COMMON=$(cat common_params.txt)
+if [ -f ssh_params.txt ]
+then
+    SSH_PARAMS=$(cat ssh_params.txt)
+    echo "read aux ssh params: ${SSH_PARAMS}"
+else
+    SSH_PARAMS=""
+fi
 TEST=$(cat $1)
 RESULTS="$2"
 NODES="$3"
@@ -11,7 +18,7 @@ pids=()
 i=0
 while read line
 do
-    ssh -n -J sb.exp.einebox.net:57333 -l astr -o StrictHostKeyChecking=no $line "${COMMON} ${TEST}" >> "${RESULTS}" && echo "finished ${line}" &
+    ssh -n "$line" "$SSH_PARAMS" "${COMMON} ${TEST}" >> "${RESULTS}" && echo "finished ${line}" &
     pids[${i}]=$!
     echo "started on ${line}"
     i=$((i + 1))
