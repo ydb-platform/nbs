ydb-platform/nbs

commit bc2f8e0966f19b88d9c3eb8fbc940d3a4d9b0871

issue-1384: check active sessions before destroying fs (#1441)
issue: #1384

Reject DestroyFileStore request in case of available active sessions.
Destroy session after executing commands in filestore client.

cloud/filestore/libs/storage/service/service_actor_destroyfs.cpp
@@ -2,6 +2,9 @@
 
 #include <cloud/filestore/libs/diagnostics/profile_log_events.h>
 #include <cloud/filestore/libs/storage/api/ss_proxy.h>
+#include <cloud/filestore/libs/storage/api/tablet.h>
+#include <cloud/filestore/libs/storage/api/tablet_proxy.h>
+#include <cloud/filestore/private/api/protos/tablet.pb.h>
 
 #include <contrib/ydb/library/actors/core/actor_bootstrapped.h>
 
@@ -32,6 +35,11 @@ public:
 private:
     STFUNC(StateWork);
 
+    void DescribeSessions(const TActorContext& ctx);
+    void HandleDescribeSessionsResponse(
+        const TEvIndexTablet::TEvDescribeSessionsResponse::TPtr& ev,
+        const TActorContext& ctx);
+
     void DestroyFileStore(const TActorContext& ctx);
     void HandleDestroyFileStoreResponse(
         const TEvSSProxy::TEvDestroyFileStoreResponse::TPtr& ev,
@@ -57,8 +65,42 @@ TDestroyFileStoreActor::TDestroyFileStoreActor(
 
 void TDestroyFileStoreActor::Bootstrap(const TActorContext& ctx)
 {
-    DestroyFileStore(ctx);
+    DescribeSessions(ctx);
     Become(&TThis::StateWork);
+}
+
+void TDestroyFileStoreActor::DescribeSessions(const TActorContext& ctx)
+{
+    auto requestToTablet =
+        std::make_unique<TEvIndexTablet::TEvDescribeSessionsRequest>();
+    requestToTablet->Record.SetFileSystemId(FileSystemId);
+
+    NCloud::Send(
+        ctx,
+        MakeIndexTabletProxyServiceId(),
+        std::move(requestToTablet));
+
+    Become(&TThis::StateWork);
+}
+
+void TDestroyFileStoreActor::HandleDescribeSessionsResponse(
+    const TEvIndexTablet::TEvDescribeSessionsResponse::TPtr& ev,
+    const TActorContext& ctx)
+{
+    const auto* msg = ev->Get();
+    if (HasError(msg->GetError())) {
+        ReplyAndDie(ctx, msg->GetError());
+        return;
+    }
+
+    if (msg->Record.SessionsSize() != 0) {
+        ReplyAndDie(
+            ctx,
+            MakeError(E_REJECTED, "FileStore has active sessions"));
+        return;
+    }
+
+    DestroyFileStore(ctx);
 }
 
 void TDestroyFileStoreActor::DestroyFileStore(const TActorContext& ctx)
@@ -99,6 +141,10 @@ STFUNC(TDestroyFileStoreActor::StateWork)
 {
     switch (ev->GetTypeRewrite()) {
         HFunc(TEvents::TEvPoisonPill, HandlePoisonPill);
+
+        HFunc(
+            TEvIndexTablet::TEvDescribeSessionsResponse,
+            HandleDescribeSessionsResponse);
 
         HFunc(TEvSSProxy::TEvDestroyFileStoreResponse, HandleDestroyFileStoreResponse);
 

cloud/filestore/libs/storage/service/service_ut.cpp
@@ -3377,6 +3377,41 @@ Y_UNIT_TEST_SUITE(TStorageServiceTest)
             counters->GetCounter("AppCriticalEvents/NodeNotFoundInFollower");
         UNIT_ASSERT_EQUAL(1, counter->GetAtomic());
     }
+
+    Y_UNIT_TEST(DestroyFileStoreWithActiveSessionShouldFail)
+    {
+        TTestEnv env;
+        env.CreateSubDomain("nfs");
+
+        ui32 nodeIdx = env.CreateNode("nfs");
+
+        const TString fsId = "test";
+        const auto initialBlockCount = 1'000;
+        TServiceClient service(env.GetRuntime(), nodeIdx);
+        service.CreateFileStore(fsId, initialBlockCount);
+
+        auto headers = THeaders{fsId, "client", ""};
+        auto createSessionResponse = service.CreateSession(headers);
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            S_OK,
+            createSessionResponse->GetStatus(),
+            createSessionResponse->GetErrorReason());
+        service.AssertDestroyFileStoreFailed(fsId);
+
+        headers.SessionId =
+            createSessionResponse->Record.GetSession().GetSessionId();
+        auto destroySessionResponse = service.DestroySession(headers);
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            S_OK,
+            destroySessionResponse->GetStatus(),
+            destroySessionResponse->GetErrorReason());
+
+        auto destroyFileStoreResponse = service.DestroyFileStore(fsId);
+        UNIT_ASSERT_VALUES_EQUAL_C(
+            S_OK,
+            destroyFileStoreResponse->GetStatus(),
+            destroyFileStoreResponse->GetErrorReason());
+    }
 }
 
 }   // namespace NCloud::NFileStore::NStorage

cloud/filestore/apps/client/lib/command.cpp
@@ -293,7 +293,7 @@ void TFileStoreCommand::Stop()
     TFileStoreServiceCommand::Stop();
 }
 
-void TFileStoreCommand::CreateSession()
+TFileStoreCommand::TSessionGuard TFileStoreCommand::CreateSession()
 {
     // TODO use ISession instead
     auto request = std::make_shared<NProto::TCreateSessionRequest>();
@@ -309,6 +309,24 @@ void TFileStoreCommand::CreateSession()
 
     Headers.SetClientId(ClientId);
     Headers.SetSessionId(sessionId);
+
+    return TSessionGuard(*this);
+}
+
+void TFileStoreCommand::DestroySession()
+{
+    if (Headers.GetSessionId().Empty()) {
+        return;
+    }
+
+    auto request = std::make_shared<NProto::TDestroySessionRequest>();
+    request->SetFileSystemId(FileSystemId);
+    request->MutableHeaders()->SetSessionId(Headers.GetSessionId());
+    request->MutableHeaders()->SetClientId(Headers.GetClientId());
+
+    TCallContextPtr ctx = MakeIntrusive<TCallContext>();
+    auto response = WaitFor(Client->DestroySession(ctx, std::move(request)));
+    CheckResponse(response);
 }
 
 ////////////////////////////////////////////////////////////////////////////////

cloud/filestore/apps/client/lib/command.h
@@ -139,8 +139,6 @@ public:
     void Start() override;
     void Stop() override;
 
-    void CreateSession();
-
 protected:
     template <typename T>
     std::shared_ptr<T> CreateRequest()
@@ -173,6 +171,27 @@ protected:
     TVector<TPathEntry> ResolvePath(
         TStringBuf path,
         bool ignoreMissing);
+
+    class TSessionGuard final
+    {
+        TFileStoreCommand& FileStoreCmd;
+
+    public:
+        explicit TSessionGuard(TFileStoreCommand& fileStoreCmd)
+            : FileStoreCmd(fileStoreCmd)
+        {
+        }
+
+        ~TSessionGuard()
+        {
+            FileStoreCmd.DestroySession();
+        }
+    };
+
+    [[nodiscard]] TSessionGuard CreateSession();
+
+private:
+    void DestroySession();
 };
 
 ////////////////////////////////////////////////////////////////////////////////

cloud/filestore/apps/client/lib/ls.cpp
@@ -284,7 +284,7 @@ public:
     {
         Y_ENSURE(CliArgs.Mode != EModeType::Unknown, "Mode type is not set");
 
-        CreateSession();
+        auto sessionGuard = CreateSession();
 
         TPageInfo page;
         switch (CliArgs.Mode) {

cloud/filestore/apps/client/lib/mkdir.cpp
@@ -33,7 +33,7 @@ public:
 
     bool Execute() override
     {
-        CreateSession();
+        auto sessionGuard = CreateSession();
 
         auto makeDir = [&] (ui64 nodeId, TStringBuf name) {
             auto request = CreateRequest<NProto::TCreateNodeRequest>();

cloud/filestore/apps/client/lib/read.cpp
@@ -38,7 +38,7 @@ public:
 
     bool Execute() override
     {
-        CreateSession();
+        auto sessionGuard = CreateSession();
 
         const auto resolved = ResolvePath(Path, false);
 

cloud/filestore/apps/client/lib/rm.cpp
@@ -32,7 +32,7 @@ public:
 
     bool Execute() override
     {
-        CreateSession();
+        auto sessionGuard = CreateSession();
 
         const auto resolved = ResolvePath(Path, false);
 

commit 289c7d1c464d7d6d1771257361a0d0766b9260d4

issue-1384: destruction of an already destroyed fs should return S_FALSE (#1659)
Problem:
DestroyFileStore for an already destroyed fs returns NKikimrScheme::EStatus::StatusPathDoesNotExist instead of S_FALSE

Bug was introduced in #1441 because DestroyFileStore actor and DescribeSessions actor are handling missing fs in different ways.

cloud/filestore/libs/storage/service/service_actor_destroyfs.cpp
@@ -97,6 +97,16 @@ void TDestroyFileStoreActor::HandleDescribeSessionsResponse(
 {
     const auto* msg = ev->Get();
     if (HasError(msg->GetError())) {
+        if (msg->GetStatus() ==
+            MAKE_SCHEMESHARD_ERROR(
+                NKikimrScheme::EStatus::StatusPathDoesNotExist))
+        {
+            ReplyAndDie(
+                ctx,
+                MakeError(S_FALSE, FileSystemId.Quote() + " does not exist"));
+            return;
+        }
+
         ReplyAndDie(ctx, msg->GetError());
         return;
     }

cloud/filestore/libs/storage/service/service_ut.cpp
@@ -3539,6 +3539,51 @@ Y_UNIT_TEST_SUITE(TStorageServiceTest)
             destroyFileStoreResponse->GetErrorReason());
     }
 
+    Y_UNIT_TEST(DestroyDestroyedFileStoreShouldFail)
+    {
+        TTestEnv env;
+        env.CreateSubDomain("nfs");
+
+        ui32 nodeIdx = env.CreateNode("nfs");
+
+        const TString fsId = "test";
+        const TString fsId2 = "test2";
+        const auto initialBlockCount = 1'000;
+        TServiceClient service(env.GetRuntime(), nodeIdx);
+        service.CreateFileStore(fsId, initialBlockCount);
+        service.CreateFileStore(fsId2, initialBlockCount);
+
+        {
+            auto destroyFileStoreResponse = service.DestroyFileStore(fsId);
+            UNIT_ASSERT_VALUES_EQUAL_C(
+                S_OK,
+                destroyFileStoreResponse->GetStatus(),
+                destroyFileStoreResponse->GetErrorReason());
+
+            auto alreadyDestroyedFileStoreResponse =
+                service.DestroyFileStore(fsId);
+            UNIT_ASSERT_VALUES_EQUAL_C(
+                S_FALSE,
+                alreadyDestroyedFileStoreResponse->GetStatus(),
+                alreadyDestroyedFileStoreResponse->GetErrorReason());
+        }
+
+        {
+            auto destroyFileStoreResponse = service.DestroyFileStore(fsId2);
+            UNIT_ASSERT_VALUES_EQUAL_C(
+                S_OK,
+                destroyFileStoreResponse->GetStatus(),
+                destroyFileStoreResponse->GetErrorReason());
+
+            auto alreadyDestroyedFileStoreResponse =
+                service.DestroyFileStore(fsId);
+            UNIT_ASSERT_VALUES_EQUAL_C(
+                S_FALSE,
+                alreadyDestroyedFileStoreResponse->GetStatus(),
+                alreadyDestroyedFileStoreResponse->GetErrorReason());
+        }
+    }
+
     Y_UNIT_TEST(ShouldValidateRequestsWithFollowerId)
     {
         TTestEnv env;