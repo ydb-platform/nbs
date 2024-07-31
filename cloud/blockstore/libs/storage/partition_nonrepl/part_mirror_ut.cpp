26b94084dbd85615308a9e01c786311ee9a1a974
Darya Frolova <47457802+WilyTiger@users.noreply.github.com>
2024-07-31 11:30:33 +0200

    fix test, we should wait until scrubbing will definitely scan all disk (#1694)
    * fix test, we should wait until scrubbing will definitely scan all disk
    
    * add comment

--- cloud/blockstore/libs/storage/partition_nonrepl/part_mirror_ut.cpp
+++ cloud/blockstore/libs/storage/partition_nonrepl/part_mirror_ut.cpp
@@ -1242,7 +1242,10 @@ Y_UNIT_TEST_SUITE(TMirrorPartitionTest)
         env.WriteReplica(1, range3, 'B');
 
         iterations = 0;
-        while (fullCyclesCount < 3 && iterations++ < 100) {
+        // at this point, scrubbing may not start from the beginning,
+        // so we need to wait for 2 cycles to be sure that
+        // it has scanned the entire disk at least once
+        while (fullCyclesCount < 4 && iterations++ < 100) {
             if (prevScrubbingProgress != 0 &&
                 counters.Simple.ScrubbingProgress.Value == 0)
             {
@@ -1257,7 +1260,10 @@ Y_UNIT_TEST_SUITE(TMirrorPartitionTest)
 
         // check that all ranges was resynced and there is no more mismatches
         iterations = 0;
-        while (fullCyclesCount < 4 && iterations++ < 100) {
+        // at this point, scrubbing may not start from the beginning,
+        // so we need to wait for 2 cycles to be sure that
+        // it has scanned the entire disk at least once
+        while (fullCyclesCount < 6 && iterations++ < 100) {
             if (prevScrubbingProgress != 0 &&
                 counters.Simple.ScrubbingProgress.Value == 0)
             {
