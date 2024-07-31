840e7345e0598d7cb86301de5b2e47f33010030f
I will never push <test@nebius.ai>
2024-07-31 17:45:15 +0000

    add csi-client

---
+++ cloud/blockstore/tools/csi_driver/client/main.go
@@ -0,0 +1,329 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"os"
+	"time"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func dialGrpcContext(ctx context.Context, endpoint, certificateFile string) (*grpc.ClientConn, error) {
+	var err error
+	grpcTlsCredentials := credentials.NewClientTLSFromCert(nil, "")
+	if certificateFile != "" {
+		grpcTlsCredentials, err = credentials.NewClientTLSFromFile(certificateFile, "")
+		if err != nil {
+			return nil, err
+		}
+	}
+	conn, err := grpc.DialContext(
+		ctx,
+		endpoint,
+		grpc.WithTransportCredentials(grpcTlsCredentials),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return conn, err
+}
+
+func newNodeClient(ctx context.Context, endpoint, certificateFile string) (csi.NodeClient, error) {
+	conn, err := dialGrpcContext(ctx, endpoint, certificateFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return csi.NewNodeClient(conn), nil
+}
+
+func newControllerClient(ctx context.Context, endpoint, certificateFile string) (csi.ControllerClient, error) {
+	conn, err := dialGrpcContext(ctx, endpoint, certificateFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return csi.NewControllerClient(conn), nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func newCreateVolumeCommand(endpoint, certificateFile *string) *cobra.Command {
+	var name string
+	var size int64
+	cmd := cobra.Command{
+		Use:   "createvolume",
+		Short: "Send create volume request to the controller",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFunc := context.WithTimeout(context.Background(), 120*time.Second)
+			defer cancelFunc()
+			client, err := newControllerClient(ctx, *endpoint, *certificateFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			response, err := client.CreateVolume(
+				ctx,
+				&csi.CreateVolumeRequest{
+					Name: name,
+					CapacityRange: &csi.CapacityRange{
+						RequiredBytes: size,
+						LimitBytes:    0,
+					},
+					VolumeCapabilities: []*csi.VolumeCapability{
+						{
+							AccessType: &csi.VolumeCapability_Mount{
+								Mount: nil,
+							},
+							AccessMode: &csi.VolumeCapability_AccessMode{
+								Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+							},
+						},
+					},
+					AccessibilityRequirements: &csi.TopologyRequirement{
+						Requisite: []*csi.Topology{
+							{
+								Segments: map[string]string{
+									"topology.nbs.csi/node": "minikube",
+								},
+							},
+						},
+						Preferred: []*csi.Topology{
+							{
+								Segments: map[string]string{
+									"topology.nbs.csi/node": "minikube",
+								},
+							},
+						},
+					},
+				},
+			)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Response: %v", response)
+		},
+	}
+	cmd.Flags().StringVar(&name, "name", "", "The suggested name for the storage space.")
+	cmd.Flags().Int64Var(&size, "size", 0, "The size of the disk in bytes")
+	err := cmd.MarkFlagRequired("name")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.MarkFlagRequired("size")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &cmd
+}
+
+func newDeleteVolumeCommand(endpoint, certificateFile *string) *cobra.Command {
+	var volumeId string
+	cmd := cobra.Command{
+		Use:   "deletevolume",
+		Short: "Send delete volume request to the controller",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFunc := context.WithTimeout(context.Background(), 120*time.Second)
+			defer cancelFunc()
+			client, err := newControllerClient(ctx, *endpoint, *certificateFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			response, err := client.DeleteVolume(ctx, &csi.DeleteVolumeRequest{
+				VolumeId: volumeId,
+				Secrets:  nil,
+			})
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Response: %v", response)
+		},
+	}
+	cmd.Flags().StringVar(&volumeId, "id", "", "volume id")
+	err := cmd.MarkFlagRequired("id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &cmd
+}
+
+func newPublishVolumeCommand(endpoint, certificateFile *string) *cobra.Command {
+	var volumeId, podId, stagingTargetPath, podName string
+	var readOnly bool
+	cmd := cobra.Command{
+		Use:   "publishvolume",
+		Short: "Send publish volume request to the CSI node",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFunc := context.WithTimeout(context.Background(), 120*time.Second)
+			defer cancelFunc()
+			client, err := newNodeClient(ctx, *endpoint, *certificateFile)
+
+			response, err := client.NodePublishVolume(
+				ctx,
+				&csi.NodePublishVolumeRequest{
+					VolumeId:          volumeId,
+					PublishContext:    nil,
+					StagingTargetPath: "",
+					TargetPath: fmt.Sprintf(
+						"/var/lib/kubelet/pods/%s/volumes/kubernetes.io~csi/%s/mount",
+						podId,
+						volumeId,
+					),
+					VolumeCapability: &csi.VolumeCapability{
+						AccessType: &csi.VolumeCapability_Mount{
+							Mount: nil,
+						},
+						AccessMode: &csi.VolumeCapability_AccessMode{
+							Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+						},
+					},
+					Readonly: false,
+					Secrets:  nil,
+					VolumeContext: map[string]string{
+						"csi.storage.k8s.io/pod.uid":                   podId,
+						"csi.storage.k8s.io/serviceAccount.name":       "default",
+						"csi.storage.k8s.io/ephemeral":                 "false",
+						"csi.storage.k8s.io/pod.namespace":             "default",
+						"csi.storage.k8s.io/pod.name":                  podName,
+						"storage.kubernetes.io/csiProvisionerIdentity": "someIdentity",
+					},
+				},
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Response: %v", response)
+		},
+	}
+	cmd.Flags().StringVar(&volumeId, "volume-id", "", "volume id")
+	cmd.Flags().StringVar(&podId, "pod-id", "", "pod id")
+	cmd.Flags().StringVar(
+		&stagingTargetPath,
+		"staging-target-path",
+		"/var/lib/kubelet/plugins/kubernetes.io/csi/nbs.csi.nebius.ai/a/globalmount",
+		"staging target path")
+	cmd.Flags().StringVar(&podName, "pod-name", "some-pod", "pod name")
+	cmd.Flags().BoolVar(&readOnly, "readonly", false, "volume is read only")
+	err := cmd.MarkFlagRequired("volume-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.MarkFlagRequired("pod-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &cmd
+}
+
+func newUnpublishVolumeCommand(endpoint, certificateFile *string) *cobra.Command {
+	var volumeId, podId string
+	cmd := cobra.Command{
+		Use:   "unpublishvolume",
+		Short: "Send unpublish volume request to the CSI node",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancelFunc := context.WithTimeout(context.Background(), 120*time.Second)
+			defer cancelFunc()
+			client, err := newNodeClient(ctx, *endpoint, *certificateFile)
+
+			response, err := client.NodeUnpublishVolume(
+				ctx,
+				&csi.NodeUnpublishVolumeRequest{
+					VolumeId: volumeId,
+					TargetPath: fmt.Sprintf(
+						"/var/lib/kubelet/pods/%s/volumes/kubernetes.io~csi/%s/mount",
+						podId,
+						volumeId,
+					),
+				},
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Response: %v", response)
+		},
+	}
+	cmd.Flags().StringVar(&volumeId, "volume-id", "", "volume id")
+	cmd.Flags().StringVar(&podId, "pod-id", "", "pod id")
+	err := cmd.MarkFlagRequired("volume-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.MarkFlagRequired("pod-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &cmd
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func newCsiNodeCommand(endpoint, certificateFile *string) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "node",
+		Short: "CSI Driver node commands",
+	}
+	cmd.AddCommand(
+		newPublishVolumeCommand(endpoint, certificateFile),
+		newUnpublishVolumeCommand(endpoint, certificateFile),
+	)
+	return &cmd
+}
+
+func newCsiControllerCommand(endpoint, certificateFile *string) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "controller",
+		Short: "CSI Driver controller command",
+	}
+	cmd.AddCommand(
+		newCreateVolumeCommand(endpoint, certificateFile),
+		newDeleteVolumeCommand(endpoint, certificateFile),
+	)
+	return &cmd
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func main() {
+	var grpcEndpoint, certificateFile string
+	rootCmd := &cobra.Command{
+		Use:   "csi-client",
+		Short: "CSI driver console client for debug",
+	}
+	rootCmd.PersistentFlags().StringVar(
+		&grpcEndpoint,
+		"endpoint",
+		"grpc://localhost:8774",
+		"Path to the client config file",
+	)
+	rootCmd.PersistentFlags().StringVar(
+		&certificateFile,
+		"certificate-path",
+		"",
+		"Path to the server config file",
+	)
+	rootCmd.AddCommand(
+		newCsiNodeCommand(&grpcEndpoint, &certificateFile),
+		newCsiControllerCommand(&grpcEndpoint, &certificateFile),
+	)
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
