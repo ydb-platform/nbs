0c1747fbd19a12f6dc7fbea0c4071ed49c2e057c
I will never push <test@nebius.ai>
2024-08-01 21:17:09 +0000

    Add test for csi nbd qemu

---
+++ cloud/blockstore/tests/csi_driver/test.py
@@ -0,0 +1,177 @@
+import logging
+import os
+import subprocess
+
+import yatest.common as common
+
+import contrib.ydb.tests.library.common.yatest_common as yatest_common
+from cloud.blockstore.config.server_pb2 import TServerAppConfig, TServerConfig, TKikimrServiceConfig
+from cloud.blockstore.config.storage_pb2 import TStorageServiceConfig
+from cloud.blockstore.tests.python.lib.loadtest_env import LocalLoadTest
+from cloud.blockstore.tests.python.lib.test_base import (
+    thread_count
+)
+from cloud.storage.core.protos.endpoints_pb2 import (
+    EEndpointStorageType,
+)
+
+BINARY_PATH = common.binary_path("cloud/blockstore/apps/client/blockstore-client")
+
+
+class CsiLoadTest(LocalLoadTest):
+    def __init__(self, sockets_dir: str, *args, **kwargs):
+        super(CsiLoadTest, self).__init__(*args, **kwargs)
+        self.csi = NbsCsiDriverRunner(sockets_dir)
+        self.csi.start()
+
+    def tear_down(self):
+        self.csi.stop()
+        super(CsiLoadTest, self).tear_down()
+
+
+def setup():
+    server_config_patch = TServerConfig()
+    server_config_patch.NbdEnabled = True
+    endpoints_dir = os.path.join(
+        common.output_path(),
+        "endpoints-%s" % hash(common.context.test_name))
+    server_config_patch.EndpointStorageType = EEndpointStorageType.ENDPOINT_STORAGE_FILE
+    server_config_patch.EndpointStorageDir = endpoints_dir
+    server_config_patch.AllowAllRequestsViaUDS = True
+    sockets_dir = "/run/nbsd"
+    server_config_patch.UnixSocketPath = sockets_dir
+    server_config_patch.VhostEnabled = False
+    server_config_patch.NbdDevicePrefix = "/dev/nbd"
+    server = TServerAppConfig()
+    server.ServerConfig.CopyFrom(server_config_patch)
+    server.ServerConfig.ThreadsCount = thread_count()
+    server.ServerConfig.StrictContractValidation = True
+    server.KikimrServiceConfig.CopyFrom(TKikimrServiceConfig())
+    subprocess.check_call(["modprobe", "nbd"])
+    env = CsiLoadTest(
+        sockets_dir=sockets_dir,
+        endpoint="",
+        server_app_config=server,
+        storage_config_patches=None,
+        use_in_memory_pdisks=True)
+
+    env.results_path = yatest_common.output_path() + "/results.txt"
+    env.results_file = open(env.results_path, "w")
+    env.csi_driver = NbsCsiDriverRunner(sockets_dir)
+
+    def run(*args, **kwargs):
+        args = [BINARY_PATH] + list(args) + [
+            "--host", "localhost",
+            "--port", str(env.nbs_port)]
+        input = kwargs.get("input")
+        if input is not None:
+            input = (input + "\n").encode("utf8")
+
+        logging.info("running command: %s" % args)
+        process = subprocess.Popen(
+            args,
+            stdout=env.results_file,
+            stdin=subprocess.PIPE,
+            cwd=kwargs.get("cwd")
+        )
+        process.communicate(input=input)
+
+        assert process.returncode == kwargs.get("code", 0)
+    return env, run
+
+
+class NbsCsiDriverRunner:
+    def __init__(self, sockets_dir):
+        self._binary_path = common.binary_path("cloud/blockstore/tools/csi_driver/cmd/nbs-csi-driver/nbs-csi-driver")
+        self._client_binary_path = common.binary_path("cloud/blockstore/tools/csi_driver/client/csi-client")
+        self._sockets_dir = sockets_dir
+        self._endpoint = "csi.sock"
+        self._proc = None
+
+    def start(self):
+        self._proc = subprocess.Popen(
+            [
+                self._binary_path,
+                "--name=nbs.csi.nebius.ai",
+                "--version",
+                "v1",
+                "--node-id=localhost",
+                f"--sockets-dir={self._sockets_dir}",
+                f"--endpoint={self._endpoint}"
+            ],
+        )
+
+    def _client_run(self, *args):
+        subprocess.check_call(
+            [
+                self._client_binary_path,
+                *args,
+                "--endpoint",
+                self._endpoint,
+            ]
+        )
+
+    def _node_run(self, *args):
+        return self._client_run("node", *args)
+
+    def _controller_run(self, *args):
+        return self._client_run("controller", *args)
+
+    def create_volume(self, name: str, size: int):
+        return self._controller_run("createvolume", "--name", name, "--size", str(size))
+
+    def delete_volume(self, name: str):
+        return self._controller_run("deletevolume", "--name", name)
+
+    def publish_volume(self, pod_id: str, volume_id: str, pod_name: str):
+        return self._node_run(
+            "publishvolume",
+            "--pod-id",
+            pod_id,
+            "--volume-id",
+            volume_id,
+            "--pod-name",
+            pod_name,
+        )
+
+    def unpublish_volume(self, pod_id: str, volume_id: str):
+        return self._node_run(
+            "unpublishvolume",
+            "--pod-id",
+            pod_id,
+            "--volume-id",
+            volume_id,
+        )
+
+    def stop(self):
+        self._proc.kill()
+
+
+def tear_down(env):
+    if env is None:
+        return
+    env.results_file.close()
+    env.csi_driver.stop()
+    env.tear_down()
+
+
+def test_nbs_csi_driver_mounted_disk_protected_from_deletion():
+    env, run = setup()
+    try:
+        volume_name = "example-disk"
+        volume_size = 10 * 1024 ** 3
+        pod_name = "example-pod"
+        pod_id = "deadbeef"
+        env.csi.create_volume(name=volume_name, size=volume_size)
+        env.csi.publish_volume(pod_id, volume_name, pod_name)
+        run(
+            "destroyvolume",
+            "--disk-id",
+            volume_name,
+            input=volume_name,
+            code=1,
+        )
+        env.csi.unpublish_volume(pod_id, volume_name)
+        env.csi.delete_volume(volume_name)
+    finally:
+        tear_down(env)
---
+++ cloud/blockstore/tests/csi_driver/ya.make
@@ -0,0 +1,35 @@
+PY3TEST()
+
+IF (OPENSOURCE)
+    INCLUDE(${ARCADIA_ROOT}/cloud/storage/core/tests/recipes/large.inc)
+ELSE()
+    INCLUDE(${ARCADIA_ROOT}/cloud/storage/core/tests/recipes/medium.inc)
+ENDIF()
+
+
+TEST_SRCS(
+    test.py
+)
+
+DEPENDS(
+    cloud/blockstore/tools/csi_driver/cmd/nbs-csi-driver
+    cloud/blockstore/tools/csi_driver/client
+    cloud/blockstore/apps/client
+    cloud/blockstore/apps/disk_agent
+    cloud/blockstore/apps/endpoint_proxy
+    cloud/blockstore/apps/server
+    contrib/ydb/apps/ydbd
+)
+
+PEERDIR(
+    cloud/blockstore/config
+    cloud/blockstore/tests/python/lib
+    cloud/storage/core/protos
+    contrib/ydb/core/protos
+    contrib/ydb/tests/library
+)
+SET_APPEND(QEMU_INVOKE_TEST YES)
+SET_APPEND(QEMU_VIRTIO none)
+INCLUDE(${ARCADIA_ROOT}/cloud/blockstore/tests/csi_driver/recipe/recipe.inc)
+
+END()
