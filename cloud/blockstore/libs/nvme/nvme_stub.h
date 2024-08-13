be6d830318a63d10b4b18711778ef457cfcb8037
Evgeny Budilovsky <evgenybud@nebius.com>
2024-08-13 12:44:08 +0300

    NBSNEBIUS-351: Erase devices using deallocate in 1GB chunks (#1717) (#1779)
    * NBSNEBIUS-351: Erase devices using deallocate in 1GB chunks
    
    Deallocating large device chunks (e.g., 93GB) on certain SSD models resulted in
    incomplete erasure, leaving ~20 pages uncleared. This commit mitigates the issue
    by deallocating in smaller 1GB chunks, ensuring complete device erasure.
    
    * fix compilation error on clang-14

---
+++ cloud/blockstore/libs/nvme/nvme_stub.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "nvme.h"
+
+namespace NCloud::NBlockStore::NNvme {
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TDeallocateReq
+{
+    ui64 Offset;
+    ui64 Size;
+
+    TDeallocateReq(ui64 offset, ui64 size)
+        : Offset(offset)
+        , Size(size)
+    {}
+
+    bool operator==(const TDeallocateReq& other) const
+    {
+        return Offset == other.Offset && Size == other.Size;
+    }
+};
+
+using TNvmeDeallocateHistory = TVector<TDeallocateReq>;
+using TNvmeDeallocateHistoryPtr = std::shared_ptr<TNvmeDeallocateHistory>;
+
+INvmeManagerPtr CreateNvmeManagerStub(
+    bool isDeviceSsd = true,
+    TNvmeDeallocateHistoryPtr deallocateHistory = nullptr);
+
+}   // namespace NCloud::NBlockStore::NNvme
