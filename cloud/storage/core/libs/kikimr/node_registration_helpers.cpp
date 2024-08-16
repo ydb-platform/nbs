5af8b261988657f4dff0616a62e37a9d1fb8a72f
Egor Masharskii <aym001@yandex.ru>
2024-08-16 13:59:41 +0000

    fix build

--- cloud/storage/core/libs/kikimr/node_registration_helpers.cpp
+++ cloud/storage/core/libs/kikimr/node_registration_helpers.cpp
@@ -1,7 +1,7 @@
 #include "node_registration_helpers.h"
 
-#include <contrib/ydb/core/protos/node_broker.pb.h>
-#include <contrib/ydb/public/sdk/cpp/client/ydb_discovery/discovery.h>
+#include <ydb/core/protos/node_broker.pb.h>
+#include <ydb/public/sdk/cpp/client/ydb_discovery/discovery.h>
 
 namespace NCloud::NStorage {
 
