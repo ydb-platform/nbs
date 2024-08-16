5bf06a7ffab5aaedb7c47a9463cfeea05a5e4fc3
Maxim Deb Natkh <debnatkh@gmail.com>
2024-08-16 12:23:51 +0200

    issue-1794: support aliases for filestore names (#1774)
    * issue-1794: support aliases for filestore names

--- cloud/filestore/libs/storage/tablet_proxy/tablet_proxy_ut.cpp
+++ cloud/filestore/libs/storage/tablet_proxy/tablet_proxy_ut.cpp
@@ -113,6 +113,62 @@ Y_UNIT_TEST_SUITE(TIndexTabletProxyTest)
         env.GetRuntime().DispatchEvents({}, TDuration::MilliSeconds(100));
         tabletProxy.WaitReady("test");
     }
+
+    Y_UNIT_TEST(ShouldForwardRequestsByAliases)
+    {
+        const TString originalFs = "test";
+        const TString mirroredFs = "test-mirrored";
+
+        NProto::TStorageConfig::TFilestoreAliasEntry entry;
+        entry.SetAlias(mirroredFs);
+        entry.SetFsId(originalFs);
+        NProto::TStorageConfig::TFilestoreAliases aliases;
+        aliases.MutableEntries()->Add(std::move(entry));
+        NProto::TStorageConfig storageConfig;
+        storageConfig.MutableFilestoreAliases()->Swap(&aliases);
+
+        TTestEnvConfig cfg{.StaticNodes = 1, .DynamicNodes = 2};
+        TTestEnv env(cfg, storageConfig);
+        env.CreateSubDomain("nfs");
+
+        auto& runtime = env.GetRuntime();
+        ui32 nodeIdx = env.CreateNode("nfs");
+
+        TSSProxyClient ssProxy(env.GetStorageConfig(), runtime, nodeIdx);
+        ssProxy.CreateFileStore(originalFs, 1000);
+        ssProxy.CreateFileStore(mirroredFs, 1000);
+
+        TIndexTabletProxyClient tabletProxy(env.GetRuntime(), nodeIdx);
+
+        TVector<TActorId> responseTabletIds;
+        bool responseSent = false;
+
+        env.GetRuntime().SetEventFilter(
+            [&](TTestActorRuntimeBase&, TAutoPtr<IEventHandle>& event)
+            {
+                switch (event->GetTypeRewrite()) {
+                    case TEvService::EvListNodesResponse: {
+                        if (!responseSent) {
+                            responseTabletIds.push_back(event->Sender);
+                            responseSent = true;
+                        }
+                        break;
+                    }
+                }
+
+                return false;
+            });
+
+        tabletProxy.SendListNodesRequest(originalFs, RootNodeId);
+        tabletProxy.RecvListNodesResponse();
+        responseSent = false;
+        tabletProxy.SendListNodesRequest(mirroredFs, RootNodeId);
+        tabletProxy.RecvListNodesResponse();
+
+        // Both responses should be sent by the same actor
+        UNIT_ASSERT_EQUAL(responseTabletIds.size(), 2);
+        UNIT_ASSERT_EQUAL(responseTabletIds[0], responseTabletIds[1]);
+    }
 }
 
 }   // namespace NCloud::NFileStore::NStorage
