ef663fbdcdd15872ca5adf2f94115dc0dcecde35
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2024-08-20 18:28:57 +0700

    Making an explicit unsafe conversion from TBlockDataRef to IOutputStream::TPart (#1804)
    * Making an explicit unsafe conversion from TBlockDataRef to IOutputStream::TPart
    
    * Add alignof compare
    
    * Replace TContIoVector by TSgList
    
    * Make additional overload
    
    * Remove third overload fo Serialize
    
    * Rename Serialize -> SerializeWithDataLength
    
    * Make SerializeWithData method

--- cloud/blockstore/libs/endpoints_rdma/rdma_server.cpp
+++ cloud/blockstore/libs/endpoints_rdma/rdma_server.cpp
@@ -199,18 +199,17 @@ NProto::TError TRdmaEndpoint::HandleReadBlocksRequest(
         auto response = ExtractResponse(future);
 
         TaskQueue->ExecuteSimple([=] {
-            Y_UNUSED(buffer);
-
             auto guard = guardedSgList.Acquire();
             Y_ENSURE(guard);
 
             const auto& sglist = guard.Get();
-            size_t responseBytes = Serializer->Serialize(
-                out,
-                TBlockStoreProtocol::ReadBlocksResponse,
-                0, // flags
-                response,
-                TContIOVector((IOutputStream::TPart*)sglist.begin(), sglist.size()));
+            size_t responseBytes =
+                NRdma::TProtoMessageSerializer::SerializeWithData(
+                    out,
+                    TBlockStoreProtocol::ReadBlocksResponse,
+                    0,   // flags
+                    response,
+                    sglist);
 
             Endpoint->SendResponse(context, responseBytes);
         });
@@ -247,12 +246,11 @@ NProto::TError TRdmaEndpoint::HandleWriteBlocksRequest(
         TaskQueue->ExecuteSimple([=] {
             Y_UNUSED(guardedSgList);
 
-            size_t responseBytes = Serializer->Serialize(
+            size_t responseBytes = NRdma::TProtoMessageSerializer::Serialize(
                 out,
                 TBlockStoreProtocol::WriteBlocksResponse,
-                0, // flags
-                response,
-                TContIOVector(nullptr, 0));
+                0,   // flags
+                response);
 
             Endpoint->SendResponse(context, responseBytes);
         });
@@ -279,12 +277,11 @@ NProto::TError TRdmaEndpoint::HandleZeroBlocksRequest(
     future.Subscribe([=] (auto future) {
         auto response = ExtractResponse(future);
 
-        size_t responseBytes = Serializer->Serialize(
+        size_t responseBytes = NRdma::TProtoMessageSerializer::Serialize(
             out,
             TBlockStoreProtocol::ZeroBlocksResponse,
-            0, // flags
-            response,
-            TContIOVector(nullptr, 0));
+            0,   // flags
+            response);
 
         Endpoint->SendResponse(context, responseBytes);
     });
