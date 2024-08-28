cc411df9ce8712e20ecefc4068d534891aa1ecac
Slava Sazonov <156317320+Sazonov99@users.noreply.github.com>
2024-08-28 16:52:08 +0700

    issue-1742: remove unnecessary network metrics (#1743)
    * unnecessary metrics removed
    
    * added the possibility to edit the coefficient via nfs-configs.txt

--- cloud/filestore/libs/storage/core/config.h
+++ cloud/filestore/libs/storage/core/config.h
@@ -237,12 +237,15 @@ public:
     void DumpHtml(IOutputStream& out) const;
     void DumpOverridesHtml(IOutputStream& out) const;
 
-    ui32 GetBlobCompressionRate() const;
-    TString GetBlobCompressionCodec() const;
     TString GetNodeRegistrationToken() const;
     TString GetNodeType() const;
     TString GetNodeRegistrationRootCertsFile() const;
     TCertificate GetNodeRegistrationCert() const;
+
+    ui32 GetBlobCompressionRate() const;
+    TString GetBlobCompressionCodec() const;
+
+    ui32 GetNonNetworkMetricsBalancingFactor() const;
 
     const NProto::TStorageConfig& GetStorageConfigProto() const;
 
