1081e2b9878c6d13bc10e4cdd301ae02cf133988
Mikhail Montsev <svartmetal@github.com>
2024-09-04 17:07:16 +0000

    fix includes

--- cloud/blockstore/libs/storage/service/service_actor_actions_backup_path_descriptions.cpp
+++ cloud/blockstore/libs/storage/service/service_actor_actions_backup_path_descriptions.cpp
@@ -3,10 +3,10 @@
 #include <cloud/blockstore/libs/storage/api/ss_proxy.h>
 #include <cloud/blockstore/libs/storage/core/probes.h>
 
-#include <contrib/ydb/library/actors/core/actor_bootstrapped.h>
-#include <contrib/ydb/library/actors/core/events.h>
-#include <contrib/ydb/library/actors/core/hfunc.h>
-#include <contrib/ydb/library/actors/core/log.h>
+#include <library/cpp/actors/core/actor_bootstrapped.h>
+#include <library/cpp/actors/core/events.h>
+#include <library/cpp/actors/core/hfunc.h>
+#include <library/cpp/actors/core/log.h>
 
 namespace NCloud::NBlockStore::NStorage {
 
