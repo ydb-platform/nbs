aedb0c3a66876fc6d3fa9064862125e43134e0ec
Andrei Strelkovskii <thinkingwhat@yandex.ru>
2024-09-19 19:38:23 +0300

    issue-1541: implemented a ring buffer over a file to use it in HandleOpsQueue (#2047)
    * issue-1541: ring buffer over a file
    
    * issue-1541: TFileRingBufferTest::Should{Restore,Validate} + fixes
    
    * issue-1541: randomized test for TFileRingBuffer + fixes
    
    * issue-1541: moved TFileRingBuffer implementation to .cpp
    
    * issue-1541: minor rename

--- cloud/filestore/libs/vfs_fuse/handle_ops_queue.h
+++ cloud/filestore/libs/vfs_fuse/handle_ops_queue.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cloud/filestore/libs/vfs_fuse/protos/queue_entry.pb.h>
 
 #include <util/generic/queue.h>
@@ -9,14 +11,14 @@ namespace NCloud::NFileStore::NFuse {
 class THandleOpsQueue
 {
 private:
-     TQueue<NProto::TQueueEntry> Requests;
+    TQueue<NProto::TQueueEntry> Requests;
 
 public:
-     void AddDestroyRequest(ui64 nodeId, ui64 handle);
-     const NProto::TQueueEntry& Front();
-     void Pop();
-     ui64 Size() const;
-     bool Empty() const;
+    void AddDestroyRequest(ui64 nodeId, ui64 handle);
+    const NProto::TQueueEntry& Front();
+    void Pop();
+    ui64 Size() const;
+    bool Empty() const;
 };
 
 }   // namespace NCloud::NFileStore::NFuse
