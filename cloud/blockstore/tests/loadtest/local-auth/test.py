a60824e01643e426e4e54f7983c4ad9f90ab2217
Sergei <sergei.vorobev@nebius.com>
2024-09-20 19:49:15 +0000

    Introduce test for new access service

--- cloud/blockstore/tests/loadtest/local-auth/test.py
+++ cloud/blockstore/tests/loadtest/local-auth/test.py
@@ -1,18 +1,24 @@
+import logging
+import subprocess
+
 import yatest.common as common
+
+from pathlib import Path
+
+from google.protobuf.text_format import MessageToString
 
 from cloud.blockstore.config.client_pb2 import TClientConfig
 from cloud.blockstore.config.server_pb2 import TServerAppConfig, TServerConfig, TKikimrServiceConfig
 from cloud.blockstore.config.storage_pb2 import TStorageServiceConfig
-from cloud.blockstore.tests.python.lib.loadtest_env import LocalLoadTest
+from cloud.blockstore.tests.python.lib.loadtest_env import LocalLoadTest, logger
 from cloud.blockstore.tests.python.lib.test_base import thread_count, run_test
 from cloud.storage.core.protos.authorization_mode_pb2 import EAuthorizationMode
+from cloud.storage.core.tools.testing.access_service.lib import AccessService
+from cloud.storage.core.tools.testing.access_service_nebius.lib import NewAccessService
 
-
-def test_load():
+def create_server_app_config():
     server = TServerAppConfig()
     server.ServerConfig.CopyFrom(TServerConfig())
-    server.ServerConfig.ThreadsCount = thread_count()
-    server.ServerConfig.StrictContractValidation = False
     server.ServerConfig.RootCertsFile = common.source_path(
         "cloud/blockstore/tests/certs/server.crt")
     cert = server.ServerConfig.Certs.add()
@@ -21,10 +27,22 @@ def test_load():
     cert.CertPrivateKeyFile = common.source_path(
         "cloud/blockstore/tests/certs/server.key")
     server.KikimrServiceConfig.CopyFrom(TKikimrServiceConfig())
+    return server
 
+
+def create_storage_service_config(folder_id="test_folder_id"):
     storage = TStorageServiceConfig()
     storage.AuthorizationMode = EAuthorizationMode.Value("AUTHORIZATION_REQUIRE")
-    storage.FolderId = "test_folder_id"
+    storage.FolderId = folder_id
+    return storage
+
+
+def test_load():
+    server = create_server_app_config()
+    server.ServerConfig.StrictContractValidation = False
+    server.ServerConfig.ThreadsCount = thread_count()
+
+    storage = create_storage_service_config("test_folder_id")
 
     env = LocalLoadTest(
         "",
@@ -38,9 +56,7 @@ def test_load():
     env.access_service.authenticate("test_auth_token")
     env.access_service.authorize("test_auth_token")
 
-    client = TClientConfig()
-    client.RootCertsFile = common.source_path(
-        "cloud/blockstore/tests/certs/server.crt")
+    client = create_client_config()
     client.AuthToken = "test_auth_token"
 
     try:
@@ -58,3 +74,146 @@ def test_load():
         env.tear_down()
 
     return ret
+
+
+def create_client_config():
+    client = TClientConfig()
+    client.RootCertsFile = common.source_path(
+        "cloud/blockstore/tests/certs/server.crt")
+    return client
+
+class _TestFixture:
+    _binary_path = common.binary_path("cloud/blockstore/apps/client/blockstore-client")
+
+    def __init__(self, access_service_type=AccessService, folder_id="test_folder_id"):
+        server = create_server_app_config()
+        storage = create_storage_service_config(folder_id)
+        self._env = LocalLoadTest(
+            "",
+            server_app_config=server,
+            enable_access_service=True,
+            storage_config_patches=[storage],
+            enable_tls=True,
+            use_in_memory_pdisks=True,
+            access_service_type=access_service_type,
+        )
+        self._client_config_path = Path(common.output_path()) / "client-config.txt"
+        self._client_config = create_client_config()
+        self._client_config.ClientConfig.RetryTimeout = 1
+        self._flush_config()
+        self.folder_id = folder_id
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._env.tear_down()
+
+    def run(self, *args, **kwargs):
+        args = [self._binary_path, *args, "--config", str(self._client_config_path)]
+        script_input = kwargs.get("input")
+        if script_input is not None:
+            script_input = script_input + "\n"
+
+        logging.info("running command: %s" % args)
+        result = subprocess.run(
+            args,
+            cwd=kwargs.get("cwd"),
+            check=False,
+            capture_output=True,
+            input=script_input,
+            text=True,
+        )
+        return result
+
+    def create_volume(self):
+        result = self.run("createvolume", "--disk-id", "vol0", "--blocks-count", "25000")
+        logging.info("Disk creation stdout: %s, stderr: %s", result.stdout, result.stderr)
+        return result
+    @property
+    def access_service(self):
+        return self._env.access_service
+
+    def set_auth_token(self, token: str):
+        self._client_config.AuthToken = token
+        self._flush_config()
+
+    def _flush_config(self):
+        self._client_config_path.write_text(MessageToString(self._client_config))
+
+
+def test_auth_unauthorized():
+    with _TestFixture() as env:
+        token = "test_auth_token"
+        env.set_auth_token(token)
+        env.access_service.authenticate(token)
+        result = env.create_volume()
+        assert result.returncode != 0
+
+
+def test_auth_unauthenticated():
+    with _TestFixture() as env:
+        token = "test_auth_token"
+        env.set_auth_token(token)
+        env.access_service.authorize(token)
+        result = env.create_volume()
+        assert result.returncode != 0
+
+def test_auth_empty_token():
+    with _TestFixture() as env:
+        env.set_auth_token("")
+        env.access_service.authorize("test_auth_token")
+        result = env.create_volume()
+        assert result.returncode != 0
+
+def test_new_auth_authorization_ok():
+    with _TestFixture(NewAccessService) as env:
+        token = "test_auth_token"
+        env.set_auth_token(token)
+        env.access_service.create_account(
+            "test_user",
+            token,
+            is_unknown_subject=False,
+            permissions=[
+                {"permission": "nbsInternal.disks.create", "resource": env.folder_id},
+            ],
+        )
+        result = env.create_volume()
+        assert result.returncode == 0
+
+def test_new_auth_unauthorized():
+    with _TestFixture(NewAccessService) as env:
+        with _TestFixture(NewAccessService) as env:
+            token = "test_auth_token"
+        env.set_auth_token(token)
+        env.access_service.create_account(
+            "test_user",
+            token,
+            is_unknown_subject=False,
+            permissions=[
+                {"permission": "nbsInternal.disks.create", "resource": "some_other_folder"},
+            ],
+        )
+        result = env.create_volume()
+        assert result.returncode != 0
+
+def test_new_auth_unauthenticated():
+    with _TestFixture(NewAccessService) as env:
+        env.set_auth_token("some_other_token")
+        result = env.create_volume()
+        assert result.returncode != 0
+
+def test_new_auth_unknown_subject():
+    with _TestFixture(NewAccessService) as env:
+        token = "test_token"
+        env.set_auth_token(token)
+        env.access_service.create_account(
+            "test_user",
+            token,
+            is_unknown_subject=True,
+            permissions=[
+                {"permission": "nbsInternal.disks.create", "resource": env.folder_id},
+            ],
+        )
+        result = env.create_volume()
+        assert result.returncode != 0
