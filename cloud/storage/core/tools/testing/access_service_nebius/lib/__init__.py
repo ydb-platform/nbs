558fc4b1f79dff044992879e12d57e9102fb1a8d
Sergei <sergei.vorobev@nebius.com>
2024-09-20 16:35:54 +0000

    Add create_account method for AccessServiceLauncher

--- cloud/storage/core/tools/testing/access_service_nebius/lib/__init__.py
+++ cloud/storage/core/tools/testing/access_service_nebius/lib/__init__.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+import requests
+
 from cloud.storage.core.tools.common.python.daemon import Daemon
 
 from cloud.tasks.test.common.processes import register_process, kill_processes
@@ -35,15 +37,13 @@ class AccessServiceLauncher:
             cert_file: str,
             cert_key_file: str,
         ):
-
-
         config = {
             "port": port,
             "control_port": control_port,
             "cert_file": cert_file,
             "cert_key_file": cert_key_file,
         }
-
+        self._control_port = control_port
         config_file = os.path.join(working_dir, 'access_service_config.txt')
         with open(config_file, 'w') as file:
             json.dump(config, file)
@@ -56,6 +56,23 @@ class AccessServiceLauncher:
     def start(self):
         self.__daemon.start()
         register_process(SERVICE_NAME, self.__daemon.pid)
+
+    def create_account(
+        self,
+        user_id: str,
+        token: str,
+        is_unknown_subject: bool,
+        permissions: list
+    ):
+        requests.post(
+            f"localhost:{self._control_port}/",
+            json={
+                "is_unknown_subject": is_unknown_subject,
+                "token": token,
+                "id": user_id,
+                "permissions": permissions,
+            }
+        ).raise_for_status()
 
     @staticmethod
     def stop():
