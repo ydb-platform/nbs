940f7b18068641843c3876f0d4a94d81a060b516
proller <proller@github.com>
2024-10-03 18:47:19 +0000

    clean

--- cloud/filestore/tools/testing/loadtest/test/load1.sh
+++
@@ -1,3 +0,0 @@
-
-ffmpeg -f lavfi -i testsrc=duration=1:size=1080x1080:rate=30 -c:v rawvideo  testsrc.mpg
-ffmpeg -f rawvideo -pix_fmt rgb24 -video_size 1080x1080  -i testsrc.mpg -r 30  -c:v rawvideo -pix_fmt rgb24 -f caca -
--- cloud/filestore/tools/testing/loadtest/test/load3.sh
+++
@@ -1,7 +0,0 @@
-
-# strace --follow-forks -- git clone --depth 1 --recursive https://github.com/google/leveldb 2>&1 | tee strace.log
-git clone --depth 1 --recursive https://github.com/google/leveldb
-cd leveldb
-cmake -G Ninja .
-ninja
-ctest
