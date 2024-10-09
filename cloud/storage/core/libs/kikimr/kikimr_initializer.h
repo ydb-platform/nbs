11f31f62bf6f7918c4defb2d276b4ba1c70dfcb5
Egor Masharskii <aym001@yandex.ru>
2024-10-09 16:57:41 +0000

    issue-2250: start ydb grpc proxy to enable web monitoring authorization

---
+++ cloud/storage/core/libs/kikimr/kikimr_initializer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "public.h"
+
+#include <contrib/ydb/core/driver_lib/run/kikimr_services_initializers.h>
+
+namespace NCloud::NStorage {
+
+////////////////////////////////////////////////////////////////////////////////
+
+using namespace NKikimr;
+
+using namespace NCloud::NStorage;
+
+class TKikimrServicesInitializer final
+    : public IServiceInitializer
+{
+private:
+    const NKikimrConfig::TAppConfigPtr Config;
+
+public:
+    explicit TKikimrServicesInitializer(NKikimrConfig::TAppConfigPtr config)
+        : Config{std::move(config)}
+    {}
+
+    void InitializeServices(
+        TActorSystemSetup* setup,
+        const TAppData* appData) override;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+}   // namespace NCloud::NStorage
