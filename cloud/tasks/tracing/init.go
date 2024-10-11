58a1b5da98a6d4a44339bcf2a1b8faf45f8797bf
gayurgin <g.y.98@yandex.ru>
2024-10-11 20:08:07 +0300

    [Disk Manager] do not print stacktrace in error in traces, do not print retriable errors above non-retriable errors in logs and traces

--- cloud/tasks/tracing/init.go
+++ cloud/tasks/tracing/init.go
@@ -53,7 +53,7 @@ func SpanFromContext(ctx context.Context) trace.Span {
 
 func SetError(span trace.Span, err *error) {
 	if *err != nil {
-		span.SetStatus(codes.Error, (*err).Error())
+		span.SetStatus(codes.Error, errors.GetShortError(*err))
 	}
 }
 
