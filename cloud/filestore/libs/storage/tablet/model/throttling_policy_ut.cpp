ad8ab52828b91f976f0c2357d1aa051097850977
proller <proller@github.com>
2024-10-23 17:12:10 +0000

    Add test

--- cloud/filestore/libs/storage/tablet/model/throttling_policy_ut.cpp
+++ cloud/filestore/libs/storage/tablet/model/throttling_policy_ut.cpp
@@ -374,6 +374,45 @@ Y_UNIT_TEST_SUITE(TIndexTabletThrottlingPolicyTest)
             {1_MB, static_cast<ui32>(EOpType::Read), 1}
         ).Defined());
     }
+
+    Y_UNIT_TEST(MoreThan4g)
+    {
+       // From SeparateReadAndWriteLimits 
+        constexpr ui64 X = 10000;
+        const auto config = MakeConfig(
+            1_MB * X,           // maxReadBandwidth
+            2_MB * X,           // maxWriteBandwidth
+            10 * X,             // maxReadIops
+            50 * X,             // maxWriteIops
+            100,                // burstPercentage
+            0,                  // boostTime
+            0,                  // boostRefillTime
+            0,                  // boostPercentage
+            10_MB * X,          // maxPostponedWeight
+            TDuration::Max(),   // maxPostponedTime
+            Max<ui32>(),        // maxWriteCostMultiplier
+            1                   // defaultPostponedRequestWeight
+        );
+        TThrottlingPolicy tp(config);
+
+        for (ui32 i = 0; i < 9; ++i) {
+            DO_TEST(tp, 0, 10'000, 4_KB*X, static_cast<ui32>(EOpType::Read));
+        }
+        DO_TEST(tp, 0, 10'000, 4_KB*X, static_cast<ui32>(EOpType::Read));
+        tp.OnPostponedEvent({}, {4_KB*X, static_cast<ui32>(EOpType::Read), 1});
+        DO_TEST(tp, 0, 48'131, 4_KB*X, static_cast<ui32>(EOpType::Read));
+
+        for (ui32 i = 0; i < 49; ++i) {
+            DO_TEST(tp, 0, 10'000'000, 4_KB*X, static_cast<ui32>(EOpType::Write));
+        }
+        DO_TEST(tp, 0, 10'000'000, 4_KB*X, static_cast<ui32>(EOpType::Write));
+        tp.OnPostponedEvent({}, {4_KB*X, static_cast<ui32>(EOpType::Write), 1});
+        DO_TEST(tp, 0, 10'005'900, 4_KB*X, static_cast<ui32>(EOpType::Write));
+
+        DO_TEST(tp,68901, 10'005'900, 1_MB*X, static_cast<ui32>(EOpType::Read));
+        
+        DO_TEST(tp, 0, 10'005'900, 1_MB*X, static_cast<ui32>(EOpType::Write));
+    }
 }
 
 }   // namespace NCloud::NFileStore::NStorage
