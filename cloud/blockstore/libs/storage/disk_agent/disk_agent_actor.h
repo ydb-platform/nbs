e81dad12ac86f87c5a023c7767a6001fd0b35a3f
Taras Pashkin <32797206+tpashkin@users.noreply.github.com>
2024-10-23 10:19:05 +0300

    configure rdma target thread pool size; issue-2267
    Also clean up TRdmaTarget interface

--- cloud/blockstore/libs/storage/disk_agent/disk_agent_actor.h
+++ cloud/blockstore/libs/storage/disk_agent/disk_agent_actor.h
@@ -10,6 +10,7 @@
 #include <cloud/blockstore/libs/kikimr/helpers.h>
 #include <cloud/blockstore/libs/nvme/public.h>
 #include <cloud/blockstore/libs/spdk/iface/env.h>
+#include <cloud/blockstore/libs/rdma/iface/config.h>
 #include <cloud/blockstore/libs/storage/api/disk_agent.h>
 #include <cloud/blockstore/libs/storage/api/disk_registry.h>
 #include <cloud/blockstore/libs/storage/api/disk_registry_proxy.h>
