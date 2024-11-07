8333d22b7c5f5f215d9699f4c297cec180809eaa
Taras Pashkin <tpashkin@nebius.com>
2024-11-07 20:18:44 +0200

    add block validation mode to eternal_tests/range-validator

--- cloud/blockstore/tools/testing/eternal_tests/range-validator/main.cpp
+++ cloud/blockstore/tools/testing/eternal_tests/range-validator/main.cpp
@@ -10,13 +10,30 @@ using namespace NCloud::NBlockStore;
 
 ////////////////////////////////////////////////////////////////////////////////
 
+constexpr ui64 MAX_MIRROR_REPLICAS = 3;
+constexpr ui64 PAGE_SIZE = 4096;
+
+////////////////////////////////////////////////////////////////////////////////
+
+enum class EValidationMode {
+    Checksum,
+    Mirror,
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 struct TOptions
 {
+
     TString ConfigPath;
     TString DevicePath;
     TString RangesStr;
+    TString BlocksStr;
     TString LogPath;
     TVector<ui32> Ranges;
+    TVector<ui64> Blocks;
+    EValidationMode Mode = EValidationMode::Checksum;
+    ui64 BlockSize = 4096;
 
     void Parse(int argc, const char** argv)
     {
@@ -26,7 +43,6 @@ struct TOptions
         opts.AddHelpOption();
 
         opts.AddLongOption("config-path", "path to the eternal-test config")
-            .Required()
             .RequiredArgument("STR")
             .StoreResult(&ConfigPath);
 
@@ -38,14 +54,12 @@ struct TOptions
         opts.AddLongOption(
             "log-path",
             "path to the folder, where you want to save logs of validation")
-            .Required()
             .RequiredArgument("STR")
             .StoreResult(&LogPath);
 
         opts.AddLongOption(
             "ranges",
             "specify range indexes for validation, separated by comma")
-            .Required()
             .RequiredArgument("STR")
             .StoreResult(&RangesStr)
             .Handler0([this] {
@@ -56,21 +70,64 @@ struct TOptions
                 }
             });
 
+        opts.AddLongOption(
+            "blocks",
+            "specify individual blocks NUM or ranges NUM-NUM for validation, separated by comma")
+            .RequiredArgument("STR")
+            .StoreResult(&BlocksStr)
+            .Handler0([this] {
+                TStringBuf buf(BlocksStr);
+                TStringBuf range;
+                while (buf.NextTok(',', range)) {
+                    TStringBuf buf2(range);
+                    TStringBuf firstStr;
+                    TStringBuf lastStr;
+                    buf2.NextTok('-', firstStr);
+                    buf2.NextTok('-', lastStr);
+                    ui64 first = FromString<ui64>(firstStr);
+                    ui64 last = lastStr.empty() ? first : FromString<ui64>(lastStr);
+                    for (ui64 block = first; block <= last; block++) {
+                        Blocks.push_back(block);
+                    }
+                }
+            });
+
+        opts.AddLongOption(
+            "block-size",
+            "specify block-size (default 4096)")
+            .RequiredArgument("NUM")
+            .StoreResult(&BlockSize);
+
+        opts.AddLongOption(
+            "mode",
+            "specify checksum (default) or mirror validation mode")
+            .Handler1T<TString>([this] (const auto& s) {
+                if (s == "checksum") {
+                    Mode = EValidationMode::Checksum;
+                } else if (s == "mirror") {
+                    Mode = EValidationMode::Mirror;
+                } else {
+                    Y_ABORT("validation mode must be either checksum or mirror");
+                }
+            });
+
         TOptsParseResultException(&opts, argc, argv);
+
+        if (Mode == EValidationMode::Checksum) {
+            Y_ENSURE(ConfigPath.size(), "you must specify config-path in checksum validation mode");
+            Y_ENSURE(Ranges.size(), "you must specify ranges in checksum validation mode");
+            Y_ENSURE(LogPath.size(), "you must specify log-path in checksum validation mode");
+        }
+
+        if (Mode == EValidationMode::Mirror) {
+            Y_ENSURE(Blocks.size(), "you must specify blocks in mirror validation mode");
+        }
     }
 };
 
-int main(int argc, const char** argv)
+void ValidateRanges(TOptions options)
 {
-    TOptions options;
-    try {
-        options.Parse(argc, argv);
-    } catch (...) {
-        Cout << CurrentExceptionMessage() << Endl;
-        return 1;
-    }
-
-    TFile File(options.DevicePath, EOpenModeFlag::RdWr);
+    TFile File(options.DevicePath, EOpenModeFlag::RdOnly);
 
     const auto& configHolder = CreateTestConfig(options.ConfigPath);
     for (ui32 rangeIdx: options.Ranges) {
@@ -119,6 +176,61 @@ int main(int argc, const char** argv)
 
         Cout << "Finish validation of " << rangeIdx << " range" << Endl;
         Cout << "Log written to " << path.Quote() << Endl;
+    }
+}
+
+void ValidateBlocks(TOptions options)
+{
+    TFile file(options.DevicePath, EOpenModeFlag::RdOnly | EOpenModeFlag::DirectAligned);
+
+    auto compare = [](auto& x, auto& y) {
+        return memcmp(&x, &y, sizeof(TBlockData));
+    };
+
+    for (ui64 blockIndex: options.Blocks) {
+        std::set<TBlockData, decltype(compare)> uniqueBlocks(compare);
+
+        for (ui64 i = 0; i < MAX_MIRROR_REPLICAS; i++) {
+            alignas(PAGE_SIZE) TBlockData buf;
+            file.Seek(blockIndex * options.BlockSize, sSet);
+            file.Read(&buf, options.BlockSize);
+            uniqueBlocks.emplace(std::move(buf));
+        }
+
+        if (uniqueBlocks.size() == 1) {
+            continue;
+        }
+
+        for (const auto& block: uniqueBlocks) {
+            Cout
+                << "mismatch in block data {"
+                << " " << block.RequestNumber
+                << " " << block.PartNumber
+                << " " << block.BlockIndex
+                << " " << block.RangeIdx
+                << " " << TInstant::MicroSeconds(block.RequestTimestamp)
+                << " " << TInstant::MicroSeconds(block.TestTimestamp)
+                << " " << block.TestId
+                << " " << block.Checksum
+                << " }\n";
+        }
+    }
+}
+
+int main(int argc, const char** argv)
+{
+    TOptions options;
+    try {
+        options.Parse(argc, argv);
+    } catch (...) {
+        Cout << CurrentExceptionMessage() << Endl;
+        return 1;
+    }
+
+    if (options.Mode == EValidationMode::Checksum) {
+        ValidateRanges(std::move(options));
+    } else {
+        ValidateBlocks(std::move(options));
     }
 
     return 0;
