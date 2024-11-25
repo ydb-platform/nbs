9fe6cf12e3ab57a49ba6b20fd3c93b3a5c9cd7b5
proller <proller@github.com>
2024-11-25 23:36:43 +0000

    Try speedup qemu-kikimr-multishard-nemesis-test under sanitizer

--- cloud/filestore/tests/fio_index/qemu-kikimr-multishard-nemesis-test/test.py
+++ cloud/filestore/tests/fio_index/qemu-kikimr-multishard-nemesis-test/test.py
@@ -1,12 +1,15 @@
 import pytest
+import os
 
 import cloud.storage.core.tools.testing.fio.lib as fio
 
 from cloud.filestore.tests.python.lib.common import get_filestore_mount_path
 
+duration=30
+if os.environ['SANITIZER_TYPE'] != '' :
+    duration=5
 
-TESTS = fio.generate_index_tests()
-
+TESTS = fio.generate_index_tests(duration=duration)
 
 @pytest.mark.parametrize("name", TESTS.keys())
 def test_fio(name):
