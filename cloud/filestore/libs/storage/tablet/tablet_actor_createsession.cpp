3f335a8812dd778e1f1fa907bd1e9e05e6f6a5ee
Andrei Strelkovskii <thinkingwhat@yandex.ru>
2024-12-02 21:06:49 +0100

    issue-1932: disabling tablet-side shard count check by default, can be reenabled via EnforceCorrectFileSystemShardCountUponSessionCreation StorageServiceConfig flag (#2601)

--- cloud/filestore/libs/storage/tablet/tablet_actor_createsession.cpp
+++ cloud/filestore/libs/storage/tablet/tablet_actor_createsession.cpp
@@ -148,16 +148,25 @@ void TIndexTabletActor::HandleCreateSession(
     if (actualShardCount < expectedShardCount) {
         auto message = TStringBuilder() << "Shard count smaller than expected: "
             << actualShardCount << " < " << expectedShardCount;
+        const bool shouldReject =
+            Config->GetEnforceCorrectFileSystemShardCountUponSessionCreation();
+        if (shouldReject) {
+            LOG_INFO(ctx, TFileStoreComponents::TABLET,
+                "%s CreateSession rejected: %s",
+                LogTag.c_str(),
+                message.c_str());
+
+            using TResponse = TEvIndexTablet::TEvCreateSessionResponse;
+            auto response = std::make_unique<TResponse>(
+                MakeError(E_REJECTED, std::move(message)));
+            NCloud::Reply(ctx, *requestInfo, std::move(response));
+            return;
+        }
+
         LOG_INFO(ctx, TFileStoreComponents::TABLET,
-            "%s CreateSession rejected: %s",
+            "%s CreateSession: %s",
             LogTag.c_str(),
             message.c_str());
-
-        using TResponse = TEvIndexTablet::TEvCreateSessionResponse;
-        auto response = std::make_unique<TResponse>(
-            MakeError(E_REJECTED, std::move(message)));
-        NCloud::Reply(ctx, *requestInfo, std::move(response));
-        return;
     }
 
     AddTransaction<TEvIndexTablet::TCreateSessionMethod>(*requestInfo);
