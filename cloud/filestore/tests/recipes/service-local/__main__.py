9cbaa1fd02608b7877f68126d9c4864b4c40e7f1
Mikhail Montsev <svartmetal@github.com>
2024-12-04 19:37:46 +0000

    Output 'ss -tpna' when stopping filestore recipes

--- cloud/filestore/tests/recipes/service-local/__main__.py
+++ cloud/filestore/tests/recipes/service-local/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 
 import yatest.common as common
@@ -56,6 +57,8 @@ def start(argv):
 
 
 def stop(argv):
+    logging.info(os.system("ss -tpna"))
+
     if not os.path.exists(PID_FILE_NAME):
         return
 
