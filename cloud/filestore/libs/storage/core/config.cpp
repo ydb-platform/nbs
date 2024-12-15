08297b290c58add79793671bff4551bacb5f868e
Maxim Deb Natkh <debnatkh@yandex.ru>
2024-12-15 20:51:10 +0000

    try to set non-zero NodeIndexCacheMaxNodes

--- cloud/filestore/libs/storage/core/config.cpp
+++ cloud/filestore/libs/storage/core/config.cpp
@@ -177,7 +177,7 @@ using TAliases = NProto::TStorageConfig::TFilestoreAliases;
     xxx(ReadAheadCacheRangeSize,                ui32,       0                 )\
     xxx(ReadAheadMaxGapPercentage,              ui32,       20                )\
     xxx(ReadAheadCacheMaxHandlesPerNode,        ui32,      128                )\
-    xxx(NodeIndexCacheMaxNodes,                 ui32,        0                )\
+    xxx(NodeIndexCacheMaxNodes,                 ui32,        32               )\
     xxx(EntryTimeout,                    TDuration, TDuration::Zero()         )\
     xxx(NegativeEntryTimeout,            TDuration, TDuration::Zero()         )\
     xxx(AttrTimeout,                     TDuration, TDuration::Zero()         )\
