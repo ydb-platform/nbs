527dc893b29c7690d330296364a6979033111568
Mikhail Montsev <svartmetal@nebius.com>
2024-12-24 00:03:38 +0000

    Backport https://github.com/ydb-platform/ydb/pull/12693 (#2759)

--- contrib/ydb/core/tx/schemeshard/user_attributes.h
+++ contrib/ydb/core/tx/schemeshard/user_attributes.h
@@ -15,6 +15,7 @@ constexpr TStringBuf ATTR_VOLUME_SPACE_LIMIT_SSD_NONREPL = "__volume_space_limit
 constexpr TStringBuf ATTR_VOLUME_SPACE_LIMIT_SSD_SYSTEM = "__volume_space_limit_ssd_system";
 constexpr TStringBuf ATTR_FILESTORE_SPACE_LIMIT_SSD = "__filestore_space_limit_ssd";
 constexpr TStringBuf ATTR_FILESTORE_SPACE_LIMIT_HDD = "__filestore_space_limit_hdd";
+constexpr TStringBuf ATTR_FILESTORE_SPACE_LIMIT_SSD_SYSTEM = "__filestore_space_limit_ssd_system";
 constexpr TStringBuf ATTR_EXTRA_PATH_SYMBOLS_ALLOWED = "__extra_path_symbols_allowed";
 constexpr TStringBuf ATTR_DOCUMENT_API_VERSION = "__document_api_version";
 constexpr TStringBuf ATTR_ASYNC_REPLICATION = "__async_replication";
@@ -32,6 +33,7 @@ enum class EAttribute {
     ASYNC_REPLICATION,
     FILESTORE_SPACE_LIMIT_SSD,
     FILESTORE_SPACE_LIMIT_HDD,
+    FILESTORE_SPACE_LIMIT_SSD_SYSTEM,
 };
 
 enum class EUserAttributesOp {
