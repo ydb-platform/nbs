06e867a6a7d6a941453e4790fe3b3e96f7a295b7
Maxim Deb Natkh <debnatkh@yandex.ru>
2025-01-12 22:22:10 +0000

    issue-2831: dump iolog for fio_index mount_kikimr tests

--- cloud/filestore/tests/fio_index/mount-kikimr-test/test.py
+++ cloud/filestore/tests/fio_index/mount-kikimr-test/test.py
@@ -13,4 +13,5 @@ def test_fio(name):
     mount_dir = get_filestore_mount_path()
     dir_name = fio.get_dir_name(mount_dir, name)
 
-    fio.run_index_test(dir_name, TESTS[name], fail_on_errors=True)
+    # TODO(#2831): remove this debug information
+    fio.run_index_test(dir_name, TESTS[name], fail_on_errors=True, verbose=True)
