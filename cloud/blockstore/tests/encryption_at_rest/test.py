c6e67971ef91b7558413c54d9e8988506b7c29dd
Pavel Misko <misko.pawel@gmail.com>
2025-01-14 19:45:38 +0100

    [NBS] ability to override the NBS's binary file path; tweak encryption_at_rest

--- cloud/blockstore/tests/encryption_at_rest/test.py
+++ cloud/blockstore/tests/encryption_at_rest/test.py
@@ -177,7 +177,7 @@ def test_create_volume_with_default_ecnryption(nbs, disk_agent):
     assert vol0.EncryptionDesc.Mode == ENCRYPTION_DEFAULT_AES_XTS
 
     assert vol0.EncryptionDesc.EncryptionKey.KekId == KEK_ID
-    assert len(vol0.EncryptionDesc.EncryptionKey.EncryptedDEK) == 512
+    assert len(vol0.EncryptionDesc.EncryptionKey.EncryptedDEK) >= 32
 
     expected_data = os.urandom(4096)
 
