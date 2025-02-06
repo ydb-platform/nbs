9c3f69c6dcd98afa5750801b3200a6be75b75dea
Maxim Deb Natkh <debnatkh@nebius.com>
2025-02-06 14:34:14 +0100

    issue-1932: set performance profile of the main tablet based on the filesystem size (#2986)

--- cloud/filestore/libs/storage/core/model_ut.cpp
+++ cloud/filestore/libs/storage/core/model_ut.cpp
@@ -2298,6 +2298,10 @@ Y_UNIT_TEST_SUITE(TModel)
         StorageConfig.SetAutomaticShardCreationEnabled(true);
         StorageConfig.SetShardAllocationUnit(4_TB);
         StorageConfig.SetAutomaticallyCreatedShardSize(5_TB);
+        StorageConfig.SetSSDMaxWriteIops(Max<ui32>());
+
+        auto OldMaxWriteIops = ClientPerformanceProfile.GetMaxWriteIops();
+        ClientPerformanceProfile.ClearMaxWriteIops();
 
         KikimrConfig.SetBlocksCount(4_TB / 4_KB);
         auto fs = SetupMultiShardFileStorePerformanceAndChannels(
@@ -2306,6 +2310,13 @@ Y_UNIT_TEST_SUITE(TModel)
             ClientPerformanceProfile,
             0);
         UNIT_ASSERT_VALUES_EQUAL(1, fs.ShardConfigs.size());
+        // MaxWriteIops per allocation unit is 1000, so (4096 GiB / 32 GiB) *
+        // 1000 = 128000
+        UNIT_ASSERT_VALUES_EQUAL(
+            128000,
+            fs.MainFileSystemConfig.GetPerformanceProfileMaxWriteIops());
+
+        ClientPerformanceProfile.SetMaxWriteIops(OldMaxWriteIops);
 
         KikimrConfig.SetBlocksCount(4_TB / 4_KB + 1);
         fs = SetupMultiShardFileStorePerformanceAndChannels(
