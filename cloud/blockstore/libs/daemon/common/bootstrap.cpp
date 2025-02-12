d1d13c94587082ac8b3080f364acbe295be58e5a
Anton Myagkov <antomyagkov@gmail.com>
2025-02-12 19:06:04 +0100

    trigger restore endpoints after starting EndpointManager

--- cloud/blockstore/libs/daemon/common/bootstrap.cpp
+++ cloud/blockstore/libs/daemon/common/bootstrap.cpp
@@ -881,16 +881,16 @@ void TBootstrapBase::Start()
     START_KIKIMR_COMPONENT(ActorSystem);
     START_COMMON_COMPONENT(EndpointProxyClient);
     START_COMMON_COMPONENT(EndpointManager);
+
+    // Start to restore endpoints before starting server and nbd server to avoid
+    // the race between stop endpoint calls from clients and restore endpoint
+    auto restoreFuture = EndpointManager->RestoreEndpoints();
+
     START_COMMON_COMPONENT(Service);
     START_COMMON_COMPONENT(VhostServer);
     START_COMMON_COMPONENT(NbdServer);
     START_COMMON_COMPONENT(GrpcEndpointListener);
     START_COMMON_COMPONENT(Executor);
-
-    // Start to restore endpoints before starting server to avoid the race between
-    // stop endpoint and restore endpoint
-    auto restoreFuture = EndpointManager->RestoreEndpoints();
-
     START_COMMON_COMPONENT(Server);
     START_COMMON_COMPONENT(ServerStatsUpdater);
     START_COMMON_COMPONENT(BackgroundThreadPool);
