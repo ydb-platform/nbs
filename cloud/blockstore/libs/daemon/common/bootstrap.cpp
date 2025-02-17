7ebccd6a130d22f0e34f5dfbcc3e7fd5b47a114c
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-02-17 11:35:06 +0000

    issue-2947: fix build error

--- cloud/blockstore/libs/daemon/common/bootstrap.cpp
+++ cloud/blockstore/libs/daemon/common/bootstrap.cpp
@@ -900,7 +900,7 @@ void TBootstrapBase::Start()
     START_COMMON_COMPONENT(Scheduler);
 
     if (!Configs->Options->TemporaryServer) {
-        WarmupBSGroupsConnections();
+        WarmupBSGroupConnections();
     }
 
     auto restoreFuture = EndpointManager->RestoreEndpoints();
