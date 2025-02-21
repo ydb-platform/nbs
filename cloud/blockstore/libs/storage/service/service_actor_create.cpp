f14b29365bf9f612ae902c4f9a7bd192ee8bf322
Pavel Misko <misko.pawel@gmail.com>
2025-02-21 14:50:10 +0100

    issue-3071: return error on attempt to create an encrypted overlay NRD (#3073)

--- cloud/blockstore/libs/storage/service/service_actor_create.cpp
+++ cloud/blockstore/libs/storage/service/service_actor_create.cpp
@@ -159,6 +159,15 @@ bool TCreateVolumeActor::ShouldCreateVolumeWithEncryptionAtRest() const
 void TCreateVolumeActor::CreateVolume(const TActorContext& ctx)
 {
     if (ShouldCreateVolumeWithEncryptionAtRest()) {
+        if (!Request.GetBaseDiskId().empty()) {
+            ReplyAndDie(
+                ctx,
+                std::make_unique<TEvService::TEvCreateVolumeResponse>(MakeError(
+                    E_NOT_IMPLEMENTED,
+                    "Encrypted overlay disks are not supported")));
+            return;
+        }
+
         LOG_INFO_S(
             ctx,
             TBlockStoreComponents::SERVICE,
