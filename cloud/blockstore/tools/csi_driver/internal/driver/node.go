4c5bb5ccc1b64bb668439a1790e450b7bbbe18a2
Anton Myagkov <antomyagkov@gmail.com>
2025-03-11 17:09:47 +0100

    issue-2270: remove device name checks (#2974)

--- cloud/blockstore/tools/csi_driver/internal/driver/node.go
+++ cloud/blockstore/tools/csi_driver/internal/driver/node.go
@@ -707,10 +707,6 @@ func (s *nodeService) nodeStageDiskAsFilesystem(
 		return fmt.Errorf("failed to start NBS endpoint: %w", err)
 	}
 
-	if resp.NbdDeviceFile == "" {
-		return fmt.Errorf("NbdDeviceFile shouldn't be empty")
-	}
-
 	logVolume(req.VolumeId, "endpoint started with device: %q", resp.NbdDeviceFile)
 
 	// startNbsEndpointForNBD is async function. Kubelet will retry
@@ -771,10 +767,6 @@ func (s *nodeService) nodeStageDiskAsBlockDevice(
 	resp, err := s.startNbsEndpointForNBD(ctx, "", diskId, req.VolumeContext)
 	if err != nil {
 		return fmt.Errorf("failed to start NBS endpoint: %w", err)
-	}
-
-	if resp.NbdDeviceFile == "" {
-		return fmt.Errorf("NbdDeviceFile shouldn't be empty")
 	}
 
 	logVolume(req.VolumeId, "endpoint started with device: %q", resp.NbdDeviceFile)
