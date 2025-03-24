522c4a46e2cca74c61f7131e9d402f203cc1e00f
proller <proller@github.com>
2025-03-24 15:53:16 +0100

    simpler

--- cloud/blockstore/libs/common/page_size.cpp
+++ cloud/blockstore/libs/common/page_size.cpp
@@ -2,26 +2,20 @@
 
 #include <unistd.h>
 
-namespace {
-
-static size_t PageSizeInit()
-{
-    const long result = sysconf(_SC_PAGESIZE);
-    if (result <= 0) {
-        return 4096;
-    }
-    return result;
-}
-
-const size_t pageSizeValue = PageSizeInit();
-
-}   // namespace
-
 namespace NCloud::NBlockStore {
 
 size_t GetPageSize()
 {
-    return pageSizeValue;
+    const static size_t size = []() -> size_t
+    {
+        const long result = sysconf(_SC_PAGESIZE);
+        if (result <= 0) {
+            return 4096;
+        }
+        return result;
+    }();
+
+    return size;
 }
 
 }   // namespace NCloud::NBlockStore
