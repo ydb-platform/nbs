dc38fccac41d14b6876ad649388e129683aacd41
proller <proller@users.noreply.github.com>
2025-03-25 12:45:52 +0100

    issue-3182: Support 64 KiB platform memory page size (#3232)

--- cloud/blockstore/tools/testing/rdma-test/storage.cpp
+++ cloud/blockstore/tools/testing/rdma-test/storage.cpp
@@ -1,5 +1,7 @@
 #include "storage.h"
 
+#include <cloud/storage/core/libs/common/page_size.h>
+
 #include <util/random/random.h>
 
 namespace NCloud::NBlockStore {
@@ -8,7 +10,7 @@ namespace NCloud::NBlockStore {
 
 TStorageBuffer IStorage::AllocateBuffer(size_t len)
 {
-    void* p = aligned_alloc(PLATFORM_PAGE_SIZE, len);
+    void* p = aligned_alloc(GetPlatformPageSize(), len);
     return { static_cast<char*>(p), free };
 }
 
