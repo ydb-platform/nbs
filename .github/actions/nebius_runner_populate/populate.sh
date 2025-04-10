d0a34136408163b56c689e0c5181068203b560f8
Nikita Menkovich <librarian@nebius.com>
2025-04-10 18:13:22 +0200

    wip: adding threads count for vm

--- .github/actions/nebius_runner_populate/populate.sh
+++ .github/actions/nebius_runner_populate/populate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 nebius compute instance list --parent-id "${PARENT_ID}" --format json | jq --arg owner "${OWNER}" --arg repo "${REPO}" --arg flavor "${FLAVOR}" -r '
 .items[] |
 select(.metadata.labels.repo == $repo) |
@@ -6,7 +7,7 @@ select(.metadata.labels.owner == $owner) |
 select(.metadata.labels.idle == "true") |
 select(.status.state == "RUNNING") |
 select(.metadata.labels."runner-flavor" == $flavor) |
-"\(.metadata.created_at) \(.metadata.id) \(.metadata.name) \(.status.state) \(.metadata.labels."runner-label") \((.metadata.labels | to_entries | map("\(.key)=\(.value)") | join(",")))"' >instances.list || touch instances.list
+"\(.metadata.created_at) \(.metadata.id) \(.metadata.name) \(.status.state) \(.metadata.labels."runner-label") \(.metadata.labels.idle) \((.metadata.labels | to_entries | map("\(.key)=\(.value)") | join(",")))"' >instances.list || touch instances.list
 
 RUNNING_VMS_COUNT=$(wc -l instances.list | awk '{print $1}')
 VMS_TO_REMOVE='[]'
@@ -14,14 +15,30 @@ echo "$VMS_TO_REMOVE" >vms_to_remove.json
 echo 0 >vms_count_to_remove
 VMS_COUNT_TO_REMOVE=0
 DATE=$(date +%s)
-while read -r vm_creation_date vm_id _; do
+while read -r vm_creation_date vm_id name state label idle labels; do
     echo "Checking $vm_id"
     vm_creation_date_ts=$(date -u -d "$vm_creation_date" +%s)
     echo "VM $vm_id created at $vm_creation_date ($vm_creation_date_ts)"
     echo "VM $vm_id is $((DATE - vm_creation_date_ts)) seconds old"
     if [ $((DATE - vm_creation_date_ts)) -gt "$VMS_OLDER_THAN" ]; then
         echo "VM $vm_id is older than $VMS_OLDER_THAN seconds"
-        VMS_TO_REMOVE=$(echo "$VMS_TO_REMOVE" | jq --arg vm_id $vm_id '. + [$vm_id]')
+        echo "Checking if it is idle"
+        if [ "$idle" == "true" ]; then
+            echo "VM $vm_id is idle"
+        else
+            echo "VM $vm_id is not idle"
+            continue
+        fi
+        echo "Checking if vm is idle in GH"
+        gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/librarian-test/nbs/actions/runners?per_page=100 | jq -r --arg vm_id "$vm_id" '.runners[] | select(.name == $vm_id)' | tee -a "gh_vm_${vm_id}_status.json"
+        gh_vm_status=$(jq -r '.busy' "gh_vm_${vm_id}_status.json")
+        if [ "$gh_vm_status" == "false" ]; then
+            echo "VM $vm_id is idle in GH"
+        else
+            echo "VM $vm_id is not idle in GH"
+            continue
+        fi
+        VMS_TO_REMOVE=$(echo "$VMS_TO_REMOVE" | jq -c --arg vm_id "$vm_id" '. + [$vm_id]')
         VMS_COUNT_TO_REMOVE=$((VMS_COUNT_TO_REMOVE + 1))
         echo "$VMS_TO_REMOVE" >vms_to_remove.json
     fi
@@ -38,7 +55,7 @@ if [ $NUMBER_VMS_TO_CREATE -le 0 ]; then
 else
     for i in $(seq 1 $NUMBER_VMS_TO_CREATE); do
         echo "${FLAVOR}-${GITHUB_REPOSITORY_OWNER}-${REPOSITORY_NAME}-$DATE-$i"
-    done | jq -R -s 'split("\n") | map(select(. != ""))' >vms_to_create.json
+    done | jq -R -s -c 'split("\n") | map(select(. != ""))' >vms_to_create.json
 fi
 {
     echo "RUNNING_VMS_COUNT=${RUNNING_VMS_COUNT}"
