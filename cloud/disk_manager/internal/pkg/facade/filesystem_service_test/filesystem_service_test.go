6c52833293d315a217a3dfcd68660a33e1aae146
Evgeny Budilovsky <evgenybud@nebius.com>
2025-05-06 17:14:56 +0300

    CR2

--- cloud/disk_manager/internal/pkg/facade/filesystem_service_test/filesystem_service_test.go
+++ cloud/disk_manager/internal/pkg/facade/filesystem_service_test/filesystem_service_test.go
@@ -23,26 +23,30 @@ func TestFilesystemServiceCreateEmptyFilesystem(t *testing.T) {
 	filesystemID := t.Name()
 
 	reqCtx := testcommon.GetRequestContext(t, ctx)
-	operation, err := client.CreateFilesystem(reqCtx, &disk_manager.CreateFilesystemRequest{
-		FilesystemId: &disk_manager.FilesystemId{
-			ZoneId:       "zone-a",
-			FilesystemId: filesystemID,
-		},
-		BlockSize: 4096,
-		Size:      4096000,
-		Kind:      disk_manager.FilesystemKind_FILESYSTEM_KIND_HDD,
-	})
+	operation, err := client.CreateFilesystem(
+		reqCtx,
+		&disk_manager.CreateFilesystemRequest{
+			FilesystemId: &disk_manager.FilesystemId{
+				ZoneId:       "zone-a",
+				FilesystemId: filesystemID,
+			},
+			BlockSize: 4096,
+			Size:      4096000,
+			Kind:      disk_manager.FilesystemKind_FILESYSTEM_KIND_HDD,
+		})
 	require.NoError(t, err)
 	require.NotEmpty(t, operation)
 	err = internal_client.WaitOperation(ctx, client, operation.Id)
 	require.NoError(t, err)
 
 	reqCtx = testcommon.GetRequestContext(t, ctx)
-	operation, err = client.DeleteFilesystem(reqCtx, &disk_manager.DeleteFilesystemRequest{
-		FilesystemId: &disk_manager.FilesystemId{
-			FilesystemId: filesystemID,
-		},
-	})
+	operation, err = client.DeleteFilesystem(
+		reqCtx,
+		&disk_manager.DeleteFilesystemRequest{
+			FilesystemId: &disk_manager.FilesystemId{
+				FilesystemId: filesystemID,
+			},
+		})
 	require.NoError(t, err)
 	require.NotEmpty(t, operation)
 	err = internal_client.WaitOperation(ctx, client, operation.Id)
@@ -64,20 +68,27 @@ func TestFilesystemServiceCreateExternalFilesystem(t *testing.T) {
 	filesystemID := t.Name()
 
 	reqCtx := testcommon.GetRequestContext(t, ctx)
-	operation, err := client.CreateFilesystem(reqCtx, &disk_manager.CreateFilesystemRequest{
-		FilesystemId: &disk_manager.FilesystemId{
-			ZoneId:       "zone-a",
-			FilesystemId: filesystemID,
-		},
-		BlockSize:  4096,
-		Size:       4096000,
-		Kind:       disk_manager.FilesystemKind_FILESYSTEM_KIND_HDD,
-		IsExternal: true,
-	})
+	operation, err := client.CreateFilesystem(
+		reqCtx,
+		&disk_manager.CreateFilesystemRequest{
+			FilesystemId: &disk_manager.FilesystemId{
+				ZoneId:       "zone-a",
+				FilesystemId: filesystemID,
+			},
+			BlockSize:  4096,
+			Size:       4096000,
+			Kind:       disk_manager.FilesystemKind_FILESYSTEM_KIND_HDD,
+			IsExternal: true,
+		})
 	require.NoError(t, err)
 	require.NotEmpty(t, operation)
 
-	waitOperationWithTimeout := func(msg string, shouldTimeout bool, timeout time.Duration, opId string) {
+	waitOperationWithTimeout := func(
+		msg string,
+		shouldTimeout bool,
+		timeout time.Duration,
+		opId string) {
+
 		opCtx, _ := context.WithTimeout(ctx, timeout)
 		err = internal_client.WaitOperation(opCtx, client, operation.Id)
 		if shouldTimeout {
@@ -89,31 +100,50 @@ func TestFilesystemServiceCreateExternalFilesystem(t *testing.T) {
 
 	}
 	// validate CreateFilesystem task is stuck
-	waitOperationWithTimeout("CreateFilesystem.1", true /* should timeout */, 60*time.Second, operation.Id)
+	waitOperationWithTimeout(
+		"CreateFilesystem.1",
+		true, /* should timeout */
+		60*time.Second, operation.Id)
 
 	// Force finish CreateExternalFilesystem task
-	externalTask, err := testcommon.FindRunningTaskByType(ctx, taskStorage, "filesystem.CreateExternalFilesystem", 100)
+	externalTask, err := testcommon.FindRunningTaskByType(
+		ctx,
+		taskStorage,
+		"filesystem.CreateExternalFilesystem",
+		100)
 	require.NoError(t, err)
 
 	err = taskStorage.ForceFinishTask(ctx, externalTask.ID)
 	require.NoError(t, err)
 
 	// validate CreateFilesystem finish succesfully
-	waitOperationWithTimeout("CreateFilesystem.2", false /* should not timeout */, 30*time.Second, operation.Id)
+	waitOperationWithTimeout(
+		"CreateFilesystem.2",
+		false, /* should not timeout */
+		30*time.Second, operation.Id)
 
 	reqCtx = testcommon.GetRequestContext(t, ctx)
-	operation, err = client.DeleteFilesystem(reqCtx, &disk_manager.DeleteFilesystemRequest{
-		FilesystemId: &disk_manager.FilesystemId{
-			FilesystemId: filesystemID,
-		},
-	})
+	operation, err = client.DeleteFilesystem(
+		reqCtx,
+		&disk_manager.DeleteFilesystemRequest{
+			FilesystemId: &disk_manager.FilesystemId{
+				FilesystemId: filesystemID,
+			},
+		})
 	require.NoError(t, err)
 	require.NotEmpty(t, operation)
 
 	// validate DeleteFilesystem task is stuck
-	waitOperationWithTimeout("DeleteFilesystem.1", true /* should timeout */, 60*time.Second, operation.Id)
+	waitOperationWithTimeout(
+		"DeleteFilesystem.1",
+		true, /* should timeout */
+		60*time.Second, operation.Id)
 
-	externalTask, err = testcommon.FindRunningTaskByType(ctx, taskStorage, "filesystem.DeleteExternalFilesystem", 100)
+	externalTask, err = testcommon.FindRunningTaskByType(
+		ctx,
+		taskStorage,
+		"filesystem.DeleteExternalFilesystem",
+		100)
 	require.NoError(t, err)
 
 	// Force finish DeleteExternalFilesystem task
@@ -121,7 +151,11 @@ func TestFilesystemServiceCreateExternalFilesystem(t *testing.T) {
 	require.NoError(t, err)
 
 	// validate DeleteFilesystem finish succesfully
-	waitOperationWithTimeout("DeleteFilesystem.2", false /* should not timeout */, 30*time.Second, operation.Id)
+	waitOperationWithTimeout(
+		"DeleteFilesystem.2",
+		false, /* should not timeout */
+		30*time.Second,
+		operation.Id)
 
 	testcommon.CheckConsistency(t, ctx)
 }
