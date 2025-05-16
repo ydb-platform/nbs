e3e9ea4e2cb5880826909a06abab45cf3a920f31
Slava Sazonov <156317320+Sazonov99@users.noreply.github.com>
2025-05-16 18:06:46 +0700

    fix (#3524)
    Std::move of the const variable 'error' has no effect; remove std::move() or make the variable non-const

--- cloud/blockstore/libs/storage/partition_common/actor_checkrange.cpp
+++ cloud/blockstore/libs/storage/partition_common/actor_checkrange.cpp
@@ -64,7 +64,7 @@ void TCheckRangeActor::ReplyAndDie(
     const NProto::TError& error)
 {
     auto response =
-        std::make_unique<TEvVolume::TEvCheckRangeResponse>(std::move(error));
+        std::make_unique<TEvVolume::TEvCheckRangeResponse>(error);
 
     NCloud::Reply(ctx, *RequestInfo, std::move(response));
 
