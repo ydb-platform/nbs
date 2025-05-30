d5a8ee41b55746c33973a6f8851106f943dc15a8
Slava Sazonov <sazonov99@yandex-team.ru>
2025-05-30 07:30:58 +0000

    naming fix

--- cloud/blockstore/libs/storage/ss_proxy/ss_proxy_actor_describevolume.cpp
+++ cloud/blockstore/libs/storage/ss_proxy/ss_proxy_actor_describevolume.cpp
@@ -43,7 +43,7 @@ public:
         TRequestInfoPtr requestInfo,
         TStorageConfigPtr config,
         TString diskId,
-        bool useSchemeShard);
+        bool useSchemeCache);
 
     void DescribeVolumeFromCache(const TActorContext& ctx);
 
