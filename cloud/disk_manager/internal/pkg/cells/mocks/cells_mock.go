af4629ad6939119ae680fd843c871741425d6465
Aleksey Skvortsov <46933547+Rattysed@users.noreply.github.com>
2025-06-07 15:30:11 +0300

    [Disk Manager] Introduce Cells component (#3618)

---
+++ cloud/disk_manager/internal/pkg/cells/mocks/cells_mock.go
@@ -0,0 +1,29 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+	disk_manager "github.com/ydb-platform/nbs/cloud/disk_manager/api"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+type CellSelectorMock struct {
+	mock.Mock
+}
+
+func NewCellSelectorMock() *CellSelectorMock {
+	return &CellSelectorMock{}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (s *CellSelectorMock) SelectCell(
+	ctx context.Context,
+	disk *disk_manager.DiskId,
+) string {
+
+	args := s.Called(ctx, disk)
+	return args.String(0)
+}
