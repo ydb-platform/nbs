a378a5aa861dff8e3437e957ca0e8be0d185e6e7
Taras Pashkin <tpashkin@nebius.com>
2025-06-21 11:28:52 +0300

    Revert "Propagate link to follower (#3562)"
    This reverts commit e3cd1629782126ea5b5d8af14ba1cb1ae2ffddca.

--- cloud/blockstore/libs/storage/volume/model/follower_disk.cpp
+++ cloud/blockstore/libs/storage/volume/model/follower_disk.cpp
@@ -7,117 +7,28 @@
 
 namespace NCloud::NBlockStore::NStorage {
 
-namespace {
-
 ////////////////////////////////////////////////////////////////////////////////
 
-TString IdForPrint(const TString& diskId, const TString& shardId)
+TString TFollowerDiskInfo::GetDiskIdForPrint() const
 {
-    return shardId ? (shardId + "/" + diskId) : diskId;
+    return ScaleUnitId ? (ScaleUnitId + "/" + FollowerDiskId) : FollowerDiskId;
 }
-
-////////////////////////////////////////////////////////////////////////////////
-
-}   // namespace
-
-TString TLeaderFollowerLink::LeaderDiskIdForPrint() const
-{
-    return IdForPrint(LeaderDiskId, LeaderShardId);
-}
-
-TString TLeaderFollowerLink::FollowerDiskIdForPrint() const
-{
-    return IdForPrint(FollowerDiskId, FollowerShardId);
-}
-
-TString TLeaderFollowerLink::Describe() const
-{
-    auto builder = TStringBuilder();
-    builder << "[";
-    builder << FollowerDiskIdForPrint().Quote();
-    builder << " -> ";
-    builder << LeaderDiskIdForPrint().Quote();
-    builder << ", ";
-    builder << LinkUUID.Quote();
-    builder << "]";
-    return builder;
-}
-
-bool TLeaderFollowerLink::Match(const TLeaderFollowerLink& rhs) const
-{
-    if (LinkUUID == rhs.LinkUUID) {
-        return true;
-    }
-    if (LinkUUID && rhs.LinkUUID) {
-        return false;
-    }
-    auto withoutUUID = [](const TLeaderFollowerLink& o)
-    {
-        return std::tie(
-            o.LeaderDiskId,
-            o.LeaderShardId,
-            o.FollowerDiskId,
-            o.FollowerShardId);
-    };
-    return withoutUUID(*this) == withoutUUID(rhs);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-TString TLeaderDiskInfo::Describe() const
-{
-    auto builder = TStringBuilder();
-    builder << "{ State:" << ToString(State) << " }";
-    return builder;
-}
-
-bool TLeaderDiskInfo::operator==(const TLeaderDiskInfo& rhs) const
-{
-    auto doTie = [](const TLeaderDiskInfo& o)
-    {
-        return std::tie(o.CreatedAt, o.State, o.ErrorMessage);
-    };
-    return Link.Match(rhs.Link) && doTie(*this) == doTie(rhs);
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 TString TFollowerDiskInfo::Describe() const
 {
     auto builder = TStringBuilder();
-    builder << "{ State: " << ToString(State);
-
-    if (MediaKind) {
-        builder << ", MediaKind: "
-                << NProto::EStorageMediaKind_Name(*MediaKind);
-    }
+    builder << "{ State:" << ToString(State) << ", MigratedBytes:";
 
     if (MigratedBytes) {
-        builder << ", MigratedBytes: " << FormatByteSize(*MigratedBytes);
+        builder << FormatByteSize(*MigratedBytes);
+    } else {
+        builder << "-";
     }
-
-    if (ErrorMessage) {
-        builder << ", ErrorMessage: " << ErrorMessage.Quote();
-    }
-
-    builder << " }";
+    builder << "}";
     return builder;
 }
 
-bool TFollowerDiskInfo::operator==(const TFollowerDiskInfo& rhs) const
-{
-    auto doTie = [](const TFollowerDiskInfo& o)
-    {
-        return std::tie(
-            o.CreatedAt,
-            o.State,
-            o.MediaKind,
-            o.MigratedBytes,
-            o.ErrorMessage);
-    };
-    return Link.Match(rhs.Link) && doTie(*this) == doTie(rhs);
-}
-
-////////////////////////////////////////////////////////////////////////////////
+bool TFollowerDiskInfo::operator==(
+    const TFollowerDiskInfo& rhs) const = default;
 
 }   // namespace NCloud::NBlockStore::NStorage
