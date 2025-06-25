7464a1ceb58c78f6bf15a4589fd98da6802dbc5a
proller <proller@github.com>
2025-06-25 18:04:16 +0200

    Fix build

--- cloud/filestore/tools/testing/loadtest/lib/request_replay_fs.cpp
+++ cloud/filestore/tools/testing/loadtest/lib/request_replay_fs.cpp
@@ -1043,7 +1043,7 @@ private:
                 Started,
                 MakeError(E_CANCELLED, "cancelled")});
         }
-        const auto dirs = NLowLevel::ListDirAt(dir, true);
+        const auto dirs = NLowLevel::ListDirAt(dir, 0, 0, true).DirEntries;
         if (logRequest.GetNodeInfo().GetSize() != dirs.size()) {
             STORAGE_DEBUG(
                 "Dir size differs %s log=%lu local=%zu",
