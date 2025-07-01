195707b59df0d754ad919950c2738f5ab645c453
Evgeny Budilovsky <evgenybud@nebius.com>
2025-07-01 11:05:12 +0300

    [Filestore] issue-3617: trust guest kernel to validate file permissions in local filestore (#3762)
    * Revert "[Filestore] issue-3617: trust guest kernel to validate file permissions in local filestore (#3632)"
    
    This reverts commit 74bb5a9a9d10f2ca637a55b29930bd560a1d23b8.
    
    * [Filestore] issue-3617: trust guest kernel to validate file permissions in local filestore
    
    After reverting previous not atomic implementation (create + chown). Use
    CAP_DAC_OVERRIDE to skip permission checks on the backend fs. The file/directory
    are going to be created atomically with proper uid/gid since with change
    euid/egid before executing the creation. But the backend won't complain on
    permissions since we restore the CAP_DAC_OVERRIDE capability

--- cloud/filestore/libs/service_local/lowlevel_ut.cpp
+++ cloud/filestore/libs/service_local/lowlevel_ut.cpp
@@ -11,54 +11,6 @@ namespace NCloud::NFileStore {
 
 Y_UNIT_TEST_SUITE(TLowlevelTest)
 {
-    Y_UNIT_TEST(ShouldOpenOrCreateFile)
-    {
-        const TTempDir TempDir;
-        auto node = NLowLevel::Open(TempDir.Name(), O_PATH, 0);
-
-        // WasCreated should be true or false based on whether file already
-        // exist or not
-        auto res =
-            NLowLevel::OpenOrCreateAt(node, "1.txt", O_CREAT | O_WRONLY, 0755);
-        UNIT_ASSERT(res.WasCreated);
-
-        TString expectedData = "abdef";
-        res.Handle.Write(
-            const_cast<char*>(expectedData.c_str()),
-            expectedData.size());
-
-        res =
-            NLowLevel::OpenOrCreateAt(node, "1.txt", O_CREAT | O_RDONLY, 0755);
-        UNIT_ASSERT(!res.WasCreated);
-        res.Handle.Flush();
-
-        char buf[256] = {};
-        UNIT_ASSERT_EQUAL(
-            static_cast<i32>(expectedData.size()),
-            res.Handle.Read(buf, expectedData.size()));
-
-        // O_EXCL should still behave properly
-        res = NLowLevel::OpenOrCreateAt(
-            node,
-            "2.txt",
-            O_CREAT | O_EXCL | O_WRONLY,
-            0755);
-        UNIT_ASSERT(res.WasCreated);
-
-        UNIT_ASSERT_EXCEPTION_CONTAINS(
-            NLowLevel::OpenOrCreateAt(
-                node,
-                "2.txt",
-                O_CREAT | O_EXCL | O_WRONLY,
-                0755),
-            yexception,
-            "File exists");
-
-        // Just open existing file also works
-        res = NLowLevel::OpenOrCreateAt(node, "2.txt", O_WRONLY, 0755);
-        UNIT_ASSERT(!res.WasCreated);
-    }
-
     Y_UNIT_TEST(ShouldDoIterativeListDir)
     {
         const TTempDir TempDir;
