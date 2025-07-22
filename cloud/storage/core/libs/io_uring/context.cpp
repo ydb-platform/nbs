2b60455558b9f398157321238b85b69e88787154
Pavel Misko <misko.pawel@gmail.com>
2025-07-22 12:32:17 +0200

    move TIoUring to context.h (#3960)

---
+++ cloud/storage/core/libs/io_uring/context.cpp
@@ -0,0 +1,381 @@
+#include "context.h"
+
+#include <cloud/storage/core/libs/common/file_io_service.h>
+#include <cloud/storage/core/libs/common/task_queue.h>
+#include <cloud/storage/core/libs/common/thread.h>
+#include <cloud/storage/core/libs/common/thread_pool.h>
+
+#include <util/string/builder.h>
+#include <util/system/error.h>
+#include <util/system/sanitizers.h>
+
+namespace NCloud::NIoUring {
+
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+NProto::TError MakeSystemError(int code, TStringBuf message)
+{
+    return MakeError(
+        MAKE_SYSTEM_ERROR(code),
+        TStringBuilder() << "(" << LastSystemErrorText(code) << ") "
+                         << message);
+}
+
+bool IsRetriable(int error)
+{
+    return error == EINTR || error == EAGAIN || error == EBUSY;
+}
+
+NProto::TError Submit(io_uring* ring)
+{
+    for (;;) {
+        const int ret = io_uring_submit(ring);
+        if (ret >= 0) {
+            return {};
+        }
+
+        if (!IsRetriable(-ret)) {
+            return MakeSystemError(-ret, "unable to submit async IO operation");
+        }
+    }
+}
+
+NProto::TError SetMaxWorkers(io_uring* ring, ui32 bound)
+{
+    ui32 workers[2] = {bound, 0};
+
+    const int ret = io_uring_register_iowq_max_workers(ring, workers);
+    if (ret < 0) {
+        return MakeSystemError(-ret, "io_uring_register_iowq_max_workers");
+    }
+    return {};
+}
+
+NProto::TError InitRing(io_uring* ring, ui32 entries, io_uring* wqOwner)
+{
+    io_uring_params params{
+        .flags = IORING_SETUP_R_DISABLED | IORING_SETUP_SINGLE_ISSUER};
+
+    if (wqOwner) {
+        params.flags |= IORING_SETUP_ATTACH_WQ;
+        params.wq_fd = wqOwner->ring_fd;
+    }
+
+    for (;;) {
+        const int ret = io_uring_queue_init_params(entries, ring, &params);
+
+        if (ret == -EINVAL && params.flags & IORING_SETUP_SINGLE_ISSUER) {
+            // IORING_SETUP_SINGLE_ISSUER is not supported
+            params.flags &= ~IORING_SETUP_SINGLE_ISSUER;
+            continue;
+        }
+
+        if (ret < 0) {
+            return MakeSystemError(-ret, "io_uring_queue_init_params");
+        }
+
+        return {};
+    }
+}
+
+NProto::TError EnableRing(io_uring* ring)
+{
+    for (;;) {
+        const int ret = io_uring_enable_rings(ring);
+        if (!ret) {
+            return {};
+        }
+
+        if (!IsRetriable(-ret)) {
+            return MakeSystemError(-ret, "io_uring_enable_rings");
+        }
+    }
+}
+
+}   // namespace
+
+////////////////////////////////////////////////////////////////////////////////
+
+TContext::TContext(TParams params)
+    : SubmissionThread(CreateThreadPool(params.SubmissionThreadName, 1))
+    , CompletionThread(
+          std::bind_front(
+              &TContext::CompletionThreadProc,
+              this,
+              std::move(params.CompletionThreadName)))
+{
+    const auto error = InitRing(
+        &Ring,
+        params.SubmissionQueueEntries,
+        params.WqOwner ? &params.WqOwner->Ring : nullptr);
+
+    Y_ABORT_IF(
+        HasError(error),
+        "can't initialize the ring: %s",
+        FormatError(error).c_str());
+
+    if (!params.WqOwner && params.MaxKernelWorkersCount) {
+        const auto error = SetMaxWorkers(&Ring, params.MaxKernelWorkersCount);
+        Y_ABORT_IF(
+            HasError(error),
+            "can't set max workers: %s",
+            FormatError(error).c_str());
+    }
+}
+
+TContext::~TContext()
+{
+    Stop();
+
+    io_uring_queue_exit(&Ring);
+    Ring = {};
+}
+
+void TContext::Start()
+{
+    SubmissionThread->Start();
+
+    // Since IORING_SETUP_R_DISABLED and IORING_SETUP_SINGLE_ISSUER
+    // are set, we must enable the ring in the context of the
+    // submission thread.
+    Started = SubmissionThread->Execute(
+        [this]
+        {
+            const auto error = EnableRing(&Ring);
+            Y_ABORT_IF(
+                HasError(error),
+                "can't enable the ring: %s",
+                FormatError(error).c_str());
+
+            // Start the completion thread only after enabling the ring.
+            CompletionThread.Start();
+        });
+}
+
+void TContext::Stop()
+{
+    if (!Started.Initialized()) {
+        return;
+    }
+
+    Started.Wait();
+    Started = {};
+
+    SubmissionThread->ExecuteSimple([this] { SubmitStopSignal(); });
+
+    CompletionThread.Join();
+    SubmissionThread->Stop();
+}
+
+void TContext::AsyncIO(
+    int op,
+    int fd,
+    const void* addr,
+    ui32 len,
+    ui64 offset,
+    TFileIOCompletion* completion,
+    ui32 flags)
+{
+    SubmissionThread->ExecuteSimple(
+        [=, this] { SubmitIO(op, fd, addr, len, offset, completion, flags); });
+}
+
+void TContext::AsyncNOP(TFileIOCompletion* completion, ui32 flags)
+{
+    SubmissionThread->ExecuteSimple([=, this] { SubmitNOP(completion, flags); });
+}
+
+void TContext::SubmitIO(
+    int op,
+    int fd,
+    const void* addr,
+    ui32 len,
+    ui64 offset,
+    TFileIOCompletion* completion,
+    ui32 flags)
+{
+    io_uring_sqe* sqe = io_uring_get_sqe(&Ring);
+    if (!sqe) {
+        completion->Func(
+            completion,
+            MakeError(E_REJECTED, "Overloaded"),
+            0);
+        return;
+    }
+
+    io_uring_prep_rw(op, sqe, fd, addr, len, offset);
+    io_uring_sqe_set_data(sqe, completion);
+    io_uring_sqe_set_flags(sqe, flags);
+
+    NSan::Release(completion);
+
+    const auto error = Submit(&Ring);
+    if (HasError(error)) {
+        completion->Func(completion, error, 0);
+    }
+}
+
+void TContext::SubmitNOP(TFileIOCompletion* completion, ui32 flags)
+{
+    io_uring_sqe* sqe = io_uring_get_sqe(&Ring);
+    if (!sqe) {
+        completion->Func(
+            completion,
+            MakeError(E_REJECTED, "Overloaded"),
+            0);
+        return;
+    }
+
+    io_uring_prep_nop(sqe);
+    io_uring_sqe_set_data(sqe, completion);
+    io_uring_sqe_set_flags(sqe, flags);
+
+    NSan::Release(completion);
+
+    const auto error = Submit(&Ring);
+    if (HasError(error)) {
+        completion->Func(completion, error, 0);
+    }
+}
+
+void TContext::SubmitStopSignal()
+{
+    for (;;) {
+        io_uring_sqe* sqe = io_uring_get_sqe(&Ring);
+        if (sqe) {
+            io_uring_prep_nop(sqe);
+            io_uring_sqe_set_data(sqe, nullptr);
+
+            const auto error = Submit(&Ring);
+            Y_ABORT_IF(
+                HasError(error),
+                "can't submit a stop signal: %s",
+                FormatError(error).c_str());
+            break;
+        }
+
+        // the submission queue is full, so we're waiting
+        SpinLockPause();
+    }
+}
+
+void TContext::ProcessCompletion(io_uring_cqe* cqe)
+{
+    void* data = io_uring_cqe_get_data(cqe);
+    Y_DEBUG_ABORT_UNLESS(data);
+
+    if (!data) {
+        return;
+    }
+
+    auto* completion = static_cast<TFileIOCompletion*>(data);
+    NSan::Acquire(completion);
+
+    if (cqe->res < 0) {
+        completion->Func(
+            completion,
+            MakeSystemError(-cqe->res, "async IO operation failed"),
+            0);
+    } else {
+        completion->Func(completion, {}, cqe->res);
+    }
+}
+
+void TContext::CompletionThreadProc(const TString& threadName)
+{
+    SetHighestThreadPriority();
+    NCloud::SetCurrentThreadName(threadName);
+
+    for (;;) {
+        io_uring_cqe* cqe = nullptr;
+        const int ret = io_uring_wait_cqe(&Ring, &cqe);
+        if (ret < 0) {
+            Y_ABORT_UNLESS(
+                IsRetriable(-ret),
+                "io_uring_wait_cqe: %s (%d)",
+                LastSystemErrorText(-ret),
+                -ret);
+            continue;
+        }
+
+        if (!io_uring_cqe_get_data(cqe)) {
+            break;
+        }
+
+        ProcessCompletion(cqe);
+
+        io_uring_cqe_seen(&Ring, cqe);
+    }
+}
+
+void TContext::AsyncWrite(
+    int fd,
+    TArrayRef<const char> buffer,
+    ui64 offset,
+    TFileIOCompletion* completion,
+    ui32 flags)
+{
+    AsyncIO(
+        IORING_OP_WRITE,
+        fd,
+        buffer.data(),
+        buffer.size(),
+        offset,
+        completion,
+        flags);
+}
+
+void TContext::AsyncRead(
+    int fd,
+    TArrayRef<char> buffer,
+    ui64 offset,
+    TFileIOCompletion* completion,
+    ui32 flags)
+{
+    AsyncIO(
+        IORING_OP_READ,
+        fd,
+        buffer.data(),
+        buffer.size(),
+        offset,
+        completion,
+        flags);
+}
+
+void TContext::AsyncWriteV(
+    int fd,
+    TArrayRef<const TArrayRef<const char>> buffers,
+    ui64 offset,
+    TFileIOCompletion* completion,
+    ui32 flags)
+{
+    AsyncIO(
+        IORING_OP_WRITEV,
+        fd,
+        buffers.data(),
+        buffers.size(),
+        offset,
+        completion,
+        flags);
+}
+
+void TContext::AsyncReadV(
+    int fd,
+    TArrayRef<const TArrayRef<char>> buffers,
+    ui64 offset,
+    TFileIOCompletion* completion,
+    ui32 flags)
+{
+    AsyncIO(
+        IORING_OP_READV,
+        fd,
+        buffers.data(),
+        buffers.size(),
+        offset,
+        completion,
+        flags);
+}
+
+}   // namespace NCloud::NIoUring
--- cloud/storage/core/libs/io_uring/service.cpp
+++ cloud/storage/core/libs/io_uring/service.cpp
@@ -1,5 +1,7 @@
 #include "service.h"
 
+#include "context.h"
+
 #include <cloud/storage/core/libs/common/file_io_service.h>
 #include <cloud/storage/core/libs/common/task_queue.h>
 #include <cloud/storage/core/libs/common/thread.h>
@@ -19,336 +21,29 @@
 
 namespace NCloud {
 
+using namespace NIoUring;
+
 namespace {
-
-////////////////////////////////////////////////////////////////////////////////
-
-NProto::TError MakeSystemError(int code, TStringBuf message)
-{
-    return MakeError(
-        MAKE_SYSTEM_ERROR(code),
-        TStringBuilder() << "(" << LastSystemErrorText(code) << ") "
-                         << message);
-}
-
-bool IsRetriable(int error)
-{
-    return error == EINTR || error == EAGAIN || error == EBUSY;
-}
-
-NProto::TError Submit(io_uring* ring)
-{
-    for (;;) {
-        const int ret = io_uring_submit(ring);
-        if (ret >= 0) {
-            return {};
-        }
-
-        if (!IsRetriable(-ret)) {
-            return MakeSystemError(-ret, "unable to submit async IO operation");
-        }
-    }
-}
-
-NProto::TError SetMaxWorkers(io_uring* ring, ui32 bound)
-{
-    ui32 workers[2] = {bound, 0};
-
-    const int ret = io_uring_register_iowq_max_workers(ring, workers);
-    if (ret < 0) {
-        return MakeSystemError(-ret, "io_uring_register_iowq_max_workers");
-    }
-    return {};
-}
-
-NProto::TError InitRing(io_uring* ring, ui32 entries, io_uring* wqOwner)
-{
-    io_uring_params params{
-        .flags = IORING_SETUP_R_DISABLED | IORING_SETUP_SINGLE_ISSUER};
-
-    if (wqOwner) {
-        params.flags |= IORING_SETUP_ATTACH_WQ;
-        params.wq_fd = wqOwner->ring_fd;
-    }
-
-    for (;;) {
-        const int ret = io_uring_queue_init_params(entries, ring, &params);
-
-        if (ret == -EINVAL && params.flags & IORING_SETUP_SINGLE_ISSUER) {
-            // IORING_SETUP_SINGLE_ISSUER is not supported
-            params.flags &= ~IORING_SETUP_SINGLE_ISSUER;
-            continue;
-        }
-
-        if (ret < 0) {
-            return MakeSystemError(-ret, "io_uring_queue_init_params");
-        }
-
-        return {};
-    }
-}
-
-NProto::TError EnableRing(io_uring* ring)
-{
-    for (;;) {
-        const int ret = io_uring_enable_rings(ring);
-        if (!ret) {
-            return {};
-        }
-
-        if (!IsRetriable(-ret)) {
-            return MakeSystemError(-ret, "io_uring_enable_rings");
-        }
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-class TIoUring
-{
-private:
-    io_uring Ring = {};
-
-    ITaskQueuePtr SubmissionThread;
-    TThread CompletionThread;
-
-    NThreading::TFuture<void> Started;
-
-public:
-    // Share kernel worker threads with `wqOwner`
-    TIoUring(TIoUringServiceParams params, TIoUring* wqOwner)
-        : SubmissionThread(CreateThreadPool(params.SubmissionThreadName, 1))
-        , CompletionThread(
-              std::bind_front(
-                  &TIoUring::CompletionThreadProc,
-                  this,
-                  std::move(params.CompletionThreadName)))
-    {
-        const auto error = InitRing(
-            &Ring,
-            params.SubmissionQueueEntries,
-            wqOwner ? &wqOwner->Ring : nullptr);
-
-        Y_ABORT_IF(
-            HasError(error),
-            "can't initialize the ring: %s",
-            FormatError(error).c_str());
-
-        if (!wqOwner && params.MaxKernelWorkersCount) {
-            const auto error =
-                SetMaxWorkers(&Ring, params.MaxKernelWorkersCount);
-            Y_ABORT_IF(
-                HasError(error),
-                "can't set max workers: %s",
-                FormatError(error).c_str());
-        }
-    }
-
-    ~TIoUring()
-    {
-        Stop();
-
-        io_uring_queue_exit(&Ring);
-        Ring = {};
-    }
-
-    void Start()
-    {
-        SubmissionThread->Start();
-
-        // Since IORING_SETUP_R_DISABLED and IORING_SETUP_SINGLE_ISSUER
-        // are set, we must enable the ring in the context of the
-        // submission thread.
-        Started = SubmissionThread->Execute(
-            [this]
-            {
-                const auto error = EnableRing(&Ring);
-                Y_ABORT_IF(
-                    HasError(error),
-                    "can't enable the ring: %s",
-                    FormatError(error).c_str());
-
-                // Start the completion thread only after enabling the ring.
-                CompletionThread.Start();
-            });
-    }
-
-    void Stop()
-    {
-        if (!Started.Initialized()) {
-            return;
-        }
-
-        Started.Wait();
-        Started = {};
-
-        SubmissionThread->ExecuteSimple([this] { SubmitStopSignal(); });
-
-        CompletionThread.Join();
-        SubmissionThread->Stop();
-    }
-
-    void AsyncIO(
-        int op,
-        int fd,
-        const void* addr,
-        ui32 len,
-        ui64 offset,
-        TFileIOCompletion* completion)
-    {
-        SubmissionThread->ExecuteSimple([=, this] {
-            SubmitIO(op, fd, addr, len, offset, completion);
-        });
-    }
-
-    void AsyncNOP(TFileIOCompletion* completion)
-    {
-        SubmissionThread->ExecuteSimple([=, this] {
-            SubmitNOP(completion);
-        });
-    }
-
-private:
-    void SubmitIO(
-        int op,
-        int fd,
-        const void* addr,
-        ui32 len,
-        ui64 offset,
-        TFileIOCompletion* completion)
-    {
-        io_uring_sqe* sqe = io_uring_get_sqe(&Ring);
-        if (!sqe) {
-            completion->Func(
-                completion,
-                MakeError(E_REJECTED, "Overloaded"),
-                0);
-            return;
-        }
-
-        io_uring_prep_rw(op, sqe, fd, addr, len, offset);
-        io_uring_sqe_set_data(sqe, completion);
-
-        NSan::Release(completion);
-
-        const auto error = Submit(&Ring);
-        if (HasError(error)) {
-            completion->Func(completion, error, 0);
-        }
-    }
-
-    void SubmitNOP(TFileIOCompletion* completion)
-    {
-        io_uring_sqe* sqe = io_uring_get_sqe(&Ring);
-        if (!sqe) {
-            completion->Func(
-                completion,
-                MakeError(E_REJECTED, "Overloaded"),
-                0);
-            return;
-        }
-
-        io_uring_prep_nop(sqe);
-        io_uring_sqe_set_data(sqe, completion);
-
-        NSan::Release(completion);
-
-        const auto error = Submit(&Ring);
-        if (HasError(error)) {
-            completion->Func(completion, error, 0);
-        }
-    }
-
-    void SubmitStopSignal()
-    {
-        for (;;) {
-            io_uring_sqe* sqe = io_uring_get_sqe(&Ring);
-            if (sqe) {
-                io_uring_prep_nop(sqe);
-                io_uring_sqe_set_data(sqe, nullptr);
-
-                const auto error = Submit(&Ring);
-                Y_ABORT_IF(
-                    HasError(error),
-                    "can't submit a stop signal: %s",
-                    FormatError(error).c_str());
-                break;
-            }
-
-            // the submission queue is full, so we're waiting
-            SpinLockPause();
-        }
-    }
-
-    void ProcessCompletion(io_uring_cqe* cqe)
-    {
-        void* data = io_uring_cqe_get_data(cqe);
-        Y_DEBUG_ABORT_UNLESS(data);
-
-        if (!data) {
-            return;
-        }
-
-        auto* completion = static_cast<TFileIOCompletion*>(data);
-        NSan::Acquire(completion);
-
-        if (cqe->res < 0) {
-            completion->Func(
-                completion,
-                MakeSystemError(-cqe->res, "async IO operation failed"),
-                0);
-        } else {
-            completion->Func(completion, {}, cqe->res);
-        }
-    }
-
-    void CompletionThreadProc(const TString& threadName)
-    {
-        SetHighestThreadPriority();
-        NCloud::SetCurrentThreadName(threadName);
-
-        for (;;) {
-            io_uring_cqe* cqe = nullptr;
-            const int ret = io_uring_wait_cqe(&Ring, &cqe);
-            if (ret < 0) {
-                Y_ABORT_UNLESS(
-                    IsRetriable(-ret),
-                    "io_uring_wait_cqe: %s (%d)",
-                    LastSystemErrorText(-ret),
-                    -ret);
-                continue;
-            }
-
-            if (!io_uring_cqe_get_data(cqe)) {
-                break;
-            }
-
-            ProcessCompletion(cqe);
-
-            io_uring_cqe_seen(&Ring, cqe);
-        }
-    }
-};
 
 ////////////////////////////////////////////////////////////////////////////////
 
 struct TIoUringServiceBase
     : public IFileIOService
 {
-    TIoUring IoUring;
+    TContext Context;
 
-    TIoUringServiceBase(TIoUringServiceParams params, TIoUring* wqOwner)
-        : IoUring(std::move(params), wqOwner)
+    explicit TIoUringServiceBase(TContext::TParams params)
+        : Context(std::move(params))
     {}
 
     void Start() final
     {
-        IoUring.Start();
+        Context.Start();
     }
 
     void Stop() final
     {
-        IoUring.Stop();
+        Context.Stop();
     }
 };
 
@@ -365,13 +60,7 @@ struct TIoUringService final
         TArrayRef<char> buffer,
         TFileIOCompletion* completion) final
     {
-        IoUring.AsyncIO(
-            IORING_OP_READ,
-            file,
-            buffer.data(),
-            buffer.size(),
-            offset,
-            completion);
+        Context.AsyncRead(file, buffer, offset, completion);
     }
 
     void AsyncReadV(
@@ -380,13 +69,7 @@ struct TIoUringService final
         const TVector<TArrayRef<char>>& buffers,
         TFileIOCompletion* completion) final
     {
-        IoUring.AsyncIO(
-            IORING_OP_READV,
-            file,
-            buffers.data(),
-            buffers.size(),
-            offset,
-            completion);
+        Context.AsyncReadV(file, buffers, offset, completion);
     }
 
     void AsyncWrite(
@@ -395,13 +78,7 @@ struct TIoUringService final
         TArrayRef<const char> buffer,
         TFileIOCompletion* completion) final
     {
-        IoUring.AsyncIO(
-            IORING_OP_WRITE,
-            file,
-            buffer.data(),
-            buffer.size(),
-            offset,
-            completion);
+        Context.AsyncWrite(file, buffer, offset, completion);
     }
 
     void AsyncWriteV(
@@ -410,13 +87,7 @@ struct TIoUringService final
         const TVector<TArrayRef<const char>>& buffers,
         TFileIOCompletion* completion) final
     {
-        IoUring.AsyncIO(
-            IORING_OP_WRITEV,
-            file,
-            buffers.data(),
-            buffers.size(),
-            offset,
-            completion);
+        Context.AsyncWriteV(file, buffers, offset, completion);
     }
 };
 
@@ -435,7 +106,7 @@ struct TIoUringServiceNull final
     {
         Y_UNUSED(file, offset, buffer);
 
-        IoUring.AsyncNOP(completion);
+        Context.AsyncNOP(completion);
     }
 
     void AsyncReadV(
@@ -446,7 +117,7 @@ struct TIoUringServiceNull final
     {
         Y_UNUSED(file, offset, buffers);
 
-        IoUring.AsyncNOP(completion);
+        Context.AsyncNOP(completion);
     }
 
     void AsyncWrite(
@@ -457,7 +128,7 @@ struct TIoUringServiceNull final
     {
         Y_UNUSED(file, offset, buffer);
 
-        IoUring.AsyncNOP(completion);
+        Context.AsyncNOP(completion);
     }
 
     void AsyncWriteV(
@@ -468,7 +139,7 @@ struct TIoUringServiceNull final
     {
         Y_UNUSED(file, offset, buffers);
 
-        IoUring.AsyncNOP(completion);
+        Context.AsyncNOP(completion);
     }
 };
 
@@ -481,7 +152,7 @@ class TIoUringServiceFactory final
 private:
     const TIoUringServiceParams Params;
 
-    std::shared_ptr<TIoUring> WqOwner;
+    std::shared_ptr<TContext> WqOwner;
     ui32 Index = 0;
 
 public:
@@ -493,17 +164,20 @@ public:
     {
         const ui32 index = Index++;
 
-        TIoUringServiceParams params = Params;
-        params.SubmissionThreadName = TStringBuilder()
-                                      << Params.SubmissionThreadName << index;
-        params.CompletionThreadName = TStringBuilder()
-                                      << Params.CompletionThreadName << index;
+        TContext::TParams params{
+            .SubmissionThreadName = TStringBuilder()
+                                    << Params.SubmissionThreadName << index,
+            .CompletionThreadName = TStringBuilder()
+                                    << Params.CompletionThreadName << index,
+            .SubmissionQueueEntries = Params.SubmissionQueueEntries,
+            .MaxKernelWorkersCount = Params.MaxKernelWorkersCount,
+            .WqOwner = WqOwner.get(),
+        };
 
-        auto service =
-            std::make_shared<TService>(std::move(params), WqOwner.get());
+        auto service = std::make_shared<TService>(std::move(params));
 
         if (Params.ShareKernelWorkers && !WqOwner) {
-            WqOwner = std::shared_ptr<TIoUring>(service, &service->IoUring);
+            WqOwner = std::shared_ptr<TContext>(service, &service->Context);
         }
 
         return service;
