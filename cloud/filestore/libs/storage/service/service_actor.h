001bd9b4a51970a3bc9a752dfb685fc2f6b39ae4
ruslan-gar <ruslan.g@nebius.com>
2025-07-29 19:26:37 +0200

    issue-3777: Action actor that sets HasXAttrs (#4014)
    * An action actor that sets HasXAttrs flag is added
    
    * comments addressed

--- cloud/filestore/libs/storage/service/service_actor.h
+++ cloud/filestore/libs/storage/service/service_actor.h
@@ -255,6 +255,10 @@ private:
         TRequestInfoPtr requestInfo,
         TString input);
 
+    NActors::IActorPtr CreateSetHasXAttrsActionActor(
+        TRequestInfoPtr requestInfo,
+        TString input);
+
 private:
     void RenderSessions(IOutputStream& out);
     void RenderLocalFileStores(IOutputStream& out);
