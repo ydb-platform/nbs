c26c2be1621151d7d7e3af1d6976cf3937528b0a
Daniil Komarevtsev <komarevtsev@yandex-team.ru>
2025-08-06 10:19:54 +0000

    Device handler factory refactoring
    WIP

--- cloud/blockstore/libs/service/aligned_device_handler.cpp
+++ cloud/blockstore/libs/service/aligned_device_handler.cpp
@@ -92,24 +92,21 @@ NProto::TError TryToNormalize(
 ////////////////////////////////////////////////////////////////////////////////
 
 TAlignedDeviceHandler::TAlignedDeviceHandler(
-        IStoragePtr storage,
-        TString diskId,
-        TString clientId,
-        ui32 blockSize,
-        ui32 maxSubRequestSize,
-        ui32 maxZeroBlocksSubRequestSize,
-        bool checkBufferModificationDuringWriting,
-        NProto::EStorageMediaKind storageMediaKind)
+        TDeviceHandlerParams params,
+        ui32 maxSubRequestSize)
     : Storage(
-          checkBufferModificationDuringWriting
-              ? CreateChecksumStorageWrapper(std::move(storage), diskId)
-              : std::move(storage))
-    , DiskId(std::move(diskId))
-    , ClientId(std::move(clientId))
-    , BlockSize(blockSize)
+          params.CheckBufferModificationDuringWriting
+              ? CreateChecksumStorageWrapper(
+                    std::move(params.Storage),
+                    params.DiskId)
+              : std::move(params.Storage))
+    , DiskId(std::move(params.DiskId))
+    , ClientId(std::move(params.ClientId))
+    , BlockSize(params.BlockSize)
     , MaxBlockCount(maxSubRequestSize / BlockSize)
-    , MaxBlockCountForZeroBlocksRequest(maxZeroBlocksSubRequestSize / BlockSize)
-    , StorageMediaKind(storageMediaKind)
+    , MaxBlockCountForZeroBlocksRequest(
+          params.MaxZeroBlocksSubRequestSize / BlockSize)
+    , StorageMediaKind(params.StorageMediaKind)
 {
     Y_ABORT_UNLESS(MaxBlockCount > 0);
     Y_ABORT_UNLESS(MaxBlockCountForZeroBlocksRequest > 0);
@@ -483,6 +480,11 @@ void TAlignedDeviceHandler::ReportCriticalError(
     } else {
         ReportErrorWasSentToTheGuestForNonReliableDisk(message);
     }
+}
+
+ui32 TAlignedDeviceHandler::GetBlockSize() const
+{
+    return BlockSize;
 }
 
 }   // namespace NCloud::NBlockStore
