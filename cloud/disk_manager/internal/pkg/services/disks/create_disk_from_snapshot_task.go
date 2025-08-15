44450c358cf232e6d3e12695698f448aa2a6a8e7
Rattysed <46933547+Rattysed@users.noreply.github.com>
2025-08-15 14:40:39 +0000

    add comment

--- cloud/disk_manager/internal/pkg/services/disks/create_disk_from_snapshot_task.go
+++ cloud/disk_manager/internal/pkg/services/disks/create_disk_from_snapshot_task.go
@@ -89,7 +89,7 @@ func (t *createDiskFromSnapshotTask) Run(
 	}
 
 	// If the disk has already been added to the database,
-	// idempotently retrieve the correct zone where it should be created,
+	// idempotently retrieve the correct zone where it was created,
 	// because cellSelector is not idempotent.
 	params.Disk.ZoneId = diskMeta.ZoneID
 	client, err := t.nbsFactory.GetClient(ctx, params.Disk.ZoneId)
