277df11470138a4914a81b69fdf6bd1b31e8da38
Anton Myagkov <antomyagkov@gmail.com>
2025-08-18 20:20:56 +0200

    add test

--- cloud/filestore/tests/client_two_stage_read/test.py
+++ cloud/filestore/tests/client_two_stage_read/test.py
@@ -32,3 +32,24 @@ def test_unaligned_read():
 
     ret = common.canonical_file(results_path, local=True)
     return ret
+
+
+def test_zero_range_read():
+    data_file = os.path.join(common.output_path(), "data.txt")
+    with open(data_file, "w") as f:
+        f.write('a' * 10)
+        f.write('b' * 10)
+
+    client, results_path = __init_test()
+    client.create("fs0", "test_cloud", "test_folder")
+    client.write("fs0", "/aaa", "--data", data_file, "--offset", "10")
+    client.write("fs0", "/aaa", "--data", data_file, "--offset", "40")
+    result = client.read("fs0", "/aaa", "--length", "120").decode("utf8")
+
+    client.destroy("fs0")
+
+    with open(results_path, "w") as results_file:
+        results_file.write(result + '\n')
+
+    ret = common.canonical_file(results_path, local=True)
+    return ret
