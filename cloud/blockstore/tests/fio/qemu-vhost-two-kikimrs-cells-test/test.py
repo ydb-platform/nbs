62d982b1db91bd471f7603944326951937982d46
boykovrtm <madpunk@yandex-team.ru>
2025-08-22 10:04:20 +0000

    cells test wip

---
+++ cloud/blockstore/tests/fio/qemu-vhost-two-kikimrs-cells-test/test.py
@@ -0,0 +1,32 @@
+import pytest
+import os
+import logging
+
+import cloud.storage.core.tools.testing.fio.lib as fio
+
+from cloud.blockstore.tests.python.lib.test_base import get_nbs_device_path_by_index, get_file_size
+
+
+KB = 1024
+MB = 1024*KB
+
+logger = logging.getLogger(__name__)
+
+DEVICE_SIZE = 128*MB
+TESTS = fio.generate_tests(size=DEVICE_SIZE, duration=60)
+nbs_instances_count = int(os.getenv("CLUSTERS_COUNT"))
+
+
+@pytest.mark.parametrize("name", TESTS.keys())
+def test_fio(name):
+    logger.info("instans index :{}".format(1))
+    path = get_nbs_device_path_by_index(1)
+
+    device_size = get_file_size(path)
+    if DEVICE_SIZE != device_size:
+        raise RuntimeError("Invalid device size (expected: {}, actual: {})".format(
+            DEVICE_SIZE,
+            device_size))
+
+    out = fio.run_test(path, TESTS[name])
+    assert "errors: 0" == out
--- cloud/blockstore/tests/fio/ya.make
+++ cloud/blockstore/tests/fio/ya.make
@@ -2,4 +2,6 @@ RECURSE_FOR_TESTS(
    qemu-vhost-kikimr-test
    qemu-vhost-local-test
    qemu-vhost-null-test
+   qemu-vhost-two-kikimrs-test
+   qemu-vhost-two-kikimrs-cells-test
 )
