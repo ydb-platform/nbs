00a8ff508be9ae0589dc6a2d6905cb0c32d72fe3
Taras Pashkin <tpashkin@nebius.com>
2025-09-03 17:43:38 +0300

    remove bogus declaration

--- cloud/blockstore/libs/server/config.h
+++ cloud/blockstore/libs/server/config.h
@@ -132,7 +132,6 @@ public:
     bool GetNbdNetlink() const;
     TDuration GetNbdRequestTimeout() const;
     TDuration GetNbdConnectionTimeout() const;
-    TString GetSocketPath() const;
     bool GetAllowAllRequestsViaUDS() const;
     bool GetEndpointStorageNotImplementedErrorIsFatal() const;
     TDuration GetVhostServerTimeoutAfterParentExit() const;
