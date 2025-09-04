9759e4ef12460b0e6488fd6f42adf653a62dfc94
Aleksey Skvortsov <46933547+Rattysed@users.noreply.github.com>
2025-09-04 20:41:01 +0300

    Revert "[Disk Manager] Set disk status deleted for non-existing disks on stor…"
    This reverts commit c501739838f5105fb10285be87a51d960d3d0998.

--- cloud/disk_manager/internal/pkg/resources/disks.go
+++ cloud/disk_manager/internal/pkg/resources/disks.go
@@ -523,24 +523,12 @@ func (s *storageYDB) deleteDisk(
 				return nil, err
 			}
 
-			// Should be idempotent.
-			if state.status == diskStatusDeleted {
-				return nil, nil
-			}
-
 			return state.toDiskMeta(), nil
 		}
-
-		state.status = diskStatusDeleting
-	} else {
-		// Create a tombstone disk entry to avoid race conditions between
-		// concurrent disk deletion and disk creation operations. The tombstone
-		// prevents new disk creation with the same ID.
-		state.status = diskStatusDeleted
-		state.deletedAt = deletingAt
 	}
 
 	state.id = diskID
+	state.status = diskStatusDeleting
 	state.deleteTaskID = taskID
 	state.deletingAt = deletingAt
 
@@ -552,11 +540,6 @@ func (s *storageYDB) deleteDisk(
 	err = tx.Commit(ctx)
 	if err != nil {
 		return nil, err
-	}
-
-	// Should be idempotent.
-	if state.status == diskStatusDeleted {
-		return nil, nil
 	}
 
 	return state.toDiskMeta(), nil
--- cloud/disk_manager/internal/pkg/services/disks/delete_disk_task.go
+++ cloud/disk_manager/internal/pkg/services/disks/delete_disk_task.go
@@ -65,10 +65,19 @@ func (t *deleteDiskTask) deleteDisk(
 	}
 
 	if diskMeta == nil {
-		return nil
+		return errors.NewNonCancellableErrorf(
+			"id %v is not accepted",
+			diskID,
+		)
 	}
 
 	zoneID := diskMeta.ZoneID
+	if len(zoneID) == 0 {
+		zoneID = t.request.Disk.ZoneId
+	}
+	if len(zoneID) == 0 {
+		return t.storage.DiskDeleted(ctx, diskID, time.Now())
+	}
 
 	taskID, err := t.scheduler.ScheduleTask(
 		headers.SetIncomingIdempotencyKey(
@@ -108,7 +117,7 @@ func (t *deleteDiskTask) deleteDisk(
 	}
 
 	// Only overlay disks (created from image) should be released.
-	if diskMeta != nil && len(diskMeta.SrcImageID) != 0 {
+	if len(diskMeta.SrcImageID) != 0 {
 		taskID, err = t.poolService.ReleaseBaseDisk(
 			headers.SetIncomingIdempotencyKey(
 				ctx,
