ydb-platform/nbs

commit 0266f2d256d48ec76080f9917e16fdfad2044bd8

[Filestore] fix double-free while stopping filestore-vhost when 'Forget' request is in-flight (#4283)

cloud/filestore/libs/vfs_fuse/vhost/fuse_virtio.c
@@ -174,11 +174,6 @@ static bool is_ignored_request(struct fuse_in_header* in)
     return in->opcode == FUSE_INTERRUPT;
 }
 
-static bool is_oneway_request(struct fuse_in_header* in)
-{
-    return in->opcode == FUSE_FORGET || in->opcode == FUSE_BATCH_FORGET;
-}
-
 struct iov_iter {
     struct iovec *iov;
     size_t count;
@@ -285,16 +280,6 @@ static int process_request(struct fuse_session* se, struct vhd_io* io)
         vhd_free(pbufv);
     }
 
-    /*
-     * These requests don't assume response and thus don't call
-     * virtio_send_msg.  Be sure to complete and free the request and recycle
-     * the virtio descriptors.
-     * FIXME: wire completion into fuse_reply_none
-     */
-    if (is_oneway_request(&in_hdr)) {
-        complete_request(req, 0);
-    }
-
     return 0;
 }
 
@@ -382,6 +367,15 @@ int fuse_cancel_request(
             break;
     }
     return 0;
+}
+
+void fuse_reply_none(fuse_req_t req)
+{
+    struct fuse_chan* ch = req->ch;
+    struct fuse_virtio_request* vhd_req = VIRTIO_REQ_FROM_CHAN(ch);
+    complete_request(vhd_req, 0);
+
+    fuse_free_req(req);
 }
 
 int virtio_session_mount(struct fuse_session* se)

cloud/filestore/libs/vfs_fuse/fs_ut.cpp
@@ -2354,6 +2354,24 @@ Y_UNIT_TEST_SUITE(TFileSystemTest)
 
         UNIT_ASSERT_VALUES_EQUAL(1, static_cast<int>(*writeBackCacheError));
     }
+
+    Y_UNIT_TEST(ShouldNotCrashWhileStoppingWhenForgetRequestIsInFlight)
+    {
+        auto scheduler = std::make_shared<TTestScheduler>();
+        TBootstrap bootstrap(CreateWallClockTimer(), scheduler);
+
+        bootstrap.Start();
+
+        const ui64 nodeId = 123;
+        const ui64 refCount = 10;
+
+        auto future = bootstrap.Fuse->SendRequest<TForgetRequest>(nodeId, refCount);
+        bootstrap.Stop();
+
+        scheduler->RunAllScheduledTasks();
+
+        UNIT_ASSERT_EXCEPTION(future.GetValue(WaitTimeout), yexception);
+    }
 }
 
 }   // namespace NCloud::NFileStore::NFuse

commit 82f1b1fb70702085fec0e19ded3a40b9630e51dc

[Filestore] fix linkage error after #4283 (#4313)
Error was introduced in #4283.
When building with `-D FORCE_STATIC_LINKING=yes` we get:
```
ld.lld: error: duplicate symbol: fuse_reply_none
>>> defined at fuse_lowlevel.c:304 (/home/svartmetal/nbs-github2/contrib/libs/virtiofsd/fuse_lowlevel.c:304)
>>>            fuse_lowlevel.c.o:(fuse_reply_none) in archive contrib/libs/virtiofsd/static/libvirtiofsd.a
>>> defined at fuse_virtio.c:373 (/home/svartmetal/nbs-github2/cloud/filestore/libs/vfs_fuse/vhost/fuse_virtio.c:373)
>>>            fuse_virtio.c.o:(.text.fuse_reply_none+0x0) in archive cloud/filestore/libs/vfs_fuse/vhost/liblibs-vfs_fuse-vhost.a
clang-16: error: linker command failed with exit code 1 (use -v to see invocation)
```

cloud/filestore/libs/vfs_fuse/fs.cpp
@@ -21,7 +21,7 @@ int ReplyNone(
     requestStats.ResponseSent(callContext);
     FILESTORE_TRACK(ResponseSent, (&callContext), "None");
 
-    fuse_reply_none(req);
+    fuse_reply_none_override(req);
 
     requestStats.RequestCompleted(log, callContext, error);
 

cloud/filestore/libs/vfs_fuse/fuse.cpp
@@ -16,3 +16,8 @@ int fuse_cancel_request(
     (void)code;
     return fuse_reply_err(req, EINTR);
 }
+
+void fuse_reply_none_override(fuse_req_t req)
+{
+    fuse_reply_none(req);
+}

cloud/filestore/libs/vfs_fuse/fuse.h
@@ -37,6 +37,12 @@ int fuse_cancel_request(
     fuse_req_t req,
     enum fuse_cancelation_code code);
 
+// 'overrides' fuse_reply_none, needed for VIRTIO-specific request completion
+// handling.
+// See https://github.com/ydb-platform/nbs/pull/4283
+// and https://github.com/ydb-platform/nbs/pull/4313
+void fuse_reply_none_override(fuse_req_t req);
+
 #if defined(__cplusplus)
 }   // extern "C"
 #endif

cloud/filestore/libs/vfs_fuse/vhost/fuse_virtio.c
@@ -369,13 +369,19 @@ int fuse_cancel_request(
     return 0;
 }
 
-void fuse_reply_none(fuse_req_t req)
+// 'overrides' fuse_reply_none, needed for VIRTIO-specific request completion
+// handling.
+// See https://github.com/ydb-platform/nbs/pull/4283
+// and https://github.com/ydb-platform/nbs/pull/4313
+void fuse_reply_none_override(fuse_req_t req)
 {
+    // complete attached fuse virtio request
     struct fuse_chan* ch = req->ch;
     struct fuse_virtio_request* vhd_req = VIRTIO_REQ_FROM_CHAN(ch);
     complete_request(vhd_req, 0);
 
-    fuse_free_req(req);
+    // calling 'base' implementation
+    fuse_reply_none(req);
 }
 
 int virtio_session_mount(struct fuse_session* se)
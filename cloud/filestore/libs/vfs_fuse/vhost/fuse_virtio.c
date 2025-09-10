82f1b1fb70702085fec0e19ded3a40b9630e51dc
Mikhail Montsev <svartmetal@nebius.com>
2025-09-10 15:04:26 +0100

    [Filestore] fix linkage error after #4283 (#4313)
    Error was introduced in #4283.
    When building with `-D FORCE_STATIC_LINKING=yes` we get:
    ```
    ld.lld: error: duplicate symbol: fuse_reply_none
    >>> defined at fuse_lowlevel.c:304 (/home/svartmetal/nbs-github2/contrib/libs/virtiofsd/fuse_lowlevel.c:304)
    >>>            fuse_lowlevel.c.o:(fuse_reply_none) in archive contrib/libs/virtiofsd/static/libvirtiofsd.a
    >>> defined at fuse_virtio.c:373 (/home/svartmetal/nbs-github2/cloud/filestore/libs/vfs_fuse/vhost/fuse_virtio.c:373)
    >>>            fuse_virtio.c.o:(.text.fuse_reply_none+0x0) in archive cloud/filestore/libs/vfs_fuse/vhost/liblibs-vfs_fuse-vhost.a
    clang-16: error: linker command failed with exit code 1 (use -v to see invocation)
    ```

--- cloud/filestore/libs/vfs_fuse/vhost/fuse_virtio.c
+++ cloud/filestore/libs/vfs_fuse/vhost/fuse_virtio.c
@@ -369,13 +369,19 @@ int fuse_cancel_request(
     return 0;
 }
 
-void fuse_reply_none(fuse_req_t req)
+// 'overrides' fuse_reply_none, needed for VIRTIO-specific request completion
+// handling.
+// See https://github.com/ydb-platform/nbs/pull/4283
+// and https://github.com/ydb-platform/nbs/pull/4313
+void fuse_reply_none_override(fuse_req_t req)
 {
+    // complete attached fuse virtio request
     struct fuse_chan* ch = req->ch;
     struct fuse_virtio_request* vhd_req = VIRTIO_REQ_FROM_CHAN(ch);
     complete_request(vhd_req, 0);
 
-    fuse_free_req(req);
+    // calling 'base' implementation
+    fuse_reply_none(req);
 }
 
 int virtio_session_mount(struct fuse_session* se)
