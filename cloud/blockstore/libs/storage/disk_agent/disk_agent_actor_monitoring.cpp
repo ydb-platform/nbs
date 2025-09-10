28c579013cf71118a5d9104a94689d03749ac3e6
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-09-10 05:36:28 +0000

    fix small issues

--- cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_monitoring.cpp
+++ cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_monitoring.cpp
@@ -1,17 +1,13 @@
 #include "disk_agent_actor.h"
 
 #include <cloud/blockstore/libs/rdma/iface/server.h>
-#include <cloud/blockstore/libs/storage/core/monitoring_utils.h>
 #include <cloud/blockstore/libs/storage/disk_common/monitoring_utils.h>
 
 #include <cloud/storage/core/libs/common/format.h>
 
 #include <library/cpp/monlib/service/pages/templates.h>
-#include <library/cpp/protobuf/json/proto2json.h>
 
 #include <util/stream/str.h>
-
-#include <utility>
 
 namespace NCloud::NBlockStore::NStorage {
 
@@ -148,16 +144,6 @@ void TDiskAgentActor::RenderDevices(IOutputStream& out) const
                                 }
                             }
                         }
-                    }
-
-                    TABLED() {
-                        out << Sprintf(
-                            R"(<form name="closeDevice" method="POST">
-                                <input type='hidden' name='action' value='closeDevice'/>
-                                <input type='hidden' name='deviceName' value='%s'/>
-                                <button type="submit">Close device</button>
-                            </form>)",
-                            config.GetDeviceName().c_str());
                     }
 
                     TABLED () {
