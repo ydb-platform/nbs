982938a6c2c6cc04fb11605406ce774ab8ce9d3b
Pavel Misko <misko.pawel@gmail.com>
2025-09-17 13:04:33 +0200

    cleanup configs

--- cloud/blockstore/libs/logbroker/iface/config.cpp
+++ cloud/blockstore/libs/logbroker/iface/config.cpp
@@ -9,50 +9,40 @@ namespace {
 ////////////////////////////////////////////////////////////////////////////////
 
 #define BLOCKSTORE_LOGBROKER_CONFIG(xxx)                                       \
-    xxx(Address,                  TString,      ""                            )\
-    xxx(Port,                     ui32,         2135                          )\
-    xxx(Database,                 TString,      ""                            )\
-    xxx(UseLogbrokerCDS,          bool,         false                         )\
-    xxx(CaCertFilename,           TString,      ""                            )\
-    xxx(Topic,                    TString,      ""                            )\
-    xxx(SourceId,                 TString,      ""                            )\
-    xxx(MetadataServerAddress,    TString,      ""                            )\
-    xxx(Protocol,                                                              \
+    xxx(LogbrokerConfig, Address,                  TString,      ""           )\
+    xxx(LogbrokerConfig, Port,                     ui32,         2135         )\
+    xxx(LogbrokerConfig, Database,                 TString,      ""           )\
+    xxx(LogbrokerConfig, UseLogbrokerCDS,          bool,         false        )\
+    xxx(LogbrokerConfig, CaCertFilename,           TString,      ""           )\
+    xxx(LogbrokerConfig, Topic,                    TString,      ""           )\
+    xxx(LogbrokerConfig, SourceId,                 TString,      ""           )\
+    xxx(LogbrokerConfig, MetadataServerAddress,    TString,      ""           )\
+    xxx(LogbrokerConfig, Protocol,                                             \
         NProto::TLogbrokerConfig::EProtocol,                                   \
         NProto::TLogbrokerConfig::PROTOCOL_UNSPECIFIED                        )\
 // BLOCKSTORE_LOGBROKER_CONFIG
 
-#define BLOCKSTORE_DECLARE_CONFIG(name, type, value)                           \
-    Y_DECLARE_UNUSED static const type Default##name = value;                  \
-// BLOCKSTORE_DECLARE_CONFIG
-
-BLOCKSTORE_LOGBROKER_CONFIG(BLOCKSTORE_DECLARE_CONFIG)
-
-#undef BLOCKSTORE_DECLARE_CONFIG
-
 ////////////////////////////////////////////////////////////////////////////////
 
 #define BLOCKSTORE_LOGBROKER_IAM_JWT_FILE_CONFIG(xxx)                          \
-    xxx(IamEndpoint,                     TString,          ""                 )\
-    xxx(JwtFilename,                     TString,          ""                 )\
+    xxx(IamJwtFile, IamEndpoint,                     TString,          ""     )\
+    xxx(IamJwtFile, JwtFilename,                     TString,          ""     )\
 // BLOCKSTORE_LOGBROKER_IAM_JWT_FILE_CONFIG
 
-#define  BLOCKSTORE_DECLARE_CONFIG(name, type, value)                          \
-    Y_DECLARE_UNUSED static const type DefaultIamJwtFile##name = value;        \
-// BLOCKSTORE_DECLARE_CONFIG
-
-BLOCKSTORE_LOGBROKER_IAM_JWT_FILE_CONFIG(BLOCKSTORE_DECLARE_CONFIG)
-
-#undef BLOCKSTORE_DECLARE_CONFIG
+////////////////////////////////////////////////////////////////////////////////
 
 #define BLOCKSTORE_LOGBROKER_IAM_METADATA_SERVER_CONFIG(xxx)                   \
-    xxx(Endpoint,                        TString,          ""                 )\
+    xxx(IamMetadataServer, Endpoint,                 TString,          ""     )\
 // BLOCKSTORE_LOGBROKER_IAM_METADATA_SERVER_CONFIG
 
-#define  BLOCKSTORE_DECLARE_CONFIG(name, type, value)                          \
-    Y_DECLARE_UNUSED static const type DefaultIamMetadataServer##name = value; \
+////////////////////////////////////////////////////////////////////////////////
+
+#define BLOCKSTORE_DECLARE_CONFIG(ns, name, type, value)                       \
+    Y_DECLARE_UNUSED static const type Default##ns##name = value;              \
 // BLOCKSTORE_DECLARE_CONFIG
 
+BLOCKSTORE_LOGBROKER_CONFIG(BLOCKSTORE_DECLARE_CONFIG)
+BLOCKSTORE_LOGBROKER_IAM_JWT_FILE_CONFIG(BLOCKSTORE_DECLARE_CONFIG)
 BLOCKSTORE_LOGBROKER_IAM_METADATA_SERVER_CONFIG(BLOCKSTORE_DECLARE_CONFIG)
 
 #undef BLOCKSTORE_DECLARE_CONFIG
@@ -69,26 +59,19 @@ IOutputStream& operator <<(
     IOutputStream& out,
     NProto::TLogbrokerConfig::EProtocol pt)
 {
-    switch (pt) {
-        case NProto::TLogbrokerConfig::PROTOCOL_UNSPECIFIED:
-            return out << "PROTOCOL_UNSPECIFIED";
-        case NProto::TLogbrokerConfig::PROTOCOL_PQ0:
-            return out << "PROTOCOL_PQ0";
-        case NProto::TLogbrokerConfig::PROTOCOL_TOPIC_API:
-            return out << "PROTOCOL_TOPIC_API";
-        default:
-            return out
-                << "(Unknown TLogbrokerConfig::EProtocol value "
-                << static_cast<int>(pt)
-                << ")";
+    const TString& s = NProto::TLogbrokerConfig_EProtocol_Name(pt);
+    if (s) {
+        return out << s;
     }
+    return out << "(Unknown TLogbrokerConfig::EProtocol value "
+               << static_cast<int>(pt) << ")";
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#define CONFIG_ITEM_IS_SET_CHECKER(name, ...)                                  \
+#define CONFIG_ITEM_IS_SET_CHECKER(ns, name, ...)                              \
     template <typename TProto>                                                 \
-    [[nodiscard]] bool Is##name##Set(const TProto& proto)                      \
+    [[nodiscard]] bool Is##ns##name##Set(const TProto& proto)                  \
     {                                                                          \
         if constexpr (requires() { proto.name##Size(); }) {                    \
             return proto.name##Size() > 0;                                     \
@@ -103,8 +86,8 @@ BLOCKSTORE_LOGBROKER_IAM_METADATA_SERVER_CONFIG(CONFIG_ITEM_IS_SET_CHECKER);
 
 #undef CONFIG_ITEM_IS_SET_CHECKER
 
-#define BLOCKSTORE_CONFIG_GET_CONFIG_VALUE(config, name, type, value) \
-    (Is##name##Set(config) ? ConvertValue<type>(config.Get##name()) : value)
+#define BLOCKSTORE_CONFIG_GET_CONFIG_VALUE(ns, config, name, type, value)      \
+    (Is##ns##name##Set(config) ? ConvertValue<type>(config.Get##name()) : value)
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -137,24 +120,11 @@ TAuthConfig TLogbrokerConfig::GetAuthConfig() const
     }
 }
 
-#define BLOCKSTORE_CONFIG_GETTER(name, type, ...)                              \
-type TLogbrokerConfig::Get##name() const                                       \
-{                                                                              \
-    return BLOCKSTORE_CONFIG_GET_CONFIG_VALUE(                                 \
-        Config,                                                                \
-        name,                                                                  \
-        type,                                                                  \
-        Default##name);                                                        \
-}                                                                              \
-// BLOCKSTORE_CONFIG_GETTER
-
-BLOCKSTORE_LOGBROKER_CONFIG(BLOCKSTORE_CONFIG_GETTER)
-
-#undef BLOCKSTORE_CONFIG_GETTER
+////////////////////////////////////////////////////////////////////////////////
 
 void TLogbrokerConfig::Dump(IOutputStream& out) const
 {
-#define BLOCKSTORE_CONFIG_DUMP(name, ...)                                      \
+#define BLOCKSTORE_CONFIG_DUMP(ns, name, ...)                                  \
     out << #name << ": " << Get##name() << Endl;                               \
 // BLOCKSTORE_CONFIG_DUMP
     BLOCKSTORE_LOGBROKER_CONFIG(BLOCKSTORE_CONFIG_DUMP);
@@ -163,7 +133,7 @@ void TLogbrokerConfig::Dump(IOutputStream& out) const
 
 void TLogbrokerConfig::DumpHtml(IOutputStream& out) const
 {
-#define BLOCKSTORE_CONFIG_DUMP(name, ...)                                      \
+#define BLOCKSTORE_CONFIG_DUMP(ns, name, ...)                                  \
     TABLER() {                                                                 \
         TABLED() { out << #name; }                                             \
         TABLED() { out << Get##name(); }                                       \
@@ -181,34 +151,20 @@ void TLogbrokerConfig::DumpHtml(IOutputStream& out) const
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#define BLOCKSTORE_CONFIG_GETTER(name, type, ...)                              \
-type TIamJwtFile::Get##name() const                                            \
+#define BLOCKSTORE_CONFIG_GETTER(ns, name, type, ...)                          \
+type T##ns::Get##name() const                                                  \
 {                                                                              \
     return BLOCKSTORE_CONFIG_GET_CONFIG_VALUE(                                 \
+        ns,                                                                    \
         Config,                                                                \
         name,                                                                  \
         type,                                                                  \
-        DefaultIamJwtFile##name);                                              \
+        Default##ns##name);                                                    \
 }                                                                              \
 // BLOCKSTORE_CONFIG_GETTER
 
+BLOCKSTORE_LOGBROKER_CONFIG(BLOCKSTORE_CONFIG_GETTER)
 BLOCKSTORE_LOGBROKER_IAM_JWT_FILE_CONFIG(BLOCKSTORE_CONFIG_GETTER)
-
-#undef BLOCKSTORE_CONFIG_GETTER
-
-////////////////////////////////////////////////////////////////////////////////
-
-#define BLOCKSTORE_CONFIG_GETTER(name, type, ...)                              \
-type TIamMetadataServer::Get##name() const                                     \
-{                                                                              \
-    return BLOCKSTORE_CONFIG_GET_CONFIG_VALUE(                                 \
-        Config,                                                                \
-        name,                                                                  \
-        type,                                                                  \
-        DefaultIamMetadataServer##name);                                       \
-}                                                                              \
-// BLOCKSTORE_CONFIG_GETTER
-
 BLOCKSTORE_LOGBROKER_IAM_METADATA_SERVER_CONFIG(BLOCKSTORE_CONFIG_GETTER)
 
 #undef BLOCKSTORE_CONFIG_GETTER
