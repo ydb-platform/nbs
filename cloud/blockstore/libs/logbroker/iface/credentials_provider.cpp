f4f1365b3e880a0391bfc7d0c558caac1c891409
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2025-09-19 14:34:05 +0300

    Make code simpler to debug compilation errors. Remove templates. (#4383)
    было https://pastebin.com/2jk326Df
    стало https://pastebin.com/guNtyZPQ

--- cloud/blockstore/libs/logbroker/iface/credentials_provider.cpp
+++ cloud/blockstore/libs/logbroker/iface/credentials_provider.cpp
@@ -4,7 +4,6 @@
 
 #include <contrib/ydb/public/sdk/cpp/client/iam/common/iam.h>
 
-#include <util/generic/overloaded.h>
 #include <util/string/cast.h>
 
 namespace NCloud::NBlockStore::NLogbroker {
@@ -14,28 +13,31 @@ namespace NCloud::NBlockStore::NLogbroker {
 std::shared_ptr<NYdbICredentialsProviderFactory>
 CreateCredentialsProviderFactory(const TLogbrokerConfig& config)
 {
-    return std::visit(TOverloaded(
-        [] (const TIamJwtFile& jwt) {
-            NYdb::TIamJwtFilename iamJwtFilename;
-            iamJwtFilename.Endpoint = jwt.GetIamEndpoint();
-            iamJwtFilename.JwtFilename = jwt.GetJwtFilename();
+    auto authConfig = config.GetAuthConfig();
+    static_assert(std::variant_size_v<decltype(authConfig)> == 3);
 
-            return NYdb::CreateIamJwtFileCredentialsProviderFactory(
-                iamJwtFilename);
+    if (const auto* jwt = std::get_if<TIamJwtFile>(&authConfig)) {
+        NYdb::TIamJwtFilename iamJwtFilename;
+        iamJwtFilename.Endpoint = jwt->GetIamEndpoint();
+        iamJwtFilename.JwtFilename = jwt->GetJwtFilename();
 
-        }, [] (const TIamMetadataServer& server) {
-            TStringBuf hostRef;
-            TStringBuf portRef;
-            TStringBuf{server.GetEndpoint()}.Split(':', hostRef, portRef);
+        return NYdb::CreateIamJwtFileCredentialsProviderFactory(iamJwtFilename);
+    }
 
-            return NYdb::CreateIamCredentialsProviderFactory({
-                .Host = TString(hostRef),
-                .Port = FromString<ui32>(portRef),
-            });
+    if (const auto* server = std::get_if<TIamMetadataServer>(&authConfig)) {
+        TStringBuf hostRef;
+        TStringBuf portRef;
+        TStringBuf{server->GetEndpoint()}.Split(':', hostRef, portRef);
 
-        }, [] (std::monostate) {
-            return std::shared_ptr<NYdbICredentialsProviderFactory>();
-        }), config.GetAuthConfig());
+        return NYdb::CreateIamCredentialsProviderFactory({
+            .Host = TString(hostRef),
+            .Port = FromString<ui32>(portRef),
+        });
+    }
+
+    Y_ABORT_UNLESS(std::holds_alternative<std::monostate>(authConfig));
+
+    return std::shared_ptr<NYdbICredentialsProviderFactory>();
 }
 
 }   // namespace NCloud::NBlockStore::NLogbroker
