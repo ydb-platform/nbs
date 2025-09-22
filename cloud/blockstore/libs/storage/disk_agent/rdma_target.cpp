245216123fd0c885c66c8fe912590a85c5e9e5d3
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-09-22 16:48:37 +0000

    issue-4293: refactor

--- cloud/blockstore/libs/storage/disk_agent/rdma_target.cpp
+++ cloud/blockstore/libs/storage/disk_agent/rdma_target.cpp
@@ -249,7 +249,7 @@ public:
         token->RecentBlocksTracker.Reset();
     }
 
-    void OpenDevice(const TString& deviceUUID, TStorageAdapterPtr device)
+    void AttachDevice(const TString& deviceUUID, TStorageAdapterPtr device)
     {
         auto* d = Devices.FindPtr(deviceUUID);
         if (!d) {
@@ -259,7 +259,7 @@ public:
         d->Device = std::move(device);
     }
 
-    void CloseDevice(const TString& deviceUUID)
+    void DetachDevice(const TString& deviceUUID)
     {
         auto* d = Devices.FindPtr(deviceUUID);
         if (!d) {
@@ -1241,14 +1241,14 @@ public:
         Handler->DeviceSecureEraseFinish(deviceUUID, error);
     }
 
-    void OpenDevice(const TString& deviceUUID, TStorageAdapterPtr device) override
+    void AttachDevice(const TString& deviceUUID, TStorageAdapterPtr device) override
     {
-        Handler->OpenDevice(deviceUUID, device);
+        Handler->AttachDevice(deviceUUID, device);
     }
 
-    void CloseDevice(const TString& deviceUUID) override
+    void DetachDevice(const TString& deviceUUID) override
     {
-        Handler->CloseDevice(deviceUUID);
+        Handler->DetachDevice(deviceUUID);
     }
 };
 
