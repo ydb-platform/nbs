764802818bd3aef9fd30e6b9463434039fcd3189
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-09-23 13:53:47 +0000

    issue-4293: added logs and fix test

--- cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_ut.cpp
+++ cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_ut.cpp
@@ -22,7 +22,6 @@
 
 #include <util/folder/tempdir.h>
 
-#include <chrono>
 #include <filesystem>
 
 namespace NCloud::NBlockStore::NStorage {
@@ -216,7 +215,6 @@ public:
 
     void ApplyDrGenerationIfNeeded()
     {
-        auto was = DrGeneration;
         DrGeneration = Max(DrGeneration, GeneratedDrGeneration);
     }
 
@@ -7152,7 +7150,7 @@ Y_UNIT_TEST_SUITE(TDiskAgentTest)
         auto resp = diskAgent.RecvAttachPathResponse();
         UNIT_ASSERT_VALUES_EQUAL(E_FAIL, resp->GetError().GetCode());
         UNIT_ASSERT_VALUES_EQUAL(
-            "outdated device generation",
+            "outdated path generation",
             resp->GetError().GetMessage());
 
         UNIT_ASSERT_VALUES_EQUAL(0, FindProcessesWithOpenFile(filePath).size());
@@ -7162,7 +7160,7 @@ Y_UNIT_TEST_SUITE(TDiskAgentTest)
         resp = diskAgent.RecvAttachPathResponse();
         UNIT_ASSERT_VALUES_EQUAL(E_FAIL, resp->GetError().GetCode());
         UNIT_ASSERT_VALUES_EQUAL(
-            "outdated request",
+            "outdated disk registry generation",
             resp->GetError().GetMessage());
 
         UNIT_ASSERT_VALUES_EQUAL(0, FindProcessesWithOpenFile(filePath).size());
