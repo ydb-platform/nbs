fd597409263138982b8deb0621b04d1ff95d6c5a
Boris Barkov <barkovbg@gmail.com>
2025-09-30 15:05:48 +0000

    resolve issues

--- cloud/blockstore/tools/disaster_recovery/merge_tablet_boot_info_backups/options.cpp
+++ cloud/blockstore/tools/disaster_recovery/merge_tablet_boot_info_backups/options.cpp
@@ -2,19 +2,19 @@
 
 using namespace NLastGetopt;
 
-////////////////////////////////////////////////////////////////////////////////
-
 TOptions::TOptions(int argc, char** argv)
 {
     TOpts opts;
     opts.AddHelpOption();
 
+    // This is the path to the directory containing subdirectories with
+    // tablet boot info (hive) backups.
     opts.AddLongOption("src-root")
         .RequiredArgument("STR")
         .Required()
         .StoreResult(&SrcRoot);
 
-    opts.AddLongOption("output")
+    opts.AddLongOption("text-output")
         .RequiredArgument("STR")
         .StoreResult(&TextOutputPath);
 
