d2d45d4d18c7df5da796ba0a92b26b5882d30b68
Rattysed <46933547+Rattysed@users.noreply.github.com>
2025-10-01 15:15:54 +0000

    rename method

--- cloud/disk_manager/internal/pkg/cells/cells_test.go
+++ cloud/disk_manager/internal/pkg/cells/cells_test.go
@@ -142,7 +142,7 @@ func TestCellSelectorReturnsCorrectNBSClientIfConfigsIsNotSet(t *testing.T) {
 	require.Equal(t, otherZoneID, selectedCell)
 }
 
-func TestCellSelectorReturnsCorrectCellMaxFreeBytesPolicy(t *testing.T) {
+func TestCellSelectorReturnsCorrectCellWithMaxFreeBytesPolicy(t *testing.T) {
 	ctx := context.Background()
 	cellStorage := storage_mocks.NewStorageMock()
 
