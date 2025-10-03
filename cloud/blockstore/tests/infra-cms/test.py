859e326daf38137b7efb8c9240ab1ba098784f5b
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-10-03 10:11:22 +0000

    issue-4293: added integration tests

--- cloud/blockstore/tests/infra-cms/test.py
+++ cloud/blockstore/tests/infra-cms/test.py
@@ -12,7 +12,8 @@ from cloud.blockstore.config.server_pb2 import TServerConfig, TServerAppConfig, 
     TKikimrServiceConfig
 from cloud.blockstore.config.storage_pb2 import TStorageServiceConfig
 
-from cloud.blockstore.public.sdk.python.client import CreateClient
+from cloud.blockstore.tests.python.lib.test_client import CreateTestClient
+
 
 from cloud.blockstore.tests.python.lib.nbs_runner import LocalNbs
 from cloud.blockstore.tests.python.lib.test_base import thread_count, wait_for_nbs_server, \
@@ -55,6 +56,18 @@ class CMS:
         r = self.__nbs_client.cms_action(request)
         return r
 
+    def add_device(self, host, device):
+        logging.info(
+            '[CMS] Try to add device "{}" on agent"{}"'.format(device, host))
+
+        request = protos.TCmsActionRequest()
+        action = request.Actions.add()
+        action.Type = protos.TAction.ADD_DEVICE
+        action.Host = host
+        action.Device = device
+        r = self.__nbs_client.cms_action(request)
+        return r
+
     def remove_agent(self, host):
         logging.info('[CMS] Try to remove agent "{}"'.format(host))
 
@@ -86,13 +99,49 @@ class CMS:
         r = self.__nbs_client.cms_action(request)
         return r
 
+    def wait_for_no_paths_to_attach_detach(self, expected_paths_count=0, poll_interval=1):
+        while True:
+            bkp = self.__nbs_client.backup_disk_registry_state()
+            if len(bkp.get("PathsToAttachDetach", [])) == expected_paths_count:
+                break
+            time.sleep(poll_interval)
+
+
+def file_descriptors_count(path):
+    try:
+        out = subprocess.check_output(["lsof", path]).decode()
+        return len(out.split("\n")) - 2
+    except subprocess.CalledProcessError:
+        return 0
+
 
 class _TestCmsRemoveAgentNoUserDisks:
 
     def __init__(self, name):
         self.name = name
 
-    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd):
+    def run(self, storage, nbs, disk_agent, cms: CMS, devices, always_allocate_local_ssd, attach_detach_paths):
+
+        fds_count = []
+        for device in devices:
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count != 0
+            fds_count.append(fd_count)
+
+        if attach_detach_paths:
+            response = cms.remove_agent("localhost")
+
+            assert response.ActionResults[0].Result.Code == EResult.E_TRY_AGAIN.value
+            assert response.ActionResults[0].Timeout != 0
+
+            cms.wait_for_no_paths_to_attach_detach()
+
+        if attach_detach_paths:
+            for i in range(len(devices)):
+                device = devices[i]
+                fd_count = file_descriptors_count(device.path)
+                assert fd_count + 1 == fds_count[i]
+
         response = cms.remove_agent("localhost")
 
         assert response.ActionResults[0].Timeout == 0
@@ -129,7 +178,16 @@ class _TestCmsRemoveAgentNoUserDisks:
         else:
             nbs.wait_for_stats(AgentsInWarningState=1)
             assert nbs.get_stats("UnknownDevices") == 0
-            nbs.change_agent_state("localhost", 0)
+
+            response = cms.add_agent("localhost")
+            assert response.ActionResults[0].Timeout == 0
+            cms.wait_for_no_paths_to_attach_detach()
+            wait_for_secure_erase(nbs.mon_port)
+
+        for i in range(len(devices)):
+            device = devices[i]
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count == fds_count[i]
 
         nbs.create_volume("vol0")
         nbs.read_blocks("vol0", start_index=0, block_count=32)
@@ -142,8 +200,14 @@ class _TestCmsRemoveAgent:
     def __init__(self, name):
         self.name = name
 
-    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd):
+    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd, attach_detach_paths):
         nbs.create_volume("vol0")
+
+        fds_count = []
+        for device in devices:
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count != 0
+            fds_count.append(fd_count)
 
         response = cms.remove_agent("localhost")
 
@@ -158,6 +222,13 @@ class _TestCmsRemoveAgent:
         assert response.ActionResults[0].Timeout != 0
 
         nbs.destroy_volume("vol0", sync=True)
+
+        cms.wait_for_no_paths_to_attach_detach()
+
+        for i in range(len(devices)):
+            device = devices[i]
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count + 1 == fds_count[i]
 
         response = cms.remove_agent("localhost")
 
@@ -188,7 +259,16 @@ class _TestCmsRemoveAgent:
         else:
             nbs.wait_for_stats(AgentsInWarningState=1)
             assert nbs.get_stats("UnknownDevices") == 0
-            nbs.change_agent_state("localhost", 0)
+
+            response = cms.add_agent("localhost")
+            assert response.ActionResults[0].Timeout == 0
+            cms.wait_for_no_paths_to_attach_detach()
+            wait_for_secure_erase(nbs.mon_port)
+
+        for i in range(len(devices)):
+            device = devices[i]
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count == fds_count[i]
 
         nbs.create_volume("vol1")
         nbs.read_blocks("vol1", start_index=0, block_count=32)
@@ -201,10 +281,14 @@ class _TestCmsPurgeAgentNoUserDisks:
     def __init__(self, name):
         self.name = name
 
-    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd):
-        response = cms.remove_agent("localhost")
-        assert response.ActionResults[0].Result.Code == 0
-        assert response.ActionResults[0].Timeout == 0
+    def run(self, storage, nbs, disk_agent, cms: CMS, devices, always_allocate_local_ssd, attach_detach_paths):
+        nbs.change_agent_state("localhost", 1)
+
+        fds_count = []
+        for device in devices:
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count != 0
+            fds_count.append(fd_count)
 
         nbs.wait_for_stats(AgentsInWarningState=1)
 
@@ -214,6 +298,22 @@ class _TestCmsPurgeAgentNoUserDisks:
         nbs.create_volume("vol0", return_code=1)
         if always_allocate_local_ssd:
             nbs.destroy_volume("local0", sync=True)
+
+        if attach_detach_paths:
+            response = cms.remove_agent("localhost")
+            assert response.ActionResults[0].Result.Code == EResult.E_TRY_AGAIN.value
+            assert response.ActionResults[0].Timeout != 0
+
+            cms.wait_for_no_paths_to_attach_detach()
+
+            for i in range(len(devices)):
+                device = devices[i]
+                fd_count = file_descriptors_count(device.path)
+                assert fd_count + 1 == fds_count[i]
+
+        response = cms.remove_agent("localhost")
+        assert response.ActionResults[0].Result.Code == 0
+        assert response.ActionResults[0].Timeout == 0
 
         response = cms.purge_agent("localhost")
         nbs.wait_for_stats(UnknownDevices=4)
@@ -243,6 +343,11 @@ class _TestCmsPurgeAgentNoUserDisks:
         nbs.wait_for_stats(UnknownDevices=0)
         wait_for_secure_erase(nbs.mon_port, pool="local-ssd", kind="local")
 
+        for i in range(len(devices)):
+            device = devices[i]
+            fd_count = file_descriptors_count(device.path)
+            assert fd_count == fds_count[i]
+
         nbs.create_volume("local1", blocks_count=DEFAULT_BLOCK_COUNT_PER_DEVICE,
                           kind="ssd_local")
         nbs.create_volume("vol1")
@@ -256,7 +361,7 @@ class _TestCmsPurgeAgent:
     def __init__(self, name):
         self.name = name
 
-    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd):
+    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd, attach_detach_paths):
         nbs.create_volume("vol0")
         assert nbs.get_stats("AgentsInWarningState") == 0
 
@@ -302,10 +407,13 @@ class _TestCmsRemoveDevice:
     def __init__(self, name):
         self.name = name
 
-    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd):
+    def run(self, storage, nbs, disk_agent, cms: CMS, devices, always_allocate_local_ssd, attach_detach_paths):
         nbs.create_volume("vol0")
 
         device = devices[0]
+
+        fd_count = file_descriptors_count(device.path)
+        assert fd_count != 0
 
         response = cms.remove_device("localhost", device.path)
 
@@ -321,11 +429,22 @@ class _TestCmsRemoveDevice:
 
         nbs.change_device_state(device.uuid, 2)
 
+        if attach_detach_paths:
+            cms.wait_for_no_paths_to_attach_detach()
+
+            assert file_descriptors_count(device.path) + 1 == fd_count
+
         response = cms.remove_device("localhost", device.path)
 
         assert response.ActionResults[0].Timeout == 0
 
         nbs.change_device_state(device.uuid, 0)
+
+        if attach_detach_paths:
+            response = cms.add_device("localhost", device.path)
+            assert response.ActionResults[0].Timeout == 0
+            cms.wait_for_no_paths_to_attach_detach()
+            assert file_descriptors_count(device.path) == fd_count
 
         nbs.destroy_volume("vol0", sync=True)
 
@@ -340,8 +459,16 @@ class _TestCmsRemoveDeviceNoUserDisks:
     def __init__(self, name):
         self.name = name
 
-    def run(self, storage, nbs, disk_agent, cms, devices, always_allocate_local_ssd):
+    def run(self, storage, nbs, disk_agent, cms: CMS, devices, always_allocate_local_ssd, attach_detach_paths):
         device = devices[0]
+
+        fd_count = file_descriptors_count(device.path)
+        assert fd_count != 0
+
+        if attach_detach_paths:
+            cms.remove_device("localhost", device.path)
+            cms.wait_for_no_paths_to_attach_detach()
+            assert file_descriptors_count(device.path) + 1 == fd_count
 
         response = cms.remove_device("localhost", device.path)
 
@@ -358,6 +485,13 @@ class _TestCmsRemoveDeviceNoUserDisks:
 
         nbs.change_device_state(device.uuid, 2)
         nbs.change_device_state(device.uuid, 0)
+
+        response = cms.add_device("localhost", device.path)
+        assert response.ActionResults[0].Timeout == 0
+
+        if attach_detach_paths:
+            cms.wait_for_no_paths_to_attach_detach()
+            assert file_descriptors_count(device.path) == fd_count
 
         nbs.create_volume("vol1")
         nbs.read_blocks("vol1", start_index=0, block_count=32)
@@ -376,7 +510,8 @@ TESTS = [
 
 
 class Nbs(LocalNbs):
-    BINARY_PATH = yatest_common.binary_path("cloud/blockstore/apps/client/blockstore-client")
+    BINARY_PATH = yatest_common.binary_path(
+        "cloud/blockstore/apps/client/blockstore-client")
 
     def create_volume(self, disk_id, blocks_count=DISK_SIZE,
                       kind="nonreplicated", return_code=0):
@@ -400,7 +535,8 @@ class Nbs(LocalNbs):
         self.__rpc(*args, input=disk_id, stdout=subprocess.PIPE)
 
     def read_blocks(self, disk_id, start_index, block_count):
-        logging.info('[NBS] read blocks (%d:%d) from "%s"' % (start_index, block_count, disk_id))
+        logging.info('[NBS] read blocks (%d:%d) from "%s"' %
+                     (start_index, block_count, disk_id))
 
         self.__rpc(
             "readblocks",
@@ -414,16 +550,19 @@ class Nbs(LocalNbs):
         self.__rpc(
             "executeaction",
             "--action", "diskregistrychangestate",
-            "--input-bytes", "{'Message': 'test', 'ChangeAgentState': {'AgentId': '%s', 'State': %d}}" % (agent_id, state),
+            "--input-bytes", "{'Message': 'test', 'ChangeAgentState': {'AgentId': '%s', 'State': %d}}" % (
+                agent_id, state),
             stdout=subprocess.PIPE)
 
     def change_device_state(self, device_id, state):
-        logging.info('[NBS] change device "%s" state to %d' % (device_id, state))
+        logging.info('[NBS] change device "%s" state to %d' %
+                     (device_id, state))
 
         self.__rpc(
             "executeaction",
             "--action", "diskregistrychangestate",
-            "--input-bytes", "{'Message': 'test', 'ChangeDeviceState': {'DeviceUUID': '%s', 'State': %d}}" % (device_id, state),
+            "--input-bytes", "{'Message': 'test', 'ChangeDeviceState': {'DeviceUUID': '%s', 'State': %d}}" % (
+                device_id, state),
             stdout=subprocess.PIPE)
 
     def __rpc(self, *args, **kwargs):
@@ -476,8 +615,9 @@ class Nbs(LocalNbs):
         return get_sensor_by_name(sensors, 'disk_registry', name)
 
 
-def __run_test(test_case, always_allocate_local_ssd):
-    kikimr_binary_path = yatest_common.binary_path("contrib/ydb/apps/ydbd/ydbd")
+def __run_test(test_case, always_allocate_local_ssd, attach_detach_paths):
+    kikimr_binary_path = yatest_common.binary_path(
+        "contrib/ydb/apps/ydbd/ydbd")
 
     configurator = KikimrConfigGenerator(
         erasure=None,
@@ -488,7 +628,8 @@ def __run_test(test_case, always_allocate_local_ssd):
             dict(name="dynamic_storage_pool:2", kind="ssd", pdisk_user_kind=0)
         ])
 
-    nbs_binary_path = yatest_common.binary_path("cloud/blockstore/apps/server/nbsd")
+    nbs_binary_path = yatest_common.binary_path(
+        "cloud/blockstore/apps/server/nbsd")
 
     kikimr_cluster = kikimr_cluster_factory(configurator=configurator)
     kikimr_cluster.start()
@@ -512,7 +653,8 @@ def __run_test(test_case, always_allocate_local_ssd):
 
     certs_dir = yatest_common.source_path('cloud/blockstore/tests/certs')
 
-    server_app_config.ServerConfig.RootCertsFile = os.path.join(certs_dir, 'server.crt')
+    server_app_config.ServerConfig.RootCertsFile = os.path.join(
+        certs_dir, 'server.crt')
     cert = server_app_config.ServerConfig.Certs.add()
     cert.CertFile = os.path.join(certs_dir, 'server.crt')
     cert.CertPrivateKeyFile = os.path.join(certs_dir, 'server.key')
@@ -529,6 +671,7 @@ def __run_test(test_case, always_allocate_local_ssd):
     storage.NonReplicatedDontSuspendDevices = True
     storage.DiskRegistryAlwaysAllocatesLocalDisks = always_allocate_local_ssd
     storage.DiskRegistryCleanupConfigOnRemoveHost = not always_allocate_local_ssd
+    storage.AttachDetachPathsEnabled = attach_detach_paths
     storage.NodeType = 'main'
 
     nbs = Nbs(
@@ -543,17 +686,19 @@ def __run_test(test_case, always_allocate_local_ssd):
     nbs.start()
     wait_for_nbs_server(nbs.nbs_port)
 
-    nbs_client_binary_path = yatest_common.binary_path("cloud/blockstore/apps/client/blockstore-client")
+    nbs_client_binary_path = yatest_common.binary_path(
+        "cloud/blockstore/apps/client/blockstore-client")
     enable_writable_state(nbs.nbs_port, nbs_client_binary_path)
     setup_disk_registry_config_simple(
         devices,
         nbs.nbs_port,
         nbs_client_binary_path)
 
-    nbs_client = CreateClient("localhost:{}".format(nbs.nbs_port))
+    nbs_client = CreateTestClient("localhost:{}".format(nbs.nbs_port))
 
     config = nbs_client.describe_disk_registry_config()
-    nbs_client.update_disk_registry_config(MessageToDict(config) | KNOWN_DEVICE_POOLS)
+    nbs_client.update_disk_registry_config(
+        MessageToDict(config) | KNOWN_DEVICE_POOLS)
 
     # node with DiskAgent
 
@@ -577,7 +722,7 @@ def __run_test(test_case, always_allocate_local_ssd):
 
     try:
         ret = test_case.run(storage, nbs, disk_agent, CMS(
-            nbs_client), devices, always_allocate_local_ssd)
+            nbs_client), devices, always_allocate_local_ssd, attach_detach_paths)
     finally:
         nbs.stop()
         disk_agent.stop()
@@ -592,5 +737,7 @@ def __run_test(test_case, always_allocate_local_ssd):
 
 @pytest.mark.parametrize("test_case", TESTS, ids=[x.name for x in TESTS])
 @pytest.mark.parametrize("always_allocate_local_ssd", [True, False], ids=["locals_in_warning", "locals_in_online"])
-def test_cms(test_case, always_allocate_local_ssd):
-    assert __run_test(test_case, always_allocate_local_ssd) is True
+@pytest.mark.parametrize("attach_detach_paths", [True, False], ids=["attach-detach-paths", ""])
+def test_cms(test_case, always_allocate_local_ssd, attach_detach_paths):
+    assert __run_test(test_case, always_allocate_local_ssd,
+                      attach_detach_paths) is True
