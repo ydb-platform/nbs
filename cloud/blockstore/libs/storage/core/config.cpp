1944019a1d4969bd3a3162702812e742318359f2
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-10-07 09:46:01 +0000

    issue-4293: removed some attaches, optimized registration,  add comments, replaced E_ARGUMENT with E_PRECONDITION_FAILED and corrected some other issues

--- cloud/blockstore/libs/storage/core/config.cpp
+++ cloud/blockstore/libs/storage/core/config.cpp
@@ -643,6 +643,8 @@ NProto::TLinkedDiskFillBandwidth GetBandwidth(
     xxx(AttachDetachPathsEnabled,             bool,        false              )\
                                                                                \
     xxx(MaxInflightAttachDetachPathRequestsProcessing,  ui64,  100            )\
+                                                                               \
+    xxx(AttachDetachPathRequestTimeout,       TDuration,   Seconds(5)         )\
 
 // BLOCKSTORE_STORAGE_CONFIG_RW
 
