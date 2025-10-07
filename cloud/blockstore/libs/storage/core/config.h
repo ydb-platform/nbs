c9051a799f183f39676b1debf4ec3ccd2dde0c3c
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-10-07 09:46:01 +0000

    issue-4293: removed some attaches, optimized registration,  add comments, replaced E_ARGUMENT with E_PRECONDITION_FAILED and corrected some other issues

--- cloud/blockstore/libs/storage/core/config.h
+++ cloud/blockstore/libs/storage/core/config.h
@@ -734,6 +734,8 @@ public:
 
     [[nodiscard]] bool GetAttachDetachPathsEnabled() const;
     [[nodiscard]] ui64 GetMaxInflightAttachDetachPathRequestsProcessing() const;
+
+    [[nodiscard]] TDuration GetAttachDetachPathRequestTimeout() const;
 };
 
 ui64 GetAllocationUnit(
