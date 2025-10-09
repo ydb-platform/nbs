e68ac0dbfa9447d75367f64bd7e704af8eb57208
Rattysed <46933547+Rattysed@users.noreply.github.com>
2025-10-09 14:16:05 +0000

    change constants to multiplies

--- cloud/disk_manager/internal/pkg/facade/image_service_test/image_service_cells_test.go
+++ cloud/disk_manager/internal/pkg/facade/image_service_test/image_service_cells_test.go
@@ -12,7 +12,7 @@ func TestImageServiceCellsCreateImageFromDisk(t *testing.T) {
 	testImageServiceCreateImageFromDiskWithKind(
 		t,
 		disk_manager.DiskKind_DISK_KIND_SSD,
-		uint64(4194304), // diskSize.
+		uint64(4*1024*1024), // diskSize.
 		shardedZoneID,
 		cellID, // diskCellID. Zone is sharded. Policy is FIRST_IN_CONFIG.
 	)
