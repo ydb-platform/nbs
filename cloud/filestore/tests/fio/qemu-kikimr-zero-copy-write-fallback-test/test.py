ca1f83074370118f4ddafceefb925c091440fbe2
Anton Myagkov <antomyagkov@gmail.com>
2025-10-09 12:48:05 +0200

    test: set numjobs 1 for unaligned fio tests

--- cloud/filestore/tests/fio/qemu-kikimr-zero-copy-write-fallback-test/test.py
+++ cloud/filestore/tests/fio/qemu-kikimr-zero-copy-write-fallback-test/test.py
@@ -16,14 +16,14 @@ UNALIGNED_TESTS = fio.generate_tests(
     offset=100,
     sizes=SIZES,
     iodepths=[1],
-    numjobs=[4],
+    numjobs=[1],
     scenarios=SCENARIOS)
 
 ALIGNED_TESTS = fio.generate_tests(
     offset=0,
     sizes=SIZES,
     iodepths=[1],
-    numjobs=[4],
+    numjobs=[1],
     scenarios=SCENARIOS)
 
 
