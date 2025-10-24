6980be2abd5363cc1d8cef004021472f508e6a97
Andrei Nasonov <nasonov@nebius.com>
2025-10-24 16:36:38 +0200

    Rename to TAdativeIntervalLock

--- cloud/filestore/libs/vfs_fuse/write_back_cache/write_back_cache_ut.cpp
+++ cloud/filestore/libs/vfs_fuse/write_back_cache/write_back_cache_ut.cpp
@@ -987,7 +987,7 @@ struct TStatsCalculator
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TSimpleIntervalLock
+class TAdativeIntervalLock
 {
 private:
     std::mutex Mutex;
@@ -1394,7 +1394,7 @@ Y_UNIT_TEST_SUITE(TWriteBackCacheTest)
         // Concurrent overlapping writes can be completed in any order.
         // There is no way to validate cache in this case.
         // Therefore, we allow only non-overlapping conurrent writes.
-        TSimpleIntervalLock intervalLock;
+        TAdativeIntervalLock intervalLock;
 
         TVector<std::thread> threads;
 
