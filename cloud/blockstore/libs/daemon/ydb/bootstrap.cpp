ab9167968a71e53293022cddfe9b180b52ff888b
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2025-10-24 10:54:21 +0000

    DA: corrected  some issues after refactoring

--- cloud/blockstore/libs/daemon/ydb/bootstrap.cpp
+++ cloud/blockstore/libs/daemon/ydb/bootstrap.cpp
@@ -848,6 +848,7 @@ void TBootstrapYdb::InitKikimrService()
     args.EndpointEventHandler = EndpointEventHandler;
     args.RootKmsKeyProvider = RootKmsKeyProvider;
     args.TemporaryServer = Configs->Options->TemporaryServer;
+    args.BackgroundThreadPool = BackgroundThreadPool;
 
     ActorSystem = NStorage::CreateActorSystem(args);
 
