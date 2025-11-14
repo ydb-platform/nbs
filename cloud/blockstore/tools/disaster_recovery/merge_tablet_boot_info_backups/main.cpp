86dc73eae2d1cb7faa07b1d9968a37938979f308
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2025-11-14 13:02:06 +0300

    Fix backup merging (#4669)

--- cloud/blockstore/tools/disaster_recovery/merge_tablet_boot_info_backups/main.cpp
+++ cloud/blockstore/tools/disaster_recovery/merge_tablet_boot_info_backups/main.cpp
@@ -41,13 +41,14 @@ bool LoadTabletBootInfoBackup(
     }
 
     TFile file(backupPath, OpenExisting | RdOnly | Seq);
-    TUnbufferedFileInput input(file);
+    TString fileContent = TUnbufferedFileInput(file).ReadAll();
+    auto input = TStringInput(fileContent);
 
     return TryMergeFromTextFormat(
                input,
                *backupProto,
                EParseFromTextFormatOption::AllowUnknownField) ||
-           backupProto->MergeFromString(input.ReadAll());
+           backupProto->MergeFromString(fileContent);
 }
 
 void ProcessDir(
