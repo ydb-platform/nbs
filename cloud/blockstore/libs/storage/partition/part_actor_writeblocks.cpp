93ad9d4855ed2dd95f8278b66ac7ae7fda587eb7
neihar <ihar@nebius.com>
2025-11-22 21:34:48 +0100

    - use determenistic flag in writeblocks isntead of fuzzy one - use more consistent naming

--- cloud/blockstore/libs/storage/partition/part_actor_writeblocks.cpp
+++ cloud/blockstore/libs/storage/partition/part_actor_writeblocks.cpp
@@ -347,7 +347,7 @@ void TPartitionActor::HandleWriteBlocksCompleted(
         ProfileLog->Write(std::move(record));
     }
 
-    if (msg->UnconfirmedBlobsAdded && !HasError(msg->GetError())) {
+    if (msg->UnconfirmedBlobsUsed && !HasError(msg->GetError())) {
         // blobs are confirmed, but AddBlobs request will be executed
         // (for this commit) later
         State->BlobsConfirmed(commitId, std::move(msg->BlobsToConfirm));
