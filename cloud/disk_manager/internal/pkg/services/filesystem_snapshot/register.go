97371a732f758dc8cdb62771af1e8290d4f59d6c
Sergei Vorobev <sergei.vorobev@nebius.com>
2025-11-28 15:50:22 +0100

    [Disk Manager] Add task for clear deleted filesystem snapshots

--- cloud/disk_manager/internal/pkg/services/filesystem_snapshot/register.go
+++ cloud/disk_manager/internal/pkg/services/filesystem_snapshot/register.go
@@ -2,8 +2,11 @@ package filesystem_snapshot
 
 import (
 	"context"
+	"time"
 
 	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/cells"
+	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/resources"
+	filesystem_snapshot_config "github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/services/filesystem_snapshot/config"
 	"github.com/ydb-platform/nbs/cloud/tasks"
 )
 
@@ -11,11 +14,28 @@ import (
 
 func RegisterForExecution(
 	ctx context.Context,
+	config *filesystem_snapshot_config.FilesystemSnapshotsConfig,
 	taskRegistry *tasks.Registry,
 	taskScheduler tasks.Scheduler,
 	cellSelector cells.CellSelector,
+	storage resources.Storage,
 ) error {
-	err := taskRegistry.RegisterForExecution(
+
+	deletedExpirationTimeout, err := time.ParseDuration(
+		config.GetDeletedExpirationTimeout(),
+	)
+	if err != nil {
+		return err
+	}
+
+	clearDeletedTaskScheduleInterval, err := time.ParseDuration(
+		config.GetClearDeletedTaskScheduleInterval(),
+	)
+	if err != nil {
+		return err
+	}
+
+	err = taskRegistry.RegisterForExecution(
 		"filesystem_snapshot.CreateFilesystemSnapshot",
 		func() tasks.Task {
 			return &createFilesystemSnapshotTask{
@@ -38,6 +58,27 @@ func RegisterForExecution(
 	if err != nil {
 		return err
 	}
+
+	err = taskRegistry.RegisterForExecution(
+		"filesystem_snapshot.ClearDeletedFilesystemSnapshots", func() tasks.Task {
+			return &clearDeletedFilesystemSnapshotsTask{
+				storage:           storage,
+				expirationTimeout: deletedExpirationTimeout,
+				limit:             int(config.GetClearDeletedLimit()),
+			}
+		})
+	if err != nil {
+		return err
+	}
+
+	taskScheduler.ScheduleRegularTasks(
+		ctx,
+		"snapfilesystem_snapshotshots.ClearDeletedFilesystemSnapshots",
+		tasks.TaskSchedule{
+			ScheduleInterval: clearDeletedTaskScheduleInterval,
+			MaxTasksInflight: 1,
+		},
+	)
 
 	return nil
 }
