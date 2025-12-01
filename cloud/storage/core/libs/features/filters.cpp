86447d3420973881127a3486dcdc41df4b8a457e
Evgeny Budilovsky <evgenybud@nebius.com>
2025-12-01 12:27:56 +0200

    issue-4748: extend features config to allow matching using regexp

--- cloud/storage/core/libs/features/filters.cpp
+++ cloud/storage/core/libs/features/filters.cpp
@@ -1,6 +1,27 @@
 #include "filters.h"
 
+#include <util/generic/algorithm.h>
+
+#include <regex>
+
 namespace NCloud::NFeatures {
+
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool RegexpMatchItemInSet(const THashSet<TString>& set, const TString& item)
+{
+    return AnyOf(
+        set,
+        [&](const TString& setElem)
+        {
+            std::regex r{setElem.data(), setElem.size()};
+            return std::regex_match(item.data(), r);
+        });
+}
+
+}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -15,6 +36,8 @@ TFilters::TFilters(NProto::TFilters config)
     for (const auto& entityId: config.GetEntityIds()) {
         EntityIds.emplace(entityId);
     }
+
+    MatchAlgorithm = config.GetMatchAlgorithm();
 }
 
 bool TFilters::Contains(
@@ -22,8 +45,16 @@ bool TFilters::Contains(
     const TString& folderId,
     const TString& entityId) const
 {
-    return CloudIds.contains(cloudId) || FolderIds.contains(folderId) ||
-        EntityIds.contains(entityId);
+    switch (MatchAlgorithm) {
+        case NProto::FILTER_MATCH_ALGORITHM_STRING:
+            return CloudIds.contains(cloudId) || FolderIds.contains(folderId) ||
+                   EntityIds.contains(entityId);
+
+        case NProto::FILTER_MATCH_ALGORITHM_REGEXP:
+            return RegexpMatchItemInSet(CloudIds, cloudId) ||
+                   RegexpMatchItemInSet(FolderIds, folderId) ||
+                   RegexpMatchItemInSet(EntityIds, entityId);
+    }
 }
 
 }   // namespace NCloud::NFeatures
