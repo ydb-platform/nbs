669d5644e1706753277645d0831c9a47c4ec5eb0
Ruslan Garashchuk <ruslan.g@nebius.com>
2025-12-04 12:26:06 +0100

    Add  StrictFileSystemSizeEnforcementEnabled and DirectoryCreationInShardsEnabled sensors

--- cloud/filestore/libs/storage/tablet/tablet_actor_counters.cpp
+++ cloud/filestore/libs/storage/tablet/tablet_actor_counters.cpp
@@ -355,6 +355,11 @@ void TIndexTabletActor::TMetrics::Register(
     REGISTER_AGGREGATABLE_SUM(SessionTimeouts, EMetricType::MT_DERIVATIVE);
     REGISTER_AGGREGATABLE_SUM(SessionCleanupAttempts, EMetricType::MT_DERIVATIVE);
 
+    REGISTER_LOCAL(
+        StrictFileSystemSizeEnforcementEnabled,
+        EMetricType::MT_ABSOLUTE);
+    REGISTER_LOCAL(DirectoryCreationInShardsEnabled, EMetricType::MT_ABSOLUTE);
+
     REGISTER_AGGREGATABLE_SUM(ReassignCount, EMetricType::MT_ABSOLUTE);
     REGISTER_AGGREGATABLE_SUM(WritableChannelCount, EMetricType::MT_ABSOLUTE);
     REGISTER_AGGREGATABLE_SUM(UnwritableChannelCount, EMetricType::MT_ABSOLUTE);
@@ -589,6 +594,13 @@ void TIndexTabletActor::TMetrics::Update(
     Store(UsedDirectHandlesCount, handlesStats.UsedDirectHandlesCount);
     Store(SevenBytesHandlesCount, handlesStats.SevenBytesHandlesCount);
     Store(UsedLocksCount, stats.GetUsedLocksCount());
+
+    Store(
+        StrictFileSystemSizeEnforcementEnabled,
+        fileSystem.GetStrictFileSystemSizeEnforcementEnabled());
+    Store(
+        DirectoryCreationInShardsEnabled,
+        fileSystem.GetDirectoryCreationInShardsEnabled());
 
     Store(FreshBytesCount, stats.GetFreshBytesCount());
     Store(FreshBytesItemCount, stats.GetFreshBytesItemCount());
