afcbc00dbab766d7e9e78f26f3f474b697c1e42f
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2025-12-07 10:52:18 +0300

    Calculating statistics of zero blocks for ranges of different sizes (#4784)
    ```
    DiskId	Known_4	Zero_4	Ratio_4%	Known_8	Zero_8	Ratio_8%	Known_16	Zero_16	Ratio_16%	Known_32	Zero_32	Ratio_32%	Known_64	Zero_64	Ratio_64%	Known_128	Zero_128	Ratio_128%	Known_256	Zero_256	Ratio_256%	Known_512	Zero_512	Ratio_512%	Known_1024	Zero_1024	Ratio_1024%	
    disk-id	119040	7329	6.156754032	59520	3443	5.784610215	29760	1517	5.097446237	14880	549	3.689516129	7440	245	3.293010753	3720	7	0.188172043	1860	3	0.1612903226	930	1	0.1075268817	465	0	0
    
    ```

--- cloud/blockstore/tools/analytics/dump-event-log/zero_ranges_stat.cpp
+++ cloud/blockstore/tools/analytics/dump-event-log/zero_ranges_stat.cpp
@@ -12,9 +12,10 @@ namespace {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-constexpr size_t NonreplicatedDeviceSize = 24379392 * 4_KB;   // 93 GiB
+constexpr size_t ReservationSize = 8192;
 constexpr size_t RangeSize = 4_MB;
-constexpr size_t RangesPerDevice = NonreplicatedDeviceSize / RangeSize;
+constexpr size_t MinSegmentSize = RangeSize;
+constexpr size_t MaxSegmentSize = 1_GB;
 
 ui32 CalcZeroChecksum()
 {
@@ -25,6 +26,53 @@ ui32 CalcZeroChecksum()
 }
 
 }   // namespace
+
+void TZeroRangesStat::TZeroRanges::Set(ui64 rangeIndx, bool isZero)
+{
+    if (KnownRanges.Size() <= rangeIndx) {
+        KnownRanges.Reserve(KnownRanges.Size() + ReservationSize);
+        NonZeroRanges.Reserve(KnownRanges.Size() + ReservationSize);
+    }
+
+    KnownRanges.Set(rangeIndx);
+    if (!isZero) {
+        NonZeroRanges.Set(rangeIndx);
+    }
+}
+
+TString TZeroRangesStat::TZeroRanges::Print() const
+{
+    TStringBuilder sb;
+    size_t known = KnownRanges.Count();
+    size_t nonZero = NonZeroRanges.Count();
+    size_t zero = known - nonZero;
+    sb << known << "\t" << zero << "\t" << (100.0 * zero / known);
+
+    return sb;
+}
+void TZeroRangesStat::TZeroRangesBySegmentSize::Set(
+    ui64 rangeIndx4Mib,
+    bool isZero)
+{
+    for (size_t segmentSize = MinSegmentSize, rangeIndx = rangeIndx4Mib;
+         segmentSize <= MaxSegmentSize;
+         segmentSize *= 2, rangeIndx /= 2)
+    {
+        BySegmentSize[segmentSize].Set(rangeIndx, isZero);
+    }
+}
+
+TString TZeroRangesStat::TZeroRangesBySegmentSize::Print() const
+{
+    TStringBuilder sb;
+    for (size_t segmentSize = MinSegmentSize; segmentSize <= MaxSegmentSize;
+         segmentSize *= 2)
+    {
+        sb << BySegmentSize.FindPtr(segmentSize)->Print()
+           << (segmentSize == MaxSegmentSize ? "" : "\t");
+    }
+    return sb;
+}
 
 TZeroRangesStat::TZeroRangesStat(const TString& filename)
     : Filename(filename)
@@ -59,32 +107,30 @@ void TZeroRangesStat::ProcessRequest(
     }
 
     auto& volume = Volumes[diskId];
-    const ui32 rangeIndex = blockRange.Start / blockRange.Size();
-    if (volume.KnownRanges.Size() <= rangeIndex) {
-        const ui32 deviceIndex = rangeIndex / RangesPerDevice;
-        const ui32 rangeCount = (deviceIndex + 1) * RangesPerDevice;
-        volume.KnownRanges.Reserve(rangeCount);
-        volume.ZeroRanges.Reserve(rangeCount);
-    }
-    volume.KnownRanges.Set(rangeIndex);
-    if (isZeroRange) {
-        volume.ZeroRanges.Set(rangeIndex);
-    } else {
-        volume.ZeroRanges.Reset(rangeIndex);
-    }
+    const ui32 rangeIndex4MiB = blockRange.Start / blockRange.Size();
+    volume.Set(rangeIndex4MiB, isZeroRange);
 }
 
 void TZeroRangesStat::Dump()
 {
     TFileOutput out(Filename);
-    out.Write("DiskId\tZeroRanges\tKnownRanges\tRatio %\n");
+    {
+        TStringBuilder sb;
+        sb << "DiskId\t";
+        for (size_t segmentSize = MinSegmentSize; segmentSize <= MaxSegmentSize;
+             segmentSize *= 2)
+        {
+            sb << "Known_" << segmentSize / 1_MB << "\t";
+            sb << "Zero_" << segmentSize / 1_MB << "\t";
+            sb << "Ratio_" << segmentSize / 1_MB << "%\t";
+        }
+        sb << "\n";
+        out.Write(sb);
+    }
 
     for (const auto& [diskId, volume]: Volumes) {
-        const ui32 knownRanges = volume.KnownRanges.Count();
-        const ui32 zeroRanges = volume.ZeroRanges.Count();
         TStringBuilder sb;
-        sb << diskId << "\t" << zeroRanges << "\t" << knownRanges << "\t"
-           << 100.0 * zeroRanges / knownRanges << "\n";
+        sb << diskId << "\t" << volume.Print() << "\n";
         out.Write(sb);
     }
 }
