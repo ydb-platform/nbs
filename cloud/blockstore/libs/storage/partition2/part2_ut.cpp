c683c52c4a4657796d62dadd3a10b26848fc594d
Yudov Maksim <yudovmaksim@yandex-team.ru>
2025-12-08 16:24:48 +0000

    checkrange: parameter calculate_checksums was deleted. It's always computed now.

--- cloud/blockstore/libs/storage/partition2/part2_ut.cpp
+++ cloud/blockstore/libs/storage/partition2/part2_ut.cpp
@@ -851,13 +851,12 @@ public:
     }
 
     std::unique_ptr<TEvVolume::TEvCheckRangeRequest>
-    CreateCheckRangeRequest(TString id, ui32 startIndex, ui32 size, bool calculateChecksums = false)
+    CreateCheckRangeRequest(TString id, ui32 startIndex, ui32 size)
     {
         auto request = std::make_unique<TEvVolume::TEvCheckRangeRequest>();
         request->Record.SetDiskId(id);
         request->Record.SetStartIndex(startIndex);
         request->Record.SetBlocksCount(size);
-        request->Record.SetCalculateChecksums(calculateChecksums);
         return request;
     }
 
