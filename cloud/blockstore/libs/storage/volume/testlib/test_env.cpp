a396ad7f0bed22f49bca85e9cce667e1e349f3d2
BarkovBG <barkovbg@gmail.com>
2025-12-10 14:19:20 +0300

    [Disk Manager] use dmCopyVolumeClient instead of CopyVolumeClient for dm relocation (#4801)

--- cloud/blockstore/libs/storage/volume/testlib/test_env.cpp
+++ cloud/blockstore/libs/storage/volume/testlib/test_env.cpp
@@ -1018,18 +1018,33 @@ std::unique_ptr<TTestActorRuntime> TTestRuntimeBuilder::Build()
 
 NProto::TVolumeClientInfo CreateVolumeClientInfo(
     TString clientId,
+    TString clientId,
     NProto::EVolumeAccessMode accessMode,
     NProto::EVolumeMountMode mountMode,
     ui32 mountFlags,
     ui64 mountSeqNumber)
 {
     NProto::TVolumeClientInfo info;
-    info.SetClientId(clientId);
+    info.SetClientId();
     info.SetVolumeAccessMode(accessMode);
     info.SetMountSeqNumber(mountSeqNumber);
     info.SetVolumeMountMode(mountMode);
     info.SetMountFlags(mountFlags);
     return info;
+}
+
+NProto::TVolumeClientInfo CreateVolumeClientInfo(
+    NProto::EVolumeAccessMode accessMode,
+    NProto::EVolumeMountMode mountMode,
+    ui32 mountFlags,
+    ui64 mountSeqNumber)
+{
+    return CreateVolumeClientInfo(
+        CreateGuidAsString(),
+        accessMode,
+        mountMode,
+        mountFlags,
+        mountSeqNumber);
 }
 
 NProto::TVolumeClientInfo CreateVolumeClientInfo(
