50347de34b3f817df1935818262ee3ad510e0763
Anton Myagkov <antomyagkov@gmail.com>
2025-12-15 14:55:55 +0100

    fix NBS/NFS build

--- cloud/filestore/libs/storage/tablet/tablet_actor.cpp
+++ cloud/filestore/libs/storage/tablet/tablet_actor.cpp
@@ -137,7 +137,7 @@ void TIndexTabletActor::OnActivateExecutor(const TActorContext& ctx)
 
     RegisterCounters(ctx);
 
-    if (!Executor()->GetStats().IsFollower) {
+    if (!Executor()->GetStats().IsFollower()) {
         ExecuteTx<TInitSchema>(ctx, UseNoneCompactionPolicy);
     }
 }
