11f7e811a4efa282cf4bf752d54fbfee0e4e5ce1
BarkovBG <barkovbg@gmail.com>
2025-12-15 23:40:10 +0300

    [Disk Manager] support relocation between cells  (#4827)

--- cloud/disk_manager/internal/pkg/facade/disk_service_test/disk_service_cells_test.go
+++ cloud/disk_manager/internal/pkg/facade/disk_service_test/disk_service_cells_test.go
@@ -1,11 +1,18 @@
 package disk_service_test
 
 import (
+	"context"
+	"crypto/rand"
+	"hash/crc32"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	disk_manager "github.com/ydb-platform/nbs/cloud/disk_manager/api"
+	internal_client "github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/client"
+	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/clients/nbs"
 	"github.com/ydb-platform/nbs/cloud/disk_manager/internal/pkg/facade/testcommon"
+	sdk_client "github.com/ydb-platform/nbs/cloud/disk_manager/pkg/client"
+	"github.com/ydb-platform/nbs/cloud/tasks/logging"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -25,6 +32,116 @@ func cellsTestCases() []TestCase {
 			name:   "Cell in sharded zone",
 			zoneID: cellID1,
 		},
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func successfullyMigrateDiskBetweenCells(
+	t *testing.T,
+	ctx context.Context,
+	client sdk_client.Client,
+	params migrationTestParams,
+) {
+	srcZoneNBSClient := testcommon.NewNbsTestingClient(t, ctx, params.SrcZoneID)
+
+	diskSize := uint64(params.DiskSize)
+	diskContentInfo, err := srcZoneNBSClient.CalculateCrc32(params.DiskID, diskSize)
+	require.NoError(t, err)
+
+	reqCtx := testcommon.GetRequestContext(t, ctx)
+	operation, err := client.MigrateDisk(reqCtx, &disk_manager.MigrateDiskRequest{
+		DiskId: &disk_manager.DiskId{
+			DiskId: params.DiskID,
+			ZoneId: params.SrcZoneID,
+		},
+		DstZoneId:               params.DstZoneID,
+		IsMigrationBetweenCells: true,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, operation)
+
+	// TODO: Make freeze operation transactional.
+	// See: https://github.com/ydb-platform/nbs/issues/4847#issuecomment-3656284845
+	var confirmedDiskContentInfoCrc32, unconfirmedDiskContentInfoCrc32 uint32
+	for {
+		data := make([]byte, 4096)
+		_, err := rand.Read(data)
+		require.NoError(t, err)
+
+		crc32 := crc32.ChecksumIEEE(data)
+
+		logging.Info(
+			ctx,
+			"Writing zero block with crc32 %+v to disk %+v",
+			crc32,
+			params.DiskID,
+		)
+		unconfirmedDiskContentInfoCrc32 = crc32
+		err = srcZoneNBSClient.Write(
+			params.DiskID,
+			0, // startIndex
+			data,
+		)
+		if err != nil {
+			if nbs.IsDiskNotFoundError(err) {
+				break
+			}
+
+			require.NoError(t, err)
+		} else {
+			logging.Info(
+				ctx,
+				"Writed zero block with crc32 %+v to disk %+v",
+				crc32,
+				params.DiskID,
+			)
+			confirmedDiskContentInfoCrc32 = crc32
+		}
+	}
+
+	err = internal_client.WaitOperation(ctx, client, operation.Id)
+	require.NoError(t, err)
+
+	_, err = srcZoneNBSClient.Describe(ctx, params.DiskID)
+	require.Error(t, err)
+	require.ErrorContains(t, err, "Path not found")
+
+	dstZoneNBSClient := testcommon.NewNbsTestingClient(t, ctx, params.DstZoneID)
+
+	diskContentInfo.BlockCrc32s[0] = confirmedDiskContentInfoCrc32
+	err = dstZoneNBSClient.ValidateCrc32(
+		ctx,
+		params.DiskID,
+		diskContentInfo,
+	)
+	logging.Info(
+		ctx,
+		"Error for confirmed disk content info crc32 %+v: %+v",
+		confirmedDiskContentInfoCrc32,
+		err,
+	)
+
+	if err != nil {
+		logging.Info(
+			ctx,
+			"Trying to validate unconfirmed disk content info crc32 %+v",
+			unconfirmedDiskContentInfoCrc32,
+		)
+		diskContentInfo.BlockCrc32s[0] = unconfirmedDiskContentInfoCrc32
+		err = dstZoneNBSClient.ValidateCrc32(
+			ctx,
+			params.DiskID,
+			diskContentInfo,
+		)
+		logging.Info(
+			ctx,
+			"Error for unconfirmed disk content info crc32 %+v: %+v",
+			unconfirmedDiskContentInfoCrc32,
+			err,
+		)
+
+		require.NoError(t, err)
 	}
 }
 
@@ -106,7 +223,7 @@ func TestDiskServiceMigrateDiskBetweenCells(t *testing.T) {
 	ctx, client := setupMigrationTest(t, params)
 	defer client.Close()
 
-	successfullyMigrateDisk(t, ctx, client, params)
+	successfullyMigrateDiskBetweenCells(t, ctx, client, params)
 
 	testcommon.DeleteDisk(t, ctx, client, diskID)
 
