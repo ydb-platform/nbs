cfab36f5a79894fae63c714029f8e33e96c229af
Andrei Kilvein <akilvein@nebius.com>
2025-12-16 13:24:38 +0100

    fix python format issues

--- cloud/filestore/tests/guest_cache/guest_cache_entry_timeout/without_regular_entry_timeout/test.py
+++ cloud/filestore/tests/guest_cache/guest_cache_entry_timeout/without_regular_entry_timeout/test.py
@@ -4,6 +4,5 @@ import cloud.filestore.tests.guest_cache.guest_cache_entry_timeout.lib \
 
 def test():
     timeout_utils.test_guest_cache_enty_timeout(
-        expected_dir_stat_count=0,
-        expected_file_stat_count=0
-        )
+        expected_dir_stat_count=0, expected_file_stat_count=0
+    )
