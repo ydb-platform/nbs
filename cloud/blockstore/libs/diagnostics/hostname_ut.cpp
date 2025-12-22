f81fa121d1268c5b4a8e92062fe499e952469468
Andrei Kilvein <akilvein@nebius.com>
2025-12-22 13:19:27 +0100

    Improve tests

--- cloud/blockstore/libs/diagnostics/hostname_ut.cpp
+++ cloud/blockstore/libs/diagnostics/hostname_ut.cpp
@@ -20,30 +20,60 @@ Y_UNIT_TEST_SUITE(TMonitoringUrlTest)
 {
     Y_UNIT_TEST(ShouldReturnCorrectMonitoringVolumeUrl)
     {
-        const TString protoText = R"(
-            MonitoringUrlData {
-                MonitoringClusterName: "company_devlab"
-                MonitoringUrl: "https://monitoring.company.com"
-                MonitoringProject: "development.cloud"
-                MonitoringVolumeDashboard: "monitoring_dashboard"
-                MonitoringUrlTemplate: "{MonitoringUrl}/projects/{MonitoringProject}/dashboards/{MonitoringVolumeDashboard}?from=now-1d&to=now&refresh=60000&p.cluster={MonitoringClusterName}&p.volume={diskId}"
-            }
-        )";
-
         NProto::TDiagnosticsConfig protoConfig;
-        NCloud::ParseProtoTextFromStringRobust(protoText, protoConfig);
+        auto* monitorngUrlConfig = protoConfig.MutableMonitoringUrlData();
+        monitorngUrlConfig->SetMonitoringClusterName("company_devlab");
+        monitorngUrlConfig->SetMonitoringUrl("https://monitoring.company.com");
+        monitorngUrlConfig->SetMonitoringProject("development.cloud");
+        monitorngUrlConfig->SetMonitoringVolumeDashboard(
+            "monitoring_dashboard");
+        monitorngUrlConfig->SetMonitoringUrlTemplate(
+            "{MonitoringUrl}/projects/{MonitoringProject}/dashboards/"
+            "{MonitoringVolumeDashboard}?from=now-1d&to=now&refresh=60000&p."
+            "cluster={MonitoringClusterName}&p.volume={diskId}");
 
         TDiagnosticsConfig config(protoConfig);
 
         auto url = GetMonitoringVolumeUrl(config, "vol-1234567890abcdef");
 
-        UNIT_ASSERT_EQUAL(
-            url,
+        UNIT_ASSERT_VALUES_EQUAL(
             "https://monitoring.company.com/projects/development.cloud/"
             "dashboards/monitoring_dashboard"
             "?from=now-1d&to=now&refresh=60000"
             "&p.cluster=company_devlab"
-            "&p.volume=vol-1234567890abcdef");
+            "&p.volume=vol-1234567890abcdef",
+        url);
+    }
+
+    Y_UNIT_TEST(ShouldSubstituteAllTemplateFields)
+    {
+        NProto::TDiagnosticsConfig protoConfig;
+        auto* monitorngUrlConfig = protoConfig.MutableMonitoringUrlData();
+
+        monitorngUrlConfig->SetMonitoringUrl("1");
+        monitorngUrlConfig->SetMonitoringProject("2");
+        monitorngUrlConfig->SetMonitoringClusterName("3");
+        monitorngUrlConfig->SetMonitoringVolumeDashboard("4");
+        monitorngUrlConfig->SetMonitoringNBSAlertsDashboard("5");
+        monitorngUrlConfig->SetMonitoringNBSTVDashboard("6");
+        monitorngUrlConfig->SetMonitoringYDBProject("7");
+        monitorngUrlConfig->SetMonitoringYDBGroupDashboard("8");
+
+        monitorngUrlConfig->SetMonitoringUrlTemplate(
+            "{MonitoringUrl}/"
+            "{MonitoringProject}/"
+            "{MonitoringClusterName}/"
+            "{MonitoringVolumeDashboard}/"
+            "{MonitoringNBSAlertsDashboard}/"
+            "{MonitoringNBSTVDashboard}/"
+            "{MonitoringYDBProject}/"
+            "{MonitoringYDBGroupDashboard}/"
+            "{diskId}");
+
+        TDiagnosticsConfig config(protoConfig);
+        auto url = GetMonitoringVolumeUrl(config, "9");
+
+        UNIT_ASSERT_VALUES_EQUAL("1/2/3/4/5/6/7/8/9", url);
     }
 }
 
