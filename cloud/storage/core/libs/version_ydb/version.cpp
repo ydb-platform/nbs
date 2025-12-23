24e23c73a3106a698fa2481ff5b3f1c5037ff408
Anton Myagkov <antomyagkov@gmail.com>
2025-12-23 18:36:37 +0100

    set ydb compatible versions

--- cloud/storage/core/libs/version_ydb/version.cpp
+++ cloud/storage/core/libs/version_ydb/version.cpp
@@ -6,8 +6,38 @@ NKikimr::TCompatibilityInfo::MakeCurrent()
     using namespace NKikimr;
     using TCurrentConstructor =
         TCompatibilityInfo::TProtoConstructor::TCurrentCompatibilityInfo;
+    using TVersionConstructor =
+        NKikimr::TCompatibilityInfo::TProtoConstructor::TVersion;
+    using TCompatibilityRuleConstructor =
+        NKikimr::TCompatibilityInfo::TProtoConstructor::TCompatibilityRule;
 
     return TCurrentConstructor{
         .Application = "nbs",
-    }.ToPB();
+        .Version =
+            TVersionConstructor{
+                .Year = 25,
+                .Major = 1,
+            },
+        .CanLoadFrom =
+            {
+                TCompatibilityRuleConstructor{
+                    .LowerLimit = TVersionConstructor{.Year = 24, .Major = 1},
+                    .UpperLimit = TVersionConstructor{.Year = 25, .Major = 1},
+                },
+            },
+        .StoresReadableBy =
+            {
+                TCompatibilityRuleConstructor{
+                    .LowerLimit = TVersionConstructor{.Year = 24, .Major = 1},
+                    .UpperLimit = TVersionConstructor{.Year = 25, .Major = 1},
+                },
+            },
+        .CanConnectTo =
+            {
+                TCompatibilityRuleConstructor{
+                    .LowerLimit = TVersionConstructor{.Year = 24, .Major = 1},
+                    .UpperLimit = TVersionConstructor{.Year = 25, .Major = 1},
+                },
+            }}
+        .ToPB();
 }
