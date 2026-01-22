b23ed2fd8476e90a2bbe91f01029efa8eb622a6b
Andrei Galibin <agalibin@yandex-team.ru>
2026-01-22 18:36:16 +0000

    [nfs] fix launching nfs eternal tests with db

--- cloud/blockstore/tools/testing/eternal_tests/test_runner/scripts/mysql-nfs.sh
+++ cloud/blockstore/tools/testing/eternal_tests/test_runner/scripts/mysql-nfs.sh
@@ -1,17 +1,17 @@
 # disable packages autoupdate
-sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
-sed 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
-sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/10periodic -i
+sudo sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
+sudo sed 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
+sudo sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/10periodic -i
 
 # change mysql data dir
-service mysql stop
-cp -R -p /var/lib/mysql /DB/mysql
-sed 's/\/var\/lib\/mysql/\/DB\/mysql/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
-sed 's/# datadir/datadir/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
-sed 's/.*binlog_expire_logs_seconds.*/binlog_expire_logs_seconds = 604800/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
-sed 's/\/var\/lib\/mysql/\/DB\/mysql/' /etc/apparmor.d/usr.sbin.mysqld -i
-service apparmor reload
-service mysql restart
+sudo systemctl stop mysql
+sudo cp -R -p /var/lib/mysql /DB/mysql
+sudo sed 's/\/var\/lib\/mysql/\/DB\/mysql/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
+sudo sed 's/# datadir/datadir/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
+sudo sed 's/.*binlog_expire_logs_seconds.*/binlog_expire_logs_seconds = 604800/' /etc/mysql/mysql.conf.d/mysqld.cnf -i
+sudo sed 's/\/var\/lib\/mysql/\/DB\/mysql/' /etc/apparmor.d/usr.sbin.mysqld -i
+sudo systemctl reload apparmor
+sudo systemctl start mysql
 
 # prepare database
-mysql --user='root' --execute='CREATE DATABASE sbtest;'
+sudo mysql --user='root' --execute='CREATE DATABASE sbtest;'
--- cloud/blockstore/tools/testing/eternal_tests/test_runner/scripts/postgresql-nfs.sh
+++ cloud/blockstore/tools/testing/eternal_tests/test_runner/scripts/postgresql-nfs.sh
@@ -1,16 +1,16 @@
 # disable packages autoupdate
-sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
-sed 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
-sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/10periodic -i
+sudo sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
+sudo sed 's/APT::Periodic::Unattended-Upgrade "1";/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades -i
+sudo sed 's/APT::Periodic::Update-Package-Lists "1";/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/10periodic -i
 
-service postgresql stop
+sudo systemctl stop stop
 
-cp -R -p  /var/lib/postgresql /DB/postgresql
+sudo cp -R -p  /var/lib/postgresql /DB/postgresql
 sudo chmod 700 -R /DB/postgresql
 
-sed 's/\/var\/lib\/postgresql/\/DB\/postgresql/' /etc/postgresql/*/main/postgresql.conf -i
-sed 's/peer/trust/' /etc/postgresql/*/main/pg_hba.conf -i
+sudo sed 's/\/var\/lib\/postgresql/\/DB\/postgresql/' /etc/postgresql/*/main/postgresql.conf -i
+sudo sed 's/peer/trust/' /etc/postgresql/*/main/pg_hba.conf -i
 
-service postgresql start
+sudo systemctl start stop
 
 sudo -u postgres -i psql -c "CREATE DATABASE pgbench;"
