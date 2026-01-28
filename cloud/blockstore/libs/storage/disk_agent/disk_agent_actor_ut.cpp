f136c17f91869894d94f5b5f97ba932b98dc2702
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2026-01-28 09:43:37 +0000

    issue-4293: attach detach paths on DA registration

--- cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_ut.cpp
+++ cloud/blockstore/libs/storage/disk_agent/disk_agent_actor_ut.cpp
@@ -7063,5 +7063,105 @@ Y_UNIT_TEST_SUITE(TDiskAgentTest)
             1,
             FindProcessesWithOpenFile(Devices[0]).size());
     }
+
+    Y_UNIT_TEST_F(ShouldAttachDetachPathsOnRegistration, TFixture)
+    {
+        auto storageConfig = NProto::TStorageServiceConfig();
+        storageConfig.SetAttachDetachPathsEnabled(true);
+
+        auto env = TTestEnvBuilder(*Runtime)
+                       .With(CreateDiskAgentConfig())
+                       .With(storageConfig)
+                       .Build();
+
+        NProto::TControlPlaneRequestNumber requestNumber;
+        requestNumber.SetDiskRegistryGeneration(1);
+        requestNumber.SetRequestNumber(10);
+        TVector<TString> pathsToAttach{PartLabels[0], PartLabels[2]};
+
+        Runtime->SetObserverFunc(
+            [&](TAutoPtr<IEventHandle>& event)
+            {
+                switch (event->GetTypeRewrite()) {
+                    case TEvDiskRegistry::EvRegisterAgentResponse: {
+                        auto* response = event->Get<
+                            TEvDiskRegistry::TEvRegisterAgentResponse>();
+                        response->Record.MutableControlPlaneRequestNumber()
+                            ->CopyFrom(requestNumber);
+                        response->Record.MutablePathsToAttach()->Assign(
+                            pathsToAttach.begin(),
+                            pathsToAttach.end());
+                    } break;
+                }
+
+                return TTestActorRuntime::DefaultObserverFunc(event);
+            });
+
+        TDiskAgentClient diskAgent(*Runtime);
+        diskAgent.WaitReady();
+
+        diskAgent.RegisterAgent();
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            1,
+            FindProcessesWithOpenFile(Devices[0]).size());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            0,
+            FindProcessesWithOpenFile(Devices[1]).size());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            1,
+            FindProcessesWithOpenFile(Devices[2]).size());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            0,
+            FindProcessesWithOpenFile(Devices[3]).size());
+
+        pathsToAttach = {PartLabels[1], PartLabels[3]};
+        requestNumber.SetDiskRegistryGeneration(2);
+
+        diskAgent.RegisterAgent();
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            0,
+            FindProcessesWithOpenFile(Devices[0]).size());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            1,
+            FindProcessesWithOpenFile(Devices[1]).size());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            0,
+            FindProcessesWithOpenFile(Devices[2]).size());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            1,
+            FindProcessesWithOpenFile(Devices[3]).size());
+    }
+
+    Y_UNIT_TEST_F(
+        ShouldNotDetachAnyDevicesIfDrNotSupportingAttachDetachPaths,
+        TFixture)
+    {
+        auto storageConfig = NProto::TStorageServiceConfig();
+        storageConfig.SetAttachDetachPathsEnabled(true);
+
+        auto env = TTestEnvBuilder(*Runtime)
+                       .With(CreateDiskAgentConfig())
+                       .With(storageConfig)
+                       .Build();
+
+        TDiskAgentClient diskAgent(*Runtime);
+        diskAgent.WaitReady();
+
+        diskAgent.RegisterAgent();
+
+        for (auto& device: Devices) {
+            UNIT_ASSERT_VALUES_EQUAL(
+                1,
+                FindProcessesWithOpenFile(device).size());
+        }
+    }
 }
 }   // namespace NCloud::NBlockStore::NStorage
