57cb91d675b64a4d1a8e11768eef800748e17eb7
Sergei Vorobev <sergei.vorobev@nebius.com>
2026-02-04 20:09:17 +0100

    issue-5144: [Disk Manager] Add test that all sessions are correctly destroyed during traversal

--- cloud/disk_manager/internal/pkg/dataplane/filesystem_snapshot/filesystem_traversal/traversal_test.go
+++ cloud/disk_manager/internal/pkg/dataplane/filesystem_snapshot/filesystem_traversal/traversal_test.go
@@ -236,3 +236,59 @@ func TestRandomFilesystemTraversal(t *testing.T) {
 	)
 	require.ElementsMatch(t, expectedNodeNames, actualNodeNames)
 }
+
+
+func TestTraversalCloseSessionOnError(t *testing.T) {
+	fixture := newFixture(t)
+	defer fixture.close(t)
+
+	filesystemID := t.Name()
+	fixture.prepareFilesystem(t, filesystemID)
+	defer fixture.cleanupFilesystem(t, filesystemID)
+
+
+	session, err := fixture.client.CreateSession(
+		fixture.ctx,
+		filesystemID,
+		"",
+		false,
+	)
+	require.NoError(t, err)
+	defer fixture.client.DestroySession(fixture.ctx, session)
+	rootDir := nfs_testing.Root(nfs_testing.Dir("Some"))
+	fsModel := nfs_testing.NewFileSystemModel(
+		t,
+		fixture.ctx,
+		fixture.client,
+		session,
+		rootDir,
+	)
+	fsModel.CreateAllNodesRecursively()
+
+	traverser := NewFilesystemTraverser(
+		fmt.Sprintf("snapshot_%v", filesystemID),
+		filesystemID,
+		"",
+		fixture.client,
+		fixture.storage,
+		func(ctx context.Context) error {
+			return nil
+		},
+		10,
+		10,
+		false,
+	)
+
+	err = traverser.Traverse(
+		fixture.ctx,
+		func(
+			ctx context.Context,
+			nodes []nfs.Node,
+			_ nfs.Session,
+			_ nfs.Client,
+		) error {
+			return fmt.Errorf("some error")
+		},
+	)
+	require.Error(t, err)
+}
