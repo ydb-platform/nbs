ed21e21d9f50165b0180ed9d10bc8d23a2df2acf
Sergei Vorobev <sergei.vorobev@nebius.com>
2026-02-05 14:48:21 +0100

    Add repro test

--- cloud/disk_manager/internal/pkg/facade/image_service_test/image_service_test.go
+++ cloud/disk_manager/internal/pkg/facade/image_service_test/image_service_test.go
@@ -1255,3 +1255,106 @@ func TestImageServiceCreateIncrementalImageFromDisk(t *testing.T) {
 	testcommon.RequireCheckpointsDoNotExist(t, ctx, diskID1)
 	testcommon.CheckConsistency(t, ctx)
 }
+
+func TestImageServiceCreateImageFromDiskWithRootKmsEncryption(t *testing.T) {
+	ctx := testcommon.NewContext()
+
+	client, err := testcommon.NewClient(ctx)
+	require.NoError(t, err)
+	defer client.Close()
+
+	diskSize := uint64(1024 * 1024 * 1024)
+	diskID := t.Name() + "_disk"
+	rootKmsEncryption := types.EncryptionMode_ENCRYPTION_WITH_ROOT_KMS_PROVIDED_KEY
+
+	reqCtx := testcommon.GetRequestContext(t, ctx)
+	operation, err := client.CreateDisk(reqCtx, &disk_manager.CreateDiskRequest{
+		Src: &disk_manager.CreateDiskRequest_SrcEmpty{
+			SrcEmpty: &empty.Empty{},
+		},
+		Size: int64(diskSize),
+		Kind: disk_manager.DiskKind_DISK_KIND_HDD_NONREPLICATED,
+		DiskId: &disk_manager.DiskId{
+			ZoneId: defaultZoneID,
+			DiskId: diskID,
+		},
+		EncryptionDesc: &disk_manager.EncryptionDesc{
+			Mode: disk_manager.EncryptionMode_ENCRYPTION_WITH_ROOT_KMS_PROVIDED_KEY,
+		},
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, operation)
+	err = internal_client.WaitOperation(ctx, client, operation.Id)
+	require.NoError(t, err)
+
+	nbsClient := testcommon.NewNbsTestingClient(t, ctx, defaultZoneID)
+
+	diskParams, err := nbsClient.Describe(ctx, diskID)
+	require.NoError(t, err)
+	require.Equal(t, rootKmsEncryption, diskParams.EncryptionDesc.Mode)
+
+	diskContentInfo, err := nbsClient.FillDisk(ctx, diskID, diskSize)
+	require.NoError(t, err)
+
+	imageID := t.Name() + "_image"
+
+	reqCtx = testcommon.GetRequestContext(t, ctx)
+	operation, err = client.CreateImage(
+		reqCtx,
+		&disk_manager.CreateImageRequest{
+			Src: &disk_manager.CreateImageRequest_SrcDiskId{
+				SrcDiskId: &disk_manager.DiskId{
+					ZoneId: defaultZoneID,
+					DiskId: diskID,
+				},
+			},
+			DstImageId: imageID,
+			FolderId:   "folder",
+			Pooled:     true,
+		})
+	require.NoError(t, err)
+	require.NotEmpty(t, operation)
+
+	response := disk_manager.CreateImageResponse{}
+	err = internal_client.WaitResponse(ctx, client, operation.Id, &response)
+	require.NoError(t, err)
+	require.Equal(t, int64(diskSize), response.Size)
+
+	meta := disk_manager.CreateImageMetadata{}
+	err = internal_client.GetOperationMetadata(ctx, client, operation.Id, &meta)
+	require.NoError(t, err)
+	require.Equal(t, float64(1), meta.Progress)
+
+	diskID2 := t.Name() + "_disk2"
+
+	reqCtx = testcommon.GetRequestContext(t, ctx)
+	operation, err = client.CreateDisk(reqCtx, &disk_manager.CreateDiskRequest{
+		Src: &disk_manager.CreateDiskRequest_SrcImageId{
+			SrcImageId: imageID,
+		},
+		Size: int64(diskSize),
+		Kind: disk_manager.DiskKind_DISK_KIND_SSD,
+		DiskId: &disk_manager.DiskId{
+			ZoneId: defaultZoneID,
+			DiskId: diskID2,
+		},
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, operation)
+	err = internal_client.WaitOperation(ctx, client, operation.Id)
+	require.NoError(t, err)
+
+	err = nbsClient.ValidateCrc32(ctx, diskID2, diskContentInfo)
+	require.NoError(t, err)
+
+	reqCtx = testcommon.GetRequestContext(t, ctx)
+	operation, err = client.DeleteImage(reqCtx, &disk_manager.DeleteImageRequest{
+		ImageId: imageID,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, operation)
+	err = internal_client.WaitOperation(ctx, client, operation.Id)
+	require.NoError(t, err)
+
+	testcommon.CheckConsistency(t, ctx)
+}
