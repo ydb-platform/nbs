e3aaeb3357d29af0953f936044fae6528ceb854f
Mikhail Montsev <svartmetal@nebius.com>
2026-02-18 21:13:42 +0000

    Fix stupid mistake: changed wrong line in PR #5256 (#5271)
    Follow-up for #5256 and #5253

--- contrib/ydb/tests/library/harness/kikimr_runner.py
+++ contrib/ydb/tests/library/harness/kikimr_runner.py
@@ -529,7 +529,7 @@ class KiKiMR(kikimr_cluster_interface.KiKiMRClusterInterface):
         self._bs_config_invoke(request)
 
     def _bs_config_invoke(self, request):
-        timeout = yatest_common.plain_or_under_sanitizer(480, 960)
+        timeout = yatest_common.plain_or_under_sanitizer(120, 240)
         sleep = 5
         retries, success = timeout / sleep, False
         while retries > 0 and not success:
@@ -582,7 +582,7 @@ class KiKiMR(kikimr_cluster_interface.KiKiMRClusterInterface):
         def predicate():
             return blobstorage_controller_has_started_on_some_node(monitors)
 
-        timeout_seconds = yatest_common.plain_or_under_sanitizer(120, 240)
+        timeout_seconds = yatest_common.plain_or_under_sanitizer(480, 960)
         bs_controller_started = wait_for(
             predicate=predicate, timeout_seconds=timeout_seconds, step_seconds=1.0, multiply=1.3
         )
