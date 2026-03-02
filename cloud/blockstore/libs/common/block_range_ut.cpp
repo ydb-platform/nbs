288c79348d83742f29ee83d6291b0e0baae30add
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2026-03-02 14:11:15 +0700

    Split block range by stripe size

--- cloud/blockstore/libs/common/block_range_ut.cpp
+++ cloud/blockstore/libs/common/block_range_ut.cpp
@@ -71,6 +71,81 @@ Y_UNIT_TEST_SUITE(TBlockRangeTest)
                     static_cast<ui64>(TBlockRange32::MaxIndex) + 10));
         }
     }
+
+    Y_UNIT_TEST(SplitByStripe)
+    {
+        const auto src = TBlockRange64::WithLength(10, 10);
+
+        {
+            const auto splitted4 = src.Split(4);
+            UNIT_ASSERT_VALUES_EQUAL(3, splitted4.size());
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(10, 2),
+                splitted4[0]);
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(12, 4),
+                splitted4[1]);
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(16, 4),
+                splitted4[2]);
+        }
+
+        {
+            auto splitted5 = src.Split(5);
+            UNIT_ASSERT_VALUES_EQUAL(2, splitted5.size());
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(10, 5),
+                splitted5[0]);
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(15, 5),
+                splitted5[1]);
+        }
+
+        {
+            auto splitted5 = src.Split(10);
+            UNIT_ASSERT_VALUES_EQUAL(1, splitted5.size());
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(10, 10),
+                splitted5[0]);
+        }
+
+        {
+            auto splitted10 = src.Split(10);
+            UNIT_ASSERT_VALUES_EQUAL(1, splitted10.size());
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(10, 10),
+                splitted10[0]);
+        }
+
+        {
+            auto splitted100 = src.Split(100);
+            UNIT_ASSERT_VALUES_EQUAL(1, splitted100.size());
+            UNIT_ASSERT_VALUES_EQUAL(
+                TBlockRange64::WithLength(10, 10),
+                splitted100[0]);
+        }
+    }
+
+    Y_UNIT_TEST(SplitByZeroStripe)
+    {
+        const auto src = TBlockRange64::WithLength(10, 10);
+
+        const auto nonSplitted = src.Split(0);
+        UNIT_ASSERT_VALUES_EQUAL(1, nonSplitted.size());
+        UNIT_ASSERT_VALUES_EQUAL(
+            TBlockRange64::WithLength(10, 10),
+            nonSplitted[0]);
+    }
+
+    Y_UNIT_TEST(SplitByOneBlock)
+    {
+        const auto src = TBlockRange64::WithLength(10, 2);
+
+        const auto splitted = src.Split(1);
+        UNIT_ASSERT_VALUES_EQUAL(2, splitted.size());
+        UNIT_ASSERT_VALUES_EQUAL(TBlockRange64::MakeOneBlock(10), splitted[0]);
+        UNIT_ASSERT_VALUES_EQUAL(TBlockRange64::MakeOneBlock(11), splitted[1]);
+    }
 }
 
 } // namespace NCloud::NBlockStore
