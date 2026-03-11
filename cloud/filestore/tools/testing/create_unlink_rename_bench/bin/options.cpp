4a36b316f15f4bd3dd4e1a58857b7e31cc24bbba
Andrei Strelkovskii <thinkingwhat@yandex.ru>
2026-03-11 19:53:42 +0100

    issue-2674: create/unlink/rename (and, actually, list) benchmark and validator

---
+++ cloud/filestore/tools/testing/create_unlink_rename_bench/bin/options.cpp
@@ -0,0 +1,49 @@
+#include "options.h"
+
+#include <library/cpp/getopt/small/last_getopt.h>
+
+namespace NCloud::NFileStore {
+
+using namespace NLastGetopt;
+
+////////////////////////////////////////////////////////////////////////////////
+
+void TOptions::Parse(int argc, char** argv)
+{
+    TOpts opts;
+    opts.AddHelpOption();
+
+    opts.AddLongOption("test-dir", "path to test directory")
+        .RequiredArgument("STR")
+        .Required()
+        .StoreResult(&TestDir);
+
+    opts.AddLongOption("producer-threads", "number of producer threads")
+        .RequiredArgument("NUM")
+        .DefaultValue(4)
+        .StoreResult(&ProducerThreads);
+
+    opts.AddLongOption("stealer-threads", "number of stealer threads")
+        .RequiredArgument("NUM")
+        .DefaultValue(2)
+        .StoreResult(&StealerThreads);
+
+    opts.AddLongOption("files-per-producer", "max files per producer")
+        .RequiredArgument("NUM")
+        .DefaultValue(1000)
+        .StoreResult(&FilesPerProducer);
+
+    opts.AddLongOption("duration", "test duration in seconds")
+        .RequiredArgument("NUM")
+        .DefaultValue(60)
+        .StoreResult(&TestDurationSec);
+
+    opts.AddLongOption("file-size", "file size in bytes")
+        .RequiredArgument("NUM")
+        .DefaultValue(4096)
+        .StoreResult(&FileSize);
+
+    TOptsParseResultException(&opts, argc, argv);
+}
+
+}   // namespace NCloud::NFileStore
