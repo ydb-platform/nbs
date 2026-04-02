94d853780750c84a94c4126abdda7bc689c95347
Mikhail Peshkov <44564007+PeshkovMikhail@users.noreply.github.com>
2026-04-02 16:55:22 +0500

    [Disk Agent] Add timeout for ioctl calls (#5514)
    ### Notes
    Set `timeout_ms` to `IoctlAdminCmdTimeout` in ioctl calls to prevent the
    system from hanging on NVMe failures.
    
    ### Issue
    https://github.com/ydb-platform/nbs/issues/5338
    
    ---------
    
    Co-authored-by: Daniil Komarevtsev <komarevtsev@yandex-team.ru>

--- cloud/blockstore/libs/nvme/nvme.h
+++ cloud/blockstore/libs/nvme/nvme.h
@@ -46,6 +46,7 @@ struct INvmeManager
 
 INvmeManagerPtr CreateNvmeManager(
     ILoggingServicePtr logging,
-    TDuration timeout);
+    TDuration secureEraseTimeout,
+    TDuration adminCmdTimeout);
 
 }   // namespace NCloud::NBlockStore::NNvme
--- cloud/blockstore/libs/storage/disk_agent/model/config.h
+++ cloud/blockstore/libs/storage/disk_agent/model/config.h
@@ -77,6 +77,7 @@ public:
     TDuration GetDeviceIOTimeout() const;
     bool GetDeviceIOTimeoutsDisabled() const;
     TDuration GetShutdownTimeout() const;
+    TDuration GetNVMeAdminCmdTimeout() const;
 
     NProto::EDiskAgentBackendType GetBackend() const;
     NProto::EDeviceEraseMethod GetDeviceEraseMethod() const;
