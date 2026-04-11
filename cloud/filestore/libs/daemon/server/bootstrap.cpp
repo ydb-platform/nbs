c8fc2c813d3822f609e833c135d862f42bac7025
Andrei Strelkovskii <thinkingwhat@yandex.ru>
2026-04-11 14:34:35 +0200

    issue-5340: support for multiple THandlerActors + rr request distribution - fix filestore-server build

--- cloud/filestore/libs/daemon/server/bootstrap.cpp
+++ cloud/filestore/libs/daemon/server/bootstrap.cpp
@@ -149,7 +149,10 @@ void TBootstrapServer::InitLWTrace()
 void TBootstrapServer::InitKikimrService()
 {
     Y_ABORT_UNLESS(ActorSystem, "Actor system MUST be initialized to create kikimr filestore");
-    Service = CreateKikimrFileStore(ActorSystem, false /* usePermanentActor */);
+    Service = CreateKikimrFileStore(
+        ActorSystem,
+        false /* usePermanentActor */,
+        0 /* permanentActorCount */);
 
     Service = CreateAuthService(
         std::move(Service),
