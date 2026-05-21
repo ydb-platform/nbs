4b73b0907332559b96d630f8609882ad03594369
gayurgin <g.y.98@yandex.ru>
2026-05-21 16:24:56 +0300

    add randomized test on compaction map

--- cloud/blockstore/libs/storage/core/compaction_map_ut.cpp
+++ cloud/blockstore/libs/storage/core/compaction_map_ut.cpp
@@ -2,6 +2,10 @@
 
 #include <library/cpp/testing/unittest/registar.h>
 
+#include <util/generic/algorithm.h>
+#include <util/generic/hash.h>
+#include <util/random/random.h>
+
 namespace NCloud::NBlockStore::NStorage {
 
 namespace {
@@ -13,6 +17,212 @@ const ui32 RangeSize = 1024;
 ui32 GetGroupIndex(ui32 group)
 {
     return group * RangeSize * TCompactionMap::GroupSize;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TReferenceImplementation
+{
+    const ui32 RangeSize;
+    const ICompactionPolicyPtr Policy;
+    const float CompactedRangeScore = -1000;
+
+    THashMap<ui32, TRangeStat> Stats;
+
+    TReferenceImplementation(ui32 rangeSize, ICompactionPolicyPtr policy)
+        : RangeSize(rangeSize)
+        , Policy(std::move(policy))
+    {}
+
+    void Update(
+        ui32 blockIndex,
+        ui32 blobCount,
+        ui32 blockCount,
+        ui32 usedBlockCount,
+        ui32 newlyZeroedBlocks,
+        bool compacted)
+    {
+        const ui32 rangeStart =
+            TCompactionMap::GetRangeStart(blockIndex, RangeSize);
+        auto& stat = Stats[rangeStart];
+
+        TCompactionMap::UpdateCompactionCounter(blobCount, &stat.BlobCount);
+        TCompactionMap::UpdateCompactionCounter(blockCount, &stat.BlockCount);
+        TCompactionMap::UpdateCompactionCounter(
+            usedBlockCount,
+            &stat.UsedBlockCount);
+        TCompactionMap::UpdateCompactionCounter(
+            newlyZeroedBlocks,
+            &stat.NewlyZeroedBlocks);
+
+        if (compacted) {
+            stat.ReadRequestCount = 0;
+            stat.ReadRequestBlobCount = 0;
+            stat.ReadRequestBlockCount = 0;
+        }
+        stat.Compacted = compacted;
+
+        stat.CompactionScore = compacted
+            ? TCompactionScore(CompactedRangeScore)
+            : Policy->CalculateScore(stat);
+    }
+
+    const TRangeStat* Get(ui32 blockIndex) const
+    {
+        const ui32 rangeStart =
+            TCompactionMap::GetRangeStart(blockIndex, RangeSize);
+
+        if (const auto* stat = Stats.FindPtr(rangeStart)) {
+            return stat;
+        }
+        return nullptr;
+    }
+
+    TCompactionCounter GetTop() const
+    {
+        TCompactionCounter top(0, {});
+        top.Stat.CompactionScore = Policy->CalculateScore({});
+
+        for (const auto& [rangeStart, stat] : Stats) {
+            if (stat.CompactionScore.Score > top.Stat.CompactionScore.Score) {
+                top = {rangeStart, stat};
+            }
+        }
+        return top;
+    }
+
+    TCompactionCounter GetTopByGarbageBlockCount() const
+    {
+        TCompactionCounter top(0, {});
+        ui16 maxGarbage = 0;
+
+        for (const auto& [rangeStart, stat] : Stats) {
+            if (!stat.Compacted) {
+                const auto garbage = stat.GarbageBlockCount();
+                if (garbage > maxGarbage) {
+                    maxGarbage = garbage;
+                    top = {rangeStart, stat};
+                }
+            }
+        }
+        return top;
+    }
+
+    TCompactionCounter GetTopByGarbageIgnoringZeroed() const
+    {
+        TCompactionCounter top(0, {});
+        ui16 maxGarbage = 0;
+
+        for (const auto& [rangeStart, stat] : Stats) {
+            if (!stat.Compacted) {
+                const auto garbage = stat.GarbageIgnoringZeroed();
+                if (garbage > maxGarbage) {
+                    maxGarbage = garbage;
+                    top = {rangeStart, stat};
+                }
+            }
+        }
+        return top;
+    }
+
+    TVector<TCompactionCounter> GetTop(size_t count) const
+    {
+        TVector<TCompactionCounter> result(Reserve(Stats.size()));
+        for (const auto& [rangeStart, stat] : Stats) {
+            if (stat.BlobCount > 0) {
+                result.emplace_back(rangeStart, stat);
+            }
+        }
+
+        Sort(result, [](const auto& l, const auto& r) {
+            return l.Stat.CompactionScore.Score > r.Stat.CompactionScore.Score;
+        });
+
+        result.crop(count);
+        return result;
+    }
+
+    TVector<TCompactionCounter> GetTopByGarbageBlockCount(size_t count) const
+    {
+        TVector<TCompactionCounter> result(Reserve(Stats.size()));
+        for (const auto& [rangeStart, stat] : Stats) {
+            if (stat.BlobCount > 0) {
+                result.emplace_back(rangeStart, stat);
+            }
+        }
+
+        Sort(result, [](const auto& l, const auto& r) {
+            if (l.Stat.Compacted != r.Stat.Compacted) {
+                return r.Stat.Compacted;
+            }
+
+            return l.Stat.GarbageBlockCount() > r.Stat.GarbageBlockCount();
+        });
+
+        result.crop(count);
+        return result;
+    }
+
+    TVector<TCompactionCounter> GetTopByGarbageIgnoringZeroed(
+        size_t count) const
+    {
+        TVector<TCompactionCounter> result(Reserve(Stats.size()));
+        for (const auto& [rangeStart, stat] : Stats) {
+            if (stat.BlobCount > 0) {
+                result.emplace_back(rangeStart, stat);
+            }
+        }
+
+        Sort(result, [](const auto& l, const auto& r) {
+            if (l.Stat.Compacted != r.Stat.Compacted) {
+                return r.Stat.Compacted;
+            }
+
+            return l.Stat.GarbageIgnoringZeroed() >
+                   r.Stat.GarbageIgnoringZeroed();
+        });
+
+        result.crop(count);
+        return result;
+    }
+
+    ui32 GetNonEmptyRangeCount() const
+    {
+        ui32 count = 0;
+        for (const auto& [_, stat] : Stats) {
+            if (stat.BlobCount > 0) {
+                ++count;
+            }
+        }
+        return count;
+    }
+};
+
+void AssertRangeStatEqual(const TRangeStat& expected, const TRangeStat& actual)
+{
+    UNIT_ASSERT_VALUES_EQUAL(expected.BlobCount, actual.BlobCount);
+    UNIT_ASSERT_VALUES_EQUAL(expected.BlockCount, actual.BlockCount);
+    UNIT_ASSERT_VALUES_EQUAL(expected.UsedBlockCount, actual.UsedBlockCount);
+    UNIT_ASSERT_VALUES_EQUAL(
+        expected.ReadRequestCount,
+        actual.ReadRequestCount);
+    UNIT_ASSERT_VALUES_EQUAL(
+        expected.ReadRequestBlobCount,
+        actual.ReadRequestBlobCount);
+    UNIT_ASSERT_VALUES_EQUAL(
+        expected.ReadRequestBlockCount,
+        actual.ReadRequestBlockCount);
+    UNIT_ASSERT_VALUES_EQUAL(
+        expected.NewlyZeroedBlocks,
+        actual.NewlyZeroedBlocks);
+    UNIT_ASSERT_VALUES_EQUAL(expected.Compacted, actual.Compacted);
+    UNIT_ASSERT_DOUBLES_EQUAL(
+        expected.CompactionScore.Score,
+        actual.CompactionScore.Score,
+        1e-6);
+    UNIT_ASSERT_VALUES_EQUAL(
+        static_cast<int>(expected.CompactionScore.Type),
+        static_cast<int>(actual.CompactionScore.Type));
 }
 
 }   // namespace
@@ -332,6 +542,107 @@ Y_UNIT_TEST_SUITE(TCompactionMapTest)
             const auto nonEmptyCount = map.GetNonEmptyRanges().size();
             UNIT_ASSERT_VALUES_EQUAL(nonEmptyCount, map.GetNonEmptyRangeCount());
             UNIT_ASSERT_VALUES_EQUAL(nonEmptyCount, 99);
+        }
+    }
+
+    Y_UNIT_TEST(RandomizedUpdates)
+    {
+        const TVector<ui32> blockIndices = {
+            GetGroupIndex(0),
+            GetGroupIndex(0) + RangeSize,
+            GetGroupIndex(0) + 2 * RangeSize,
+            GetGroupIndex(1),
+            GetGroupIndex(1) + 3 * RangeSize,
+            GetGroupIndex(3),
+        };
+
+        const auto policy = BuildDefaultCompactionPolicy(5);
+        TCompactionMap map(RangeSize, policy);
+        TReferenceImplementation ref(RangeSize, policy);
+
+        const ui32 iterationsCount = 1000;
+
+        for (size_t i = 0; i < iterationsCount; ++i) {
+            const ui32 blockIndex =
+                blockIndices[
+                    RandomNumber<ui32>(blockIndices.size())];
+            const ui32 blobCount = RandomNumber<ui32>(4);
+            const ui32 blockCount = RandomNumber<ui32>(6);
+            const bool compacted = RandomNumber<ui32>(10) >= 8;
+            const ui32 usedBlockCount = compacted ? blockCount :
+                RandomNumber<ui32>(blockCount + 1);
+            const ui32 maxNewlyZeroedBlocks = blockCount - usedBlockCount;
+            const ui32 newlyZeroedBlocks = compacted ? 0 :
+                RandomNumber<ui32>(maxNewlyZeroedBlocks + 1);
+
+            map.Update(
+                blockIndex,
+                blobCount,
+                blockCount,
+                usedBlockCount,
+                newlyZeroedBlocks,
+                compacted);
+            ref.Update(
+                blockIndex,
+                blobCount,
+                blockCount,
+                usedBlockCount,
+                newlyZeroedBlocks,
+                compacted);
+
+            for (ui32 index : blockIndices) {
+                auto refStat = ref.Get(index);
+                if (refStat) {
+                    AssertRangeStatEqual(map.Get(index), *refStat);
+                }
+            }
+
+            UNIT_ASSERT_DOUBLES_EQUAL(
+                map.GetTop().Stat.CompactionScore.Score,
+                ref.GetTop().Stat.CompactionScore.Score,
+                1e-6);
+            UNIT_ASSERT_VALUES_EQUAL(
+                map.GetTopByGarbageBlockCount().Stat.GarbageBlockCount(),
+                ref.GetTopByGarbageBlockCount().Stat.GarbageBlockCount());
+            UNIT_ASSERT_VALUES_EQUAL(
+                map.GetTopByGarbageIgnoringZeroed().Stat.GarbageIgnoringZeroed(),
+                ref.GetTopByGarbageIgnoringZeroed().Stat.GarbageIgnoringZeroed());
+
+            {
+                const auto mapTops = map.GetTop(3);
+                const auto refTops = ref.GetTop(3);
+                UNIT_ASSERT_VALUES_EQUAL(mapTops.size(), refTops.size());
+                for (size_t j = 0; j < mapTops.size(); ++j) {
+                    UNIT_ASSERT_DOUBLES_EQUAL(
+                        mapTops[j].Stat.CompactionScore.Score,
+                        refTops[j].Stat.CompactionScore.Score,
+                        1e-6);
+                }
+            }
+            {
+                const auto mapTops = map.GetTopByGarbageBlockCount(3);
+                const auto refTops = ref.GetTopByGarbageBlockCount(3);
+                UNIT_ASSERT_VALUES_EQUAL(mapTops.size(), refTops.size());
+                for (size_t j = 0; j < mapTops.size(); ++j) {
+                    UNIT_ASSERT_VALUES_EQUAL(
+                        mapTops[j].Stat.GarbageBlockCount(),
+                        refTops[j].Stat.GarbageBlockCount());
+                }
+            }
+            {
+                const auto mapTops = map.GetTopByGarbageIgnoringZeroed(3);
+                const auto refTops = ref.GetTopByGarbageIgnoringZeroed(3);
+                UNIT_ASSERT_VALUES_EQUAL(mapTops.size(), refTops.size());
+                for (size_t j = 0; j < mapTops.size(); ++j) {
+                    UNIT_ASSERT_VALUES_EQUAL(
+                        mapTops[j].Stat.GarbageIgnoringZeroed(),
+                        refTops[j].Stat.GarbageIgnoringZeroed());
+                }
+            }
+
+            UNIT_ASSERT_VALUES_EQUAL(
+                map.GetNonEmptyRangeCount(),
+                ref.GetNonEmptyRangeCount());
         }
     }
 }
