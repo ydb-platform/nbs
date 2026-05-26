53fd94168f4bb429619305688b470b476e2d78ab
Taras Pashkin <32797206+tpashkin@users.noreply.github.com>
2026-05-26 19:00:36 +0300

    backport 'EXT-1634: DnsResolver Add trailing dot option' from ydb main (#6004)
    See https://github.com/ydb-platform/ydb/pull/27774
    
    >### Changelog entry
    >
    >Added an option to add a trailing dot to all resolving domains
    > 
    >nameservice_config:
    >  add_trailing_dot: true
    > 
    >### Changelog category
    >
    >   New feature
    >
    >### Description for reviewers
    >
    >If an FQDN doesn’t end with a dot, multiple search queries may be sent
    to handle the search domain configuration. This can create additional
    load on DNS.

--- contrib/ydb/library/actors/dnsresolver/dnsresolver.cpp
+++ contrib/ydb/library/actors/dnsresolver/dnsresolver.cpp
@@ -130,6 +130,14 @@ namespace NDnsResolver {
         std::atomic<size_t> Activations{ 0 };
     };
 
+    static TString AddTrailingDot(TString&& name, bool addTrailingDot) noexcept
+    {
+        if (addTrailingDot && !name.empty() && name.back() != '.') {
+            name += ".";
+        }
+        return name;
+    }
+
     class TSimpleDnsResolver
         : public TActor<TSimpleDnsResolver>
         , private TAresLibraryInitBase
@@ -290,6 +298,7 @@ namespace NDnsResolver {
             memset(&hints, 0, sizeof(hints));
             hints.ai_flags = ARES_AI_NOSORT;
             hints.ai_family = family;
+            name = AddTrailingDot(std::move(name), reqCtx->Self->Options.AddTrailingDot);
             ares_getaddrinfo(AresChannel, name.c_str(), nullptr, &hints, &TThis::GetAddrInfoAresCallback, reqCtx.Get());
         }
 
