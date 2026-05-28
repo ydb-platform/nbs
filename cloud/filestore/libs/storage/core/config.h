c9b315857246f21208df3c7564d8c60ad321cb1f
Mikhail Montsev <svartmetal@nebius.com>
2026-05-28 22:34:18 +0100

    [Filestore] support SchemeCache, part 1: DescribeScheme (#6024)
    ### Notes
    Implemented SchemeCache support and added tests for StorageSSProxy,
    ModifyScheme will be supported later
    
    ### Issue
    #4066

--- cloud/filestore/libs/storage/core/config.h
+++ cloud/filestore/libs/storage/core/config.h
@@ -403,6 +403,8 @@ public:
     [[nodiscard]] bool GetHideFileNamesInTabletDirectoryViewer() const;
 
     [[nodiscard]] bool GetUseCustomReadDataResponseParser() const;
+
+    [[nodiscard]] bool GetUseSchemeCache() const;
 };
 
 }   // namespace NCloud::NFileStore::NStorage
