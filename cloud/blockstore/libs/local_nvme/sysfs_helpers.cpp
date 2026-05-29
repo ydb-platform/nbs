ydb-platform/nbs

commit 0d74ea3da3a6c04f495346baf3b6fed163fcca78

Add support of IOMMUFD for local NVMe (#5861)
### Notes
IOMMU groups are being superseded by VFIO characted devices and IOMMUFD
interface that give much more flexibility and control.
Add support of IOMMUFD for local NVMe service.

#5250

---------

Signed-off-by: Anton Kuchin <antonkuchin@nebius.com>

cloud/blockstore/libs/local_nvme/sysfs_helpers.cpp
@@ -118,6 +118,19 @@ public:
             device.SetIOMMUGroup(std::stoul(value));
         }
 
+        const TFsPath vfioDevDir = path / "vfio-dev";
+        if (vfioDevDir.Exists()) {
+            TVector<TString> children;
+
+            vfioDevDir.ListNames(children);
+            for (const auto& child: children) {
+                if (child.StartsWith("vfio")) {
+                    device.SetVfioDevName(child);
+                    break;
+                }
+            }
+        }
+
         return device;
     }
 };

cloud/blockstore/libs/local_nvme/sysfs_helpers_ut.cpp
@@ -96,6 +96,9 @@ struct TFixture: public NUnitTest::TBaseFixture
             const auto& device = Devices[1];
             const TFsPath devicePath = GetPCIDevicePath(device);
             NFs::SymLink(VFIODriverPath, devicePath / "driver");
+
+            const TFsPath vfioDeviceDir = devicePath / "vfio-dev" / device.GetVfioDevName();
+            NFs::MakeDirectoryRecursive(vfioDeviceDir);
         }
     }
 
@@ -133,6 +136,7 @@ struct TFixture: public NUnitTest::TBaseFixture
                 SerialNumber: "NVME_1"
                 PCIAddress: "0000:31:00.0"
                 IOMMUGroup: 20
+                VfioDevName: "vfio0"
                 VendorId: 0x300
                 DeviceId: 0x400
                 Model: "Test NVMe 2"
@@ -306,6 +310,9 @@ Y_UNIT_TEST_SUITE(TSysFsHelpersTest)
             UNIT_ASSERT_VALUES_EQUAL(
                 expected.GetDeviceId(),
                 device.GetDeviceId());
+            UNIT_ASSERT_VALUES_EQUAL(
+                expected.GetVfioDevName(),
+                device.GetVfioDevName());
         }
     }
 }

cloud/blockstore/libs/local_nvme/device_provider_ut.cpp
@@ -24,6 +24,7 @@ TVector<NProto::TNVMeDevice> PrepareDeviceFile(const TString& path)
             SerialNumber: "NVME_0"
             PCIAddress: "f1:00.0"
             IOMMUGroup: 10
+            VfioDevName: "vfio0"
             VendorId: 0x100
             DeviceId: 0x200
             Model: "Test NVMe 1"

cloud/blockstore/libs/local_nvme/protos/local_nvme.proto
@@ -25,6 +25,9 @@ message TNVMeDevice
 
     // Model of NVMe.
     string Model = 6;
+
+    // vfio device name.
+    optional string VfioDevName = 7;
 }
 
 // List of NVMe devices.

cloud/blockstore/libs/local_nvme/service_linux_ut.cpp
@@ -268,6 +268,7 @@ struct TFixtureBase: public NUnitTest::TBaseFixture
                 SerialNumber: "NVME_0"
                 PCIAddress: "0000:f1:00.0"
                 IOMMUGroup: 10
+                VfioDevName: "vfio0"
                 VendorId: 0x100
                 DeviceId: 0x200
                 Model: "Test NVMe 1"

cloud/blockstore/libs/local_nvme/service_proxy.cpp
@@ -72,6 +72,9 @@ public:
                     if (src.HasIOMMUGroup()) {
                         dst->SetIOMMUGroup(src.GetIOMMUGroup());
                     }
+                    if (src.HasVfioDevName()) {
+                        dst->SetVfioDevName(src.GetVfioDevName());
+                    }
                 }
 
                 return response;

cloud/blockstore/libs/local_nvme/service_proxy_ut.cpp
@@ -100,6 +100,7 @@ struct TFixture: public NUnitTest::TBaseFixture
                 SerialNumber: "NVME_0"
                 PCIAddress: "0000:f1:00.0"
                 IOMMUGroup: 10
+                VfioDevName: "vfio0"
                 VendorId: 0x100
                 DeviceId: 0x200
                 Model: "Test NVMe 1"
@@ -165,6 +166,16 @@ Y_UNIT_TEST_SUITE(TLocalNVMeServiceProxyTest)
                 UNIT_ASSERT_VALUES_EQUAL(
                     expected.GetIOMMUGroup(),
                     device.GetIOMMUGroup());
+            }
+
+            UNIT_ASSERT_VALUES_EQUAL(
+                expected.HasVfioDevName(),
+                device.HasVfioDevName());
+
+            if (expected.HasVfioDevName()) {
+                UNIT_ASSERT_VALUES_EQUAL(
+                    expected.GetVfioDevName(),
+                    device.GetVfioDevName());
             }
         }
     }

cloud/blockstore/public/api/protos/local_nvme.proto
@@ -19,6 +19,9 @@ message TNVMeDeviceDesc
 
     // IOMMU group number.
     optional uint32 IOMMUGroup = 3;
+
+    // vfio device name.
+    optional string VfioDevName = 4;
 }
 
 ////////////////////////////////////////////////////////////////////////////////

commit 1ef6e63d0d194881936a52beff900ee106865c7b

[NBS] Local NVMe service: Fix detection of VFIO device chardev
Original IOMMUFD patch relied on wrong assumption that the directory
"/sys/bus/pci/devices/<PCI_ADDR>/vfio-dev/vfio<N>" is present only when
device supports IOMMUFD.
Turns out that when VFIO_DEVICE_CDEV is not supported by kernel or
disabled the directory still exists but it doesn't contain "dev" entry.

Detect support for vfio device chardev by checking presence of "dev"
entry.

Fixes: 0d74ea3da3a6 ("Add support of IOMMUFD for local NVMe (#5861)")
Signed-off-by: Anton Kuchin <antonkuchin@nebius.com>

cloud/blockstore/libs/local_nvme/sysfs_helpers.cpp
@@ -60,7 +60,10 @@ auto GetVFioDeviceName(const TFsPath& pciDevicePath) -> TString
                 << vfioDevDir.GetPath().Quote() << " (already have "
                 << devName.Quote() << ", now also " << child.Quote() << ")");
 
-        devName = child;
+        const TFsPath vfioDevice = vfioDevDir / child / "dev";
+        if (vfioDevice.Exists()) {
+            devName = child;
+        }
     }
 
     return devName;

cloud/blockstore/libs/local_nvme/sysfs_helpers_ut.cpp
@@ -61,7 +61,7 @@ struct TFixture: public NUnitTest::TBaseFixture
         VFIODriverPath = PrepareDriver("vfio-pci");
 
         Devices = PrepareDevices();
-        UNIT_ASSERT_VALUES_EQUAL(3, Devices.size());
+        UNIT_ASSERT_VALUES_EQUAL(4, Devices.size());
 
         for (const auto& device: Devices) {
             const TFsPath devicePath = GetPCIDevicePath(device);
@@ -99,6 +99,21 @@ struct TFixture: public NUnitTest::TBaseFixture
         // NVME_1 bound to the vfio-pci driver
         {
             const auto& device = Devices[1];
+            const TFsPath devicePath = GetPCIDevicePath(device);
+            NFs::SymLink(VFIODriverPath, devicePath / "driver");
+
+            const TFsPath vfioDeviceDir =
+                devicePath / "vfio-dev" / device.GetVfioDevName();
+            NFs::MakeDirectoryRecursive(vfioDeviceDir);
+            TFileOutput(vfioDeviceDir / "dev").Write("501:0");
+        }
+
+        // NVME_2 not bound to any driver, nothing to setup
+
+        // NVME_3 bound to the vfio-pci driver but doesn't have VFIO device
+        // (VFIO_DEVICE_CDEV not supported by kernel or disabled)
+        {
+            const auto& device = Devices[3];
             const TFsPath devicePath = GetPCIDevicePath(device);
             NFs::SymLink(VFIODriverPath, devicePath / "driver");
 
@@ -156,6 +171,15 @@ struct TFixture: public NUnitTest::TBaseFixture
                 VendorId: 0x500
                 DeviceId: 0x600
                 Model: "Test NVMe 3"
+                NumaNode: 1
+            }
+            Devices {
+                SerialNumber: "NVME_3"
+                PCIAddress: "0000:34:00.0"
+                IOMMUGroup: 40
+                VendorId: 0x700
+                DeviceId: 0x800
+                Model: "Test NVMe 4"
                 NumaNode: 1
             }
         )",