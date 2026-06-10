889377208cb9149a5a0be59c805e9cd90d1f22ca
Aleksei Kobzev <kobzonega@yandex.ru>
2026-06-10 09:54:29 +0000

    impl ipc_stub fastshard

---
+++ cloud/filestore/libs/storage/fastshard/ipc/ipc_stub.cpp
@@ -0,0 +1,21 @@
+#include "ipc.h"
+
+#include <cerrno>
+
+namespace NCloud::NFileStore::NStorage::NFastShard {
+
+////////////////////////////////////////////////////////////////////////////////
+
+int RecvAll(int fd, void* buf, size_t len)
+{
+    Y_UNUSED(fd, buf, len);
+    return EIO;
+}
+
+int SendAll(int fd, const void* buf, size_t len)
+{
+    Y_UNUSED(fd, buf, len);
+    return EIO;
+}
+
+}   // namespace NCloud::NFileStore::NStorage::NFastShard
