da38ea17aecb97a02d615732d30b435f7a00887b
proller <proller@github.com>
2026-06-22 20:33:08 +0200

    Qemu build with static libslirp

--- cloud/storage/core/tools/testing/qemu/build/__main__.py
+++ cloud/storage/core/tools/testing/qemu/build/__main__.py
@@ -6,6 +6,7 @@ import argparse
 import contextlib
 import logging
 import os
+import platform
 import re
 import shutil
 import subprocess
@@ -24,9 +25,8 @@ def tmpdir(**kwargs):
 QEMU_CONFIG = [
     '--static',
     '--prefix=/usr',
-    '--extra-cflags=-O3 -fno-semantic-interposition -falign-functions=32 -D_FORTIFY_SOURCE=2 -fPIE -Wno-maybe-uninitialized -Wno-array-bounds',
+    '--extra-cflags=-O3 -fno-semantic-interposition -falign-functions=32 -D_FORTIFY_SOURCE=2 -fPIE -Wno-maybe-uninitialized -Wno-array-bounds -Wno-deprecated-declarations',
     '--extra-ldflags=-z noexecstack -z relro -z now',
-    '--target-list=x86_64-softmmu aarch64-softmmu',
     '--audio-drv-list=',
     '--enable-attr',
     '--enable-cap-ng',
@@ -94,6 +94,7 @@ QEMU_DEPS = [
     'libpixman-1-dev',
     'libpng-dev',
     'libtool',
+    'meson',
     'ninja-build',
     'pkg-config',
     'pkg-config',
@@ -107,12 +108,16 @@ QEMU_DEPS = [
 
 QEMU_CONFIG_MIN_VERSION = [
     ('--disable-gio', (7, 0, 0)),
+    ('--enable-slirp', (7, 0, 0)),
 ]
 
 QEMU_CONFIG_MAX_VERSION = [
     ('--enable-vnc-png', (7, 0, 0)),
     ('--disable-tcmalloc', (6, 0, 0)),
 ]
+
+LIBSLIRP_BUILD_SCRIPT = 'build-libslirp-static.sh'
+LIBSLIRP_DEFAULT_REF = 'v4.9.3'
 
 GIT_TAG_VERSION_RE = re.compile(
     r'(?:^|[^0-9])(\d+)\.(\d+)(?:\.(\d+))?(?:[-.]?rc(\d+))?(?=$|[^0-9])')
@@ -154,6 +159,9 @@ def add_config(config, package):
         config.append(package)
 
 
+def has_config(config, package):
+    return package in config
+
 def config_matches_min_version(git_tag, version):
     git_tag_version = git_tag_version_key(git_tag)
     return git_tag_version is not None and not git_tag_less_than(git_tag, version)
@@ -165,11 +173,7 @@ def config_matches_max_version(git_tag, version):
 
 
 def tag_tgz_path(path, git_tag):
-    if not git_tag:
-        return path
-
     directory, filename = os.path.split(path)
-    tag = re.sub(r'[^A-Za-z0-9._-]+', '_', git_tag)
 
     if filename.endswith('.tar.gz'):
         basename = filename[:-len('.tar.gz')]
@@ -177,11 +181,100 @@ def tag_tgz_path(path, git_tag):
     else:
         basename, extension = os.path.splitext(filename)
 
-    return os.path.join(directory, '{}-{}{}'.format(basename, tag, extension))
+    tags = []
+    if git_tag:
+        tags.append(re.sub(r'[^A-Za-z0-9._-]+', '_', git_tag))
+    tags.append(platform.machine().lower())
+
+    return os.path.join(directory, '{}-{}{}'.format(
+        basename,
+        '-'.join(tags),
+        extension))
+
+
+def qemu_target_list():
+    if platform.machine().lower() in ("aarch64", "arm64"):
+        return "aarch64-softmmu"
+
+    return "x86_64-softmmu"
+
+def pkg_config_variable(package, variable):
+    try:
+        value = subprocess.check_output(
+            ["pkg-config", "--variable=" + variable, package]
+        )
+    except (OSError, subprocess.CalledProcessError):
+        return None
+
+    if isinstance(value, bytes):
+        value = value.decode("utf-8")
+
+    return value.strip()
+
+
+def has_static_slirp():
+    libdir = pkg_config_variable("slirp", "libdir")
+    return bool(libdir and os.path.exists(os.path.join(libdir, "libslirp.a")))
+
+
+def prepend_env_path(name, path):
+    value = os.environ.get(name)
+    if value:
+        os.environ[name] = path + os.pathsep + value
+    else:
+        os.environ[name] = path
+
+
+def build_static_slirp():
+    script = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), LIBSLIRP_BUILD_SCRIPT
+    )
+
+    prefix = os.path.abspath("libslirp-static")
+    src_dir = os.path.abspath("libslirp-src")
+    build_dir = os.path.abspath("build-libslirp")
+
+    cmd = [
+        script,
+        "--prefix",
+        prefix,
+        "--src-dir",
+        src_dir,
+        "--build-dir",
+        build_dir,
+    ]
+
+    libslirp_ref = os.environ.get("LIBSLIRP_REF", LIBSLIRP_DEFAULT_REF)
+    if libslirp_ref:
+        cmd += ["--ref", libslirp_ref]
+
+    run(cmd)
+
+    prepend_env_path("PKG_CONFIG_PATH", os.path.join(prefix, "lib", "pkgconfig"))
+
+
+def ensure_static_slirp(config):
+    if not has_config(config, "--static") or not has_config(config, "--enable-slirp"):
+        return
+
+    if has_static_slirp():
+        return
+
+    build_static_slirp()
+
+    if has_static_slirp():
+        return
+
+    raise RuntimeError(
+        "Static QEMU with -netdev user support requires libslirp.a. "
+        "build-libslirp-static.sh did not make it visible to pkg-config. "
+        "Check the helper output or build QEMU without --static."
+    )
 
 
 def qemu_config(args, src_dir):
     config = list(QEMU_CONFIG)
+    config.append('--target-list=' + qemu_target_list())
 
     for package, version in QEMU_CONFIG_MIN_VERSION:
         if config_matches_min_version(args.git_tag, version):
@@ -231,6 +324,7 @@ def build(args):
             src_dir = build_dir
 
         config = qemu_config(args, src_dir)
+        ensure_static_slirp(config)
 
         help = subprocess.check_output(
             [src_dir + '/configure', '--help'], cwd=build_dir)
