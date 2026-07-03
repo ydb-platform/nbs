d38f944995f4d0e88a3c6122b31829acd75486aa
Andrei Nasonov <nasonov@nebius.com>
2026-07-03 18:43:20 +0200

    Add calculation of ContiguousIntervalCount and IntervalSum for TDisjointIntervalMap

--- cloud/storage/core/libs/common/disjoint_interval_map.h
+++ cloud/storage/core/libs/common/disjoint_interval_map.h
@@ -4,8 +4,9 @@
 #include <util/generic/map.h>
 #include <util/generic/yexception.h>
 
-
 namespace NCloud {
+
+namespace NPrivate {
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -20,6 +21,12 @@ public:
         TValue Value;
     };
 
+    struct TStats
+    {
+        size_t ContiguousIntervalCount = 0;
+        TKey IntervalSum = 0;
+    };
+
     using TData = TMap<TKey, TItem>;
     using TIterator = typename TData::iterator;
     using TReverseIterator = typename TData::reverse_iterator;
@@ -31,11 +38,11 @@ public:
 private:
     TData Data;
 
-public:
+protected:
     // Add a new interval [begin, end) -> value into the map
     // An exception is thrown if it intersects with any of the existing
     // intervals
-    void Add(TKey begin, TKey end, TValue value)
+    void Add(TKey begin, TKey end, TValue value, TStats* stats)
     {
         Y_ENSURE(
             begin < end,
@@ -52,17 +59,25 @@ public:
                 << ") failed because it overlaps with the existing interval ["
                 << it->second.Begin << ", " << it->second.End << ")");
 
-        Data.emplace_hint(
+        it = Data.emplace_hint(
             it,
             end,
             TItem{.Begin = begin, .End = end, .Value = std::move(value)});
+
+        if (stats) {
+            UpdateStats(it, *stats, true);
+        }
     }
 
-    void Remove(TConstIterator iterator)
+    void Remove(TConstIterator iterator, TStats* stats)
     {
+        if (stats) {
+            UpdateStats(iterator, *stats, false);
+        }
         Data.erase(iterator);
     }
 
+public:
     // Visit each interval that intersects with [begin, end)
     // Note: it is allowed to remove the current element from the visitor
     void VisitOverlapping(TKey begin, TKey end, const TVisitor& visitor)
@@ -134,6 +149,93 @@ public:
     TConstReverseIterator rend() const
     {
         return Data.rend();
+    }
+
+private:
+    void UpdateStats(TConstIterator iterator, TStats& stats, bool isAdd) const
+    {
+        if (isAdd) {
+            stats.IntervalSum += iterator->second.End - iterator->second.Begin;
+            stats.ContiguousIntervalCount++;
+        } else {
+            stats.IntervalSum -= iterator->second.End - iterator->second.Begin;
+            stats.ContiguousIntervalCount--;
+        }
+
+        if (iterator != Data.begin()) {
+            auto prev = std::prev(iterator);
+            if (prev->second.End == iterator->second.Begin) {
+                if (isAdd) {
+                    stats.ContiguousIntervalCount--;
+                } else {
+                    stats.ContiguousIntervalCount++;
+                }
+            }
+        }
+
+        auto next = std::next(iterator);
+        if (next != Data.end() && next->second.Begin == iterator->second.End) {
+            if (isAdd) {
+                stats.ContiguousIntervalCount--;
+            } else {
+                stats.ContiguousIntervalCount++;
+            }
+        }
+    }
+};
+
+}   // namespace NPrivate
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TKey, class TValue>
+class TDisjointIntervalMap: public NPrivate::TDisjointIntervalMap<TKey, TValue>
+{
+private:
+    using TBase = NPrivate::TDisjointIntervalMap<TKey, TValue>;
+
+public:
+    void Add(TKey begin, TKey end, TValue value)
+    {
+        TBase::Add(begin, end, std::move(value), nullptr);
+    }
+
+    void Remove(typename TBase::TConstIterator iterator)
+    {
+        TBase::Remove(iterator, nullptr);
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TKey, class TValue>
+class TDisjointIntervalMapWithStats
+    : public NPrivate::TDisjointIntervalMap<TKey, TValue>
+{
+private:
+    using TBase = NPrivate::TDisjointIntervalMap<TKey, TValue>;
+
+    typename TBase::TStats Stats;
+
+public:
+    void Add(TKey begin, TKey end, TValue value)
+    {
+        TBase::Add(begin, end, std::move(value), &Stats);
+    }
+
+    void Remove(typename TBase::TConstIterator iterator)
+    {
+        TBase::Remove(iterator, &Stats);
+    }
+
+    size_t GetContiguousIntervalCount() const
+    {
+        return Stats.ContiguousIntervalCount;
+    }
+
+    TKey GetIntervalSum() const
+    {
+        return Stats.IntervalSum;
     }
 };
 
