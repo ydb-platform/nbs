224044bf1fb22f2c5b53658737ab4714e0f12871
Pavel Misko <misko.pawel@gmail.com>
2026-07-08 19:18:54 +0200

    tweak tests

--- cloud/storage/core/libs/journalled_device_tcp_server/server_ut.cpp
+++ cloud/storage/core/libs/journalled_device_tcp_server/server_ut.cpp
@@ -7,7 +7,7 @@
 #include <library/cpp/testing/unittest/registar.h>
 #include <library/cpp/testing/unittest/tests_data.h>
 
-#include <mutex>
+#include <functional>
 
 namespace NCloud::NJournalled {
 
@@ -17,45 +17,30 @@ namespace {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-struct TDevice
+struct TTestBackend: public IServerBackend
 {
-    TString Id;
-    TString ClientId;
+    using TAcquireDevicesFunc =
+        std::function<TFuture<NProto::TAcquireDevicesResponse>(
+            NProto::TAcquireDevicesRequest)>;
 
-    explicit TDevice(TString id)
-        : Id(std::move(id))
-    {}
-};
+    using TReleaseDevicesFunc =
+        std::function<TFuture<NProto::TReleaseDevicesResponse>(
+            NProto::TReleaseDevicesRequest)>;
 
-class TTestBackend: public IServerBackend
-{
-private:
-    mutable std::mutex Mutex;
-    TVector<TDevice> Devices;
+    using TReadPagesFunc = std::function<TFuture<NProto::TReadPagesResponse>(
+        NProto::TReadPagesRequest)>;
 
-public:
-    explicit TTestBackend(TVector<TDevice> devices)
-        : Devices(std::move(devices))
-    {}
+    using TWriteLogRecordFunc =
+        std::function<TFuture<NProto::TWriteLogRecordResponse>(
+            NProto::TWriteLogRecordRequest)>;
 
-    auto GetDevices() const -> TVector<TDevice>
-    {
-        std::unique_lock lock{Mutex};
-
-        return Devices;
-    }
-
-    auto GetDeviceById(const TString& id) const -> std::optional<TDevice>
-    {
-        std::unique_lock lock{Mutex};
-
-        auto it = std::ranges::find(Devices, id, &TDevice::Id);
-        if (it == Devices.end()) {
-            return std::nullopt;
-        }
-
-        return *it;
-    }
+    TPromise<TAcquireDevicesFunc> AcquireDevicesImpl =
+        NewPromise<TAcquireDevicesFunc>();
+    TPromise<TReleaseDevicesFunc> ReleaseDevicesImpl =
+        NewPromise<TReleaseDevicesFunc>();
+    TPromise<TReadPagesFunc> ReadPagesImpl = NewPromise<TReadPagesFunc>();
+    TPromise<TWriteLogRecordFunc> WriteLogRecordImpl =
+        NewPromise<TWriteLogRecordFunc>();
 
     [[nodiscard]] auto AcquireDevices(
         TInstant now,
@@ -64,16 +49,9 @@ public:
     {
         Y_UNUSED(now);
 
-        std::unique_lock lock{Mutex};
-
-        NProto::TAcquireDevicesResponse response;
-        *response.MutableError() = AcquireDevicesImpl(
-            TVector<TString>(
-                request.GetDeviceUUIDs().begin(),
-                request.GetDeviceUUIDs().end()),
-            request.GetHeaders().GetClientId());
-
-        return MakeFuture(response);
+        return AcquireDevicesImpl.GetFuture().Apply(
+            [request](const auto& future)
+            { return future.GetValue()(request); });
     }
 
     [[nodiscard]] auto ReleaseDevices(
@@ -83,106 +61,33 @@ public:
     {
         Y_UNUSED(now);
 
-        std::unique_lock lock{Mutex};
-
-        NProto::TReleaseDevicesResponse response;
-        *response.MutableError() = ReleaseDevicesImpl(
-            TVector<TString>(
-                request.GetDeviceUUIDs().begin(),
-                request.GetDeviceUUIDs().end()),
-            request.GetHeaders().GetClientId());
-
-        return MakeFuture(response);
+        return ReleaseDevicesImpl.GetFuture().Apply(
+            [request](const auto& future)
+            { return future.GetValue()(request); });
     }
 
-    [[nodiscard]] virtual auto ReadPages(
+    [[nodiscard]] auto ReadPages(
         TInstant now,
         NProto::TReadPagesRequest request)
         -> TFuture<NProto::TReadPagesResponse> final
     {
         Y_UNUSED(now);
 
-        NProto::TReadPagesResponse response;
-        auto& pageGroups = *response.MutablePageGroups();
-        pageGroups.Reserve(request.PageGroupRefsSize());
-
-        for (const NProto::TDevicePageGroupRef& ref: request.GetPageGroupRefs())
-        {
-            NProto::TDevicePageGroup& group = *pageGroups.Add();
-
-            auto& content = *group.MutableContent();
-
-            content.Reserve(ref.GetPageCount());
-            for (ui64 i = 0; i != ref.GetPageCount(); ++i) {
-                content.Add()->ReserveAndResize(ref.GetPageSize());
-            }
-        }
-
-        return MakeFuture(std::move(response));
+        return ReadPagesImpl.GetFuture().Apply(
+            [request](const auto& future)
+            { return future.GetValue()(request); });
     }
 
-    [[nodiscard]] virtual auto WriteLogRecord(
+    [[nodiscard]] auto WriteLogRecord(
         TInstant now,
         NProto::TWriteLogRecordRequest request)
         -> TFuture<NProto::TWriteLogRecordResponse> final
     {
-        Y_UNUSED(now, request);
+        Y_UNUSED(now);
 
-        return MakeFuture(NProto::TWriteLogRecordResponse());
-    }
-
-private:
-    auto AcquireDevicesImpl(
-        const TVector<TString>& uuids,
-        const TString& clientId) -> NProto::TError
-    {
-        if (!clientId) {
-            return MakeError(E_ARGUMENT, "empty client id");
-        }
-
-        for (const auto& uuid: uuids) {
-            auto it = std::ranges::find(Devices, uuid, &TDevice::Id);
-            if (it == Devices.end()) {
-                return MakeError(
-                    E_NOT_FOUND,
-                    TStringBuilder()
-                        << "Device " << uuid.Quote() << " not found");
-            }
-            if (it->ClientId && it->ClientId != clientId) {
-                return MakeError(E_BS_MOUNT_CONFLICT);
-            }
-        }
-
-        for (const auto& uuid: uuids) {
-            auto it = std::ranges::find(Devices, uuid, &TDevice::Id);
-            it->ClientId = clientId;
-        }
-
-        return {};
-    }
-
-    auto ReleaseDevicesImpl(
-        const TVector<TString>& uuids,
-        const TString& clientId) -> NProto::TError
-    {
-        if (!clientId) {
-            return MakeError(E_ARGUMENT, "empty client id");
-        }
-
-        for (const auto& uuid: uuids) {
-            auto it = std::ranges::find(Devices, uuid, &TDevice::Id);
-            if (it == Devices.end()) {
-                return MakeError(
-                    E_NOT_FOUND,
-                    TStringBuilder()
-                        << "Device " << uuid.Quote() << " not found");
-            }
-            if (it->ClientId == clientId) {
-                it->ClientId.clear();
-            }
-        }
-
-        return {};
+        return WriteLogRecordImpl.GetFuture().Apply(
+            [request](const auto& future)
+            { return future.GetValue()(request); });
     }
 };
 
@@ -200,18 +105,11 @@ struct TFixture: public NUnitTest::TBaseFixture
     void SetUp(NUnitTest::TTestContext& /*testContext*/) override
     {
         Port = PortManager.GetTcpPort();
-
-        Backend = std::make_shared<TTestBackend>(TVector{
-            TDevice("uuid-1"),
-            TDevice("uuid-2"),
-            TDevice("uuid-3"),
-        });
-
+        Backend = std::make_shared<TTestBackend>();
         Executor = TExecutor::Create("TestExecutor");
-
-        Logging =
-            CreateLoggingService("console", {.FiltrationLevel = TLOG_DEBUG});
-
+        Logging = CreateLoggingService(
+            "console",
+            {.FiltrationLevel = TLOG_RESOURCES});
         Server =
             CreateServer(TNetworkAddress{Port}, Logging, Executor, Backend);
 
@@ -282,15 +180,11 @@ public:
 
 Y_UNIT_TEST_SUITE(TDeviceTCPServerTest)
 {
-    Y_UNIT_TEST_F(ShouldAcquireDevices, TFixture)
+    Y_UNIT_TEST_F(ShouldRejectBrokenRequest, TFixture)
     {
         const ui64 requestId = 42;
-        const TString clientId = "client-id";
-        const TString uuid = "uuid-2";
 
         TTestClient client{Port};
-
-        UNIT_ASSERT_VALUES_EQUAL("", Backend->GetDeviceById(uuid)->ClientId);
 
         {
             NProto::TDeviceProtocolRequest request;
@@ -310,32 +204,292 @@ Y_UNIT_TEST_SUITE(TDeviceTCPServerTest)
                 E_ARGUMENT,
                 response.GetProtocolError().GetCode());
         }
+    }
+
+    Y_UNIT_TEST_F(ShouldDispatchRequestsToBackend, TFixture)
+    {
+        const ui64 expectedAcquireDevicesRequestId = 10;
+        const ui64 expectedReleaseDevicesRequestId = 20;
+        const ui64 expectedReadPagesRequestId = 30;
+        const ui64 expectedWriteLogRecordRequestId = 40;
+
+        const auto expectedAcquireDevicesRequest = []
+        {
+            NProto::TAcquireDevicesRequest proto;
+
+            proto.MutableHeaders()->SetClientId("acquire");
+            proto.MutableHeaders()->SetRequestTimeout(10'000);
+            proto.MutableDeviceUUIDs()->Add("uuid-1");
+            proto.MutableDeviceUUIDs()->Add("uuid-2");
+            proto.MutableDeviceUUIDs()->Add("uuid-3");
+
+            return proto;
+        }();
+
+        const auto expectedAcquireDevicesResponse = []
+        {
+            NProto::TAcquireDevicesResponse proto;
+
+            proto.MutableError()->SetCode(E_PRECONDITION_FAILED);
+            proto.MutableError()->SetMessage("expected-acquire-device-error");
+
+            return proto;
+        }();
+
+        const auto expectedReleaseDevicesRequest = []
+        {
+            NProto::TReleaseDevicesRequest proto;
+
+            proto.MutableHeaders()->SetClientId("release");
+            proto.MutableHeaders()->SetRequestTimeout(1000);
+            proto.MutableDeviceUUIDs()->Add("uuid-4");
+            proto.MutableDeviceUUIDs()->Add("uuid-5");
+            proto.MutableDeviceUUIDs()->Add("uuid-6");
+            proto.MutableDeviceUUIDs()->Add("uuid-7");
+            proto.MutableDeviceUUIDs()->Add("uuid-8");
+
+            return proto;
+        }();
+
+        const auto expectedReleaseDevicesResponse = []
+        {
+            NProto::TReleaseDevicesResponse proto;
+
+            proto.MutableError()->SetCode(E_INVALID_STATE);
+            proto.MutableError()->SetMessage("expected-release-device-error");
+
+            return proto;
+        }();
+
+        const auto expectedReadPagesRequest = []
+        {
+            NProto::TReadPagesRequest proto;
+
+            proto.MutableHeaders()->SetClientId("read");
+            proto.MutableHeaders()->SetRequestTimeout(200'000);
+            proto.SetDeviceUUID("uuid-100");
+
+            auto& refs = *proto.MutablePageGroupRefs();
+
+            auto& ref1 = *refs.Add();
+            ref1.SetFirstPageNo(0x8000);
+            ref1.SetPageCount(1024);
+            ref1.SetPageSize(32_KB);
+
+            auto& ref2 = *refs.Add();
+            ref2.SetFirstPageNo(0x1000);
+            ref2.SetPageCount(32);
+            ref2.SetPageSize(4_KB);
+
+            return proto;
+        }();
+
+        const auto expectedReadPagesResponse = []
+        {
+            NProto::TReadPagesResponse proto;
+
+            proto.MutableError()->SetCode(S_ALREADY);
+            proto.MutableError()->SetMessage("expected-read-pages-error");
+
+            auto& groups = *proto.MutablePageGroups();
+
+            auto& group = *groups.Add();
+            group.SetFirstPageNo(0x1000);
+            auto& content = *group.MutableContent();
+            content.Reserve(32);
+            for (int i = 0; i != content.Capacity(); ++i) {
+                content.Add()->resize(4_KB);
+            }
+
+            return proto;
+        }();
+
+        const auto expectedWriteLogRecordRequest = []
+        {
+            NProto::TWriteLogRecordRequest proto;
+
+            proto.MutableHeaders()->SetClientId("write");
+            proto.MutableHeaders()->SetRequestTimeout(17);
+            proto.SetDeviceUUID("uuid-999");
+            proto.SetLogSequenceNumber(10003);
+            auto& groups = *proto.MutablePageGroups();
+
+            {
+                auto& group = *groups.Add();
+                group.SetFirstPageNo(0x1000);
+                auto& content = *group.MutableContent();
+                content.Reserve(100);
+                for (int i = 0; i != content.Capacity(); ++i) {
+                    content.Add()->resize(4_KB);
+                }
+            }
+
+            {
+                auto& group = *groups.Add();
+                group.SetFirstPageNo(0x2000);
+                auto& content = *group.MutableContent();
+                content.Reserve(100);
+                for (int i = 0; i != content.Capacity(); ++i) {
+                    content.Add()->resize(4_KB);
+                }
+            }
+
+            {
+                auto& group = *groups.Add();
+                group.SetFirstPageNo(0x3000);
+                auto& content = *group.MutableContent();
+                content.Reserve(100);
+                for (int i = 0; i != content.Capacity(); ++i) {
+                    content.Add()->resize(4_KB);
+                }
+            }
+
+            return proto;
+        }();
+
+        const auto expectedWriteLogRecordResponse = []
+        {
+            NProto::TWriteLogRecordResponse proto;
+
+            proto.MutableError()->SetCode(E_PRECONDITION_FAILED);
+            proto.MutableError()->SetMessage("expected-write-log-record-error");
+
+            return proto;
+        }();
+
+        TTestClient client{Port};
 
         {
             NProto::TDeviceProtocolRequest request;
-            request.SetRequestId(requestId);
-            auto& acquireDevices = *request.MutableAcquireDevices();
-            acquireDevices.MutableHeaders()->SetClientId(clientId);
-            acquireDevices.AddDeviceUUIDs(uuid);
-
-            client.Send(std::move(request));
+            request.SetRequestId(expectedAcquireDevicesRequestId);
+            request.MutableAcquireDevices()->CopyFrom(
+                expectedAcquireDevicesRequest);
+            client.Send(request);
         }
 
         {
-            NProto::TDeviceProtocolResponse response = client.Receive();
-
-            UNIT_ASSERT_VALUES_EQUAL(requestId, response.GetRequestId());
-            UNIT_ASSERT_EQUAL(
-                NProto::TDeviceProtocolResponse::ResponseCase::kAcquireDevices,
-                response.GetResponseCase());
-
-            auto& acq = response.GetAcquireDevices();
-            UNIT_ASSERT_VALUES_EQUAL(S_OK, acq.GetError().GetCode());
+            NProto::TDeviceProtocolRequest request;
+            request.SetRequestId(expectedReleaseDevicesRequestId);
+            request.MutableReleaseDevices()->CopyFrom(
+                expectedReleaseDevicesRequest);
+            client.Send(request);
         }
 
+        {
+            NProto::TDeviceProtocolRequest request;
+            request.SetRequestId(expectedReadPagesRequestId);
+            request.MutableReadPages()->CopyFrom(expectedReadPagesRequest);
+            client.Send(request);
+        }
+
+        {
+            NProto::TDeviceProtocolRequest request;
+            request.SetRequestId(expectedWriteLogRecordRequestId);
+            request.MutableWriteLogRecord()->CopyFrom(
+                expectedWriteLogRecordRequest);
+            client.Send(request);
+        }
+
+        std::mutex mutex;
+        std::optional<NProto::TAcquireDevicesRequest> acquireDevicesRequest;
+        std::optional<NProto::TReleaseDevicesRequest> releaseDevicesRequest;
+        std::optional<NProto::TReadPagesRequest> readPagesRequest;
+        std::optional<NProto::TWriteLogRecordRequest> writeLogRecordRequest;
+
+        Backend->AcquireDevicesImpl.SetValue(
+            [&](auto request)
+            {
+                std::unique_lock lock(mutex);
+                acquireDevicesRequest = std::move(request);
+                return MakeFuture(expectedAcquireDevicesResponse);
+            });
+
+        Backend->ReleaseDevicesImpl.SetValue(
+            [&](auto request)
+            {
+                std::unique_lock lock(mutex);
+                releaseDevicesRequest = std::move(request);
+                return MakeFuture(expectedReleaseDevicesResponse);
+            });
+
+        Backend->ReadPagesImpl.SetValue(
+            [&](auto request)
+            {
+                std::unique_lock lock(mutex);
+                readPagesRequest = std::move(request);
+                return MakeFuture(expectedReadPagesResponse);
+            });
+
+        Backend->WriteLogRecordImpl.SetValue(
+            [&](auto request)
+            {
+                std::unique_lock lock(mutex);
+                writeLogRecordRequest = std::move(request);
+                return MakeFuture(expectedWriteLogRecordResponse);
+            });
+
+        TVector<NProto::TDeviceProtocolResponse> responses;
+
+        for (ui32 i = 0; i != 4; ++i) {
+            responses.push_back(client.Receive());
+        }
+
+        SortBy(
+            responses,
+            [](const auto& proto) { return proto.GetRequestId(); });
+
         UNIT_ASSERT_VALUES_EQUAL(
-            clientId,
-            Backend->GetDeviceById(uuid)->ClientId);
+            expectedAcquireDevicesRequestId,
+            responses[0].GetRequestId());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedAcquireDevicesResponse.DebugString(),
+            responses[0].GetAcquireDevices().DebugString());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedReleaseDevicesRequestId,
+            responses[1].GetRequestId());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedReleaseDevicesResponse.DebugString(),
+            responses[1].GetReleaseDevices().DebugString());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedReadPagesRequestId,
+            responses[2].GetRequestId());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedReadPagesResponse.DebugString(),
+            responses[2].GetReadPages().DebugString());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedWriteLogRecordRequestId,
+            responses[3].GetRequestId());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedWriteLogRecordResponse.DebugString(),
+            responses[3].GetWriteLogRecord().DebugString());
+
+        UNIT_ASSERT(acquireDevicesRequest);
+        UNIT_ASSERT(releaseDevicesRequest);
+        UNIT_ASSERT(readPagesRequest);
+        UNIT_ASSERT(writeLogRecordRequest);
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedAcquireDevicesRequest.DebugString(),
+            acquireDevicesRequest->DebugString());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedReleaseDevicesRequest.DebugString(),
+            releaseDevicesRequest->DebugString());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedReadPagesRequest.DebugString(),
+            readPagesRequest->DebugString());
+
+        UNIT_ASSERT_VALUES_EQUAL(
+            expectedWriteLogRecordRequest.DebugString(),
+            writeLogRecordRequest->DebugString());
     }
 }
 
