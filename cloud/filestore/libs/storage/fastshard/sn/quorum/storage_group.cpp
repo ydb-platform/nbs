5ca6681c0e533f29ab2b89e9bf7c987a9aa28c37
Andrei Strelkovskii <thinkingwhat@yandex.ru>
2026-07-11 22:37:20 +0200

    issue-5894: fastshard naive storage group stub impl

--- cloud/filestore/libs/storage/fastshard/sn/quorum/storage_group.cpp
+++ cloud/filestore/libs/storage/fastshard/sn/quorum/storage_group.cpp
@@ -1,5 +1,11 @@
 #include "storage_group.h"
 
+#include <cloud/storage/core/libs/common/error.h>
+
+#include <silk/fibers/fiber.h>
+#include <silk/fibers/future.h>
+#include <silk/util/logger.h>
+
 #include <util/generic/vector.h>
 
 namespace NCloud::NFileStore::NStorage::NFastShard {
@@ -8,10 +14,52 @@ namespace {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+struct TAcquireDevicesParams
+{
+    IStorageNodePtr Node;
+    NProto::TAcquireDevicesRequest* Request;
+    NProto::TAcquireDevicesResponse* Response;
+};
+
+int AcquireDevicesFiberMain(TAcquireDevicesParams* params) noexcept
+{
+    *params->Response = params->Node->AcquireDevices(*params->Request);
+    return 0;
+}
+
+struct TReleaseDevicesParams
+{
+    IStorageNodePtr Node;
+    NProto::TReleaseDevicesRequest* Request;
+    NProto::TReleaseDevicesResponse* Response;
+};
+
+int ReleaseDevicesFiberMain(TReleaseDevicesParams* params) noexcept
+{
+    *params->Response = params->Node->ReleaseDevices(*params->Request);
+    return 0;
+}
+
+struct TWriteLogRecordParams
+{
+    IStorageNodePtr Node;
+    NProto::TWriteLogRecordRequest* Request;
+    NProto::TWriteLogRecordResponse* Response;
+};
+
+int WriteLogRecordFiberMain(TWriteLogRecordParams* params) noexcept
+{
+    *params->Response = params->Node->WriteLogRecord(*params->Request);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 class TStorageGroupImpl: public IStorageGroup
 {
 private:
     TVector<IStorageNodePtr> Nodes;
+    std::atomic<ui32> Selector{0};
 
 public:
     explicit TStorageGroupImpl(TVector<IStorageNodePtr> nodes)
@@ -22,29 +70,87 @@ public:
     NProto::TAcquireDevicesResponse AcquireDevices(
         NProto::TAcquireDevicesRequest request) override
     {
-        Y_UNUSED(request);
-        return {};
+        return MirrorRequest<
+            NProto::TAcquireDevicesRequest,
+            NProto::TAcquireDevicesResponse,
+            TAcquireDevicesParams
+        >(AcquireDevicesFiberMain, std::move(request));
     }
 
     NProto::TReleaseDevicesResponse ReleaseDevices(
         NProto::TReleaseDevicesRequest request) override
     {
-        Y_UNUSED(request);
-        return {};
+        return MirrorRequest<
+            NProto::TReleaseDevicesRequest,
+            NProto::TReleaseDevicesResponse,
+            TReleaseDevicesParams
+        >(ReleaseDevicesFiberMain, std::move(request));
     }
 
     NProto::TWriteLogRecordResponse WriteLogRecord(
         NProto::TWriteLogRecordRequest request) override
     {
-        Y_UNUSED(request);
-        return {};
+        return MirrorRequest<
+            NProto::TWriteLogRecordRequest,
+            NProto::TWriteLogRecordResponse,
+            TWriteLogRecordParams
+        >(WriteLogRecordFiberMain, std::move(request));
     }
 
     NProto::TReadPagesResponse ReadPages(
         NProto::TReadPagesRequest request) override
     {
-        Y_UNUSED(request);
-        return {};
+        const ui32 i =
+            Selector.fetch_add(1, std::memory_order_relaxed) % Nodes.size();
+        return Nodes[i]->ReadPages(request);
+    }
+
+private:
+    template <
+        typename TRequest,
+        typename TResponse,
+        typename TParams,
+        typename TFiberMain>
+    TResponse MirrorRequest(TFiberMain fiberMain, TRequest request)
+    {
+        TVector<silk::FiberFuture> futures(Nodes.size());
+        TVector<TResponse> responses(Nodes.size());
+        for (ui32 i = 0; i < Nodes.size(); ++i) {
+            int r = silk::FiberScheduler::run(
+                fiberMain,
+                TParams{
+                    .Node = Nodes[i],
+                    .Request = &request,
+                    .Response = &responses[i]},
+                &futures[i]);
+            Y_ABORT_UNLESS(r == 0, "failed to spawn fiber: %s", ::strerror(r));
+        }
+
+        TResponse response;
+        for (ui32 i = 0; i < Nodes.size(); ++i) {
+            int r = futures[i].wait();
+            if (r) {
+                SILK_ERROR("future error: %s", ::strerror(r));
+                if (!HasError(response.GetError())) {
+                    *response.MutableError() = MakeError(MAKE_SYSTEM_ERROR(r));
+                }
+
+                continue;
+            }
+
+            auto& nodeResponse = responses[i];
+            if (HasError(nodeResponse.GetError())) {
+                SILK_ERROR(
+                    "node error: %s",
+                    FormatError(nodeResponse.GetError()).c_str());
+                if (!HasError(response.GetError())) {
+                    *response.MutableError() =
+                        std::move(*nodeResponse.MutableError());
+                }
+            }
+        }
+
+        return response;
     }
 };
 
