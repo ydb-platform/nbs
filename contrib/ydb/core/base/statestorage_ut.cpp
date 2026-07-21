6ac553ee7080c614cf81e6d169daa0523dd756b5
Ruslan Garashchuk <ruslan.g@nebius.com>
2026-07-21 18:00:04 +0200

    fix state storage replicas selection

--- contrib/ydb/core/base/statestorage_ut.cpp
+++ contrib/ydb/core/base/statestorage_ut.cpp
@@ -1,9 +1,238 @@
 #include "statestorage.h"
 
 #include <util/generic/xrange.h>
+#include <util/generic/mem_copy.h>
+#include <util/generic/algorithm.h>
 #include <library/cpp/testing/unittest/registar.h>
+#include "tabletid.h"
+#include "statestorage_ringwalker.h"
+
 
 namespace NKikimr {
+
+namespace NStateStorageOld {
+struct TStateStorageInfo : public TThrRefBase {
+        struct TSelection {
+            ui32 Sz;
+            TArrayHolder<TActorId> SelectedReplicas;
+
+            TSelection()
+                : Sz(0)
+            {}
+
+            const TActorId* begin() const { return SelectedReplicas.Get(); }
+            const TActorId* end() const { return SelectedReplicas.Get() + Sz; }
+        };
+
+        struct TRing {
+            bool IsDisabled;
+            bool UseRingSpecificNodeSelection;
+            TVector<TActorId> Replicas;
+
+            TActorId SelectReplica(ui32 hash) const;
+            ui32 ContentHash() const;
+        };
+
+        ui32 NToSelect;
+        TVector<TRing> Rings;
+
+        ui32 StateStorageVersion;
+        TVector<ui32> CompatibleVersions;
+
+        void SelectReplicas(ui64 tabletId, TSelection *selection) const;
+        TList<TActorId> SelectAllReplicas() const;
+        ui32 ContentHash() const;
+
+        TStateStorageInfo()
+            : NToSelect(0)
+            , Hash(Max<ui64>())
+        {}
+
+    private:
+        mutable ui64 Hash;
+    };
+
+class TStateStorageRingWalker {
+    const ui32 Sz;
+    const ui32 Delta;
+    ui32 A;
+public:
+    TStateStorageRingWalker(ui32 hash, ui32 sz)
+        : Sz(sz)
+        , Delta(Primes[hash % 128])
+        , A(hash + Delta)
+    {
+        Y_DEBUG_ABORT_UNLESS(Delta > Sz);
+    }
+
+    ui32 Next() {
+        A += Delta;
+        return (A % Sz);
+    }
+};
+
+void TStateStorageInfo::SelectReplicas(ui64 tabletId, TSelection *selection) const {
+    const ui32 hash = StateStorageHashFromTabletID(tabletId);
+    const ui32 total = Rings.size();
+
+    Y_ABORT_UNLESS(NToSelect <= total);
+
+    if (selection->Sz < NToSelect) {
+        selection->SelectedReplicas.Reset(new TActorId[NToSelect]);
+    }
+
+    selection->Sz = NToSelect;
+
+    if (NToSelect == total) {
+        for (ui32 idx : xrange(total)) {
+            selection->SelectedReplicas[idx] = Rings[idx].SelectReplica(hash);
+        }
+    } else { // NToSelect < total, first - select rings with walker, then select concrete node
+        NStateStorageOld::TStateStorageRingWalker walker(hash, total);
+        for (ui32 idx : xrange(NToSelect))
+            selection->SelectedReplicas[idx] = Rings[walker.Next()].SelectReplica(hash);
+    }
+}
+
+TActorId TStateStorageInfo::TRing::SelectReplica(ui32 hash) const {
+    if (Replicas.size() == 1)
+        return Replicas[0];
+
+    Y_ABORT_UNLESS(!Replicas.empty());
+    if (UseRingSpecificNodeSelection) {
+        return Replicas[CombineHashes(hash, ContentHash()) % Replicas.size()];
+    } else {
+        return Replicas[hash % Replicas.size()];
+    }
+}
+
+TList<TActorId> TStateStorageInfo::SelectAllReplicas() const {
+// TODO: we really need this method in such way?
+    TList<TActorId> replicas;
+
+    for (auto &ring : Rings) {
+        for (TActorId replica : ring.Replicas)
+            replicas.push_back(replica);
+    }
+
+    return replicas;
+}
+
+ui32 TStateStorageInfo::TRing::ContentHash() const {
+    ui64 hash = 17;
+    for (TActorId replica : Replicas) {
+        hash = Hash64to32((hash << 32) | replica.Hash32());
+    }
+    return static_cast<ui32>(hash);
+}
+
+ui32 TStateStorageInfo::ContentHash() const {
+    ui64 hash = RelaxedLoad<ui64>(&Hash);
+    if (Y_UNLIKELY(hash == Max<ui64>())) {
+        hash = 37;
+        for (const TRing &ring : Rings) {
+            hash = Hash64to32((hash << 32) | ring.ContentHash());
+        }
+        RelaxedStore<ui64>(&Hash, static_cast<ui32>(hash));
+    }
+    return static_cast<ui32>(hash);
+}
+
+static void CopyStateStorageRingInfo(
+    const NKikimrConfig::TDomainsConfig::TStateStorage::TRing &source,
+    TStateStorageInfo *info,
+    char *serviceId,
+    ui32 depth
+) {
+    info->NToSelect = source.GetNToSelect();
+
+    const bool hasRings = source.RingSize() > 0;
+    const bool hasNodes = source.NodeSize() > 0;
+
+    if (hasRings) { // has explicitely defined rings, use them as info rings
+        Y_ABORT_UNLESS(!hasNodes);
+        info->Rings.resize(source.RingSize());
+
+        for (ui32 iring = 0, ering = source.RingSize(); iring != ering; ++iring) {
+            serviceId[depth] = (iring + 1);
+
+            const NKikimrConfig::TDomainsConfig::TStateStorage::TRing &ring = source.GetRing(iring);
+            info->Rings[iring].UseRingSpecificNodeSelection = ring.GetUseRingSpecificNodeSelection();
+            info->Rings[iring].IsDisabled = ring.GetIsDisabled();
+
+            if (ring.GetUseSingleNodeActorId()) {
+                Y_ABORT_UNLESS(ring.NodeSize() == 1);
+
+                const TActorId replicaActorID = TActorId(ring.GetNode(0), TStringBuf(serviceId, serviceId + 12));
+                info->Rings[iring].Replicas.push_back(replicaActorID);
+            }
+            else {
+               Y_ABORT_UNLESS(ring.NodeSize() > 0);
+
+               for (ui32 inode = 0, enode = ring.NodeSize(); inode != enode; ++inode) {
+                    serviceId[depth + 1] = (inode + 1);
+                    const TActorId replicaActorID = TActorId(ring.GetNode(inode), TStringBuf(serviceId, serviceId + 12));
+                    info->Rings[iring].Replicas.push_back(replicaActorID);
+               }
+            }
+            // reset for next ring
+            serviceId[depth + 1] = char();
+        }
+
+        return;
+    }
+
+    if (hasNodes) { // has explicitely defined replicas, use nodes as 1-node rings
+        Y_ABORT_UNLESS(!hasRings);
+
+        info->Rings.resize(source.NodeSize());
+        for (ui32 inode = 0, enode = source.NodeSize(); inode != enode; ++inode) {
+            serviceId[depth] = (inode + 1);
+
+            const TActorId replicaActorID = TActorId(source.GetNode(inode), TStringBuf(serviceId, serviceId + 12));
+            info->Rings[inode].Replicas.push_back(replicaActorID);
+        }
+
+        return;
+    }
+
+    Y_ABORT("must have rings or legacy node config");
+}
+
+TIntrusivePtr<TStateStorageInfo> BuildStateStorageInfo(char (&namePrefix)[TActorId::MaxServiceIDLength], const NKikimrConfig::TDomainsConfig::TStateStorage& config) {
+    TIntrusivePtr<TStateStorageInfo> info = new TStateStorageInfo();
+    Y_ABORT_UNLESS(config.GetSSId() == 1);
+    info->StateStorageVersion = config.GetStateStorageVersion();
+
+    info->CompatibleVersions.reserve(config.CompatibleVersionsSize());
+    for (ui32 version : config.GetCompatibleVersions()) {
+        info->CompatibleVersions.push_back(version);
+    }
+
+    const size_t offset = FindIndex(namePrefix, char());
+    Y_ABORT_UNLESS(offset != NPOS && (offset + sizeof(ui32)) < TActorId::MaxServiceIDLength);
+
+    const ui32 stateStorageGroup = 1;
+    memcpy(namePrefix + offset, reinterpret_cast<const char *>(&stateStorageGroup), sizeof(ui32));
+    CopyStateStorageRingInfo(config.GetRing(), info.Get(), namePrefix, offset + sizeof(ui32));
+
+    return info;
+}
+
+void BuildStateStorageInfos(const NKikimrConfig::TDomainsConfig::TStateStorage& config,
+    TIntrusivePtr<TStateStorageInfo> &stateStorageInfo,
+    TIntrusivePtr<TStateStorageInfo> &boardInfo,
+    TIntrusivePtr<TStateStorageInfo> &schemeBoardInfo)
+{
+    char ssr[TActorId::MaxServiceIDLength] = { 's', 's', 'r' }; // state storage replica
+    char ssb[TActorId::MaxServiceIDLength] = { 's', 's', 'b' }; // state storage board
+    char sbr[TActorId::MaxServiceIDLength] = { 's', 'b', 'r' }; // scheme board replica
+
+    stateStorageInfo = BuildStateStorageInfo(ssr, config);
+    boardInfo = BuildStateStorageInfo(ssb, config);
+    schemeBoardInfo = BuildStateStorageInfo(sbr, config);
+}
+}
 
 Y_UNIT_TEST_SUITE(TStateStorageConfig) {
 
@@ -19,6 +248,7 @@ Y_UNIT_TEST_SUITE(TStateStorageConfig) {
         }
     }
 
+    /*
     ui64 StabilityRun(ui32 replicas, ui32 nToSelect, ui32 replicasInRing, bool useRingSpecificNodeSelection) {
         ui64 retHash = 0;
 
@@ -28,6 +258,28 @@ Y_UNIT_TEST_SUITE(TStateStorageConfig) {
         TStateStorageInfo::TSelection selection;
         for (ui64 tabletId = 8000000; tabletId < 9000000; ++tabletId) {
             info.SelectReplicas(tabletId, &selection);
+            Y_ABORT_UNLESS(nToSelect == selection.Sz);
+            for (ui32 idx : xrange(nToSelect))
+                retHash = CombineHashes<ui64>(retHash, selection.SelectedReplicas[idx].Hash());
+        }
+        return retHash;
+    }
+    */
+
+    ui64 StabilityRun(ui32 replicas, ui32 nToSelect, ui32 replicasInRing, bool useRingSpecificNodeSelection) {
+        ui64 retHash = 0;
+
+        TStateStorageInfo info;
+        FillStateStorageInfo(&info, replicas, nToSelect, replicasInRing, useRingSpecificNodeSelection);
+
+        TStateStorageInfo::TSelection selection;
+        for (ui64 tabletId = 8000000; tabletId < 9000000; ++tabletId) {
+            info.SelectReplicas(tabletId, &selection);
+            std::unordered_set<TActorId> ids;
+            for (ui32 i : xrange(selection.Sz)) {
+                ids.insert(selection.SelectedReplicas[i]);
+            }
+            Y_ABORT_UNLESS(ids.size() == selection.Sz);
             Y_ABORT_UNLESS(nToSelect == selection.Sz);
             for (ui32 idx : xrange(nToSelect))
                 retHash = CombineHashes<ui64>(retHash, selection.SelectedReplicas[idx].Hash());
@@ -57,31 +309,31 @@ Y_UNIT_TEST_SUITE(TStateStorageConfig) {
 
     Y_UNIT_TEST(TestReplicaSelection) {
         UNIT_ASSERT(StabilityRun(3, 3, 1, false) == 17606246762804570019ULL);
-        UNIT_ASSERT(StabilityRun(13, 3, 1, false) == 421354124534079828ULL);
-        UNIT_ASSERT(StabilityRun(13, 9, 1, false) == 10581416019959162949ULL);
+        UNIT_ASSERT(StabilityRun(13, 3, 1, false) == 6799095354188407094ULL);
+        UNIT_ASSERT(StabilityRun(13, 9, 1, false) == 9959984117877048199ULL);
         UNIT_ASSERT(StabilityRun(3, 3, 1, true) == 17606246762804570019ULL);
-        UNIT_ASSERT(StabilityRun(13, 3, 1, true) == 421354124534079828ULL);
-        UNIT_ASSERT(StabilityRun(13, 9, 1, true) == 10581416019959162949ULL);
+        UNIT_ASSERT(StabilityRun(13, 3, 1, true) == 6799095354188407094ULL);
+        UNIT_ASSERT(StabilityRun(13, 9, 1, true) == 9959984117877048199ULL);
     }
 
     Y_UNIT_TEST(TestMultiReplicaFailDomains) {
         UNIT_ASSERT(StabilityRun(3, 3, 3, false) == 12043409773822600429ULL);
-        UNIT_ASSERT(StabilityRun(13, 3, 5, false) == 3265154396592024904ULL);
-        UNIT_ASSERT(StabilityRun(13, 9, 8, false) == 12079940289459527060ULL);
+        UNIT_ASSERT(StabilityRun(13, 3, 5, false) == 16389704234708466102ULL);
+        UNIT_ASSERT(StabilityRun(13, 9, 8, false) == 15827315848675537518ULL);
         UNIT_ASSERT(StabilityRun(3, 3, 3, true) == 7845257406715748850ULL);
-        UNIT_ASSERT(StabilityRun(13, 3, 5, true) == 1986618578793030392ULL);
-        UNIT_ASSERT(StabilityRun(13, 9, 8, true) == 6173011524598124144ULL);
+        UNIT_ASSERT(StabilityRun(13, 3, 5, true) == 16411438521907095913ULL);
+        UNIT_ASSERT(StabilityRun(13, 9, 8, true) == 5026957911653120252ULL);
     }
 
     Y_UNIT_TEST(TestReplicaSelectionUniqueCombinations) {
-        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 1, false), 0.000206, 1e-7);
-        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 3, false), 0.000519, 1e-7);
+        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 1, false), 0.000205, 1e-7);
+        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 3, false), 0.000518, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 3, 1, false), 0.009091, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 3, 5, false), 0.045251, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 9, 1, false), 0.009237, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 9, 8, false), 0.01387, 1e-7);
-        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 1, true), 0.000206, 1e-7);
-        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 3, true), 0.004263, 1e-7);
+        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 1, true), 0.000205, 1e-7);
+        UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(13, 3, 3, true), 0.004262, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 3, 1, true), 0.009091, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 3, 5, true), 0.63673, 1e-7);
         UNIT_ASSERT_DOUBLES_EQUAL(UniqueCombinationsRun(113, 9, 1, true), 0.009237, 1e-7);
@@ -129,6 +381,72 @@ Y_UNIT_TEST_SUITE(TStateStorageConfig) {
         UNIT_ASSERT(UniformityRun(113, 3, 5, true) < 0.10);
         UNIT_ASSERT(UniformityRun(113, 9, 1, true) < 0.10);
         UNIT_ASSERT(UniformityRun(113, 9, 8, true) < 0.10);
+    }
+
+    Y_UNIT_TEST(Tablet72075186224040026Test) {
+        TStateStorageInfo info;
+        FillStateStorageInfo(&info, 9, 5, 1, false);
+        NKikimr::TStateStorageInfo::TSelection selection;
+        info.SelectReplicas(72075186224040026UL, &selection);
+        ui32 expected[] = {0, 2, 1, 3, 4};
+        for (ui32 i : xrange(5)) {
+            UNIT_ASSERT_EQUAL(selection.SelectedReplicas[i].NodeId(), expected[i]);
+        }
+    }
+
+    Y_UNIT_TEST(NonDuplicatedNodesTest) {
+        TStateStorageInfo info;
+        NStateStorageOld::TStateStorageInfo oldInfo;
+        FillStateStorageInfo(&info, 9, 5, 1, false);
+        oldInfo.Rings.resize(9);
+        oldInfo.NToSelect = 5;
+        for (ui32 i : xrange(9)) {
+            oldInfo.Rings[i].Replicas.push_back(TActorId(i, i, i, i));
+        }
+        ui32 good = 0;
+        for (ui64 tabletId : xrange(Max<ui64>() - 1000000UL, Max<ui64>())) {
+            NKikimr::TStateStorageInfo::TSelection selection;
+            NStateStorageOld::TStateStorageInfo::TSelection oldSelection;
+            info.SelectReplicas(tabletId, &selection);
+            oldInfo.SelectReplicas(tabletId, &oldSelection);
+            std::unordered_set<TActorId> nodes;
+            for (ui32 i : xrange(5)) {
+                nodes.insert(oldSelection.SelectedReplicas[i]);
+            }
+            if (nodes.size() == 5) {
+                good++;
+                for (ui32 i : xrange(5)) {
+                    UNIT_ASSERT_EQUAL(oldSelection.SelectedReplicas[i], selection.SelectedReplicas[i]);
+                }
+            } else {
+                ui32 same = 0;
+                for (ui32 i : xrange(5)) {
+                    if (oldSelection.SelectedReplicas[i] == selection.SelectedReplicas[i]) {
+                        same++;
+                    }
+                }
+                UNIT_ASSERT_EQUAL(same, nodes.size());
+            }
+        }
+        UNIT_ASSERT_EQUAL(good, 999941);
+    }
+
+    Y_UNIT_TEST(DuplicatedNodesTest) {
+        TStateStorageInfo info;
+        FillStateStorageInfo(&info, 9, 5, 1, false);
+        ui32 bad = 0;
+        for (ui64 tabletId : xrange(Max<ui64>() - 1000000UL, Max<ui64>())) {
+            NKikimr::TStateStorageInfo::TSelection selection;
+            info.SelectReplicas(tabletId, &selection);
+            std::unordered_set<TActorId> nodes;
+            for (ui32 i : xrange(5)) {
+                nodes.insert(selection.SelectedReplicas[i]);
+            }
+            if (nodes.size() != 5) {
+                bad++;
+            }
+        }
+        UNIT_ASSERT_EQUAL(bad, 0);
     }
 }
 
