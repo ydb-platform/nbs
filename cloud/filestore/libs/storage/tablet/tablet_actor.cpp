e3af8fc64f31ec4b6ad729dc11f228be5028ffe8
Sergei Vorobev <sergei.vorobev@nebius.com>
2026-07-31 19:09:59 +0200

    issue-1559: [Disk Manager]  Add MainFileSystemId to TGetFileSystemTopologyResponse

--- cloud/filestore/libs/storage/tablet/tablet_actor.cpp
+++ cloud/filestore/libs/storage/tablet/tablet_actor.cpp
@@ -987,6 +987,8 @@ void TIndexTabletActor::HandleGetFileSystemTopology(
         GetFileSystem().GetStrictFileSystemSizeEnforcementEnabled());
     response->Record.SetMaxShardCount(Config->GetMaxShardCount());
     response->Record.SetCompressNodeRef(GetCompressNodeRef());
+    response->Record.SetMainFileSystemId(
+        GetFileSystem().GetMainFileSystemId());
     LOG_INFO(
         ctx,
         TFileStoreComponents::TABLET,
