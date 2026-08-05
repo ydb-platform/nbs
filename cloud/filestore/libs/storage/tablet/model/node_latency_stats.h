575049f3ea9ca438a17d51e75b348bf8c6ab37d7
Jan Szumski <jan.szumski@nebius.com>
2026-08-05 18:59:54 +0200

    Clean up

--- cloud/filestore/libs/storage/tablet/model/node_latency_stats.h
+++ cloud/filestore/libs/storage/tablet/model/node_latency_stats.h
@@ -13,27 +13,8 @@
 
 namespace NCloud::NFileStore::NStorage {
 
-// struct TNodeLatencyStatsKey
-// {
-//     ui64 NodeId = 0;
-//     EFileStoreRequest RequestType = EFileStoreRequest::MAX;
-
-//     bool operator==(const TNodeLatencyStatsKey&) const = default;
-// };
-
-// struct TNodeLatencyStatsKeyHash
-// {
-//     size_t operator()(const TNodeLatencyStatsKey& key) const noexcept
-//     {
-//         return MultiHash(
-//             key.NodeId,
-//             static_cast<ui32>(key.RequestType));
-//     }
-// };
-
 struct TNodeLatencyStats
 {
-    // TNodeLatencyStatsKey Key;
     ui64 NodeId = 0;
     EFileStoreRequest RequestType = EFileStoreRequest::MAX;
     ui64 RequestCount = 0;
@@ -51,46 +32,50 @@ private:
             const TNodeLatencyStats& lhs,
             const TNodeLatencyStats& rhs) const
         {
-            if(lhs.AverageLatencyDecayedMs == rhs.AverageLatencyDecayedMs) {
-                return lhs.NodeId < rhs.NodeId;
-            }
-            return lhs.AverageLatencyDecayedMs < rhs.AverageLatencyDecayedMs;
+            // AverageLatencyDecayedMs ASC, NodeId ASC
+            return std::tie(lhs.AverageLatencyDecayedMs, lhs.NodeId) <
+                   std::tie(rhs.AverageLatencyDecayedMs, rhs.NodeId);
         }
     };
+
     using LatencyKey = std::pair<ui64, EFileStoreRequest>;
     size_t MaxEntries = 0;
     using LatencyRanking = TSet<TNodeLatencyStats, TNodeLatencyStatsComparator>;
-    //using LatencyKey = TNodeLatencyStatsKey;
     THashMap<LatencyKey, LatencyRanking::iterator> IdAndRequest2Stats;
     LatencyRanking NodeLatencyStats;
 
     void EvictSmallestLatencyEntries()
     {
-        while(NodeLatencyStats.size() > MaxEntries)
-        {
+        while (NodeLatencyStats.size() > MaxEntries) {
             auto it = NodeLatencyStats.begin();
             LatencyKey key = {it->NodeId, it->RequestType};
             IdAndRequest2Stats.erase(key);
             NodeLatencyStats.erase(it);
         }
-    };
+    }
 
 public:
     void Initialize(size_t maxEntries);
-    void UpdateLatencyStats(ui64 nodeId, EFileStoreRequest requestType, TInstant now, TDuration latency);
+    void UpdateLatencyStats(
+        ui64 nodeId,
+        EFileStoreRequest requestType,
+        TInstant now,
+        TDuration latency);
     void CalculateLatencyDecay(TNodeLatencyStats& stats, TInstant now) const;
+
     TVector<TNodeLatencyStats> GetLatencyStats(TInstant now) const
     {
         TVector<TNodeLatencyStats> result;
         result.reserve(NodeLatencyStats.size());
-        for(auto it = NodeLatencyStats.rbegin(); it != NodeLatencyStats.rend(); ++it)
+        for (auto it = NodeLatencyStats.rbegin(); it != NodeLatencyStats.rend();
+             ++it)
         {
             auto stats = *it;
             CalculateLatencyDecay(stats, now);
             result.push_back(stats);
         };
         return result;
-    };
+    }
 };
 
 }   // namespace NCloud::NFileStore::NStorage
