c9c84126bf907ec98dd7ad1f076b16bc234e58f9
Anton Myagkov <anton.myagkov@nebius.com>
2026-08-06 13:16:45 +0200

    cherry-pick: Fix data race on TQueue::IsConnected - #45319

--- contrib/ydb/core/blobstorage/dsproxy/dsproxy_monactor.cpp
+++ contrib/ydb/core/blobstorage/dsproxy/dsproxy_monactor.cpp
@@ -116,7 +116,7 @@ public:
 
                                                     bool ok = true;
                                                     GroupQueues->DisksByOrderNumber[vdisk.OrderNumber]->Queues.ForEachQueue([&](auto& q) {
-                                                        if (!q.IsConnected) {
+                                                        if (!q.IsConnected.load(std::memory_order_relaxed)) {
                                                             ok = false;
                                                         }
                                                     });
@@ -182,7 +182,7 @@ public:
                                             TABLED() { str << Info->GetActorId(num).NodeId(); }
                                             TABLED() {
                                                 fdom.VDisks[vdisk].Queues.ForEachQueue([&](auto& q) {
-                                                    str << (q.IsConnected ? '+' : '0');
+                                                    str << (q.IsConnected.load(std::memory_order_relaxed) ? '+' : '0');
                                                 });
                                             }
                                         }
@@ -406,8 +406,8 @@ public:
                 .Title = name,
                 .RelPath = path,
                 .ActorSystem = TActivationContext::ActorSystem(),
-                .Index = proxiesMonPage, 
-                .PreTag = false, 
+                .Index = proxiesMonPage,
+                .PreTag = false,
                 .ActorId = SelfId(),
                 .MonServiceName = "dsproxy_mon"
             });
@@ -494,4 +494,3 @@ IActor* CreateBlobStorageGroupProxyMon(TIntrusivePtr<TBlobStorageGroupProxyMon> 
 }
 
 } // NKikimr
-
