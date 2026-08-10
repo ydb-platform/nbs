8e0d00abc79736ddbdbb87a0098922517836f93a
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2026-08-10 08:45:07 +0000

    corrected some database access methods

--- cloud/blockstore/libs/storage/partition2/part2_schema.h
+++ cloud/blockstore/libs/storage/partition2/part2_schema.h
@@ -447,7 +447,7 @@ struct TPartitionSchema
             using Type = NProto::TBlobMeta;
         };
 
-        using TKey = TableKey<RangeStart, RangeEnd, BlobCommitId, BlobId>;
+        using TKey = TableKey<RangeEnd, BlobCommitId, BlobId>;
         using TColumns =
             TableColumns<RangeStart, RangeEnd, BlobCommitId, BlobId, BlobMeta>;
 
