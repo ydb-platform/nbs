50b9c38067d51ec627ea5cff586fba7e16a06fb5
Aleksei Kobzev <kobzonega@yandex.ru>
2026-08-11 08:16:48 +0000

    NBS: use compatible flock Sandbox resources

--- cloud/filestore/tools/testing/fs_posix_compliance/suite/bin/download.sh
+++ cloud/filestore/tools/testing/fs_posix_compliance/suite/bin/download.sh
@@ -1,18 +1,48 @@
 #!/usr/bin/env bash
 
-# Using lowest available OS version (jammy) to avoid GLIBC incompatibility
-# https://packages.ubuntu.com/jammy/util-linux                                                                                                               
+set -euo pipefail
 
-# To get current installed urls:
-# apt-get download --print-uris util-linux                                                                                                                   
-                                                                                                                                                             
-NAME=util-linux_2.37.2-4ubuntu3.5                                                                                                                            
-curl -O http://mirror.nebiusinfra.net/ubuntu/pool/main/u/util-linux/${NAME}_amd64.deb                                                                        
-curl -O http://mirror.nebiusinfra.net/ubuntu/pool/main/u/util-linux/${NAME}_arm64.deb                                                                        
-dpkg-deb --fsys-tarfile ${NAME}_amd64.deb  | tar -xvOf - ./usr/bin/flock > flock                                                                             
-chmod +x flock                                                                                                                                               
-tar -czvf flock_amd64.tgz flock                                                                                                                              
-rm flock                                                                                                                                                     
-dpkg-deb --fsys-tarfile ${NAME}_arm64.deb  | tar -xvOf - ./usr/bin/flock > flock                                                                             
-chmod +x flock                                                                                                                                               
-tar -czvf flock_arm64.tgz flock                                                                                                                              
+# Usage:
+#   ./download.sh oss
+#   ./download.sh arcadia
+#
+# OSS resources use Ubuntu Jammy. Arcadia Sandbox workers normally provide
+# glibc 2.31, while Jammy flock requires GLIBC_2.34. Use the Focal build for
+# Arcadia, then upload the generated archives:
+#   ya upload --ttl=inf --owner=YC_NBS flock_amd64.tgz
+#   ya upload --ttl=inf --owner=YC_NBS flock_arm64.tgz
+
+MODE="${1:-oss}"
+
+case "$MODE" in
+    oss)
+        NAME=util-linux_2.37.2-4ubuntu3.5
+        AMD64_URL=http://mirror.nebiusinfra.net/ubuntu/pool/main/u/util-linux/${NAME}_amd64.deb
+        ARM64_URL=http://mirror.nebiusinfra.net/ubuntu/pool/main/u/util-linux/${NAME}_arm64.deb
+        ;;
+    arcadia)
+        NAME=util-linux_2.34-0.1ubuntu9.6
+        AMD64_URL=https://archive.ubuntu.com/ubuntu/pool/main/u/util-linux/${NAME}_amd64.deb
+        ARM64_URL=https://ports.ubuntu.com/ubuntu-ports/pool/main/u/util-linux/${NAME}_arm64.deb
+        ;;
+    *)
+        echo "Usage: $0 [oss|arcadia]" >&2
+        exit 2
+        ;;
+esac
+
+curl --fail --location --remote-name "$AMD64_URL"
+curl --fail --location --remote-name "$ARM64_URL"
+
+package_flock() {
+    local arch="$1"
+
+    dpkg-deb --fsys-tarfile "${NAME}_${arch}.deb" \
+        | tar -xvOf - ./usr/bin/flock > flock
+    chmod +x flock
+    tar -czvf "flock_${arch}.tgz" flock
+    rm flock
+}
+
+package_flock amd64
+package_flock arm64
