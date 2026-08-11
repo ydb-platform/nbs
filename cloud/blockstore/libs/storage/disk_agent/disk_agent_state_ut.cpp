801f2ebb89d11931892017a16249dc172937fbca
Pavel Misko <misko.pawel@gmail.com>
2026-08-11 21:31:42 +0200

    add GetLockdownState and EnsureLockdown methods to INvmeManager

--- cloud/blockstore/libs/storage/disk_agent/disk_agent_state_ut.cpp
+++ cloud/blockstore/libs/storage/disk_agent/disk_agent_state_ut.cpp
@@ -127,6 +127,23 @@ struct TTestNvmeManager
 
         return {};
     }
+
+    TResultOrError<TLockdownState> GetLockdownState(
+        const TString& ctrlPath) final
+    {
+        Y_UNUSED(ctrlPath);
+
+        return TLockdownState{};
+    }
+
+    NProto::TError EnsureLockdown(
+        const TString& ctrlPath,
+        const TLockdownConfig& config) final
+    {
+        Y_UNUSED(ctrlPath, config);
+
+        return {};
+    }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
