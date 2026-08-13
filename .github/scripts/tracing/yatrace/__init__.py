21f35f71b2b8817b9320178face69cfdc757f9e2
Nikita Menkovich <librarian@nebius.com>
2026-08-13 12:27:56 +0200

    Parse ya event logs and build statistics

--- .github/scripts/tracing/yatrace/__init__.py
+++ .github/scripts/tracing/yatrace/__init__.py
@@ -1,4 +1,18 @@
-"""Normalized ya trace models."""
+"""Normalized ya trace and evlog models."""
 
+from .evlog_data import YaEvlog
+from .evlog_loader import load_ya_evlog
+from .node import YaNode
 from .trace_collection import YaTraceCollection
 from .trace_model import Chunk, SuiteTrace, TestAttempt, TestEvent
+
+__all__ = (
+    "Chunk",
+    "SuiteTrace",
+    "TestAttempt",
+    "TestEvent",
+    "YaEvlog",
+    "YaNode",
+    "YaTraceCollection",
+    "load_ya_evlog",
+)
