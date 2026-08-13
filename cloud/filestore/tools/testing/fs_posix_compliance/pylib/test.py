7fedbf204967ff69b263948068476f9de95b8820
proller <proller@github.com>
2026-08-13 13:52:27 +0200

    Try without umask fix

--- cloud/filestore/tools/testing/fs_posix_compliance/pylib/test.py
+++ cloud/filestore/tools/testing/fs_posix_compliance/pylib/test.py
@@ -40,7 +40,7 @@ def __run_test_suite(target_path, suite, tests, verbose=False):
 
     # Tests expect 002 umask
     # cloud/filestore/tests/fs_posix_compliance/mount-local-test : open/02.t : +not ok 2 (line: 12)
-    os.umask(0o002)
+    # os.umask(0o002)
 
     env = os.environ.copy()
 
