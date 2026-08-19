cd429d8152c9abd2296ecd8b6144e55ad227295f
Jan Szumski <jan.szumski@nebius.com>
2026-08-19 17:53:34 +0200

    Add new proto fields to the macro

--- cloud/filestore/libs/storage/core/config.cpp
+++ cloud/filestore/libs/storage/core/config.cpp
@@ -397,6 +397,8 @@ using TAliases = NProto::TStorageConfig::TFilestoreAliases;
     xxx(EnableLoadActor,                               bool,    false         )\
     xxx(MaxNodeDiagnosticEntries,                      ui32,    0             )\
     xxx(NodeAccessCountHalfLife,              TDuration, TDuration::Minutes(0))\
+    xxx(MaxSlowestRequestsEntries,                     ui32,    0             )\
+    xxx(NodeLatencyHalfLife,                  TDuration, TDuration::Minutes(0))\
 // FILESTORE_STORAGE_CONFIG
 
 #define FILESTORE_STORAGE_CONFIG_REF(xxx)                                      \
