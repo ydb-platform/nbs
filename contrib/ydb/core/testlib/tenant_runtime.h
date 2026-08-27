ee3a7f6ea7519538f9b53d910741199cc7718288
Aleksandr Usenko <iscander.mail@gmail.com>
2026-08-27 16:05:53 +0300

    Backport YDB 'Introduce cluster-opaque TAppConfig::PrivateDatabaseConfig' (#6914)
    ### Notes
    YDB PR: https://github.com/ydb-platform/ydb/pull/43238
    YDB Commit:
    https://github.com/ydb-platform/ydb/commit/e1ba7f0175c431698ee8e7a358c83b7183126891
    
    ### Issue
    https://github.com/ydb-platform/nbs/issues/6685
    
    Co-authored-by: Aleksandr Usenko <usenkoav@yandex-team.ru>

--- contrib/ydb/core/testlib/tenant_runtime.h
+++ contrib/ydb/core/testlib/tenant_runtime.h
@@ -3,6 +3,7 @@
 
 #include <contrib/ydb/core/base/events.h>
 #include <contrib/ydb/core/base/subdomain.h>
+#include <contrib/ydb/core/config/init/init.h>
 #include <contrib/ydb/core/mind/labels_maintainer.h>
 #include <contrib/ydb/core/mind/local.h>
 #include <contrib/ydb/core/mind/tenant_pool.h>
@@ -98,6 +99,9 @@ struct TTenantTestConfig {
     ui32 DataCenterCount;
     bool CreateConfigsDispatcher = false;
     bool RegisterFeatureFlagsConfigurator = false;
+    // Per-kind opaque config parsers injected into the framework-created
+    // ConfigsDispatcher (only used when CreateConfigsDispatcher is true).
+    THashMap<ui32, NConfig::TOpaqueConfigParser> OpaqueConfigParsers = {};
 };
 
 extern const ui64 SCHEME_SHARD1_ID;
