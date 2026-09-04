f04644a13253e83ec2b00350d447a252dd17ddcd
Aleksei Kobzev <kobzonega@yandex.ru>
2026-09-04 17:41:34 +0300

    disk_manager: rename user IP log field

--- cloud/disk_manager/internal/pkg/facade/interceptor.go
+++ cloud/disk_manager/internal/pkg/facade/interceptor.go
@@ -218,7 +218,7 @@ func (i *interceptor) intercept(
 
 	logging.Debug(
 		ctx,
-		"requestName=%v, userIP=%q received",
+		"requestName=%v, requestIP=%q received",
 		requestName,
 		auth.GetUserIP(ctx),
 	)
