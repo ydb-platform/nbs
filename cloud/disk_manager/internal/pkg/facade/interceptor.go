4e93da1e22ae6ae906c38ec8127cffd8c9510eab
Aleksei Kobzev <kobzonega@yandex.ru>
2026-09-04 17:57:23 +0300

    disk_manager: restore user IP log field

--- cloud/disk_manager/internal/pkg/facade/interceptor.go
+++ cloud/disk_manager/internal/pkg/facade/interceptor.go
@@ -218,7 +218,7 @@ func (i *interceptor) intercept(
 
 	logging.Debug(
 		ctx,
-		"requestName=%v, requestIP=%q received",
+		"requestName=%v, userIP=%q received",
 		requestName,
 		auth.GetUserIP(ctx),
 	)
