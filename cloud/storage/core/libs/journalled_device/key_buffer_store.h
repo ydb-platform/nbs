895fcb78b530db4f18a93c6862a150181d389d26
Aleksandr Rigachnyi <a.rigachnyy@gmail.com>
2026-09-09 17:07:34 +0200

    issue-6956: implement the journal on top of the record chain and page index
    TJournal now restores its state from the meta store, writes records
    through the page store, serves reads and tail reads from the page index,
    advances the last acked lsn and drives the flush cycle.

--- cloud/storage/core/libs/journalled_device/key_buffer_store.h
+++ cloud/storage/core/libs/journalled_device/key_buffer_store.h
@@ -7,11 +7,16 @@
 #include <library/cpp/threading/future/future.h>
 
 #include <util/generic/buffer.h>
-#include <util/generic/set.h>
+#include <util/generic/vector.h>
 
 #include <memory>
+#include <utility>
 
 namespace NCloud::NJournalled {
+
+////////////////////////////////////////////////////////////////////////////////
+
+using TKeyBuffers = TVector<std::pair<ui64, TBuffer>>;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -19,16 +24,14 @@ struct IKeyBufferStore
 {
     virtual ~IKeyBufferStore() = default;
 
+    [[nodiscard]] virtual auto Restore()
+        -> NThreading::TFuture<TResultOrError<TKeyBuffers>> = 0;
+
     [[nodiscard]] virtual auto Write(ui64 key, TBuffer buffer)
         -> NThreading::TFuture<NCloud::NProto::TError> = 0;
 
-    [[nodiscard]] virtual auto Read(ui64 key) const
-        -> NThreading::TFuture<TResultOrError<TBuffer>> = 0;
-
-    [[nodiscard]] virtual auto EraseUpTo(ui64 lastKey)
+    [[nodiscard]] virtual auto EraseBelow(ui64 key)
         -> NThreading::TFuture<NCloud::NProto::TError> = 0;
-
-    [[nodiscard]] virtual TSet<ui64> GetKeys() const = 0;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
--- cloud/storage/core/libs/journalled_device/ut/ya.make
+++ cloud/storage/core/libs/journalled_device/ut/ya.make
@@ -5,11 +5,13 @@ INCLUDE(${ARCADIA_ROOT}/cloud/storage/core/tests/recipes/small.inc)
 SRCS(
     device_page_store_ut.cpp
     device_ut.cpp
+    journal_ut.cpp
     journalled_device_ut.cpp
     journalled_device_v2_ut.cpp
     key_buffer_store_ut.cpp
     log_chain_ut.cpp
     log_index_ut.cpp
+    log_record_ut.cpp
     lsn_barrier_ut.cpp
 )
 
