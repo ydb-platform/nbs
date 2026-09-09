1b6113f9571da7be68118ca77895f22740545865
odddin <72134530+otbaphou@users.noreply.github.com>
2026-09-09 13:04:44 +0300

    [NBS] False positive critical event fix in merge_tablet_boot_info_backups (#6993)
    ### Notes
    Fixed a false positive critical event message by checking if the
    contents of a file are of a text format by using IsUTF. Besides the
    check, the behaviour of the LoadTabletBootInfoBackup is the same as
    before - in case of TryMergeFromTextFormat failing, we still run through
    the MergeFromString and only then return the status.
    
    ### Issue
    https://github.com/ydb-platform/nbs/issues/6987

--- cloud/blockstore/tools/disaster_recovery/merge_tablet_boot_info_backups/main.cpp
+++ cloud/blockstore/tools/disaster_recovery/merge_tablet_boot_info_backups/main.cpp
@@ -5,6 +5,7 @@
 
 #include <library/cpp/protobuf/util/pb_io.h>
 
+#include <util/charset/utf8.h>
 #include <util/datetime/base.h>
 #include <util/folder/path.h>
 #include <util/generic/map.h>
@@ -44,11 +45,18 @@ bool LoadTabletBootInfoBackup(
     TString fileContent = TUnbufferedFileInput(file).ReadAll();
     auto input = TStringInput(fileContent);
 
-    return TryMergeFromTextFormat(
-               input,
-               *backupProto,
-               EParseFromTextFormatOption::AllowUnknownField) ||
-           backupProto->MergeFromString(fileContent);
+    // This if statement supresses false-positive error logs on non-utf input
+    if (IsUtf(fileContent)) {
+        if (TryMergeFromTextFormat(
+                input,
+                *backupProto,
+                EParseFromTextFormatOption::AllowUnknownField))
+        {
+            return true;
+        }
+    }
+
+    return backupProto->MergeFromString(fileContent);
 }
 
 void ProcessDir(
