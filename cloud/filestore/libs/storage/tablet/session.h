231449881e0cb2e1aac53a593e47b48b8eb91c50
Darya Frolova <wilytiger@nebius.com>
2026-09-09 20:08:50 +0200

    cleanup

--- cloud/filestore/libs/storage/tablet/session.h
+++ cloud/filestore/libs/storage/tablet/session.h
@@ -457,7 +457,7 @@ struct TSessionHistoryEntry
 
 using TSessionList = TIntrusiveListWithAutoDelete<TSession, TDelete>;
 using TSessionMap = THashMap<TString, TSession*>;
-using TSessionOwnerMap = THashMap<NActors::TActorId, TSession*>;
+using TSessionByPipeServerMap = THashMap<NActors::TActorId, TSession*>;
 using TSessionClientMap = THashMap<TString, TSession*>;
 using TSessionHistoryList = TDeque<TSessionHistoryEntry>;
 
