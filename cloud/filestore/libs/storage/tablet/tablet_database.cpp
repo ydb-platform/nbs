584b5856d6a295734ec3dd07c761ed07da160d7e
Anton Myagkov <anton.myagkov@nebius.com>
2026-09-11 17:47:24 +0200

    pass BlockData to FreshBlocks table as TStringBuf

--- cloud/filestore/libs/storage/tablet/tablet_database.cpp
+++ cloud/filestore/libs/storage/tablet/tablet_database.cpp
@@ -1245,7 +1245,7 @@ void TIndexTabletDatabase::WriteFreshBlock(
 
     Table<TTable>()
         .Key(nodeId, blockIndex, ReverseCommitId(commitId))
-        .Update(NIceDb::TUpdate<TTable::BlockData>(TString(blockData)));
+        .Update(NIceDb::TUpdate<TTable::BlockData>(blockData));
 }
 
 void TIndexTabletDatabase::WriteFreshBlocks(
@@ -1317,7 +1317,7 @@ bool TIndexTabletDatabase::ReadFreshBlocks(TVector<TFreshBlock>& blocks)
             blockIndex,
             minCommitId,
             maxCommitId,
-            it.GetValue<TTable::BlockData>()
+            TString(it.GetValue<TTable::BlockData>())
         });
 
         if (!it.Next()) {
