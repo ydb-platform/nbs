abeea005eca0f33227facc9aeb91f3e2f24de1ab
Andrei Nasonov <nasonov@nebius.com>
2026-09-14 19:38:58 +0200

    Comment + guard

--- cloud/filestore/libs/vfs_fuse/fs_impl_data.cpp
+++ cloud/filestore/libs/vfs_fuse/fs_impl_data.cpp
@@ -295,6 +295,13 @@ void TFileSystem::Create(
 
     const ui64 version = GlobalCacheVersion.load(std::memory_order_acquire);
 
+    // TFileSystem::Create is called only when the file doesn't exist.
+    // The guarantee is tied to the Linux FUSE/virtio-fs path, a low-level FUSE
+    // create callback is invoked only for an atomic create-and-open operation.
+    //
+    // Therefore, we do not need to execute CreateHandle through WriteBackCache
+    // the same way as in TFileSystem::Open
+    //
     Session->CreateHandle(callContext, std::move(request))
         .Subscribe([=, ptr = weak_from_this()] (const auto& future) {
             auto self = ptr.lock();
@@ -307,6 +314,20 @@ void TFileSystem::Create(
             self->FSyncQueue->Dequeue(reqId, error, TNodeId {parent});
 
             if (CheckResponse(self, *callContext, req, response)) {
+                if (self->WriteBackCache) {
+                    const ui64 nodeId = response.GetNodeAttr().GetId();
+                    const auto maxWrittenOffset =
+                        self->WriteBackCache.GetMaxWrittenOffset(nodeId);
+
+                    if (maxWrittenOffset != 0) {
+                        ReportWriteBackCacheImpossibleState(Sprintf(
+                            "WriteBackCache::GetMaxWrittenOffset(%lu) returned "
+                            "%lu after TFileSystem::Create",
+                            nodeId,
+                            maxWrittenOffset));
+                    }
+                }
+
                 self->ReplyCreateWithCache(
                     *callContext,
                     error,
