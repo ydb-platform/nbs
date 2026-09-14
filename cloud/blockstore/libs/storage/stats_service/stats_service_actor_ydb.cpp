0bfe1933fdf9bdea9aeb7a429f804546beb29bac
Stepanyuk Vladislav <stepanyukvladyslav@yandex.ru>
2026-09-14 06:02:02 +0000

    removed ydb stats column

--- cloud/blockstore/libs/storage/stats_service/stats_service_actor_ydb.cpp
+++ cloud/blockstore/libs/storage/stats_service/stats_service_actor_ydb.cpp
@@ -156,8 +156,6 @@ NYdbStats::TYdbStatsRow BuildStatsForUpload(
         BLOCKSTORE_CUMULATIVE_COUNTER(CompactionByIgnoringZeroedPerDisk);
     out.CompactionByMixedBlockCountPerRange_Throughput =
         BLOCKSTORE_CUMULATIVE_COUNTER(CompactionByMixedBlockCountPerRange);
-    out.CompactionByMixedBlockCountPerDisk_Throughput =
-        BLOCKSTORE_CUMULATIVE_COUNTER(CompactionByMixedBlockCountPerDisk);
 
 #undef BLOCKSTORE_CUMULATIVE_COUNTER
 
