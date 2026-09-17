40060fbaf0aabb3242410362adfc71c7266bce67
Andrey Nasonov <e673@users.noreply.github.com>
2026-09-17 15:36:54 +0200

    [Filestore] Decrease the default value of WriteBackCacheCapacity to 256MiB (#7203)

--- cloud/filestore/libs/endpoint_vhost/config.cpp
+++ cloud/filestore/libs/endpoint_vhost/config.cpp
@@ -33,7 +33,7 @@ static constexpr int MODE0660 = S_IRGRP | S_IWGRP | S_IRUSR | S_IWUSR;
     xxx(HandleOpsQueueSize,                         ui32,       1_GB          )\
     xxx(HandleOpsQueueTotalSizeLimit,               ui64,       0             )\
     xxx(WriteBackCachePath,                         TString,    ""            )\
-    xxx(WriteBackCacheCapacity,                     ui64,       1_GB          )\
+    xxx(WriteBackCacheCapacity,                     ui64,       256_MB        )\
     xxx(WriteBackCacheTotalSizeLimit,               ui64,       10_GB         )\
     xxx(WriteBackCacheAutomaticFlushPeriod,                                    \
         TDuration,                                                             \
--- cloud/filestore/libs/vfs/config.cpp
+++ cloud/filestore/libs/vfs/config.cpp
@@ -29,7 +29,7 @@ namespace {
     xxx(HandleOpsQueueSize,     ui32,           1_GB                          )\
                                                                                \
     xxx(WriteBackCachePath,                 TString,   ""                     )\
-    xxx(WriteBackCacheCapacity,             ui64,      1_GB                   )\
+    xxx(WriteBackCacheCapacity,             ui64,      256_MB                 )\
     xxx(WriteBackCacheAutomaticFlushPeriod, TDuration,                         \
                                             TDuration::MilliSeconds(100)      )\
     xxx(WriteBackCacheFlushRetryPeriod,     TDuration,                         \
